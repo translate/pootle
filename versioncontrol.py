61ec31afe55e4bd492b99fea8f83764309efa9d8
Friedel Wolff <friedel@translate.org.za>
2007-06-25 09:11:11 +0000

    [Contributed by Lars Kruse] Rewrite the version control support to have an object oriented design. Symlinked targets are now also supported to ease integration with checkouts at other locations. No new dependencies are introduced.
    svn path=/src/trunk/Pootle/; revision=5968

--- versioncontrol.py
+++ versioncontrol.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # 
-# Copyright 2004-2006 Zuza Software Foundation
+# Copyright 2004-2007 Zuza Software Foundation
 # 
 # This file is part of translate.
 #
@@ -43,211 +43,272 @@ def shellescape(path):
   """Shell-escape any non-alphanumeric characters"""
   return re.sub(r'(\W)', r'\\\1', path)
 
-def cvsreadfile(cvsroot, path, revision=None):
-  """
-  Read a single file from the CVS repository without checking out a full working directory
-  
-  cvsroot: the CVSROOT for the repository
-  path: path to the file relative to cvs root
-  revision: revision or tag to get (retrieves from HEAD if None)
-  """
-  path = shellescape(path)
-  cvsroot = shellescape(cvsroot)
-  if revision:
-    revision = shellescape(revision)
-    command = "cvs -d %s -Q co -p -r%s %s" % (cvsroot, revision, path)
-  else:
-    command = "cvs -d %s -Q co -p %s" % (cvsroot, path)
 
-  output, error = pipe(command)
+class GenericVersionControlSystem:
+  """should be the super class for all version control classes"""
 
-  if error.startswith('cvs checkout'):
-    raise IOError("Could not read %s from %s: %s" % (path, cvsroot, output))
-  elif error.startswith('cvs [checkout aborted]'):
-    raise IOError("Could not read %s from %s: %s" % (path, cvsroot, output))
-  return output
+  # as long as noone overrides this, the test below always succeeds
+  MARKER_DIR = "."
 
-def cvsupdatefile(path, revision=None):
-  """Does a clean update of the given path"""
-  dirname = shellescape(os.path.dirname(path))
-  filename = shellescape(os.path.basename(path))
-  basecommand = ""
-  if dirname:
-    basecommand = "cd %s ; " % dirname
-  command = basecommand + "mv %s %s.bak ; " % (filename, filename)
-  if revision:
-    revision = shellescape(revision)
-    command += "cvs -Q update -C -r%s %s" % (revision, filename)
-  else:
-    command += "cvs -Q update -C %s" % (filename)
-  output, error = pipe(command)
-  if error:
-    pipe(basecommand + "mv %s.bak %s" % (filename, filename))
-    raise IOError("Error running CVS command '%s': %s" % (command, error))
-  pipe(basecommand + "rm %s.bak" % filename)
-  return output
+  def __init__(self, location):
+    """Default version control checker: test if self.MARKER_DIR exists.
+    
+    Most version control systems depend on the existence of a specific directory
+    thus you will most likely not need to touch this check - just call it.
+    The IOError exception indicates that the specified file is not controlled
+    by the given version control system.
+    """
+    parent_dir = os.path.dirname(os.path.abspath(location))
+    if not os.path.isdir(os.path.join(parent_dir, self.MARKER_DIR)):
+      raise IOError("Could not find version control information: %s" % location)
 
-def cvscommitfile(path, message=None):
-  """Commits the file and supplies the given commit message if present"""
-  dirname = shellescape(os.path.dirname(path))
-  filename = shellescape(os.path.basename(path))
-  basecommand = ""
-  if dirname:
-    basecommand = "cd %s ; " % dirname
-  if message:
-    message = ' -m %s ' % shellescape(message)
-  elif message is None:
-    message = ""
-  command = basecommand + "cvs -Q commit %s %s" % (message, filename)
-  output, error = pipe(command)
-  if error:
-    raise IOError("Error running CVS command '%s': %s" % (command, error))
 
-def getcvsrevision(cvsentries, filename):
-  """returns the revision number the file was checked out with by looking in the lines of cvsentries"""
-  for cvsentry in cvsentries:
-    cvsentryparts = cvsentry.split("/")
-    if len(cvsentryparts) < 6:
-      continue
-    if os.path.normcase(cvsentryparts[1]) == os.path.normcase(filename):
-      return cvsentryparts[2].strip()
-  return None
+class CVS(GenericVersionControlSystem):
+  """manage items versioned by CVS"""
 
-def getcvstag(cvsentries, filename):
-  """returns the sticky tag the file was checked out with by looking in the lines of cvsentries"""
-  for cvsentry in cvsentries:
-    cvsentryparts = cvsentry.split("/")
-    if len(cvsentryparts) < 6:
-      continue
-    if os.path.normcase(cvsentryparts[1]) == os.path.normcase(filename):
-      if cvsentryparts[5].startswith("T"):
-        return cvsentryparts[5][1:].strip()
-  return None
+  MARKER_DIR = "CVS"
 
-def svnreadfile(path, revision=None):
-  """Get a clean version of a file from the SVN repository"""
-  path = shellescape(path)
-  if revision:
-    revision = shellescape(revision)
-    command = "svn cat -r %s %s" % (revision, path)
-  else:
-    command = "svn cat %s" % path
-  output, error = pipe(command)
-  if error:
-    raise IOError("Subversion error running '%s': %s" % (command, error))
-  return output
+  def __init__(self, location):
+    GenericVersionControlSystem.__init__(self, location)
+    self.cvsdir = os.path.join(os.path.dirname(os.path.abspath(location)),
+        self.MARKER_DIR)
+    self.location = os.path.abspath(location)
 
-def darcsreadfile(path):
-  """Get a clean version of a file from the darcs repository"""
-  command = "cat _darcs/pristine/%s" % shellescape(path)
-  output, error = pipe(command)
-  if error:
-    raise IOError("darcs error running '%s': %s" % (command, error))
-  return output
+  def _readfile(self, cvsroot, path, revision=None):
+    """
+    Read a single file from the CVS repository without checking out a full working directory
+    
+    @param: cvsroot: the CVSROOT for the repository
+    @param path: path to the file relative to cvs root
+    @param revision: revision or tag to get (retrieves from HEAD if None)
+    """
+    path = shellescape(path)
+    if revision:
+      command = "cvs -d %s -Q co -p -r%s %s" % (cvsroot, revision, path)
+    else:
+      command = "cvs -d %s -Q co -p %s" % (cvsroot, path)
+    output, error = pipe(command)
+    if error.startswith('cvs checkout'):
+      raise IOError("Could not read %s from %s: %s" % (path, cvsroot, output))
+    elif error.startswith('cvs [checkout aborted]'):
+      raise IOError("Could not read %s from %s: %s" % (path, cvsroot, output))
+    return output
 
-def svnupdatefile(path, revision=None):
-  """Does a clean update of the given path"""
-  path = shellescape(path)
-  command = "svn revert %s ; " % path
-  if revision:
-    revision = shellescape(revision)
-    command += "svn update -r%s %s" % (revision, path)
-  else:
-    command += "svn update %s" % (path)
-  output, error = pipe(command)
-  if error:
-    raise IOError("Subversion error running '%s': %s" % (command, error))
-  return output
-
-def darcsupdatefile(path):
-  """Does a clean update of the given path"""
-  command = "darcs revert -a %s ; darcs pull -a" % shellescape(path)
-  output, error = pipe(command)
-  if error:
-    raise IOError("darcs error running '%s': %s" % (command, error))
-  return output
-
-def svncommitfile(path, message=None):
-  """Commits the file and supplies the given commit message if present"""
-  dirname = shellescape(os.path.dirname(path))
-  filename = shellescape(os.path.basename(path))
-  basecommand = ""
-  if dirname:
-    basecommand = "cd %s ; " % dirname
-  if message:
-    message = ' -m %s ' % shellescape(message)
-  elif message is None:
-    message = ""
-  command = basecommand + "svn -q --non-interactive commit %s %s" % (message, filename)
-  output, error = pipe(command)
-  if error:
-    raise IOError("Error running SVN command '%s': %s" % (command, error))
-
-def darcscommitfile(path, message=None):
-  """Commits the file and supplies the given commit message if present"""
-  file = shellescape(path)
-  if message is None:
-    message = ""
-  command = "darcs record -a --skip-long-comment -m '%s' %s; darcs push -a" % (message, file)
-  output, error = pipe(command)
-  if error:
-    raise IOError("Error running darcs command '%s': %s" % (command, error))
-
-def hascvs(parentdir):
-  cvsdir = os.path.join(parentdir, "CVS")
-  return os.path.isdir(cvsdir)
-
-def hassvn(parentdir):
-  svndir = os.path.join(parentdir, ".svn")
-  return os.path.isdir(svndir)
-
-def hasdarcs(parentdir):
-  darcsdir = os.path.join(parentdir, "../../../_darcs")
-  return os.path.isdir(darcsdir)
-
-def hasversioning(parentdir):
-  return hascvs(parentdir) or hassvn(parentdir) or hasdarcs(parentdir)
-
-def getcleanfile(filename, revision=None):
-  parentdir = os.path.dirname(filename)
-  if hascvs(parentdir):
+  def getcleanfile(self, revision=None):
+    """Get the content of the file for the given revision"""
+    parentdir = os.path.dirname(self.location)
     cvsdir = os.path.join(parentdir, "CVS")
     cvsroot = open(os.path.join(cvsdir, "Root"), "r").read().strip()
     cvspath = open(os.path.join(cvsdir, "Repository"), "r").read().strip()
-    basename = os.path.basename(filename)
-    cvsfilename = os.path.join(cvspath, basename)
+    cvsfilename = os.path.join(cvspath, os.path.basename(self.location))
     if revision is None:
       cvsentries = open(os.path.join(cvsdir, "Entries"), "r").readlines()
-      revision = getcvstag(cvsentries, basename)
+      revision = self._getcvstag(cvsentries)
     if revision == "BASE":
       cvsentries = open(os.path.join(cvsdir, "Entries"), "r").readlines()
-      revision = getcvsrevision(cvsentries, basename)
-    return cvsreadfile(cvsroot, cvsfilename, revision)
-  if hassvn(parentdir):
-    return svnreadfile(filename, revision)
-  if hasdarcs(parentdir):
-    return darcsreadfile(filename)
-  raise IOError("Could not find version control information")
+      revision = self._getcvsrevision(cvsentries)
+    return self._readfile(cvsroot, cvsfilename, revision)
 
-def updatefile(filename, revision=None):
-  parentdir = os.path.dirname(filename)
-  if hascvs(parentdir):
-    return cvsupdatefile(filename, revision)
-  if hassvn(parentdir):
-    return svnupdatefile(filename, revision)
-  if hasdarcs(parentdir):
-    return darcsupdatefile(filename)
-  raise IOError("Could not find version control information")
+  def update(self, revision=None):
+    """Does a clean update of the given path"""
+    dirname = shellescape(os.path.dirname(self.location))
+    filename = shellescape(os.path.basename(self.location))
+    basecommand = ""
+    if dirname:
+      basecommand = "cd %s ; " % dirname
+    command = basecommand + "mv %s %s.bak ; " % (filename, filename)
+    if revision:
+      command += "cvs -Q update -C -r%s %s" % (revision, filename)
+    else:
+      command += "cvs -Q update -C %s" % (filename)
+    output, error = pipe(command)
+    if error:
+      pipe(basecommand + "mv %s.bak %s" % (filename, filename))
+      raise IOError("Error running CVS command '%s': %s" % (command, error))
+    pipe(basecommand + "rm %s.bak" % filename)
+    return output
+
+  def commit(self, message=None):
+    """Commits the file and supplies the given commit message if present"""
+    dirname = shellescape(os.path.dirname(self.location))
+    filename = shellescape(os.path.basename(self.location))
+    basecommand = ""
+    if dirname:
+      basecommand = "cd %s ; " % dirname
+    if message:
+      message = ' -m "%s" ' % message
+    elif message is None:
+      message = ""
+    command = basecommand + "cvs -Q commit %s %s" % (message, filename)
+    output, error = pipe(command)
+    if error:
+      raise IOError("Error running CVS command '%s': %s" % (command, error))
+    return output
+
+  def _getcvsrevision(self, cvsentries):
+    """returns the revision number the file was checked out with by looking
+    in the lines of cvsentries
+    """
+    filename = os.path.basename(self.location)
+    for cvsentry in cvsentries:
+      cvsentryparts = cvsentry.split("/")
+      if len(cvsentryparts) < 6:
+        continue
+      if os.path.normcase(cvsentryparts[1]) == os.path.normcase(filename):
+        return cvsentryparts[2].strip()
+    return None
+
+  def _getcvstag(self, cvsentries):
+    """returns the sticky tag the file was checked out with by looking in the 
+    lines of cvsentries
+    """
+    filename = os.path.basename(self.location)
+    for cvsentry in cvsentries:
+      cvsentryparts = cvsentry.split("/")
+      if len(cvsentryparts) < 6:
+        continue
+      if os.path.normcase(cvsentryparts[1]) == os.path.normcase(filename):
+        if cvsentryparts[5].startswith("T"):
+          return cvsentryparts[5][1:].strip()
+    return None
+
+
+class SVN(GenericVersionControlSystem):
+  """manage items that are under revision control of Subversion"""
+
+  MARKER_DIR = ".svn"
+
+  def __init__(self, location):
+    GenericVersionControlSystem.__init__(self, location)
+    self.svndir = os.path.join(os.path.dirname(os.path.abspath(location)),
+        self.MARKER_DIR)
+    self.location = os.path.abspath(location)
+
+  def update(self, revision=None):
+    """update the working copy - remove local modifications if necessary"""
+    # revert the local copy (remove local changes)
+    path = shellescape(self.location)
+    command = "svn revert %s" % path
+    # update the working copy to the given revision
+    if revision is None:
+      command += "; svn update %s" % (path,)
+    else:
+      command += "; svn update -r%s %s" % (revision, path)
+    output, error = pipe(command)
+    # any error messages?
+    if error:
+      raise IOError("Subversion error running '%s': %s" % (command, error))
+    return output
+
+  def commit(self, message=None):
+    """commit the file and return the given message if present"""
+    path = shellescape(self.location)
+    if message is None:
+      message_param = ""
+    else:
+      message_param = "-m %s" % shellescape(message)
+    command = "svn -q --non-interactive commit %s %s" % \
+        (message_param, path)
+    output, error = pipe(command)
+    if error:
+      raise IOError("Error running SVN command '%s': %s" % (command, error))
+    return output
+  
+  def getcleanfile(self, revision=None):
+    """return the content of the 'head' revision of the file"""
+    path = shellescape(self.location)
+    if revision is None:
+      command = "svn cat %s" % path
+    else:
+      command = "svn cat -r %s %s" % (shellescape(revision), path)
+    output, error = pipe(command)
+    if error:
+      raise IOError("Subversion error running '%s': %s" % (command, error))
+    return output
+
+
+class DARCS(GenericVersionControlSystem):
+  """manage items that are under revision control of darcs"""
+
+  # This assumes that the whole PO directory is stored in darcs so we need to 
+  # reach the _darcs dir from po/project/language. That results in this 
+  # relative path
+  MARKER_DIR = "../../../_darcs"
+  
+  def __init__(self, location):
+    GenericVersionControlSystem.__init__(self, location)
+    self.darcsdir = os.path.join(os.path.dirname(os.path.abspath(location)),
+        self.MARKER_DIR)
+    self.location = os.path.abspath(location)
+
+  def update(self, revision=None):
+    """Does a clean update of the given path"""
+    # TODO: check if 'revert' and 'pull' work without specifying '--repodir'
+    command = "darcs revert -a %s ; darcs pull -a" % shellescape(self.location)
+    output, error = pipe(command)
+    if error:
+      raise IOError("darcs error running '%s': %s" % (command, error))
+    return output
+
+  def commit(self, message=None):
+    """Commits the file and supplies the given commit message if present"""
+    if message is None:
+      message = ""
+    command = "darcs record -a --skip-long-comment -m '%s' %s; darcs push -a" \
+        % (message, shellescape(self.location))
+    output, error = pipe(command)
+    if error:
+      raise IOError("Error running darcs command '%s': %s" % (command, error))
+    return output
+
+  def getcleanfile(self, revision=None):
+    """Get a clean version of a file from the darcs repository"""
+    command = "cat _darcs/pristine/%s" % shellescape(self.location)
+    output, error = pipe(command)
+    if error:
+      raise IOError("darcs error running '%s': %s" % (command, error))
+    return output
+
+
+# which versioning systems are supported by default?
+DEFAULT_VERSIONING_SYSTEMS = [CVS, SVN, DARCS]
+
+def get_versioned_object(
+    location,
+    versioning_systems=DEFAULT_VERSIONING_SYSTEMS,
+    follow_symlinks=True):
+  """return a versioned object for the given file"""
+  # go through all versioning systems and return a versioned object if possible
+  for vers_sys in versioning_systems:
+    try:
+      return vers_sys(location)
+    except IOError:
+      continue
+  # if 'location' is a symlink, then we should try the original file
+  if follow_symlinks and os.path.islink(location):
+    return get_versioned_object(os.path.realpath(location),
+        versioning_systems = versioning_systems,
+        follow_symlinks = False)
+  # if everything fails:
+  raise IOError("Could not find version control information: %s" % location)
+
+# stay compatible to the previous version
+def updatefile(filename):
+  return get_versioned_object(filename).update()
+
+def getcleanfile(filename, revision=None):
+  return get_versioned_object(filename).getcleanfile(revision)
 
 def commitfile(filename, message=None):
-  parentdir = os.path.dirname(filename)
-  if hascvs(parentdir):
-    return cvscommitfile(filename, message)
-  if hassvn(parentdir):
-    return svncommitfile(filename, message)
-  if hasdarcs(parentdir):
-    return darcscommitfile(filename, message)
-  raise IOError("Could not find version control information")
+  return get_versioned_object(filename).commit(message)
+
+def hasversioning(item):
+  try:
+    # try all available version control systems
+    get_versioned_object(item)
+    return True
+  except IOError:
+    return False
   
 if __name__ == "__main__":
   import sys
