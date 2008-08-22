0ccbb08619adb0647ba9454a221a0183601d98ab
Wynand Winterbach <wynand@translate.org.za>
2008-08-22 16:43:24 +0000

    Modify getquickstats to take into account that StatsCache.filetotals may now return an empty dictionary. If it does, then getquickstats should return emptyfiletotals, since Pootle expects the quickstats dictionary to contain certain values.
    svn path=/src/trunk/Pootle/; revision=8167

--- statistics.py
+++ statistics.py
@@ -34,10 +34,10 @@ class pootlestatistics:
   def getquickstats(self):
     """returns the quick statistics (totals only)"""
     try:
-      return self.statscache.filetotals(self.basefile.filename)
+      return self.statscache.filetotals(self.basefile.filename) or statsdb.emptyfiletotals()
     except:
       return statsdb.emptyfiletotals()
-    
+
   @memoize
   def getstats(self, checker=None):
     """reads the stats if neccessary or returns them from the cache"""
@@ -58,18 +58,18 @@ class pootlestatistics:
   def updatequickstats(self, save=True):
     """updates the project's quick stats on this file"""
     totals = self.getquickstats()
-    self.basefile.project.updatequickstats(self.basefile.pofilename, 
+    self.basefile.project.updatequickstats(self.basefile.pofilename,
         totals.get("translatedsourcewords", 0), totals.get("translated", 0),
         totals.get("fuzzysourcewords", 0), totals.get("fuzzy", 0),
         totals.get("totalsourcewords", 0), totals.get("total", 0),
         save)
 
   def reclassifyunit(self, item):
-    """Reclassifies all the information in the database and self._stats about 
+    """Reclassifies all the information in the database and self._stats about
     the given unit"""
     unit = self.basefile.getitem(item)
     item = self.getstats()["total"][item]
-    
+
     classes = self.statscache.recacheunit(self.basefile.filename, self.basefile.checker, unit)
     for classname, matchingitems in self.getstats().items():
       if (classname in classes) != (item in matchingitems):
