501fa44373847cb736697da28b528f8f67ddc23a
Friedel Wolff <friedel@translate.org.za>
2008-12-15 07:51:05 +0000

    Add an initial admin module to register languages and projects
    svn path=/src/trunk/Pootle/; revision=9242

---
+++ pootle_app/admin.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2008 Zuza Software Foundation
+#
+# This file is part of Pootle.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+from Pootle.pootle_app.models import Language, Project
+from Pootle.pootle_app.profile import PootleProfile, PootleProfileInline
+
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from django import forms
+from django.contrib import admin
+
+import re
+
+
+### Language
+
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ('code', 'fullname')
+    list_display_links = ('code', 'fullname')
+    fieldsets = (
+        (None, {
+            'fields': ('code', 'fullname', 'specialchars')
+        }),
+        ('Plural information', {
+            'fields': ('nplurals', 'pluralequation')
+        }),
+    )
+
+admin.site.register(Language, LanguageAdmin)
+
+
+### Project
+
+class MyProjectAdminForm(forms.ModelForm):
+
+    def clean_code(self):
+        if re.search("[^a-zA-Z0-9_]", self.cleaned_data['code']):
+            raise forms.ValidationError('Project code may only contain letters, numbers and _')
+        return self.cleaned_data["code"]
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('code', 'fullname', 'description', 'localfiletype')
+    list_display_links = ('code', 'fullname')
+    prepopulated_fields = {"fullname": ("code",)}
+    radio_fields = {"treestyle": admin.VERTICAL}
+    fieldsets = (
+        (None, {
+            'fields': ('code', 'fullname', 'description', 'localfiletype')
+        }),
+        ('Advanced options', {
+            'classes': ('collapse',),
+            'fields': ('createmofiles', 'treestyle', 'ignoredfiles')
+        }),
+    )
+    form = MyProjectAdminForm
+
+admin.site.register(Project, ProjectAdmin)
