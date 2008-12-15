07fdf3277f3ed9259f56bfa48a72e08d2bc487b3
Wynand Winterbach <wynand@translate.org.za>
2008-12-15 10:53:05 +0000

    [Contibuted by Gasper Zejn] Don't instantiate PootleServer on every request.
    svn path=/src/trunk/Pootle/; revision=9267

--- views/main.py
+++ views/main.py
@@ -4,6 +4,13 @@ from Pootle.pootle import PootleServer
 from Pootle.views.util import render_jtoolkit
 from Pootle import pan_app
 
+_pootle = None
+
 def pass_to_pootle(request, path):
-    pootle = PootleServer()
-    return render_jtoolkit(pootle.getpage(request, path))
+    global _pootle
+    
+    if _pootle is None:
+        _pootle = PootleServer()
+
+    page = _pootle.getpage(request, path)
+    return render_jtoolkit(page)
