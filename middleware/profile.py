70c849296dfd2914495a684e908f6c142f863b92
Julen Ruiz Aizpuru <julenx@gmail.com>
2009-01-11 15:28:37 +0000

    Disable profiling middleware for Python versions lower than 2.5.
    cProfile module is only available since Python 2.5 and as we use a method that breaks the interface compatibility with the profile module available in Python 2.4, we need to disable this feature for older versions of Python.
    
    This fixes bug 671.
    
    svn path=/src/trunk/Pootle/; revision=9597

--- middleware/profile.py
+++ middleware/profile.py
@@ -23,20 +23,22 @@
 # http://www.djangosnippets.org/snippets/727/
 
 import sys
-import cProfile
-from cStringIO import StringIO
 from django.conf import settings
-from Pootle.profiling import lsprofcalltree
+try:
+    import cProfile
+    from Pootle.profiling import lsprofcalltree
+except ImportError:
+    pass
 
 class ProfilerMiddleware(object):
     def process_view(self, request, callback, callback_args, callback_kwargs):
-        if settings.DEBUG and 'prof' in request.GET:
+        if not sys.version_info < (2,5) and settings.DEBUG and 'prof' in request.GET:
             self.profiler = cProfile.Profile()
             args = (request,) + callback_args
             return self.profiler.runcall(callback, *args, **callback_kwargs)
 
     def process_response(self, request, response):
-        if settings.DEBUG and 'prof' in request.GET:
+        if not sys.version_info < (2,5) and settings.DEBUG and 'prof' in request.GET:
             print "Profiling to the file %s" % request.GET['prof']
             cache_grind_data = lsprofcalltree.KCacheGrind(self.profiler)
             f = None
