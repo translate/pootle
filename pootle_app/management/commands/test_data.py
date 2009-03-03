de38f4dd2d92a0e1264e70ff2a67a2aeb1cbc082
Wynand Winterbach <wynand@translate.org.za>
2009-03-03 17:18:29 +0000

    Forgot to add this. It's meant to prime the database for testing certain features. Currently, it only gives all permissions to everyone by assigning full permissions to the default and nobody users at the root Directory node.
    svn path=/src/trunk/Pootle/; revision=10329

---
+++ pootle_app/management/commands/test_data.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Pootle.settings'
+
+from django.db import transaction
+import sys
+import md5
+
+from django.core.management.base import NoArgsCommand
+from django.core.management.color import no_style
+from django.contrib.auth.models import User, Permission
+from django.contrib.contenttypes.models import ContentType
+
+from pootle_app.permissions import PermissionSet, get_pootle_permissions
+from pootle_app.fs_models import Directory
+from pootle_app.profile import PootleProfile
+
+class Command(NoArgsCommand):
+    def handle_noargs(self, **options):
+        modify_default_permissions()
+
+def modify_default_permissions():
+    root = Directory.objects.root
+    all_permissions = get_pootle_permissions().values()
+    for username in ('default', 'nobody'):
+        profile = PootleProfile.objects.select_related(depth=1).get(user__username=username)
+        permission_set = PermissionSet.objects.get(profile=profile, directory=root)
+        permission_set.positive_permissions = all_permissions
+        permission_set.save()
+
+if __name__ == "__main__":
+    main()
