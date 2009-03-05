dbf1560e5f782f880672bdeeb0c7f17d671d05c4
Friedel Wolff <friedel@translate.org.za>
2009-03-05 12:59:42 +0000

    Remove unused module
    svn path=/src/trunk/Pootle/; revision=10363

--- pootle_app/views/main.py
+++
@@ -1,9 +0,0 @@
-from django.db import connection
-
-from Pootle.pootle import PootleServer
-from pootle_app.views.util import render_jtoolkit
-from Pootle import pan_app
-
-def pass_to_pootle(request, path):
-    page = pan_app.pootle_server.getpage(request, path)
-    return render_jtoolkit(page)
