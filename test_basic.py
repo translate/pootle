4a7e84fbf1410bb59ec071b2333a457f206aa594
Alaa Abd el Fattah <alaa@translate.org.za>
2009-03-05 15:04:34 +0000

    simple example tests run ./manage.py test pootle_app -- test_basic.py
    svn path=/src/trunk/Pootle/; revision=10372

--- test_basic.py
+++ test_basic.py
@@ -3,15 +3,17 @@ from django.test.client import Client
 from django.contrib.auth.models import User
 from pootle_app.translation_project import TranslationProject
 
-
+import re
 
 def test_frontpage():
-    """test that a front page exists
-    only useful for testing the testing environment really"""
-    
     c = Client()
-    import pdb
     response = c.get('/')
     assert response.status_code == 200
     
 
+
+def test_login():
+    c = Client()
+    response = c.post('/login.html', {'username': 'admin', 'password': 'admin'})
+    assert response.status_code == 302
+    assert re.match('http://.*/home/$', response.get('Location', None))
