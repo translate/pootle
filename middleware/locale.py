a9345c63d3c9b0624aee14db3eac03b47629ce95
Friedel Wolff <friedel@translate.org.za>
2009-03-06 15:26:30 +0000

    Remove unused import. Update license block.
    svn path=/src/trunk/Pootle/; revision=10409

--- middleware/locale.py
+++ middleware/locale.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# 
-# Copyright 2008 Zuza Software Foundation
-# 
-# This file is part of translate.
 #
-# translate is free software; you can redistribute it and/or modify
+# Copyright 2008-2009 Zuza Software Foundation
+#
+# This file is part of Pootle.
+#
+# This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
-# translate is distributed in the hope that it will be useful,
+#
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with translate; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+
 
 from django.utils.cache import patch_vary_headers
-from Pootle.i18n import gettext, jtoolkit_i18n, user_lang_discovery
+from Pootle.i18n import gettext, user_lang_discovery
 
 class LocaleMiddleware(object):
     """
