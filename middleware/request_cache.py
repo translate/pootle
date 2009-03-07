fca6aa04ef963ca8650d5a2ec1454250ee3262b5
Friedel Wolff <friedel@translate.org.za>
2009-03-07 10:43:25 +0000

    Minor cleanups: license blocks, copyright dates, unused import
    svn path=/src/trunk/Pootle/; revision=10418

--- middleware/request_cache.py
+++ middleware/request_cache.py
@@ -5,19 +5,18 @@
 #
 # This file is part of Pootle.
 #
-# Pootle is free software; you can redistribute it and/or modify
+# This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
-# Pootle is distributed in the hope that it will be useful,
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Pootle; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 from Pootle import request_cache
 
