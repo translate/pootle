0a1ea33f27ee9ad23bac5167b7608ba2be5eb669
Alaa Abd el Fattah <alaa@translate.org.za>
2009-03-10 15:35:10 +0000

    add a non provileged test user
    svn path=/src/trunk/Pootle/; revision=10476

--- pytest.py
+++ pytest.py
@@ -1,23 +1,40 @@
 import sys
+import md5
 
 from django.conf import settings
 from django.db import connection
 from django.test import utils
 from django.core.management import call_command
+from django.contrib.auth.models import User
 from pootle_app.translation_project import scan_translation_projects
 from py import test
 
 
-def run_tests(test_labels, verbosity=1, interactive=True, extra_tests=[]):
-    # Prepare django for testing.
+def setup_test_environment(verbosity, interactive):
     utils.setup_test_environment()
     old_db_name = settings.DATABASE_NAME
     test_db_name = connection.creation.create_test_db(verbosity, autoclobber=not interactive)
     call_command('initdb', verbosity=verbosity)
     scan_translation_projects()
-    
+    create_test_users()
     # Pretend it's a production environment.
     settings.DEBUG = False
+    return (old_db_name, test_db_name)
+    
+def teardown_test_environment(verbosity, interactive, old_db_name):
+    connection.creation.destroy_test_db(old_db_name, verbosity)
+    utils.teardown_test_environment()
+
+def create_test_users():
+    nonpriv = User(username=u"nonpriv",
+                  first_name="Non privileged test user",
+                  is_active=True)
+    nonpriv.password = md5.new("nonpriv").hexdigest()
+    nonpriv.save()
+                  
+def run_tests(test_labels, verbosity=1, interactive=True, extra_tests=[]):
+    # Prepare django for testing.
+    (old_db_name, test_db_name) = setup_test_environment(verbosity, interactive)
     
     pytest_argv = []
     if hasattr(settings, 'PYTEST_ARGS'):
@@ -35,6 +52,5 @@ def run_tests(test_labels, verbosity=1, interactive=True, extra_tests=[]):
     pytest_session = test.config.initsession()
     failures = pytest_session.main()
     # Clean up django.
-    connection.creation.destroy_test_db(old_db_name, verbosity)
-    utils.teardown_test_environment()
+    teardown_test_environment(verbosity, interactive, old_db_name)
     return len(failures)
