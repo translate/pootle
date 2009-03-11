abeaab12bba89804269976886af74519d8074af5
Alaa Abd el Fattah <alaa@translate.org.za>
2009-03-11 15:19:17 +0000

    use a temp podir for testing
    svn path=/src/trunk/Pootle/; revision=10499

--- pytest.py
+++ pytest.py
@@ -1,5 +1,8 @@
 import sys
 import md5
+import tempfile
+import shutil
+import os
 
 from django.conf import settings
 from django.db import connection
@@ -10,31 +13,91 @@ from pootle_app.translation_project import scan_translation_projects
 from py import test
 
 
-def setup_test_environment(verbosity, interactive):
-    utils.setup_test_environment()
-    old_db_name = settings.DATABASE_NAME
-    test_db_name = connection.creation.create_test_db(verbosity, autoclobber=not interactive)
-    call_command('initdb', verbosity=verbosity)
-    scan_translation_projects()
-    create_test_users()
-    # Pretend it's a production environment.
-    settings.DEBUG = False
-    return (old_db_name, test_db_name)
-    
-def teardown_test_environment(verbosity, interactive, old_db_name):
-    connection.creation.destroy_test_db(old_db_name, verbosity)
-    utils.teardown_test_environment()
+class TestEnv(object):
+    def __init__(self, verbosity, interactive):
+        self.verbosity = verbosity
+        self.interactive = interactive
+        
+        utils.setup_test_environment()
+        self.create_test_podir()
+        self.create_test_db()
+        self.create_test_users()
+        
+        scan_translation_projects()
+        # Pretend it's a production environment.
+        settings.DEBUG = False
+        
+            
+    def __del__(self):
+        self.teardown_test_db()
+        utils.teardown_test_environment()
+        self.teardown_test_podir()
 
-def create_test_users():
-    nonpriv = User(username=u"nonpriv",
-                  first_name="Non privileged test user",
-                  is_active=True)
-    nonpriv.password = md5.new("nonpriv").hexdigest()
-    nonpriv.save()
-                  
+
+    def create_test_db(self):
+        self.old_db_name = settings.DATABASE_NAME
+        self.test_db_name = connection.creation.create_test_db(self.verbosity, autoclobber=not self.interactive)
+        call_command('initdb', verbosity=self.verbosity)
+
+
+    def create_test_podir(self):
+        self.oldpodir = settings.PODIRECTORY
+        self.testpodir = tempfile.mkdtemp()
+        settings.PODIRECTORY = self.testpodir
+        
+        gnu = os.path.join(self.testpodir, "terminology")
+        os.mkdir(gnu)
+        potfile = file(os.path.join(gnu, "terminology.pot"), 'w')
+        potfile.write('#: test.c\nmsgid "test"\nmsgstr ""\n')
+        potfile.close()
+        pofile = file(os.path.join(gnu, "ar.po"), 'w')
+        pofile.write('#: test.c\nmsgid "test"\nmsgstr "rest"\n')
+        pofile.close()
+
+        nongnu = os.path.join(self.testpodir, "pootle")
+        os.mkdir(nongnu)
+        nongnu_ar = os.path.join(nongnu, "ar")
+        os.mkdir(nongnu_ar)
+        nongnu_af = os.path.join(nongnu, "af")
+        os.mkdir(nongnu_af)
+        xliffcontents = '''<?xml version="1.0" encoding="utf-8"?>
+<xliff version="1.1" xmlns="urn:oasis:names:tc:xliff:document:1.1">
+<file datatype="po" original="test_upload.po" source-language="en-US">
+        <body>
+            <trans-unit id="1" xml:space="preserve">
+                <source>test</source>
+                <target state="translated">rest</target>
+                <context-group name="po-reference" purpose="location">
+                    <context context-type="sourcefile">test.c</context>
+                </context-group>
+            </trans-unit>
+        </body>
+    </file>
+</xliff>'''
+        xliffile = file(os.path.join(nongnu_af, "pootle.xlf"), 'w')
+        xliffile.write(xliffcontents)
+        xliffile.close()
+
+
+    def create_test_users(self):
+        nonpriv = User(username=u"nonpriv",
+                       first_name="Non privileged test user",
+                       is_active=True)
+        nonpriv.password = md5.new("nonpriv").hexdigest()
+        nonpriv.save()
+
+
+    def teardown_test_podir(self):
+        settings.PODIRECTORY = self.oldpodir
+        shutil.rmtree(self.testpodir)
+    
+    def teardown_test_db(self):
+        connection.creation.destroy_test_db(self.old_db_name, self.verbosity)
+
+        
 def run_tests(test_labels, verbosity=1, interactive=True, extra_tests=[]):
     # Prepare django for testing.
-    (old_db_name, test_db_name) = setup_test_environment(verbosity, interactive)
+    testenv = TestEnv(verbosity, interactive)
     
     pytest_argv = []
     if hasattr(settings, 'PYTEST_ARGS'):
@@ -51,6 +114,8 @@ def run_tests(test_labels, verbosity=1, interactive=True, extra_tests=[]):
     test.config.parse(pytest_argv)
     pytest_session = test.config.initsession()
     failures = pytest_session.main()
+    
     # Clean up django.
-    teardown_test_environment(verbosity, interactive, old_db_name)
+    #del(testenv)
+    
     return len(failures)
