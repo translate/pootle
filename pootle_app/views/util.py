61bf4cce36630967e3dbb009566e202b424636bc
Alaa Abd el Fattah <alaa@translate.org.za>
2009-04-02 16:00:22 +0000

    weird things happen when u use a generator for HttpResponse
    svn path=/src/trunk/Pootle/; revision=10771

--- pootle_app/views/util.py
+++ pootle_app/views/util.py
@@ -54,7 +54,7 @@ def render(relative_template_path, **template_vars):
 
     # Render the template to a string and send the string
     # to HttpResponse
-    return HttpResponse(template.generate(output="xhtml"))
+    return HttpResponse(template.serialize(output="xhtml"))
 
 class AttrDict(dict):
     # THIS IS TAKEN FROM JTOOLKIT
