4880d490d73a71fe70c8c517720c00aea0525d7a
Alaa Abd el Fattah <alaa@translate.org.za>
2009-04-09 23:31:13 +0000

    don't pass prefix as part of url
    svn path=/src/trunk/Pootle/; revision=10854

--- wsgi.py
+++ wsgi.py
@@ -30,8 +30,6 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'pootle.settings'
 
 import django.core.handlers.wsgi
 
-_application = django.core.handlers.wsgi.WSGIHandler()
+application = django.core.handlers.wsgi.WSGIHandler()
 
-def application(environ, start_response):
-    environ['PATH_INFO'] = environ['SCRIPT_NAME'] + environ['PATH_INFO']
-    return _application(environ, start_response)
+
