b6d6c7f2691bb56a896493cc9ea82c00846d32cc
Alaa Abd el Fattah <alaa@translate.org.za>
2009-04-30 02:31:59 +0000

    move tm functions from store_file
    svn path=/src/trunk/Pootle/; revision=10943

--- local_apps/pootle_store/models.py
+++ local_apps/pootle_store/models.py
@@ -47,6 +47,7 @@ class Store(models.Model):
     
     file        = TranslationStoreField(upload_to="fish", max_length=255, storage=fs, db_index=True, null=False)
     pending     = TranslationStoreField(upload_to="fish", max_length=255, storage=fs)
+    tm          = TranslationStoreField(upload_to="fish", max_length=255, storage=fs)
     parent      = models.ForeignKey(Directory, related_name='child_stores', db_index=True)
     pootle_path = models.CharField(max_length=255, null=False, unique=True, db_index=True)
     name        = models.CharField(max_length=128, null=False)
@@ -270,6 +271,31 @@ class Store(models.Model):
                     header.allcomments[i].extend(newheader.allcomments[i])
 
         self.file.savestore()
+
+    def inittm(self):
+        """initialize translation memory file if needed"""
+        if self.tm and os.path.exists(self.tm):
+            return
+        
+        tm_filename = self.file.path + os.extsep + 'tm' + os.extsep + 'po'
+        if os.path.exists(tm_filename):
+            self.tm = tm_filename
+            self.save()
+
+    def gettmsuggestions(self, item):
+        """find all the tmsuggestion items submitted for the given
+        item"""
+
+        self.inittm()
+        if self.tm:
+            unit = self.getitem(item)
+            locations = unit.getlocations()
+            # TODO: review the matching method Can't simply use the
+            # location index, because we want multiple matches
+            suggestpos = [suggestpo for suggestpo in self.tm.store.units
+                          if suggestpo.getlocations() == locations]
+            return suggestpos
+        return []
         
 def set_store_pootle_path(sender, instance, **kwargs):
     instance.pootle_path = '%s%s' % (instance.parent.pootle_path, instance.name)
