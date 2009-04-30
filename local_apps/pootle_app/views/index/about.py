e4ceaee0d2e3d053213e9e3b1492ac16195667f0
Alaa Abd el Fattah <alaa@translate.org.za>
2009-04-30 13:32:17 +0000

    migrate about.html to django templates thanks to pjn (Pål Eivind Jacobsen Nes)
    svn path=/src/trunk/Pootle/; revision=10955

--- local_apps/pootle_app/views/index/about.py
+++ local_apps/pootle_app/views/index/about.py
@@ -20,95 +20,49 @@
 
 import sys
 import os
-try:
-# ElementTree is part of Python 2.5, so let's try that first
-    from xml.etree import ElementTree
-except ImportError:
-    from elementtree import ElementTree
 
-from kid import __version__ as kidversion
+from django.utils.translation import ugettext as _
+N_ = _
 
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.conf import settings
+
+# get versions
+import django
 from translate import __version__ as toolkitversion
-
-from pootle_app.views.util import render_jtoolkit
-from pootle_app.views.indexpage import shortdescription
-from pootle_app.views import pagelayout
 from pootle_app import __version__ as pootleversion
-from pootle_app.lib.legacy.jToolkit import __version__ as jtoolkitversion
+
+
 
 def view(request):
-    return render_jtoolkit(AboutPage(request))
+    data = {
+        'pagetitle': _(settings.TITLE),
+        'description': _(settings.DESCRIPTION),
+        'keywords' : [ 'Pootle',
+                       'locamotion',
+                       'translate',
+                       'translation',
+                       'localisation',
+                       'localization',
+                       'l10n',
+                       'traduction',
+                       'traduire',
+                       ],
+        'versiontext': _("""
+        This site is running:<br />
+        Pootle %s<br />
+        Translate Toolkit %s<br />
+        Django %s<br />
+        Python %s (on %s/%s)
+        """, (pootleversion.sver,
+              toolkitversion.sver,
+              django.get_version(),
+              sys.version,
+              sys.platform,
+              os.name,
+              )),
+        }
+                         
+    return render_to_response('index/about.html', data, context_instance=RequestContext(request))
 
-class AboutPage(pagelayout.PootlePage):
-
-    """the bar at the side describing current login details etc"""
-
-    def __init__(self, request):
-        pagetitle = pagelayout.get_title()
-        description = pagelayout.get_description()
-        meta_description = shortdescription(description)
-        keywords = [
-            'Pootle',
-            'locamotion',
-            'translate',
-            'translation',
-            'localisation',
-            'localization',
-            'l10n',
-            'traduction',
-            'traduire',
-            ]
-        abouttitle = _('About Pootle')
-        # l10n: Take care to use HTML tags correctly. A markup error
-        # could cause a display error.
-        introtext = \
-            _("<strong>Pootle</strong> is a simple web portal that should allow you to <strong>translate</strong>! Since Pootle is <strong>Free Software</strong>, you can download it and run your own copy if you like. You can also help participate in the development in many ways (you don't have to be able to program)."
-              )
-        hosttext = \
-            _('The Pootle project itself is hosted at <a href="http://translate.sourceforge.net/">translate.sourceforge.net</a> where you can find the details about source code, mailing lists etc.'
-              )
-        # l10n: If your language uses right-to-left layout and you
-        # leave the English untranslated, consider enclosing the
-        # necessary text with <span dir="ltr">.......</span> to help
-        # browsers to display it correctly. l10n: Take care to use
-        # HTML tags correctly. A markup error could cause a display
-        # error.
-        nametext = \
-            _('The name stands for <b>PO</b>-based <b>O</b>nline <b>T</b>ranslation / <b>L</b>ocalization <b>E</b>ngine, but you may need to read <a href="http://www.thechestnut.com/flumps.htm">this</a>.'
-              )
-        versiontitle = _('Versions')
-        # l10n: If your language uses right-to-left layout and you
-        # leave the English untranslated, consider enclosing the
-        # necessary text with <span dir="ltr">.......</span> to help
-        # browsers to display it correctly. l10n: Take care to use
-        # HTML tags correctly. A markup error could cause a display
-        # error.
-        versiontext = \
-            _('This site is running:<br />Pootle %s<br />Translate Toolkit %s<br />jToolkit %s<br />Kid %s<br />ElementTree %s<br />Python %s (on %s/%s)'
-               % (
-            pootleversion.ver,
-            toolkitversion.sver,
-            jtoolkitversion.ver,
-            kidversion,
-            ElementTree.VERSION,
-            sys.version,
-            sys.platform,
-            os.name,
-            ))
-        templatename = 'index/about.html'
-        instancetitle = pagelayout.get_title()
-        templatevars = {
-            'pagetitle': pagetitle,
-            'description': description,
-            'meta_description': meta_description,
-            'keywords': keywords,
-            'abouttitle': abouttitle,
-            'introtext': introtext,
-            'hosttext': hosttext,
-            'nametext': nametext,
-            'versiontitle': versiontitle,
-            'versiontext': versiontext,
-            'instancetitle': instancetitle,
-            }
-        pagelayout.PootlePage.__init__(self, templatename, templatevars,
-                                       request)
