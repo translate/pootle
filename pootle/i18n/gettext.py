8dfa707f6afa41950e571ffb3972058f4b052ee8
Friedel Wolff <friedel@translate.org.za>
2009-06-02 14:43:52 +0000

    Clean up whitespace and license header
    svn path=/src/trunk/Pootle/; revision=11274

--- pootle/i18n/gettext.py
+++ pootle/i18n/gettext.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# 
-# Copyright 2008 Zuza Software Foundation
-# 
-# This file is part of translate.
 #
-# translate is free software; you can redistribute it and/or modify
+# Copyright 2008-2009 Zuza Software Foundation
+#
+# This file is part of Pootle.
+#
+# This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
-# translate is distributed in the hope that it will be useful,
+#
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with translate; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 from django.conf import settings
 from django.utils import translation
@@ -203,8 +202,3 @@ def get_lang(language):
         return TranslationProject.objects.get(language=language, project__code='pootle')
     except TranslationProject.DoesNotExist:
         return None
-    
-        
-
-
-
