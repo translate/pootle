bfac087bcb5f0917836f4d4b97bfd27dd604de06
Friedel Wolff <friedel@translate.org.za>
2009-06-10 14:17:33 +0000

    Clean up header, whitespace and unused import
    svn path=/src/trunk/Pootle/; revision=11470

--- local_apps/pootle_app/models/search.py
+++ local_apps/pootle_app/models/search.py
@@ -1,28 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# 
-# Copyright 2009 Zuza Software Foundation
-# 
-# This file is part of translate.
 #
-# translate is free software; you can redistribute it and/or modify
+# Copyright 2009 Zuza Software Foundation
+#
+# This file is part of Pootle.
+#
+# This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
-# translate is distributed in the hope that it will be useful,
+#
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with translate; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 import bisect
 
 from django.db                import models
-from django.utils.translation import ugettext_lazy as _
 
 from translate.tools import pogrep
 
@@ -192,7 +190,7 @@ class Search(object):
             if self.search_results is not None and \
                     store.pootle_path not in self.search_results:
                 return iter([])
-            
+
             stats = metadata.property_stats(store, self.translation_project.checker)
             total = stats['total']
             result = total[range[0]:range[1]]
