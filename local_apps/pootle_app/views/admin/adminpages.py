28e6a045129c3a57978ea0febddf1d237ffd6dce
Alaa Abd el Fattah <alaa@translate.org.za>
2009-06-15 14:06:22 +0000

    Remove unneeded code. 'adminindex.html' template only needs the form object to be passed
    svn path=/src/trunk/Pootle/; revision=11589

--- local_apps/pootle_app/views/admin/adminpages.py
+++ local_apps/pootle_app/views/admin/adminpages.py
@@ -33,39 +33,6 @@ from pootle_app.forms import GeneralSettingsForm
 from djblets.siteconfig.models import SiteConfiguration
 siteconfig = SiteConfiguration.objects.get_current()
 
-def gettext():
-    """Localize the text"""
-
-    text = {}
-    text['home'] = _('Home')
-    text['admin'] = _("Main admin page"),
-    text['users'] = _('Users')
-    text['languages'] = _('Languages')
-    text['projects'] = _('Projects')
-    text['generaloptions'] = _('General options')
-    text['option'] = _('Option')
-    text['currentvalue'] = _('Current value')
-    text['savechanges'] = _('Save changes')
-    return text
-
-def getoptions():
-    optiontitles = {
-        'TITLE': _('Title'),
-        'DESCRIPTION': _('Description'),
-        'MEDIA_URL': _('Media URL'),
-        'HOMEPAGE': _('Home Page'),
-        }
-    option_values = {'TITLE': pagelayout.get_title(),
-                     'DESCRIPTION': pagelayout.get_description()}
-    options = []
-    for (optionname, optiontitle) in optiontitles.items():
-        optionvalue = getattr(settings, optionname,
-                              option_values.get(optionname, ''))
-        option = {'name': 'option-%s' % optionname, 'title': optiontitle,
-                  'value': optionvalue}
-        options.append(option)
-    return options
-
 @user_is_admin
 def view(request, path):
     if request.POST:
@@ -76,13 +43,7 @@ def view(request, path):
     else:
         setting_form = GeneralSettingsForm(siteconfig)
     template = 'admin/adminindex.html'
-    instancetitle = pagelayout.get_title()
-    text = gettext()
     template_vars = {
-        'options': getoptions(),
-        'pagetitle': _('Pootle Admin Page'),
-        'instancetitle': instancetitle,
-        'text': text,
         'form': setting_form,
         }
     return render_to_response(template, template_vars, context_instance=RequestContext(request))
