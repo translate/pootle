75cb2365135079c93c56ab662e7ace53804ba141
Frederic Wenzel <fwenzel@mozilla.com>
2009-07-16 12:35:47 +0000

    adding some logging to hooks script, along with a spoonful of PEP-8 indentation
    svn path=/src/trunk/Pootle/; revision=11922

--- pootle/scripts/amo.py
+++ pootle/scripts/amo.py
@@ -16,75 +16,75 @@ import logging
 from pootle.scripts.convert import monopo2po, po2monopo
 
 def _getfiles(file):
-  mainfile = os.path.join(os.path.split(file)[0], "messages.po")
-  combinedfile = os.path.join(os.path.split(file)[0], "messages-combined.po")
-  sourcefile = os.path.join(os.path.split(os.path.split(os.path.split(file)[0])[0])[0], "en_US", "LC_MESSAGES", "messages.po")
-  return (combinedfile, mainfile, sourcefile)
+    mainfile = os.path.join(os.path.split(file)[0], "messages.po")
+    combinedfile = os.path.join(os.path.split(file)[0], "messages-combined.po")
+    sourcefile = os.path.join(os.path.split(os.path.split(os.path.split(file)[0])[0])[0], "en_US", "LC_MESSAGES", "messages.po")
+    return (combinedfile, mainfile, sourcefile)
 
 def initialize(projectdir, languagecode):
-  """The first paramater is the path to the project directory.  It's up to this
-  script to know any internal structure of the directory"""
+    """The first paramater is the path to the project directory.  It's up to this
+    script to know any internal structure of the directory"""
 
-  logger = logging.getLogger('scripts.amo')
-  logger.info("Initializing language %s of project %s" % (languagecode, os.path.basename(projectdir)))
+    logger = logging.getLogger('scripts.amo')
+    logger.info("Initializing language %s of project %s" % (languagecode, os.path.basename(projectdir)))
 
-  # Find the files we're working with
-  mainfile     = os.path.join(projectdir, languagecode, 'LC_MESSAGES', 'messages.po')
-  combinedfile = os.path.join(projectdir, languagecode, 'LC_MESSAGES', 'messages-combined.po')
-  sourcefile   = os.path.join(projectdir, 'en_US', 'LC_MESSAGES', 'messages.po')
+    # Find the files we're working with
+    mainfile     = os.path.join(projectdir, languagecode, 'LC_MESSAGES', 'messages.po')
+    combinedfile = os.path.join(projectdir, languagecode, 'LC_MESSAGES', 'messages-combined.po')
+    sourcefile   = os.path.join(projectdir, 'en_US', 'LC_MESSAGES', 'messages.po')
 
-  # Build our combined file
-  monopo2po.convertpo(open(sourcefile,"r"), open(combinedfile,"w"), open(mainfile,"r"))
+    # Build our combined file
+    monopo2po.convertpo(open(sourcefile,"r"), open(combinedfile,"w"), open(mainfile,"r"))
 
-  # TODO
-  # Eventually we'll need to build .po files from the .thtml files in /pages/ but
-  # running html2po on localized files doesn't make sense
+    # TODO
+    # Eventually we'll need to build .po files from the .thtml files in /pages/ but
+    # running html2po on localized files doesn't make sense
 
 
 def precommit(committedfile, author, message):
-  if os.path.basename(committedfile) == "messages-combined.po":
-    logger = logging.getLogger('scripts.amo')
+    if os.path.basename(committedfile) == "messages-combined.po":
+        logger = logging.getLogger('scripts.amo')
 
-    # Get the files we'll be using
-    (combinedfile, mainfile, sourcefile) = _getfiles(committedfile)
-    
-    # Update messages.po 
-    logger.debug("Converting po %s to %s" % (combinedfile, mainfile))
-    po2monopo.convertpo(open(combinedfile,"r"), open(mainfile,"w"))
+        # Get the files we'll be using
+        (combinedfile, mainfile, sourcefile) = _getfiles(committedfile)
+        
+        # Update messages.po 
+        logger.debug("Converting po %s to %s" % (combinedfile, mainfile))
+        po2monopo.convertpo(open(combinedfile,"r"), open(mainfile,"w"))
 
-    # We want to commit messages.po
-    return [mainfile]
-  return []
+        # We want to commit messages.po
+        return [mainfile]
+    return []
 
 def postcommit(committedfile, success):
-  if os.path.basename(committedfile) == "messages.po":
-    logger = logging.getLogger('scripts.amo')
-    
-    # Get the files we'll be using
-    (combinedfile, mainfile, sourcefile) = _getfiles(committedfile)
+    if os.path.basename(committedfile) == "messages.po":
+        logger = logging.getLogger('scripts.amo')
 
-    # Recreate messages-combined.po 
-    logger.debug("Converting amo %s to %s with template %s" % (sourcefile, combinedfile, mainfile))
-    monopo2po.convertpo(open(sourcefile,"r"), open(combinedfile,"w"), open(mainfile,"r"))
+        # Get the files we'll be using
+        (combinedfile, mainfile, sourcefile) = _getfiles(committedfile)
+
+        # Recreate messages-combined.po 
+        logger.debug("Converting amo %s to %s with template %s" % (sourcefile, combinedfile, mainfile))
+        monopo2po.convertpo(open(sourcefile,"r"), open(combinedfile,"w"), open(mainfile,"r"))
 
 def preupdate(updatedfile):
-  if os.path.basename(updatedfile) == "messages-combined.po":
+    if os.path.basename(updatedfile) == "messages-combined.po":
+        logger = logging.getLogger('scripts.amo')
+
+        # Get the files we'll be using
+        (combinedfile, mainfile, sourcefile) = _getfiles(updatedfile)
+        
+        # We want to update messages.po
+        logger.debug("Updating %s" % mainfile)
+        return mainfile
+    return ""
+
+def postupdate(updatedfile):
     logger = logging.getLogger('scripts.amo')
 
     # Get the files we'll be using
     (combinedfile, mainfile, sourcefile) = _getfiles(updatedfile)
-    
-    # We want to update messages.po
-    logger.debug("Updating %s" % mainfile)
-    return mainfile
-  return ""
 
-def postupdate(updatedfile):
-  logger = logging.getLogger('scripts.amo')
-
-  # Get the files we'll be using
-  (combinedfile, mainfile, sourcefile) = _getfiles(updatedfile)
-
-  # Create the new messages-combined.po file
-  logger.debug("Converting amo %s to %s with template %s" % (sourcefile, combinedfile, mainfile))
-  monopo2po.convertpo(open(sourcefile,"r"), open(combinedfile,"w"), open(mainfile,"r"))
+    # Create the new messages-combined.po file
+    logger.debug("Converting amo %s to %s with template %s" % (sourcefile, combinedfile, mainfile))
+    monopo2po.convertpo(open(sourcefile,"r"), open(combinedfile,"w"), open(mainfile,"r"))
--- pootle/scripts/hooks.py
+++ pootle/scripts/hooks.py
@@ -4,30 +4,34 @@ import os
 import os.path
 import subprocess
 
+import logging
+
 def hook(project, hooktype, file, *args, **kwargs):
-  """
-  project should be the projectcode of any project.
-  hooktype should be precommit, postcommit, preupdate or postupdate.
-  file should be the absolute path of the file.
+    """
+    project should be the projectcode of any project.
+    hooktype should be precommit, postcommit, preupdate or postupdate.
+    file should be the absolute path of the file.
 
-  Other arguments depend on the hooktype:
-    precommit should have "author" and "message" as arguments.
-    postcommit should have "success" as arguments.
-    preupdate and postupdate have no additional arguments.
+    Other arguments depend on the hooktype:
+        precommit should have "author" and "message" as arguments.
+        postcommit should have "success" as arguments.
+        preupdate and postupdate have no additional arguments.
 
-  Return value depends on the hooktype:
-    precommit returns an array of strings indicating what files to commit .
-    preupdate returns an array of strings indicating what files to update.
-    postcommit and postupdate return unit.
+    Return value depends on the hooktype:
+        precommit returns an array of strings indicating what files to commit.
+        preupdate returns an array of strings indicating what files to update.
+        postcommit and postupdate return unit.
 
-  """
-  try:
-    activehook = __import__(project, globals(), locals(), [])
-    if hasattr(activehook, hooktype) and callable(getattr(activehook, hooktype)):
-        return getattr(activehook, hooktype)(file, *args, **kwargs)
-    else:
-        return []
-  except ImportError, e:
-    raise ImportError(e)
-  except Exception, e:
-    print "Exception in project (%s) hook (%s) for file (%s): %s" % (project, hooktype, file, e)
+    """
+    logger = logging.getLogger('pootle.scripts.hooks')
+    try:
+        activehook = __import__(project, globals(), locals(), [])
+        if hasattr(activehook, hooktype) and callable(getattr(activehook, hooktype)):
+            logger.debug("Executing hook %s for project %s on file %s", hooktype, project, file)
+            return getattr(activehook, hooktype)(file, *args, **kwargs)
+        else:
+            return []
+    except ImportError, e:
+        raise ImportError(e)
+    except Exception, e:
+        logger.error("Exception in project (%s) hook (%s) for file (%s): %s" % (project, hooktype, file, e))
