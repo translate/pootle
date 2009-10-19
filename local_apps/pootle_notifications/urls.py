64efac599758d495d733b9641b0d6bb27df00483
Alaa Abd el Fattah <alaa@translate.org.za>
2009-10-19 17:47:52 +0000

    no trailing slash in feed url
    svn path=/src/trunk/Pootle/; revision=12447

--- local_apps/pootle_notifications/urls.py
+++ local_apps/pootle_notifications/urls.py
@@ -24,7 +24,7 @@ from pootle_notifications.views import *
 
 urlpatterns = patterns('',
     (r'^notice/viewitem/(?P<notice_id>[^/]*)/$', view_notice_item),
-    (r'^(?P<url>.*)/rss.xml/$', NoticeFeeds),
+    (r'^(?P<url>.*)/rss.xml$', NoticeFeeds),
     (r'^(?P<language_code>[^/]*)/notices/$', lang_notices),
     (r'^(?P<language_code>[^/]*)/(?P<project_code>[^/]*)/notices/$', transproj_notices),
 )
