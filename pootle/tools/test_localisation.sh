3cca66d33f263d65a1da6006b5e617f9e97dd977
Dwayne Bailey <dwayne@translate.org.za>
2009-10-21 18:41:25 +0000

    Make sure we have a plural equation in the test PO file.
    svn path=/src/trunk/Pootle/; revision=12532

--- pootle/tools/test_localisation.sh
+++ pootle/tools/test_localisation.sh
@@ -12,5 +12,6 @@ lang=$1
 cd $basedir
 mkdir locale
 ./manage.py makemessages --settings=pootle/settings.py -v 1 -e ".html" -e ".py" -l pot
+sed --in-place "s/INTEGER/2/g;s/EXPRESSION/(n!=1)/g" locale/pot/LC_MESSAGES/django.po
 podebug --rewrite=xxx locale/pot/LC_MESSAGES/django.po po/pootle/$lang/pootle.po
 rm -rf locale
