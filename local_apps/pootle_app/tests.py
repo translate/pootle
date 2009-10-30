1d4199c229568ceb921d79bc809b41b31d487a6d
Alaa Abd el Fattah <alaa@translate.org.za>
2009-10-30 23:43:55 +0000

    finished porting client tests.
    now we just need more coverage
    
    svn path=/src/trunk/Pootle/; revision=12763

--- local_apps/pootle_app/tests.py
+++ local_apps/pootle_app/tests.py
@@ -308,6 +308,219 @@ msgstr "resto"
         self.assertTrue(store.pending.read().find(suggestedcontent) >= 0)
 
 
+    def test_upload_new_xliff_file(self):
+        """Tests that we can upload a new XLIFF file into a project."""
+        xliffcontent = StringIO.StringIO('''<?xml version='1.0' encoding='utf-8'?>
+        <xliff xmlns="urn:oasis:names:tc:xliff:document:1.1" version="1.1">
+        <file original="" source-language="en-US" datatype="po">
+        <body>
+          <trans-unit id="1" xml:space="preserve">
+            <source>test</source>
+            <target state="needs-review-translation">rest</target>
+            <context-group name="po-reference" purpose="location">
+              <context context-type="sourcefile">test.c</context>
+            </context-group>
+          </trans-unit>
+        </body>
+        </file>
+        </xliff>
+''')
+        xliffcontent.name = 'test_new_xliff_upload.xlf'
+
+        post_dict = {
+            'file': xliffcontent,
+            'overwrite': 'overwrite',
+            'do_upload': 'upload',
+            }
+        
+        response = self.client.post("/ar/pootle/", post_dict)
+        self.assertContains(response,' href="/ar/pootle/test_new_xliff_upload.po')
+
+        #FIXME: test conversion?
+
+    def test_upload_xliff_over_file(self):
+        """Tests that we can upload a new version of a XLIFF file into a project."""
+        pocontent = StringIO.StringIO('#: test.c\nmsgid "test"\nmsgstr "rest"\n\n#: frog.c\nmsgid "tadpole"\nmsgstr "fish"\n')
+        pocontent.name = "test_upload_xliff.po"
+        post_dict = {
+            'file': pocontent,
+            'overwrite': 'overwrite',
+            'do_upload': 'upload',
+            }
+        response = self.client.post("/ar/pootle/", post_dict)
+
+        xlfcontent = StringIO.StringIO('''<?xml version="1.0" encoding="utf-8"?>
+        <xliff version="1.1" xmlns="urn:oasis:names:tc:xliff:document:1.1">
+        <file datatype="po" original="test_existing.po" source-language="en-US">
+        <body>
+            <trans-unit id="test" xml:space="preserve" approved="yes">
+                <source>test</source>
+                <target state="translated">rested</target>
+                <context-group name="po-reference" purpose="location">
+                    <context context-type="sourcefile">test.c</context>
+                </context-group>
+            </trans-unit>
+            <trans-unit id="slink" xml:space="preserve" approved="yes">
+                <source>slink</source>
+                <target state="translated">stink</target>
+                <context-group name="po-reference" purpose="location">
+                    <context context-type="sourcefile">toad.c</context>
+                </context-group>
+            </trans-unit>
+        </body>
+        </file>
+        </xliff>''')
+        xlfcontent.name = "test_upload_xliff.xlf"
+    
+        post_dict = {
+            'file': xlfcontent,
+            'overwrite': 'merge',
+            'do_upload': 'upload',
+            }
+        response = self.client.post("/ar/pootle/", post_dict)
+
+        # NOTE: this is what we do currently: any altered strings become suggestions.
+        # It may be a good idea to change this
+        mergedcontent = '#: test.c\nmsgid "test"\nmsgstr "rest"\n\n#~ msgid "tadpole"\n#~ msgstr "fish"\n\n#: toad.c\nmsgid "slink"\nmsgstr "stink"\n'
+        suggestedcontent = '#: test.c\nmsgid ""\n"_: suggested by admin\\n"\n"test"\nmsgstr "rested"\n'
+        store = Store.objects.get(pootle_path="/ar/pootle/test_upload_xliff.po")
+        
+        self.assertTrue(os.path.isfile(store.file.path))
+        self.assertTrue(store.file.read().find(mergedcontent) >= 0)
+
+        self.assertTrue(os.path.isfile(store.pending.path))
+        self.assertTrue(store.pending.read().find(suggestedcontent) >= 0)
+        
+
+    def test_submit_translation(self):
+        """Tests that we can translate units."""
+
+        submit_dict = {
+            'trans0': 'submitted translation',
+            'submit0': 'Submit',
+            'store': '/af/pootle/pootle.po',
+            }
+        submit_dict.update(formset_dict([]))
+        response = self.client.post("/af/pootle/pootle.po", submit_dict,
+                                    QUERY_STRING='view_mode=translate')
+    
+        self.assertContains(response, 'submitted translation')
+
+        store = Store.objects.get(pootle_path="/af/pootle/pootle.po")
+        self.assertTrue(store.file.read().find('submitted translation') >= 0)
+
+    def test_submit_plural_translation(self):
+        """Tests that we can submit a translation with plurals."""
+        pocontent = StringIO.StringIO('msgid "singular"\nmsgid_plural "plural"\nmsgstr[0] ""\nmsgstr[1] ""\n')
+        pocontent.name = 'test_plural_submit.po'
+
+        post_dict = {
+            'file': pocontent,
+            'overwrite': 'overwrite',
+            'do_upload': 'upload',
+            }
+        response = self.client.post("/ar/pootle/", post_dict)
+
+        submit_dict = {
+            'trans0-0': 'a fish',
+            'trans0-1': 'some fish',
+            'trans0-2': 'lots of fish',
+            'submit0': 'Submit',
+            'store': '/ar/pootle/test_plural_submit.po',
+            }
+        submit_dict.update(formset_dict([]))
+        response = self.client.post("/ar/pootle/test_plural_submit.po", submit_dict,
+                                    QUERY_STRING='view_mode=translate')
+
+        self.assertContains(response, 'a fish')
+        self.assertContains(response, 'some fish')
+        self.assertContains(response, 'lots of fish')
+        
+    def test_submit_plural_to_singular_lang(self):
+        """Tests that we can submit a translation with plurals to a language without plurals."""
+        
+        pocontent = StringIO.StringIO('msgid "singular"\nmsgid_plural "plural"\nmsgstr[0] ""\nmsgstr[1] ""\n')
+        pocontent.name = 'test_plural_submit.po'
+
+        post_dict = {
+            'file': pocontent,
+            'overwrite': 'overwrite',
+            'do_upload': 'upload',
+            }
+        response = self.client.post("/ja/pootle/", post_dict)
+
+        submit_dict = {
+            'trans0': 'just fish',
+            'submit0': 'Submit',
+            'store': '/ja/pootle/test_plural_submit.po',
+            }
+        submit_dict.update(formset_dict([]))
+        response = self.client.post("/ja/pootle/test_plural_submit.po", submit_dict,
+                                    QUERY_STRING='view_mode=translate')
+
+        self.assertContains(response, 'just fish')
+
+        expectedcontent = 'msgid "singular"\nmsgid_plural "plural"\nmsgstr[0] "just fish"\n'
+        store = Store.objects.get(pootle_path="/ja/pootle/test_plural_submit.po")
+        self.assertTrue(store.file.read().find(expectedcontent) >= 0)
+
+
+    def test_submit_fuzzy(self):
+        """Tests that we can mark a unit as fuzzy."""
+
+        # Fetch the page and check that the fuzzy checkbox is NOT checked.
+    
+        response = self.client.get("/af/pootle/pootle.po", {'view_mode': 'translate'})
+        self.assertContains(response, '<input type="checkbox"  name="fuzzy0" accesskey="f" id="fuzzy0" class="fuzzycheck" />')
+    
+        submit_dict = {
+            'trans0': 'fuzzy translation',
+            'fuzzy0': 'on',
+            'submit0': 'Submit',
+            'store': '/af/pootle/pootle.po',
+            }
+        submit_dict.update(formset_dict([]))
+        response = self.client.post("/af/pootle/pootle.po", submit_dict,
+                                    QUERY_STRING='view_mode=translate')
+        # Fetch the page again and check that the fuzzy checkbox IS checked.
+        response = self.client.get("/af/pootle/pootle.po", {'view_mode': 'translate'})
+        self.assertContains(response, '<input type="checkbox" checked="checked" name="fuzzy0" accesskey="f" id="fuzzy0" class="fuzzycheck" />')
+
+        store = Store.objects.get(pootle_path="/af/pootle/pootle.po")
+        self.assertTrue(store.file.getitem(0).isfuzzy())
+
+        # Submit the translation again, without the fuzzy checkbox checked
+        submit_dict = {
+            'trans0': 'fuzzy translation',
+            'fuzzy0': '',
+            'submit0': 'Submit',
+            'store': '/af/pootle/pootle.po',
+            }
+        submit_dict.update(formset_dict([]))
+        response = self.client.post("/af/pootle/pootle.po", submit_dict,
+                                    QUERY_STRING='view_mode=translate')
+        # Fetch the page once more and check that the fuzzy checkbox is NOT checked.
+        response = self.client.get("/af/pootle/pootle.po", {'view_mode': 'translate'})
+        self.assertContains(response, '<input type="checkbox"  name="fuzzy0" accesskey="f" id="fuzzy0" class="fuzzycheck" />')
+        self.assertFalse(store.file.getitem(0).isfuzzy())
+        
+    def test_submit_translator_comments(self):
+        """Tests that we can edit translator comments."""
+
+        submit_dict = {
+            'trans0': 'fish',
+            'translator_comments0': 'goodbye\nand thanks for all the fish',
+            'submit0': 'Submit',
+            'store': '/af/pootle/pootle.po',
+            }
+        submit_dict.update(formset_dict([]))
+        response = self.client.post("/af/pootle/pootle.po", submit_dict,
+                                    QUERY_STRING='view_mode=translate')
+
+        store = Store.objects.get(pootle_path='/af/pootle/pootle.po')
+        self.assertEqual(store.file.getitem(0).getnotes(), 'goodbye\nand thanks for all the fish')
+
+
 class NonprivTests(PootleTestCase):
     def setUp(self):
         super(NonprivTests, self).setUp()
