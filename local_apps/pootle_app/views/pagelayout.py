f4644460b40be4632a913b1a1b8da71acbb07232
Friedel Wolff <friedel@translate.org.za>
2009-11-06 23:15:22 +0000

    Update license block
    svn path=/src/trunk/Pootle/; revision=12904

--- local_apps/pootle_app/views/pagelayout.py
+++ local_apps/pootle_app/views/pagelayout.py
@@ -1,21 +1,22 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#  Copyright 2004-2007 Zuza Software Foundation
 #
-# This file is part of translate.
+# Copyright 2004-2009 Zuza Software Foundation
 #
-# translate is free software; you can redistribute it and/or modify it under the
-# terms of the GNU General Public License as published by the Free Software
-# Foundation; either version 2 of the License, or (at your option) any later
-# version.
+# This file is part of Virtaal.
 #
-# translate is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
 #
-# You should have received a copy of the GNU General Public License along with
-# translate; if not, write to the Free Software Foundation, Inc., 59 Temple
-# Place, Suite 330, Boston, MA  02111-1307  USA
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 from django.conf import settings
 from django.utils.html import escape
