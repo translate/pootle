a67d216b1d4a09ee70795a0b46c4308faceb028a
Dwayne Bailey <dwayne@translate.org.za>
2009-11-10 17:42:45 +0000

    Spelling error
    svn path=/src/trunk/Pootle/; revision=12953

--- local_apps/pootle_misc/dbinit.py
+++ local_apps/pootle_misc/dbinit.py
@@ -92,7 +92,7 @@ def footer():
     <p>%(endmsg)s</p>
     <div><script>setTimeout("location.reload()", 10000)</script></div>
     </body></html>
-    """  % { 'endmsg': _('Initialized database, you will be redirected to the fron page in 10 seconds') }
+    """  % { 'endmsg': _('Initialized database, you will be redirected to the front page in 10 seconds') }
     return text
 
 def staggered_install(exception):
