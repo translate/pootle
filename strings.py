8b496ce2f3827ffe20e7a31b4f54f15282f0fd95
Friedel Wolff <friedel@translate.org.za>
2009-11-12 10:38:33 +0000

    Some extra strings for extraction that we inherrit from Django
    svn path=/src/trunk/Pootle/; revision=12990

---
+++ strings.py
@@ -0,0 +1,22 @@
+# These are some strings that we need for successful extraction. They come from
+# Django and is not included in our POT file otherwise. This file itself is not
+# used for a running Pootle.
+
+# Don't change any of these strings unless they changed in Django. The adding
+# of extra comments to help translators is fine.
+
+_('Hold down "Control", or "Command" on a Mac, to select more than one.')
+_("Enter a valid e-mail address.")
+_("That e-mail address doesn't have an associated user account. Are you sure you've registered?")
+_("This field is required.")
+_("Ensure this value has at most %(max)d characters (it has %(length)d).")
+_("Save and add another")
+_("Save and continue editing")
+_("Add")
+_("Change")
+
+# l10n: This is a constructed string that might end up saying "Add language" or "Add project". If you need to translate that differently, you might want to try to translate it as "Add an entry to the %(name)s table" or "Add an object of type "%(name)s".
+_("Add %(name)s")
+
+# l10n: This link takes the user to the page where the currently configured option is displayed (such as a project or language)
+_("View on site")
