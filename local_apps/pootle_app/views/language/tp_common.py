e9f9c52a4b32826b754cc689d08b2002393acb27
Alaa Abd el Fattah <alaa@translate.org.za>
2009-11-18 21:19:14 +0000

    translations in class declaration so need ugettext_lazy
    svn path=/src/trunk/Pootle/; revision=13227

--- local_apps/pootle_app/views/language/tp_common.py
+++ local_apps/pootle_app/views/language/tp_common.py
@@ -26,7 +26,7 @@ import zipfile
 
 from django import forms
 from django.utils.safestring import mark_safe
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 
 from translate.storage import factory, versioncontrol
 
