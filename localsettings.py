9076351587c2314a1747263c092e8b717ea370ca
Friedel Wolff <friedel@translate.org.za>
2009-11-19 12:24:52 +0000

    Some extra review of comments. Mention defaults, etc.
    svn path=/src/trunk/Pootle/; revision=13246

--- localsettings.py
+++ localsettings.py
@@ -1,9 +1,12 @@
 # This file contains the configuration settings for the Pootle server.
+#
+# It is in Python sytax. Everything after '#' is ignored as comments.
 
+# Import some helper functions:
 from pootle.install_dirs import *
 
-
-#Example for google as an external smtp server
+# Mail server settings
+#Example for Google as an external SMPT server
 #DEFAULT_FROM_EMAIL = 'DEFAULT_USER@YOUR_DOMAIN.com'
 #EMAIL_HOST_USER = 'USER@YOUR_DOMAIN.com'
 #EMAIL_HOST_PASSWORD = 'YOUR_PASSWORD'
@@ -11,6 +14,8 @@ from pootle.install_dirs import *
 #EMAIL_PORT = 587
 #EMAIL_USE_TLS = True
 
+
+# Database configuration
 DATABASE_ENGINE = 'sqlite3'                 # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = working_path('dbs/pootle.db') # Or path to database file if using sqlite3.
 DATABASE_USER = ''                          # Not used with sqlite3.
@@ -20,22 +25,29 @@ DATABASE_PORT = ''                          # Set to empty string for default. N
 
 STATS_DB_PATH = working_path('dbs/stats.db') # None means the default path
 
+
+# The directory where the translation files are kept
 PODIRECTORY = working_path('po')
 
-# Set this to true to enable live translation of pootle UI. This is a good way
-# to learn how to use Pootle, but it has high impact on performance.
+# Live translation means that the project called "Pootle" is used to provide
+# the localised versions of Pootle. Set this to true to enable live translation
+# of pootle UI. This is a good way to learn how to use Pootle, but it has high
+# impact on performance.
 LIVE_TRANSLATION = False
 
 
 # File parse pool settings
 #
 # To avoid rereading and reparsing translation files from disk on
-# every request, Pootle keeps a pool or already parsed files in memory.
+# every request, Pootle keeps a pool of already parsed files in memory.
 #
 # Larger pools will offer better performance, but higher memory usage
-# (per server process).
+# (per server process). When the pool fills up, 1/PARSE_POOL_CULL_FREQUENCY
+# number of files will be removed from the pool.
 
+# DEFAULT: 40
 PARSE_POOL_SIZE = 40
+# DEFAULT: 4
 PARSE_POOL_CULL_FREQUENCY = 4
 
 
@@ -59,16 +71,21 @@ CACHE_BACKEND = 'locmem:///?max_entries=4096&cull_frequency=5'
 # Uncomment this if you're using memcached as CACHE_BACKEND and running under Django 1.1
 #SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
 
-# To improve performance anonymous users get cached copies of most pages. This variable controls how old the cached copy can be
+# To improve performance, non-logged users get cached copies of most pages.
+# This variable is the number of seconds for which a page will be reused from
+# cache. If you have a small server where more real-time statistics is
+# important, you can make this lower.
+# DEFAULT: 600
 CACHE_MIDDLEWARE_SECONDS = 600
+
 
 # Set this to False. DEBUG mode is only needed when testing beta's or
 # hacking pootle
 DEBUG = True
 
 
-# Use the commented definition to authenticate first with Mozilla's LDAP system and then to fall back
-# to Django's authentication system.
+# Use the commented definition to authenticate first with an LDAP system and
+# then to fall back to Django's authentication system.
 #AUTHENTICATION_BACKENDS = ('pootle.auth.ldap_backend.LdapBackend', 'django.contrib.auth.backends.ModelBackend',)
 AUTHENTICATION_BACKENDS = ('django.contrib.auth.backends.ModelBackend',)
 
