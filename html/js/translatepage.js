b5da2fb47ae93021b6784ddfbc0e1207846f2a8d
Julen Ruiz Aizpuru <julenx@gmail.com>
2009-12-09 13:56:50 +0000

    Move autoexpand.js code over to translatepage.js.
    svn path=/src/trunk/Pootle/; revision=13494

--- html/js/translatepage.js
+++ html/js/translatepage.js
@@ -1,5 +1,70 @@
 $(document).ready(function() {
 
+/* Pootle namespace */
+$.pootle = {};
+
+/*
+ * FOCUS
+ */
+
+  // Set initial focus on page load
+  var initialFocus = $(".translate-original-focus textarea");
+  $.pootle.focusedElement = initialFocus.get(0);
+  $.pootle.focusedElement.focus();
+
+  // Update focus when appropriate
+  $(".focusthis").focus(function(e) {
+    $.pootle.focusedElement = e.target;
+  });
+
+
+/*
+ * REPLACEMENTS
+ */
+
+  // Write TM results into the currently focused element
+  $(".writetm").click(function() {
+    var tmtext = $(".tm-translation", this).html();
+    $($.pootle.focusedElement).replaceSelection(tmtext);
+  });
+
+  // Write special chars into the currently focused element
+  $(".writespecial").click(function() {
+    var specialtext = $(this).html();
+    $($.pootle.focusedElement).replaceSelection(specialtext);
+  });
+
+
+/*
+ * COPY ORIGINAL TRANSLATION
+ */
+
+  $(".copyoriginal").click(function() {
+    var transid = $(this).parent().parent().attr("id");
+    var elementNumber = transid.replace("trans", "")
+    var enelement = $("#orig-pure" + elementNumber + "-0");
+    var envalue = enelement.val().replace("\n", "\\n\n", "g").replace("\t", "\\t", "g");
+
+    // no plurals
+    var trelement = $("#areatrans" + elementNumber);
+    if (trelement.length != 0) {
+      trelement.val(envalue);
+      trelement.focus();
+      return;
+    }
+
+    // plurals
+    var trelements = $("[id^=areatrans" + elementNumber + "-]");
+    var enplelement = $("#orig-pure" + elementNumber + "-1");
+    var enplvalue = enplelement.val().replace("\n", "\\n\n", "g").replace("\t", "\\t", "g");
+    $.each(trelements, function(i) {
+      newval = i == 0 ? envalue : enplvalue;
+      $(this).val(newval);
+      $(this).focus();
+    });
+  });
+
+
 /*
  * UNFUZZY
  */
