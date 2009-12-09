f417283c8d81081b9ab348dee3615c06886dcc91
Julen Ruiz Aizpuru <julenx@gmail.com>
2009-12-09 13:55:32 +0000

    Fix indentation.
    svn path=/src/trunk/Pootle/; revision=13492

--- html/js/autoexpand.js
+++ html/js/autoexpand.js
@@ -1,7 +1,7 @@
-
 $(document).ready(function() {
     // Pootle namespace
     $.pootle = {};
+
     // Set initial focus on page load
     var initialFocus = $(".translate-original-focus textarea");
     initialFocus.focus();
@@ -9,44 +9,44 @@ $(document).ready(function() {
 
     // Update focus when appropriate
     $(".focusthis").focus(function(e) {
-        $.pootle.focusedElement = e.target;
+      $.pootle.focusedElement = e.target;
     });
 
     // Write TM results into the currently focused element
     $(".writetm").click(function() {
-       var tmtext = $(".tm-translation", this).html();
-       $($.pootle.focusedElement).replaceSelection(tmtext);
+      var tmtext = $(".tm-translation", this).html();
+      $($.pootle.focusedElement).replaceSelection(tmtext);
     });
 
     // Write special chars into the currently focused element
     $(".writespecial").click(function() {
-       var specialtext = $(this).html();
-       $($.pootle.focusedElement).replaceSelection(specialtext);
+      var specialtext = $(this).html();
+      $($.pootle.focusedElement).replaceSelection(specialtext);
     });
 
     $(".copyoriginal").click(function() {
-       var transid = $(this).parent().parent().attr("id");
-       var elementNumber = transid.replace("trans", "")
-       var enelement = $("#orig-pure" + elementNumber + "-0");
-       var envalue = enelement.val().replace("\n", "\\n\n", "g").replace("\t", "\\t", "g");
+      var transid = $(this).parent().parent().attr("id");
+      var elementNumber = transid.replace("trans", "")
+      var enelement = $("#orig-pure" + elementNumber + "-0");
+      var envalue = enelement.val().replace("\n", "\\n\n", "g").replace("\t", "\\t", "g");
 
-       // no plurals
-       var trelement = $("#areatrans" + elementNumber);
-       if (trelement.length != 0) {
-         trelement.val(envalue);
-         trelement.focus();
-         return;
-       }
+      // no plurals
+      var trelement = $("#areatrans" + elementNumber);
+      if (trelement.length != 0) {
+        trelement.val(envalue);
+        trelement.focus();
+        return;
+      }
 
-       //plurals
-       var trelements = $("[id^=areatrans" + elementNumber + "-]");
-       var enplelement = $("#orig-pure" + elementNumber + "-1");
-       var enplvalue = enplelement.val().replace("\n", "\\n\n", "g").replace("\t", "\\t", "g");
-       $.each(trelements, function(i) {
-         newval = i == 0 ? envalue : enplvalue;
-         $(this).val(newval);
-         $(this).focus();
-       });
+      //plurals
+      var trelements = $("[id^=areatrans" + elementNumber + "-]");
+      var enplelement = $("#orig-pure" + elementNumber + "-1");
+      var enplvalue = enplelement.val().replace("\n", "\\n\n", "g").replace("\t", "\\t", "g");
+      $.each(trelements, function(i) {
+        newval = i == 0 ? envalue : enplvalue;
+        $(this).val(newval);
+        $(this).focus();
+      });
     });
 
 });
