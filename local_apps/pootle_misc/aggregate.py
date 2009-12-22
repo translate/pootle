e97969ef242f0f45f45b4b4855c8a6534d7359c7
Alaa Abd el Fattah <alaa@translate.org.za>
2009-12-22 22:08:37 +0000

    function to sum columns.
    uses django's orm aggregate feature if available to let the rdbms does the summing
    
    svn path=/src/trunk/Pootle/; revision=13558

---
+++ local_apps/pootle_misc/aggregate.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2008-2009 Zuza Software Foundation
+#
+# This file is part of Pootle.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+try:
+    from django.db.models import Sum
+
+    def sum_column(queryset, column):
+        result = queryset.aggregate(result=Sum(column))['result']
+        if result is None:
+            return 0
+        else:
+            return result
+except ImportError:
+    def sum_column(queryset, column):
+        return sum(queryset.values_list(column, flat=True))
+
