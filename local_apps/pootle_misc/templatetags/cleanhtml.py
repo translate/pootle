7d087ccc0fe9541c6c40efebbca6cc494c66efbc
Alaa Abd el Fattah <alaa@translate.org.za>
2010-03-29 21:49:38 +0000

    template filter for fancyfying source and target text
    svn path=/src/trunk/Pootle/; revision=14011

--- local_apps/pootle_misc/templatetags/cleanhtml.py
+++ local_apps/pootle_misc/templatetags/cleanhtml.py
@@ -18,6 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
+import re
 
 from django import template
 from django.template.defaultfilters import stringfilter
@@ -28,11 +29,47 @@ try:
 except ImportError:
     clean_html = lambda text: text
 
+ESCAPE_RE = re.compile('<.*?>|\r\n|[\r\n\t&<>]')
+def fancy_escape(text):
+    """replace special chars with entities, and highlight xml tags and
+    whitespaces"""
+    def replace(match):
+        escape_highlight = '<span class="translation-highlight-escape">%s</span>'
+        html_highlight = '<span class="translation-highlight-html">&lt;%s&gt;</span>'
+        submap = {
+            '\r\n': (escape_highlight % '\\r\\n') + '<br/>\n',
+            '\r': (escape_highlight % '\\r') + '<br/>\n',
+            '\n': (escape_highlight % '\\n') + '<br/>\n',
+            '\t': (escape_highlight % '\\t') + '\t',
+            '&': escape_highlight % '&amp;',
+            '<': escape_highlight % '&lt;',
+            '>': escape_highlight % '&gt;',
+            }
+        try:
+            return submap[match.group()]
+        except KeyError:
+            return html_highlight %  match.group()[1:-1]
+    return ESCAPE_RE.sub(replace, text)
+
+WHITESPACE_RE = re.compile('^ +| +$|[\r\n\t] +')
+def fancy_spaces(text):
+    """Highlight spaces to make them easily visible"""
+    def replace(match):
+        fancy_space = '<span class="translation-space"> </span>'
+        if match.group().startswith(' '):
+            return fancy_space * len(match.group())
+        return match.group()[0] + fancy_space * (len(match.group()) - 1)
+    return WHITESPACE_RE.sub(replace, text)
+
 def clean_wrapper(text):
     """wrapper around lxml's html cleaner that returns SafeStrings for
     immediate rendering in templates"""
     return mark_safe(clean_html(text))
 
+def fancy_highlight(text):
+    return mark_safe(fancy_spaces(fancy_escape(text)))
+
 register = template.Library()
 register.filter('clean', stringfilter(clean_wrapper))
+register.filter('fancy_highlight', stringfilter(fancy_highlight))
 
