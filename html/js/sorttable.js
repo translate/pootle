4b935e8e77931197b4eafcdaec92c7a13555f2de
Alaa Abd el Fattah <alaa@translate.org.za>
2010-05-06 00:50:23 +0000

    be friendly to https, thanks Jacques Beaurain <Jacques.beaurain@geosoft.com> for patches
    svn path=/src/trunk/Pootle/; revision=14428

--- html/js/sorttable.js
+++ html/js/sorttable.js
@@ -360,7 +360,8 @@ if (document.addEventListener) {
 /* for Internet Explorer */
 /*@cc_on @*/
 /*@if (@_win32)
-    document.write("<script id=__ie_onload defer src=javascript:void(0)><\/script>");
+    var dummy = (location.protocol == "https:") ? "//:" : "javascript:void(0)";
+    document.write('<script id=__ie_onload defer src="' + dummy + '"><\/script>');
     var script = document.getElementById("__ie_onload");
     script.onreadystatechange = function() {
         if (this.readyState == "complete") {
