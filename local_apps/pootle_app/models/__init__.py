4efa693c775f7373204a5e5f67840c691dae5a4f
Alaa Abd el Fattah <alaa@translate.org.za>
2010-05-14 00:25:56 +0000

    PermissionSetCache no longer needed
    svn path=/src/trunk/Pootle/; revision=14503

--- local_apps/pootle_app/models/__init__.py
+++ local_apps/pootle_app/models/__init__.py
@@ -21,8 +21,8 @@
 
 from pootle_app.models.suggestion import Suggestion
 from pootle_app.models.directory import Directory
-from pootle_app.models.permissions import PermissionSet, PermissionSetCache
+from pootle_app.models.permissions import PermissionSet
 
-__all__ = ["Suggestion", "Directory", "PermissionSet", "PermissionSetCache"]
+__all__ = ["Suggestion", "Directory", "PermissionSet"]
 
 
