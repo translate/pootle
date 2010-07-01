fdaad2f36d830d3c884a857bba00feea89f6a82c
Alaa Abd el Fattah <alaa@translate.org.za>
2010-07-01 21:58:29 +0000

    group profile field in permissions form
    svn path=/src/trunk/Pootle/; revision=14871

--- local_apps/pootle_app/views/admin/permissions.py
+++ local_apps/pootle_app/views/admin/permissions.py
@@ -23,9 +23,10 @@ from django.utils.translation import ugettext as _
 from django import forms
 from django.contrib.contenttypes.models import ContentType
 
+from pootle_profile.models import PootleProfile
 from pootle_app.models import Directory, PermissionSet
-
 from pootle_app.views.admin import util
+from pootle_misc.forms import GroupedModelChoiceField
 
 class PermissionFormField(forms.ModelMultipleChoiceField):
     def label_from_instance(self, instance):
@@ -38,6 +39,17 @@ def admin_permissions(request, current_directory, template, context):
     context['submitname'] = 'changepermissions'
     context['formid'] = 'permission-manage'
 
+    project = context.get('project', None)
+    language = context.get('language', None)
+
+    base_queryset = PootleProfile.objects.filter(user__is_active=1).exclude(id__in=current_directory.permission_sets.values_list('profile_id', flat=True))
+    querysets = []
+    if project is not None:
+        querysets.append((_('Project Members'), base_queryset.filter(projects=project).order_by('user')))
+    if language is not None:
+        querysets.append((_('Language Members'), base_queryset.filter(languages=language).order_by('user')))
+    querysets.append((_('All Users'), base_queryset.order_by('user')))
+
     class PermissionSetForm(forms.ModelForm):
         class Meta:
             model = PermissionSet
@@ -45,7 +57,9 @@ def admin_permissions(request, current_directory, template, context):
 
         directory = forms.ModelChoiceField(queryset=Directory.objects.filter(pk=current_directory.pk),
                                            initial=current_directory.pk, widget=forms.HiddenInput)
+        profile = GroupedModelChoiceField(querysets=querysets, queryset=base_queryset, required=True)
         positive_permissions = PermissionFormField(label=_('Permissions'), queryset=permission_queryset, required=False)
+
     link = lambda instance: unicode(instance.profile)
     return util.edit(request, template, PermissionSet, context, link, linkfield='profile',
               queryset=current_directory.permission_sets.order_by('profile').all(), can_delete=True,
