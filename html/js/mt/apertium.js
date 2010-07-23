80bb4567bbbdb2a64416cde21c08cde17a74c2eb
Julen Ruiz Aizpuru <julenx@gmail.com>
2010-07-23 14:14:48 +0000

    Support plural forms when using Machine Translation services.
    svn path=/src/trunk/Pootle/; revision=15025

--- html/js/mt/apertium.js
+++ html/js/mt/apertium.js
@@ -3,7 +3,7 @@
  */
 
 $(document).ready(function() {
-  var target_lang = $("#id_target_f_0").attr("lang").replace('_', '-');
+  var target_lang = $.pootle.normalize_code($("#id_target_f_0").attr("lang"));
 
   if (apertium.isTranslatable(target_lang)) {
     $.pootle.addMTButton($(".translate-toolbar"),
@@ -12,11 +12,10 @@ $(document).ready(function() {
                          "Apertium");
 
     $(".apertium").click(function(){
-      var area = $("#id_target_f_0");
-      var source = $(this).parent().siblings(".translation-text");
-      var source_text = source.text();
-      var lang_from = $.pootle.normalize_code($(this).parent().siblings(".translation-text").attr("lang"));
-      var lang_to = $.pootle.normalize_code(area.attr("lang"));
+      var areas = $("[id^=id_target_f_]");
+      var sources = $(this).parent().siblings(".translation-text");
+      var lang_from = $.pootle.normalize_code(sources.eq(0).attr("lang"));
+      var lang_to = $.pootle.normalize_code(areas.eq(0).attr("lang"));
 
       // The printf regex based on http://phpjs.org/functions/sprintf:522
       var c_printf_pattern = /%%|%(\d+\$)?([-+\'#0 ]*)(\*\d+\$|\*|\d+)?(\.(\*\d+\$|\*|\d+))?([scboxXuidfegEG])/g;
@@ -24,25 +23,28 @@ $(document).ready(function() {
       var percent_number_pattern = /%\d+/g;
       var pos = 0;
       var argument_subs = new Array();
-      source_text = source_text.replace(c_printf_pattern, $.pootle.collectArguments);
-      source_text = source_text.replace(csharp_string_format_pattern, $.pootle.collectArguments);
-      source_text = source_text.replace(percent_number_pattern, $.pootle.collectArguments);
 
-      var content = new Object()
-      content.text = source_text;
-      content.type = "txt";
-      apertium.translate(content, lang_from, lang_to, function(result) {
-        if (result.translation) {
-          var translation = result.translation;
-          for (var i=0; i<argument_subs.length; i++)
-            translation = translation.replace("__" + i + "__", argument_subs[i]);
-          area.val(translation);
-          area.focus();
-          $.pootle.toggleFuzzy(true);
-          $.pootle.toggleFuzzyBox(true);
-        } else {
-          alert("Apertium Error: " + result.error.message);
-        }
+      $(sources).each(function(j) {
+        source_text = source_text.replace(c_printf_pattern, $.pootle.collectArguments);
+        source_text = source_text.replace(csharp_string_format_pattern, $.pootle.collectArguments);
+        source_text = source_text.replace(percent_number_pattern, $.pootle.collectArguments);
+
+        var content = new Object()
+        content.text = source_text;
+        content.type = "txt";
+        apertium.translate(content, lang_from, lang_to, function(result) {
+          if (result.translation) {
+            var translation = result.translation;
+            for (var i=0; i<argument_subs.length; i++)
+              translation = translation.replace("__" + i + "__", argument_subs[i]);
+            areas.eq(j).val(translation);
+            areas.eq(j).focus();
+            $.pootle.toggleFuzzy(true);
+            $.pootle.toggleFuzzyBox(true);
+          } else {
+            alert("Apertium Error: " + result.error.message);
+          }
+        });
       });
       return false;
     });
--- html/js/mt/google-translate.js
+++ html/js/mt/google-translate.js
@@ -4,7 +4,7 @@
 google.load("language", "1");
 
 google.setOnLoadCallback(function() {
-  var target_lang = $("#id_target_f_0").attr("lang").replace('_', '-');
+  var target_lang = $.pootle.normalize_code($("#id_target_f_0").attr("lang"));
 
   if (google.language.isTranslatable(target_lang)) {
     var sources = $(".translate-toolbar").prev(".translation-text");
@@ -19,11 +19,10 @@ google.setOnLoadCallback(function() {
     });
 
     $(".googletranslate").click(function(){
-      var area = $("#id_target_f_0");
-      var source = $(this).parent().siblings(".translation-text");
-      var source_text = source.text();
-      var lang_from = $.pootle.normalize_code(source.attr("lang"));
-      var lang_to = $.pootle.normalize_code(area.attr("lang"));
+      var areas = $("[id^=id_target_f_]");
+      var sources = $(this).parent().siblings(".translation-text");
+      var lang_from = $.pootle.normalize_code(sources.eq(0).attr("lang"));
+      var lang_to = $.pootle.normalize_code(areas.eq(0).attr("lang"));
 
       // The printf regex based on http://phpjs.org/functions/sprintf:522
       var c_printf_pattern = /%%|%(\d+\$)?([-+\'#0 ]*)(\*\d+\$|\*|\d+)?(\.(\*\d+\$|\*|\d+))?([scboxXuidfegEG])/g;
@@ -31,28 +30,33 @@ google.setOnLoadCallback(function() {
       var percent_number_pattern = /%\d+/g;
       var pos = 0;
       var argument_subs = new Array();
-      source_text = source_text.replace(c_printf_pattern, $.pootle.collectArguments);
-      source_text = source_text.replace(csharp_string_format_pattern, $.pootle.collectArguments);
-      source_text = source_text.replace(percent_number_pattern, $.pootle.collectArguments);
 
-      var content = new Object()
-      content.text = source_text;
-      content.type = "text";
-      google.language.translate(content, lang_from, lang_to, function(result) {
-        if (result.translation) {
-          var translation = result.translation;
-          for (var i=0; i<argument_subs.length; i++)
-            translation = translation.replace("__" + i + "__", argument_subs[i]);
-          area.val(translation);
-          area.focus();
-          $.pootle.toggleFuzzy(true);
-          $.pootle.toggleFuzzyBox(true);
-        } else {
-          alert("Google Translate Error: " + result.error.message);
-        }
+      $(sources).each(function(j) {
+        var source_text = $(this).text();
+        source_text = source_text.replace(c_printf_pattern, $.pootle.collectArguments);
+        source_text = source_text.replace(csharp_string_format_pattern, $.pootle.collectArguments);
+        source_text = source_text.replace(percent_number_pattern, $.pootle.collectArguments);
+
+        var content = new Object()
+        content.text = source_text;
+        content.type = "text";
+        google.language.translate(content, lang_from, lang_to, function(result) {
+          if (result.translation) {
+            var translation = result.translation;
+            for (var i=0; i<argument_subs.length; i++)
+              translation = translation.replace("__" + i + "__", argument_subs[i]);
+            areas.eq(j).val(translation);
+            areas.eq(j).focus();
+            $.pootle.toggleFuzzy(true);
+            $.pootle.toggleFuzzyBox(true);
+          } else {
+            alert("Google Translate Error: " + result.error.message);
+          }
+        });
       });
       return false;
     });
+
   }
 
 });
