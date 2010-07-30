8f3525a6e76d45111f9f675d7751ac6371a5ce3c
Alaa Abd el Fattah <alaa@translate.org.za>
2010-07-30 23:20:06 +0000

    manage.py command for mass update from template
    svn path=/src/trunk/Pootle/; revision=15223

---
+++ local_apps/pootle_app/management/commands/update_from_templates.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009 Zuza Software Foundation
+#
+# This file is part of Pootle.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'pootle.settings'
+
+import logging
+from optparse import make_option
+
+from django.core.management.base import NoArgsCommand
+
+from pootle_translationproject.models import TranslationProject
+
+class Command(NoArgsCommand):
+    option_list = NoArgsCommand.option_list + (
+        make_option('--directory', action='store', dest='directory', default='',
+                    help='directory to update relative to po directory'),
+        )
+    help = "mass update from templates."
+
+    def handle_noargs(self, **options):
+        update_path = options.get('directory', '')
+
+        # reduce size of parse pool early on
+        from pootle_store.fields import  TranslationStoreFieldFile
+        TranslationStoreFieldFile._store_cache.maxsize = 2
+        TranslationStoreFieldFile._store_cache.cullsize = 2
+
+
+        for translation_project in TranslationProject.objects.filter(real_path__startswith=update_path).iterator():
+            logging.info("Updating %s from templates", translation_project.fullname)
+            translation_project.update_from_templates()
