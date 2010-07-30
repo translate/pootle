578e90c50a3a416ca50ec7c15feb96adf6ee8d7d
Julen Ruiz Aizpuru <julenx@gmail.com>
2010-07-30 09:40:33 +0000

    Script to load alerts for admins.
    svn path=/src/trunk/Pootle/; revision=15180

---
+++ alerts.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+   var feed_url = "http://pootle.locamotion.org/dashboard/alerts.xml";
+   /* Number of items to display */
+   var n = 5;
+   $.ajax({
+     url: feed_url,
+     dataType: "xml",
+     type: "GET",
+     success: function(data) {
+       var items = $("item", data).slice(n);
+       var html = "<ul>\n";
+       $(items).each(function() {
+         var link = $("link", this).text();
+         var title = $("title", this).text();
+         var desc = $("description", this).text();
+         html += "<li><a href=\"" + link + "\" title=\"" + desc + "\">"
+                 + title + "</a></li>\n";
+       });
+       html += "</ul>\n";
+       $("#rss-alerts div.bd").children().remove();
+       $("#rss-alerts div.bd").append(html);
+     }
+   });
+});
