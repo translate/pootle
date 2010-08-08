f9c402b5220fe818164234f11851a99a0408e136
Alaa Abd el Fattah <alaa@translate.org.za>
2010-08-08 21:56:24 +0000

    optimize on update. hopefully will reduce open file count
    svn path=/src/trunk/Pootle/; revision=15379

--- local_apps/pootle_translationproject/models.py
+++ local_apps/pootle_translationproject/models.py
@@ -461,6 +461,7 @@ class TranslationProject(models.Model):
             indexer.begin_transaction()
             for store in self.stores.iterator():
                 self.update_index(indexer, store)
+            indexer.flush(optimize=True)
             indexer.commit_transaction()
         except Exception, e:
             logging.error("Error opening indexer for %s:\n%s", self, e)
@@ -542,7 +543,6 @@ class TranslationProject(models.Model):
         if addlist:
             for add_item in addlist:
                 indexer.index_document(add_item)
-            indexer.flush()
 
 
     ########################################################################################
