17919fc0c0222c71bf88a9ec82545b9e66a9fe92
Dwayne Bailey <dwayne@translate.org.za>
2010-08-26 15:46:06 +0000

    PEP8: cleanups
    svn path=/src/trunk/Pootle/; revision=15643

--- pootle/scripts/history/convert/monopo2po.py
+++ pootle/scripts/history/convert/monopo2po.py
@@ -8,78 +8,86 @@
 # Author: Dan Schafer <dschafer@mozilla.com>
 # Date: 11 Jun 2008
 
-from translate.storage import po
 import sys
 
+from translate.storage import po
+
+
 class amo2po:
-  def convertstore(self, engfile, forfile):
-    """converts a file to .po format"""
-    thetargetfile = po.pofile()
 
-    # A dictionary mapping msgids to their foreign units
-    key2for = {}
-    # Populate the dictionary
-    for unit in forfile.units:
-      key2for[str(unit.msgid)] = unit
-      if unit.isheader():
-        forunit = unit
+    def convertstore(self, engfile, forfile):
+        """converts a file to .po format"""
+        thetargetfile = po.pofile()
 
-    for engunit in engfile.units:
-      # This removes the depricated strings:
-      if engunit.msgid == []:
-        continue
+        # A dictionary mapping msgids to their foreign units
+        key2for = {}
+        # Populate the dictionary
+        for unit in forfile.units:
+            key2for[str(unit.msgid)] = unit
+            if unit.isheader():
+                forunit = unit
 
-      newunit = engunit.copy()
+        for engunit in engfile.units:
+            # This removes the depricated strings:
+            if engunit.msgid == []:
+                continue
 
-      if engunit.isheader():
-        try:
-          thetargetfile.addunit(forunit)
-        except: # If there's no foreign header, reluctantly use english header  
-          thetargetfile.addunit(newunit)
-        continue
+            newunit = engunit.copy()
 
-      # If there is a foreign unit corresponding, merge in its comments
-      # and use its msgstr as the new msgstr
-      try:
-        forunit = key2for[str(engunit.msgid)]
-        newunit.msgstr = forunit.msgstr
-        newunit.merge(forunit)
-      except:
-        # If there's no foreign unit, then we simply use a blank string
-        newunit.msgstr = []
+            if engunit.isheader():
+                try:
+                    thetargetfile.addunit(forunit)
+                except: # If there's no foreign header, use english header
+                    thetargetfile.addunit(newunit)
+                continue
 
-      # We want to put the english translation as the id and the key as the
-      # context
-      newunit.msgctxt = engunit.msgid
-      if type(engunit.msgstr) == dict:
-        newunit.msgid = engunit.msgstr[0]
-        newunit.msgid_plural = engunit.msgstr[1]
-      else:
-        newunit.msgid = engunit.msgstr
+            # If there is a foreign unit corresponding, merge in its comments
+            # and use its msgstr as the new msgstr
+            try:
+                forunit = key2for[str(engunit.msgid)]
+                newunit.msgstr = forunit.msgstr
+                newunit.merge(forunit)
+            except:
+                # If there's no foreign unit, then we simply use a blank string
+                newunit.msgstr = []
 
-      thetargetfile.addunit(newunit)
+            # We want to put the english translation as the id and the key as
+            # the context
+            newunit.msgctxt = engunit.msgid
+            if type(engunit.msgstr) == dict:
+                newunit.msgid = engunit.msgstr[0]
+                newunit.msgid_plural = engunit.msgstr[1]
+            else:
+                newunit.msgid = engunit.msgstr
 
-    return thetargetfile
+            thetargetfile.addunit(newunit)
+
+        return thetargetfile
+
 
 def convertpo(inputfile, outputfile, templatefile):
-  """reads in stdin using fromfileclass, converts using convertorclass, writes to stdout"""
-  engstore = po.pofile(inputfile)
-  forstore = po.pofile(templatefile)
-  convertor = amo2po()
-  outputstore = convertor.convertstore(engstore, forstore)
-  if outputstore.isempty():
-    return 0
-  outputfile.write(str(outputstore))
-  return 1
+    """reads in stdin using fromfileclass, converts using convertorclass,
+    writes to stdout"""
+    engstore = po.pofile(inputfile)
+    forstore = po.pofile(templatefile)
+    convertor = amo2po()
+    outputstore = convertor.convertstore(engstore, forstore)
+    if outputstore.isempty():
+        return 0
+    outputfile.write(str(outputstore))
+    return 1
+
 
 def main(argv=None):
-  from translate.convert import convert
-  from translate.misc import stdiotell
-  import sys
-  sys.stdout = stdiotell.StdIOWrapper(sys.stdout)
-  formats = {"*":("po",convertpo)}
-  parser = convert.ConvertOptionParser(formats, usetemplates=True, description=__doc__)
-  parser.run(argv)
+    from translate.convert import convert
+    from translate.misc import stdiotell
+    import sys
+    sys.stdout = stdiotell.StdIOWrapper(sys.stdout)
+    formats = {"*": ("po", convertpo)}
+    parser = convert.ConvertOptionParser(formats, usetemplates=True,
+                                         description=__doc__)
+    parser.run(argv)
+
 
 if __name__ == '__main__':
-  main()
+    main()
--- pootle/scripts/history/convert/po2monopo.py
+++ pootle/scripts/history/convert/po2monopo.py
@@ -10,49 +10,56 @@
 
 from translate.storage import po
 
+
 class po2amo:
-  def convertstore(self, thepofile):
-    """converts a file to .po format"""
-    thetargetfile = po.pofile()
 
-    for unit in thepofile.units:
-      newunit = unit.copy()
+    def convertstore(self, thepofile):
+        """converts a file to .po format"""
+        thetargetfile = po.pofile()
 
-      # The AMO id is stored in the context
-      newunit.msgid = unit.msgctxt
+        for unit in thepofile.units:
+            newunit = unit.copy()
 
-      # If there's a plural in the po file, then there should be in the amo file
-      if unit.msgid_plural != []:
-        newunit.msgid_plural = unit.msgctxt
+            # The AMO id is stored in the context
+            newunit.msgid = unit.msgctxt
 
-      # No context in AMO
-      newunit.msgctxt = []
+            # If there's a plural in the po file, then there should be in the
+            # amo file
+            if unit.msgid_plural != []:
+                newunit.msgid_plural = unit.msgctxt
 
-      # No need to alter the msgstr; it is foreign in the original file, and 
-      # we started with that as a base
+            # No context in AMO
+            newunit.msgctxt = []
 
-      thetargetfile.addunit(newunit)
+            # No need to alter the msgstr; it is foreign in the original file,
+            # and we started with that as a base
 
-    return thetargetfile
+            thetargetfile.addunit(newunit)
+
+        return thetargetfile
+
 
 def convertpo(inputfile, outputfile, templates=None):
-  """reads in stdin using fromfileclass, converts using convertorclass, writes to stdout"""
-  inputstore = po.pofile(inputfile)
-  convertor = po2amo()
-  outputstore = convertor.convertstore(inputstore)
-  if outputstore.isempty():
-    return 0
-  outputfile.write(str(outputstore))
-  return 1
+    """reads in stdin using fromfileclass, converts using convertorclass,
+    writes to stdout"""
+    inputstore = po.pofile(inputfile)
+    convertor = po2amo()
+    outputstore = convertor.convertstore(inputstore)
+    if outputstore.isempty():
+        return 0
+    outputfile.write(str(outputstore))
+    return 1
+
 
 def main(argv=None):
-  from translate.convert import convert
-  from translate.misc import stdiotell
-  import sys
-  sys.stdout = stdiotell.StdIOWrapper(sys.stdout)
-  formats = {"*":("po",convertpo)}
-  parser = convert.ConvertOptionParser(formats, description=__doc__)
-  parser.run(argv)
+    from translate.convert import convert
+    from translate.misc import stdiotell
+    import sys
+    sys.stdout = stdiotell.StdIOWrapper(sys.stdout)
+    formats = {"*": ("po", convertpo)}
+    parser = convert.ConvertOptionParser(formats, description=__doc__)
+    parser.run(argv)
+
 
 if __name__ == '__main__':
-  main()
+    main()
