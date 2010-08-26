4e66bb01c08948054020a4b655ae826ce5cf5851
Dwayne Bailey <dwayne@translate.org.za>
2010-08-26 17:52:47 +0000

    PEP8: E111 indentation is not a multiple of four
    svn path=/src/trunk/Pootle/; revision=15652

--- pootle/scripts/convert/po2lang.py
+++ pootle/scripts/convert/po2lang.py
@@ -10,40 +10,40 @@ from translate.storage import po
 import lang
 
 class po2lang:
-  def __init__(self, duplicatestyle="msgctxt"):
-    self.duplicatestyle = duplicatestyle
+    def __init__(self, duplicatestyle="msgctxt"):
+        self.duplicatestyle = duplicatestyle
 
-  def convertstore(self, inputstore):
-    """converts a file to .lang format"""
-    thetargetfile = lang.LangStore()
+    def convertstore(self, inputstore):
+        """converts a file to .lang format"""
+        thetargetfile = lang.LangStore()
 
-    # Run over the po units
-    for pounit in inputstore.units:
-      # Skip the header
-      if pounit.isheader():
-        continue
-      newunit = thetargetfile.addsourceunit(pounit.source)
-      newunit.settarget(pounit.target)
-    return thetargetfile
+        # Run over the po units
+        for pounit in inputstore.units:
+            # Skip the header
+            if pounit.isheader():
+                continue
+            newunit = thetargetfile.addsourceunit(pounit.source)
+            newunit.settarget(pounit.target)
+        return thetargetfile
 
 def convertlang(inputfile, outputfile, templates):
-  """reads in stdin using fromfileclass, converts using convertorclass, writes to stdout"""
-  inputstore = po.pofile(inputfile)
-  if inputstore.isempty():
-    return 0
-  convertor = po2lang()
-  outputstore = convertor.convertstore(inputstore)
-  outputfile.write(str(outputstore))
-  return 1
+    """reads in stdin using fromfileclass, converts using convertorclass, writes to stdout"""
+    inputstore = po.pofile(inputfile)
+    if inputstore.isempty():
+        return 0
+    convertor = po2lang()
+    outputstore = convertor.convertstore(inputstore)
+    outputfile.write(str(outputstore))
+    return 1
 
 def main(argv=None):
-  from translate.convert import convert
-  from translate.misc import stdiotell
-  import sys
-  sys.stdout = stdiotell.StdIOWrapper(sys.stdout)
-  formats = {"po":("lang",convertlang)}
-  parser = convert.ConvertOptionParser(formats, usepots=True, description=__doc__)
-  parser.run(argv)
+    from translate.convert import convert
+    from translate.misc import stdiotell
+    import sys
+    sys.stdout = stdiotell.StdIOWrapper(sys.stdout)
+    formats = {"po":("lang",convertlang)}
+    parser = convert.ConvertOptionParser(formats, usepots=True, description=__doc__)
+    parser.run(argv)
 
 if __name__ == '__main__':
-  main()
+    main()
--- pootle/scripts/sumo.py
+++ pootle/scripts/sumo.py
@@ -13,63 +13,63 @@ from django.conf import settings
 from translate.convert import tiki2po, po2tiki
 
 def initialize(projectdir, languagecode):
-  """The first paramater is the path to the project directory.  It's up to this
-  script to know any internal structure of the directory"""
+    """The first paramater is the path to the project directory.  It's up to this
+    script to know any internal structure of the directory"""
 
-  # Temporary code - projectdirs come from pootle with sumo/ab_CD form; we need just the former part
-  # extract project root from projectdir
-  projectroot = os.path.join(settings.PODIRECTORY, os.path.split(projectdir)[0])
+    # Temporary code - projectdirs come from pootle with sumo/ab_CD form; we need just the former part
+    # extract project root from projectdir
+    projectroot = os.path.join(settings.PODIRECTORY, os.path.split(projectdir)[0])
 
-  # Temporary code.  Language codes come from pootle with underscores right now; they need to be dashes.
-  languagecode = languagecode.replace("_","-")
+    # Temporary code.  Language codes come from pootle with underscores right now; they need to be dashes.
+    languagecode = languagecode.replace("_","-")
 
-  # Find the files we're working with
-  tikifile = os.path.join(projectroot, languagecode, 'language.php')
-  pofile   = os.path.join(projectroot, languagecode, 'language.po')
+    # Find the files we're working with
+    tikifile = os.path.join(projectroot, languagecode, 'language.php')
+    pofile   = os.path.join(projectroot, languagecode, 'language.po')
 
-  # Build our combined file
-  print "Initializing %s to %s" % (tikifile, pofile)
-  tiki2po.converttiki(open(tikifile,"r"), open(pofile,"w"))
+    # Build our combined file
+    print "Initializing %s to %s" % (tikifile, pofile)
+    tiki2po.converttiki(open(tikifile,"r"), open(pofile,"w"))
 
 def precommit(committedfile, author, message):
-  if os.path.basename(committedfile) == "language.po":
+    if os.path.basename(committedfile) == "language.po":
 
-    # Get the files we'll be using
-    tikifile = os.path.join(os.path.dirname(committedfile), 'language.php')
+        # Get the files we'll be using
+        tikifile = os.path.join(os.path.dirname(committedfile), 'language.php')
 
-    # Update tikifile with new strings
-    print "Converting po to tiki: %s to %s" % (committedfile, tikifile)
-    po2tiki.convertpo(open(committedfile,"r"), open(tikifile,"w"))
+        # Update tikifile with new strings
+        print "Converting po to tiki: %s to %s" % (committedfile, tikifile)
+        po2tiki.convertpo(open(committedfile,"r"), open(tikifile,"w"))
 
-    # We want to commit messages.php
-    return [tikifile]
-  return []
+        # We want to commit messages.php
+        return [tikifile]
+    return []
 
 def postcommit(committedfile, success):
-  if os.path.basename(committedfile) == "language.po":
+    if os.path.basename(committedfile) == "language.po":
 
-    # Get the files we'll be using
-    tikifile = os.path.join(os.path.dirname(committedfile), 'language.php')
+        # Get the files we'll be using
+        tikifile = os.path.join(os.path.dirname(committedfile), 'language.php')
 
-    # Recreate .po with any new strings in tikifile
-    print "Converting tiki to po:  %s to %s" % (tikifile, committedfile)
-    tiki2po.converttiki(open(tikifile,"r"), open(committedfile,"w"))
+        # Recreate .po with any new strings in tikifile
+        print "Converting tiki to po:  %s to %s" % (tikifile, committedfile)
+        tiki2po.converttiki(open(tikifile,"r"), open(committedfile,"w"))
 
 def preupdate(updatedfile):
-  if os.path.basename(updatedfile) == "language.po":
+    if os.path.basename(updatedfile) == "language.po":
 
-    # Get the files we'll be using
-    tikifile = os.path.join(os.path.dirname(updatedfile), 'language.php')
+        # Get the files we'll be using
+        tikifile = os.path.join(os.path.dirname(updatedfile), 'language.php')
 
-    # We want to update messages.po
-    print "Updating %s" % tikifile
-    return tikifile
-  return ""
+        # We want to update messages.po
+        print "Updating %s" % tikifile
+        return tikifile
+    return ""
 
 def postupdate(updatedfile):
-  # Get the files we'll be using
-  pofile = os.path.join(os.path.dirname(updatedfile), 'language.po')
+    # Get the files we'll be using
+    pofile = os.path.join(os.path.dirname(updatedfile), 'language.po')
 
-  # Recreate .po with any new strings in tikifile
-  print "Converting tiki to po:  %s to %s" % (pofile, updatedfile)
-  tiki2po.converttiki(open(updatedfile,"r"), open(pofile,"w"))
+    # Recreate .po with any new strings in tikifile
+    print "Converting tiki to po:  %s to %s" % (pofile, updatedfile)
+    tiki2po.converttiki(open(updatedfile,"r"), open(pofile,"w"))
