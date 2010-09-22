48948a118e1a58d7426d3a3ece6a485fe925be74
Julen Ruiz Aizpuru <julenx@gmail.com>
2010-09-22 11:06:10 +0000

    Store units in the current view in a array so we don't duplicate requests.
    svn path=/src/trunk/Pootle/; revision=15832

--- html/js/editor.js
+++ html/js/editor.js
@@ -1,26 +1,39 @@
 $(document).ready(function() {
 
+    var units = new Array();
+
     /*
      * Sets the unit view for unit 'uid'
      */
     var get_unit_view = function(store, uid) {
-      var view_url = l(store + '/unit/view/' + uid);
-      $.getJSON(view_url, function(data) {
-        if (data.success) {
-          $(data.unit.source).each(function() {
-            alert(this);
-          });
-          $(data.unit.target).each(function() {
-            alert(this);
-          });
-          var oldunit = $("td.translate-full").parent("tr");
-          oldunit.children().remove();
-          $(this).parent().siblings().remove();
-        } else {
-          // TODO: provide a proper error message and not an alert
-          alert("Something went wrong");
-        }
+      if (units[uid] == undefined) {
+        var view_url = l(store + '/unit/view/' + uid);
+        $.getJSON(view_url, function(data) {
+          if (data.success) {
+            units[uid] = data.unit;
+            display_view_unit(units[uid]);
+          } else {
+            // TODO: provide a proper error message and not an alert
+            alert("Something went wrong");
+            return false;
+          }
+        });
+      } else {
+        display_view_unit(units[uid]);
+      }
+    };
+
+    var display_view_unit = function(unit) {
+      $(unit.source).each(function() {
+        alert(this);
       });
+      $(unit.target).each(function() {
+        alert(this);
+      });
+      // FIXME: This is only for the editing widget
+      var oldunit = $("td.translate-full").parent("tr");
+      oldunit.children().remove();
+      $(this).parent().siblings().remove();
     };
 
     $("a[id^=editlink]").click(function(e) {
