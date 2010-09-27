f0ccfd411998c6d1783829f5767baa80a725c71a
Julen Ruiz Aizpuru <julenx@gmail.com>
2010-09-27 21:26:15 +0000

    This must actually be a call to reversed(), not to .reverse()
    svn path=/src/trunk/Pootle/; revision=15915

--- local_apps/pootle_store/views.py
+++ local_apps/pootle_store/views.py
@@ -489,7 +489,7 @@ def _filter_view_units(units_qs, current_index, limit):
     after = units_qs.filter(index__gt=current_index)[:limit]
     # FIXME: As a quick workaround with ordering we just return
     # a reverse of the list
-    return before.reverse(), after
+    return reversed(before), after
 
 def _build_units_list(units):
     """
