0d73c70fb33f4a920b3d4635ca3f9cf488c3afcb
Julen Ruiz Aizpuru <julenx@gmail.com>
2010-10-15 13:03:46 +0000

    Use the non-JS API for Google Translate.
    Now we have to maintain a hard-coded list of supported pairs but that's
    life. Anyway, we now get rid of the obscure Google JS libraries.
    
    svn path=/src/trunk/Pootle/; revision=16098

--- html/js/mt/google_translate.js
+++ html/js/mt/google_translate.js
@@ -3,37 +3,72 @@
 
   PTL.editor.mt.google_translate = {
 
-    url: "http://www.google.com/jsapi",
-    cookie_name: "google_pairs",
-    cookie_options: {path: '/', expires: 15},
+    url: "http://ajax.googleapis.com/ajax/services/language/translate",
+    pairs: [{"source":"af","target":"af"},
+            {"source":"sq","target":"sq"},
+            {"source":"ar","target":"ar"},
+            {"source":"be","target":"be"},
+            {"source":"bg","target":"bg"},
+            {"source":"ca","target":"ca"},
+            {"source":"zh","target":"zh"},
+            {"source":"zh-CN","target":"zh-CN"},
+            {"source":"zh-TW","target":"zh-TW"},
+            {"source":"hr","target":"hr"},
+            {"source":"cs","target":"cs"},
+            {"source":"da","target":"da"},
+            {"source":"nl","target":"nl"},
+            {"source":"en","target":"en"},
+            {"source":"et","target":"et"},
+            {"source":"tl","target":"tl"},
+            {"source":"fi","target":"fi"},
+            {"source":"fr","target":"fr"},
+            {"source":"gl","target":"gl"},
+            {"source":"de","target":"de"},
+            {"source":"el","target":"el"},
+            {"source":"ht","target":"ht"},
+            {"source":"iw","target":"iw"},
+            {"source":"hi","target":"hi"},
+            {"source":"hu","target":"hu"},
+            {"source":"is","target":"is"},
+            {"source":"id","target":"id"},
+            {"source":"ga","target":"ga"},
+            {"source":"it","target":"it"},
+            {"source":"ja","target":"ja"},
+            {"source":"ko","target":"ko"},
+            {"source":"lv","target":"lv"},
+            {"source":"lt","target":"lt"},
+            {"source":"mk","target":"mk"},
+            {"source":"ms","target":"ms"},
+            {"source":"mt","target":"mt"},
+            {"source":"no","target":"no"},
+            {"source":"fa","target":"fa"},
+            {"source":"pl","target":"pl"},
+            {"source":"pt","target":"pt"},
+            {"source":"pt-PT","target":"pt-PT"},
+            {"source":"ro","target":"ro"},
+            {"source":"ru","target":"ru"},
+            {"source":"sr","target":"sr"},
+            {"source":"sk","target":"sk"},
+            {"source":"sl","target":"sl"},
+            {"source":"es","target":"es"},
+            {"source":"sw","target":"sw"},
+            {"source":"sv","target":"sv"},
+            {"source":"tl","target":"tl"},
+            {"source":"th","target":"th"},
+            {"source":"tr","target":"tr"},
+            {"source":"uk","target":"uk"},
+            {"source":"vi","target":"vi"},
+            {"source":"cy","target":"cy"},
+            {"source":"yi","target":"yi"}],
 
     init: function(apikey) {
-      var _this = PTL.editor.mt.google_translate;
-      /* Load Google Translate library */
-      _this.url = PTL.editor.settings.secure == false ? _this.url : _this.url.replace("http", "https");
-
-      $.getScript(_this.url, function() {
-        //google.load("language", "1");
-        /* Init variables */
-        var _this = PTL.editor.mt.google_translate;
-        _this.target_lang = PTL.editor.normalize_code($("#id_target_f_0").attr("lang"));
-        _this.pairs = $.cookie(_this.cookie_name);
-        if (!_this.pairs) {
-          _this.pairs = [];
-          $.each(google.language.Languages, function(k, v) {
-            if (v != "" && google.language.isTranslatable(v)) {
-              _this.pairs.push({source: v, target: v});
-            }
-          });
-          var cookie_data = JSON.stringify(_this.pairs);
-          $.cookie(_this.cookie_name, cookie_data, _this.cookie_options);
-        } else {
-          _this.pairs = $.parseJSON(_this.pairs);
-        }
-
-        /* Bind event handler */
-        $(".googletranslate").live("click", _this.translate);
-      });
+      /* Prepare URL for requests. */
+      this.url = PTL.editor.settings.secure == false ? this.url : this.url.replace("http", "https");
+      this.url += "?callback=?";
+      /* Set target language */
+      this.target_lang = PTL.editor.normalize_code($("#id_target_f_0").attr("lang"));
+      /* Bind event handler */
+      $(".googletranslate").live("click", this.translate);
     },
 
     ready: function() {
@@ -43,7 +78,7 @@
         var sources = $("div.placeholder").prev(".translation-text");
         $(sources).each(function() {
           var source = PTL.editor.normalize_code($(this).attr("lang"));
-          if (PTL.editor.isSupportedSource(_this.pairs, _this.source)) {
+          if (PTL.editor.isSupportedSource(_this.pairs, source)) {
             PTL.editor.addMTButton($(this).parent().siblings().children(".translate-toolbar"),
                                  "googletranslate",
                                  m("images/google-translate.png"),
@@ -72,18 +107,17 @@
         source_text = source_text.replace(csharp_string_format_pattern, PTL.editor.collectArguments);
         source_text = source_text.replace(percent_number_pattern, PTL.editor.collectArguments);
 
-        var content = new Object();
-        content.text = source_text;
-        content.type = "text";
-        google.language.translate(content, lang_from, lang_to, function(result) {
-          if (result.translation) {
-            var translation = result.translation;
+        var trans_data = {v: '1.0', q: source_text,
+                         langpair: lang_from + '|' + lang_to}
+        $.getJSON(PTL.editor.mt.google_translate.url, trans_data, function(r) {
+          if (r.responseData && r.responseStatus == 200) {
+            var translation = r.responseData.translatedText;
             for (var i=0; i<argument_subs.length; i++)
               translation = translation.replace("__" + i + "__", argument_subs[i]);
-            areas.eq(j).val(translation);
+            areas.eq(j).val($("<div />").html(translation).text());
             areas.eq(j).focus();
           } else {
-            alert("Google Translate Error: " + result.error.message);
+            alert("Google Translate Error: " + r.responseDetails);
           }
         });
       });
