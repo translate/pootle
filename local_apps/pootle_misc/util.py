345ffadcce3ae4ee8e6c257526303e5311ea7092
Julen Ruiz Aizpuru <julenx@gmail.com>
2010-10-20 08:59:32 +0000

    Don't add orphans on the last page to avoid inconsistencies.
    svn path=/src/trunk/Pootle/; revision=16133

--- local_apps/pootle_misc/util.py
+++ local_apps/pootle_misc/util.py
@@ -58,7 +58,7 @@ def dictsum(x, y):
 
 
 def paginate(request, queryset, items=30, page=None):
-    paginator = Paginator(queryset, items, orphans=items/2)
+    paginator = Paginator(queryset, items)
 
     if not page:
         try:
