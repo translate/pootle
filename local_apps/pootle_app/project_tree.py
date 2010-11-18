d4c3adec4cc643dab5f3bdcf29a64b66e7e718ed
Alaa Abd el Fattah <alaa@translate.org.za>
2010-11-18 01:50:00 +0000

    set target language after updating from template
    svn path=/src/branches/Pootle-2.1/; revision=16604

--- local_apps/pootle_app/project_tree.py
+++ local_apps/pootle_app/project_tree.py
@@ -256,6 +256,7 @@ def convert_template(translation_project, template_store, target_pootle_path, ta
     if template_store.file:
         if store:
             store.update(update_structure=True, update_translation=True, conservative=False, store=output_file, fuzzy=True)
+        output_file.settargetlanguage(translation_project.language.code)
         output_file.savefile(target_path)
     elif store:
         store.mergefile(output_file, None, allownewstrings=True, suggestions=False, notranslate=False, obsoletemissing=True)
