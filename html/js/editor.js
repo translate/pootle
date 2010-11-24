ad9214cd17314c63714c1ad381527c94a5d1b745
Julen Ruiz Aizpuru <julenx@gmail.com>
2010-11-24 14:55:53 +0000

    Adapt code style, improve readability.
    svn path=/src/trunk/Pootle/; revision=16671

--- html/js/editor.js
+++ html/js/editor.js
@@ -647,32 +647,39 @@
   getReqData: function () {
     var reqData = {};
 
-    switch(this.filter) {
-    case "checks":
-	if (this.checks.length) {
-	    reqData.matchnames = this.checks.join(",");
-	}
-	break;
-    case "search":
-	reqData.search = this.searchText;
-	// Override defaults if any fields have been specified
-	if (this.searchFields.length) {
-	    reqData.sfields = this.searchFields;
-	} else {
-	    reqData.sfields = ["source", "target"];
-	}
-	break;
-    case "suggestions":
-	reqData.matchnames = 'hassuggestion'
-	break;
-    case "all":
-	break;
-    case "incomplete":
-	reqData.unitstates = "untranslated,fuzzy";
-	break;
-    default:
-	reqData.unitstates = this.filter;
-	break;
+    switch (this.filter) {
+
+      case "checks":
+        if (this.checks.length) {
+         reqData.matchnames = this.checks.join(",");
+        }
+        break;
+
+      case "search":
+        reqData.search = this.searchText;
+
+        // Override defaults if any fields have been specified
+        if (this.searchFields.length) {
+          reqData.sfields = this.searchFields;
+        } else {
+          reqData.sfields = ["source", "target"];
+        }
+        break;
+
+      case "suggestions":
+        reqData.matchnames = 'hassuggestion'
+      break;
+
+      case "all":
+        break;
+
+      case "incomplete":
+        reqData.unitstates = "untranslated,fuzzy";
+        break;
+
+      default:
+        reqData.unitstates = this.filter;
+        break;
     }
 
     return reqData;
