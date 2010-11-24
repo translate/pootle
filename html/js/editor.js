677f52e1af9485aabac79ceee8ffc57d06ee6fad
Alaa Abd el Fattah <alaa@translate.org.za>
2010-11-24 14:14:34 +0000

    use the old GET vars for filtering units
    svn path=/src/trunk/Pootle/; revision=16668

--- html/js/editor.js
+++ html/js/editor.js
@@ -645,21 +645,34 @@
 
   /* Gets common request data */
   getReqData: function () {
-    var reqData = {filter: this.filter};
+    var reqData = {};
 
-    if (this.filter == "checks" && this.checks.length) {
-      reqData.checks = this.checks.join(",");
-    }
-
-    if (this.filter == "search") {
-      reqData.search = this.searchText;
-
-      // Override defaults if any fields have been specified
-      if (this.searchFields.length) {
-        reqData.sfields = this.searchFields;
-      } else {
-        reqData.sfields = ["source", "target"];
-      }
+    switch(this.filter) {
+    case "checks":
+	if (this.checks.length) {
+	    reqData.matchnames = this.checks.join(",");
+	}
+	break;
+    case "search":
+	reqData.search = this.searchText;
+	// Override defaults if any fields have been specified
+	if (this.searchFields.length) {
+	    reqData.sfields = this.searchFields;
+	} else {
+	    reqData.sfields = ["source", "target"];
+	}
+	break;
+    case "suggestions":
+	reqData.matchnames = 'hassuggestion'
+	break;
+    case "all":
+	break;
+    case "incomplete":
+	reqData.unitstates = "untranslated,fuzzy";
+	break;
+    default:
+	reqData.unitstates = this.filter;
+	break;
     }
 
     return reqData;
