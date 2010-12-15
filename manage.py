f9b4b1dd999d4b29706a2a341c0d5371df770a2d
Alaa Abd el Fattah <alaa@translate.org.za>
2010-12-15 09:31:38 +0000

    don't suppress the traceback when import settings fails
    svn path=/src/branches/Pootle-2.1/; revision=16874

--- manage.py
+++ manage.py
@@ -21,16 +21,7 @@
 import syspath_override
 
 from django.core.management import execute_manager
-try:
-    from pootle import settings
-except ImportError:
-    import sys
-    sys.stderr.write("""Error: Can't find the file 'settings.py' in the directory containing %r.
-It appears you've customized things.
-
-You'll have to run django-admin.py, passing it your settings module.
-(If the file settings.py does indeed exist, it's causing an ImportError somehow.)""" % __file__)
-    sys.exit(1)
+from pootle import settings
 
 if __name__ == "__main__":
     execute_manager(settings)
