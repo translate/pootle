translate/pootle

commit c7b5f0f2ef601461260245dc6a45553cf2870c0f

New form class, and a factory to return a different search form for terminology projects/files
svn path=/src/trunk/Pootle/; revision=17630

local_apps/pootle_translationproject/forms.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2009 Zuza Software Foundation
+# Copyright 2010-2011 Zuza Software Foundation
 #
 # This file is part of Pootle.
 #
@@ -25,7 +25,17 @@ from django import forms
 from django.utils.translation import ugettext_lazy as _
 
 
+def make_search_form(**kwargs):
+    """A factory that instantiates one of the search forms below."""
+    terminology = kwargs.pop('terminology', False)
+    if terminology:
+        return TermSearchForm(**kwargs)
+    else:
+        return SearchForm(**kwargs)
+
+
 class SearchForm(forms.Form):
+    """Normal search form for translation projects."""
     search = forms.CharField(widget=forms.TextInput(attrs={'size': '15'}))
     sfields = forms.MultipleChoiceField(
             required=False,
@@ -37,4 +47,19 @@ class SearchForm(forms.Form):
                 ('locations', _('Locations'))
             ),
             initial=['source', 'target'],
+    )
+
+
+class TermSearchForm(SearchForm):
+    """Search form for terminology projects and pootle-terminology files."""
+    # Mostly the same as SearchForm, but defining it this way seemed easiest
+    sfields = forms.ChoiceField(
+            required=False,
+            widget=forms.CheckboxSelectMultiple,
+            choices=(
+                ('source', _('Source Terms')),
+                ('target', _('Target Terms')),
+                ('notes', _('Definitions')),
+            ),
+            initial=['source', 'target', 'notes'],
     )

local_apps/pootle_store/views.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2008-2009 Zuza Software Foundation
+# Copyright 2010-2011 Zuza Software Foundation
 #
 # This file is part of Pootle.
 #
@@ -41,7 +41,7 @@ from pootle_misc.baseurl import redirect
 from pootle_app.models.permissions import get_matching_permissions, check_permission, check_profile_permission
 from pootle_misc.util import paginate, ajax_required
 from pootle_profile.models import get_profile
-from pootle_translationproject.forms import SearchForm
+from pootle_translationproject.forms import make_search_form
 from pootle_statistics.models import Submission
 from pootle_app.models import Suggestion as SuggestionStat
 
@@ -294,7 +294,8 @@ def get_step_query(request, units_queryset):
             units_queryset = match_queryset
 
     if 'search' in request.GET and 'sfields' in request.GET:
-        search_form = SearchForm(request.GET)
+        # use the search form for validation only
+        search_form = make_search_form(request.GET)
         if search_form.is_valid():
             units_queryset = get_search_step_query(request.translation_project, search_form, units_queryset)
     return units_queryset
@@ -308,8 +309,9 @@ def translate_page(request):
     language = translation_project.language
     profile = request.profile
 
-    search_form = SearchForm()
     store = getattr(request, "store", None)
+    is_terminology = translation_project.project.is_terminology or store and store.is_terminology
+    search_form = make_search_form(terminology=is_terminology)
     context = {
         'cantranslate': cantranslate,
         'cansuggest': cansuggest,

local_apps/pootle_translationproject/templatetags/translation_project_tags.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2009 Zuza Software Foundation
+# Copyright 2010-2011 Zuza Software Foundation
 #
 # This file is part of Pootle.
 #
@@ -21,7 +21,7 @@
 from django import template
 from django.utils.translation import ugettext as _
 
-from pootle_translationproject.forms import SearchForm
+from pootle_translationproject.forms import make_search_form
 from pootle_misc.baseurl import l
 
 register = template.Library()
@@ -30,7 +30,8 @@ register = template.Library()
 def render_search(context, form=None, action=None):
     translation_project = context['translation_project']
     if form is None:
-        form = SearchForm()
+        is_terminology =  translation_project.project.is_terminology
+        form = make_search_form(terminology=is_terminology)
     if action is None:
         action = l('translate.html')
 

commit f67bc1431d57918c48f87231bfefb308c3086940

Support named and unnamed arguments. This fixes a search bug introduced in r17630.
svn path=/src/trunk/Pootle/; revision=17644

local_apps/pootle_translationproject/forms.py
@@ -25,13 +25,13 @@ from django import forms
 from django.utils.translation import ugettext_lazy as _
 
 
-def make_search_form(**kwargs):
+def make_search_form(*args, **kwargs):
     """A factory that instantiates one of the search forms below."""
     terminology = kwargs.pop('terminology', False)
     if terminology:
-        return TermSearchForm(**kwargs)
+        return TermSearchForm(*args, **kwargs)
     else:
-        return SearchForm(**kwargs)
+        return SearchForm(*args, **kwargs)
 
 
 class SearchForm(forms.Form):