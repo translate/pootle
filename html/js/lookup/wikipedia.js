e5a4944c7747f66e9e511b1eaaada442f633d444
Friedel Wolff <friedel@translate.org.za>
2012-02-08 11:44:36 +0000

    Add lookup plugin for Wikipedia
    svn path=/src/trunk/Pootle/; revision=17994

---
+++ html/js/lookup/wikipedia.js
@@ -0,0 +1,28 @@
+(function ($) {
+  window.PTL.editor.lookup = window.PTL.editor.lookup || {};
+
+  PTL.editor.lookup.wikipedia = {
+
+    buttonClassName: "wikipedia",
+    imageUri: m("images/wikipedia.png"),
+    hint: "Wikipedia",
+    url: "http://%(querylang)s.wikipedia.org/wiki/%(query)s'",
+
+    init: function () {
+      /* Bind event handler */
+      $(".wikipedia").live("click", this.lookup);
+    },
+
+    ready: function () {
+      PTL.editor.addLookupButtons(PTL.editor.lookup.wikipedia);
+    },
+
+    lookup: function (e) {
+      e.preventDefault()
+      PTL.editor.lookup(this, function(lookupText, langFrom, langTo) {
+        var url = 'http://' + langFrom + '.m.wikipedia.org/wiki/' + lookupText.replace(/ /g, "_");
+        return url;
+      });
+    }
+  };
+})(jQuery);
