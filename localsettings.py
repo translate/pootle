e5804d0ee3044831fb3c37d6980fffe1e0ef0f04
Friedel Wolff <friedel@translate.org.za>
2012-02-17 15:38:10 +0000

    Remove doubled DATABASE_NAME
    svn path=/src/branches/Pootle-terms/; revision=18030

--- localsettings.py
+++ localsettings.py
@@ -36,8 +36,6 @@ DATABASE_PASSWORD = ''                      # Not used with sqlite3.
 DATABASE_HOST = ''                          # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''                          # Set to empty string for default. Not used with sqlite3.
 
-DATABASE_NAME = working_path('dbs/toetsie2.db') # Or path to database file if using sqlite3.
-
 # The directory where the translation files are kept
 PODIRECTORY = working_path('po')
 
