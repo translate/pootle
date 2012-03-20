d91f5f47aa41eb8b179bf8c9b43d5253c5a5d926
Friedel Wolff <friedel@translate.org.za>
2012-03-20 12:38:12 +0200

    Simplify customising templates locally.
    Local template overrides can be placed in local_templates which will be
    used instead of the default templates.

--- pootle/settings.py
+++ pootle/settings.py
@@ -122,6 +122,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    data_path('local_templates'),
     data_path('templates'),
 )
 
