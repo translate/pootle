3fa127be900d40b2e8df67211aa922babfd99edc
Julen Ruiz Aizpuru <julenx@gmail.com>
2012-06-29 08:42:54 +0200

    Remove unused method.

--- pootle/apps/pootle_app/models/directory.py
+++ pootle/apps/pootle_app/models/directory.py
@@ -105,9 +105,6 @@ class Directory(models.Model):
     def get_absolute_url(self):
         return l(self.pootle_path)
 
-    def get_translate_incomplete_url(self):
-        return l(self.pootle_path) + 'translate.html#filter=incomplete'
-
     @getfromcache
     def getquickstats(self):
         """calculate aggregate stats for all directory based on stats
