b8c541ee59169a2257dd5b9b7a0f8c1316df8ea4
Julen Ruiz Aizpuru <julenx@gmail.com>
2012-07-09 15:05:00 +0200

    Don't remove context rows that shouldn't be removed.
    This fix is part of bug 2188.

--- pootle/static/js/editor.js
+++ pootle/static/js/editor.js
@@ -1527,14 +1527,19 @@
     // Make sure there are context rows before decreasing the gap and
     // removing any context rows
     if (before.length || after.length) {
+      if (before.length === PTL.editor.ctxGap) {
+        before.slice(0, PTL.editor.ctxStep).remove();
+      }
+
+      if (after.length === PTL.editor.ctxGap) {
+        after.slice(-PTL.editor.ctxStep).remove();
+      }
+
       PTL.editor.ctxGap -= PTL.editor.ctxStep;
 
       if (PTL.editor.ctxGap > 0) {
         $.cookie('ctxQty', PTL.editor.ctxGap, {path: '/'});
       }
-
-      before.slice(0, PTL.editor.ctxStep).remove();
-      after.slice(-PTL.editor.ctxStep).remove();
     }
   },
 
