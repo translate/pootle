f24849ce4e8c273864c21bd60cd262c93ee8dbe9
Friedel Wolff <friedel@translate.org.za>
2012-07-12 12:08:32 +0200

    Interact with VCS_DIRECTORY.
    Updates, commits, additions and .hasversioning will now make use of a checkout/
    clone under the VCS_DIRECTORY. This fixes bug 2314.

--- pootle/apps/pootle_misc/versioncontrol.py
+++ pootle/apps/pootle_misc/versioncontrol.py
@@ -20,6 +20,7 @@
 """Utility functions to help with version control systems."""
 
 import os.path
+import shutil
 
 from translate.storage import versioncontrol
 
@@ -30,26 +31,41 @@ from pootle_store.util import relative_real_path
 
 def to_vcs_path(path):
     path = relative_real_path(path)
+    return os.path.join(settings.VCS_DIRECTORY, path)
+
+
+def to_podir_path(path):
+    path = relative_real_path(path)
     return os.path.join(settings.PODIRECTORY, path)
 
 
 def hasversioning(path):
     path = to_vcs_path(path)
-    return versioncontrol.hasversioning(path, settings.PODIRECTORY)
+    return versioncontrol.hasversioning(path, settings.VCS_DIRECTORY)
 
 
 def commit_file(path, message, author):
     vcs_path = to_vcs_path(path)
+    path = to_podir_path(path)
+    shutil.copy2(path, vcs_path)
     versioncontrol.commitfile(vcs_path, message=message, author=author)
 
 
 def update_file(path):
     vcs_path = to_vcs_path(path)
+    path = to_podir_path(path)
     versioncontrol.updatefile(vcs_path)
+    shutil.copy2(vcs_path, path)
 
 
 def add_files(path, files, message):
     vcs_path = to_vcs_path(path)
+    path = to_podir_path(path)
     vcs = versioncontrol.get_versioned_object(vcs_path)
+    #: list of (podir_path, vcs_path) tuples
+    file_paths = [(to_podir_path(f), to_vcs_path(f)) for f in files]
+    for podir_path, vcs_path in file_paths:
+        os.makedirs(os.path.dirname(vcs_path))
+        shutil.copy(podir_path, vcs_path)
     output = vcs.add([to_vcs_path(f) for f in files], message)
     return output
