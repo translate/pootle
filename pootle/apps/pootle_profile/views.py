3bbd9583f14cb02ddbe0e3536dc6122c0ac06b54
Julen Ruiz Aizpuru <julenx@gmail.com>
2012-08-14 11:05:51 +0200

    Organize and cleanup code and imports.

--- pootle/apps/pootle_profile/views.py
+++ pootle/apps/pootle_profile/views.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2009 Zuza Software Foundation
+# Copyright 2009-2012 Zuza Software Foundation
 #
 # This file is part of Pootle.
 #
@@ -19,28 +19,31 @@
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 from django import forms
-from django.utils.translation import ugettext as _
 from django.contrib import auth
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.forms import ModelForm
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.utils.encoding import iri_to_uri
 from django.utils.http import urlquote
-from django.forms import ModelForm
-from django.contrib.auth.models import User
-from django.contrib.auth.decorators import login_required
+from django.utils.translation import ugettext as _
 
-from pootle_profile.models import PootleProfile, get_profile
 from pootle_app.models import Directory
 from pootle_app.models.permissions import check_profile_permission
 from pootle_misc.baseurl import redirect
+from pootle_profile.models import PootleProfile, get_profile
 
 from profiles.views import edit_profile
 
 
 def get_pootle_profile_form(request):
-    """return a profile form suitable for creating/editing PootleProfile"""
-    can_view = check_profile_permission(get_profile(request.user), "view", Directory.objects.root)
+    """Return a profile form suitable for creating/editing a
+       :cls:`pootle_profile.models.PootleProfile`.
+    """
+    can_view = check_profile_permission(get_profile(request.user), "view",
+                                        Directory.objects.root)
 
     if can_view:
         excluded = ('user',)
@@ -51,7 +54,9 @@ def get_pootle_profile_form(request):
         class Meta:
             model = PootleProfile
             exclude = excluded
+
     return PootleProfileForm
+
 
 def profile_edit(request):
     return edit_profile(request, form_class=get_pootle_profile_form(request))
@@ -62,26 +67,33 @@ class UserForm(ModelForm):
         model = User
         fields = ('first_name', 'last_name', 'email')
 
+
 @login_required
 def edit_personal_info(request):
     if request.POST:
         post = request.POST.copy()
         user_form = UserForm(post, instance=request.user)
+
         if user_form.is_valid():
             user_form.save()
             response = redirect('/accounts/'+request.user.username)
     else:
         user_form = UserForm(instance=request.user)
+
     template_vars = {"form": user_form}
-    response = render_to_response('profiles/edit_personal.html', template_vars, context_instance=RequestContext(request))
-    return response
+    return render_to_response('profiles/edit_personal.html', template_vars,
+                              context_instance=RequestContext(request))
 
 
 def redirect_after_login(request):
     redirect_to = request.REQUEST.get(auth.REDIRECT_FIELD_NAME, None)
+
     if not redirect_to or '://' in redirect_to or ' ' in redirect_to:
-        redirect_to = iri_to_uri('/accounts/%s/' % urlquote(request.user.username))
+        redirect_to = iri_to_uri('/accounts/%s/' % \
+                                 urlquote(request.user.username))
+
     return redirect(redirect_to)
+
 
 def language_list(request):
     """returns the list of localised language names, with 'default'"""
@@ -90,14 +102,17 @@ def language_list(request):
         tr_default = u"%s | \u202dDefault" % tr_default
 
     from pootle.i18n.override import lang_choices
+
     choices = lang_choices()
     choices.insert(0, ('', tr_default))
+
     return choices
 
 
 def login(request):
     class LangAuthenticationForm(AuthenticationForm):
-        language = forms.ChoiceField(label=_('Interface Language'), choices=language_list(request),
+        language = forms.ChoiceField(label=_('Interface Language'),
+                                     choices=language_list(request),
                                      initial="", required=False)
 
         def clean(self):
@@ -105,9 +120,14 @@ def login(request):
             password = self.cleaned_data.get('password')
 
             if username and password:
-                self.user_cache = auth.authenticate(username=username, password=password)
+                self.user_cache = auth.authenticate(username=username,
+                                                    password=password)
+
                 if self.user_cache is None:
-                    raise forms.ValidationError(_("Please enter a correct username and password. Note that both fields are case-sensitive."))
+                    raise forms.ValidationError(
+                        _("Please enter a correct username and password. "
+                          "Note that both fields are case-sensitive.")
+                    )
                 elif not self.user_cache.is_active:
                     raise forms.ValidationError(_("This account is inactive."))
 
@@ -119,13 +139,15 @@ def login(request):
     else:
         if request.POST:
             form = LangAuthenticationForm(request, data=request.POST)
-            # do login here
+
+            # Do login here
             if form.is_valid():
                 auth.login(request, form.get_user())
 
                 language = request.POST.get('language')
                 request.session['django_language'] = language
                 response = redirect_after_login(request)
+
                 return response
         else:
             form = LangAuthenticationForm(request)
@@ -133,7 +155,9 @@ def login(request):
         context = {
             'form': form,
             }
-        return render_to_response("index/login.html", context, context_instance=RequestContext(request))
+
+        return render_to_response("index/login.html", context,
+                                  context_instance=RequestContext(request))
 
 
 def logout(request):
