ba815f862a971c9415b9cf67d4eb25de61405403
Julen Ruiz Aizpuru <julenx@gmail.com>
2012-08-14 15:27:23 +0200

    Organize and cleanup code and imports.

--- pootle/apps/pootle_misc/middleware/errorpages.py
+++ pootle/apps/pootle_misc/middleware/errorpages.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2008-2009 Zuza Software Foundation
+# Copyright 2008-2012 Zuza Software Foundation
 #
 # This file is part of Pootle.
 #
@@ -18,77 +18,88 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
-import traceback
 import sys
+import traceback
 
-from django.utils import simplejson
-
-from django.core.exceptions import PermissionDenied
-from django.http import  HttpResponseForbidden, HttpResponseServerError
-from django.http import Http404, HttpResponse
-from django.template.loader import render_to_string
-from django.template import RequestContext
-from django.utils.translation import ugettext as _
 from django.conf import settings
+from django.core.exceptions import PermissionDenied
 from django.core.mail import mail_admins
+from django.http import  (Http404, HttpResponse, HttpResponseForbidden,
+                          HttpResponseServerError)
+from django.template import RequestContext
+from django.template.loader import render_to_string
+from django.utils import simplejson
+from django.utils.translation import ugettext as _
 
 from pootle_misc.baseurl import l, get_next
 
+
 class ErrorPagesMiddleware(object):
-    """
-    Friendlier Error Pages
-    """
+    """Friendlier error pages."""
+
     def _ajax_error(self, rcode, msg):
         json = {'msg': msg}
         response = simplejson.dumps(json)
         return HttpResponse(response, status=rcode, mimetype="application/json")
+
 
     def process_exception(self, request, exception):
         msg = unicode(exception)
         if isinstance(exception, Http404):
             if request.is_ajax():
                 return self._ajax_error(404, msg)
-
         elif isinstance(exception, PermissionDenied):
             if request.is_ajax():
                 return self._ajax_error(403, msg)
 
             templatevars = {}
             templatevars['permission_error'] = msg
+
             if not request.user.is_authenticated():
                 login_msg = _('You need to <a href="%(login_link)s">login</a> '
                               'to access this page.',
                               {'login_link': "%s%s" % \
                                 (l("/accounts/login/"), get_next(request))})
                 templatevars["login_message"] = login_msg
+
             return HttpResponseForbidden(render_to_string('403.html', templatevars,
                                       RequestContext(request)))
-
         else:
             #FIXME: implement better 500
             tb = traceback.format_exc()
             print >> sys.stderr, tb
+
             if not settings.DEBUG:
                 try:
                     templatevars = {}
                     templatevars['exception'] = msg
                     if hasattr(exception, 'filename'):
-                        msg = _('Error accessing %(filename)s, Filesystem sent error: %(errormsg)s',
-                                                    {'filename': exception.filename, 'errormsg': exception.strerror})
+                        msg = _('Error accessing %(filename)s, Filesystem '
+                                'sent error: %(errormsg)s', {
+                                    'filename': exception.filename,
+                                    'errormsg': exception.strerror
+                                })
                         templatevars['fserror'] = msg
-                    # send email to admins with details about exception
-                    subject = 'Error (%s IP): %s' % ((request.META.get('REMOTE_ADDR') in settings.INTERNAL_IPS and 'internal' or 'EXTERNAL'), request.path)
+
+                    # Send email to admins with details about exception
+                    ip_type = (request.META.get('REMOTE_ADDR') in \
+                            settings.INTERNAL_IPS and 'internal' or 'EXTERNAL')
+                    subject = 'Error (%s IP): %s' % (ip_type, request.path)
+
                     try:
                         request_repr = repr(request)
                     except:
                         request_repr = "Request repr() unavailable"
-                    message = "%s\n\n%s\n\n%s" % (unicode(exception.args[0]), tb, request_repr)
+
+                    message = "%s\n\n%s\n\n%s" % (unicode(exception.args[0]),
+                                                  tb, request_repr)
                     mail_admins(subject, message, fail_silently=True)
 
                     if request.is_ajax():
                         return self._ajax_error(500, msg)
-                    return HttpResponseServerError(render_to_string('500.html', templatevars,
-                                                                    RequestContext(request)))
+                    return HttpResponseServerError(
+                        render_to_string('500.html', templatevars,
+                                         RequestContext(request)))
                 except:
-                    # let's not confuse things by throwing an exception here
+                    # Let's not confuse things by throwing an exception here
                     pass
