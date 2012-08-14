12bb75311020fc7d296552ea7c261e0b5dbf1155
Julen Ruiz Aizpuru <julenx@gmail.com>
2012-08-14 15:02:49 +0200

    Fix docstring.

--- pootle/apps/pootle_misc/baseurl.py
+++ pootle/apps/pootle_misc/baseurl.py
@@ -18,7 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
-""" utility functions to help deploy Pootle under different url prefixes """
+"""Utility functions to help deploy Pootle under different url prefixes."""
 
 import os
 import urllib
