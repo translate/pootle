dc7f670cc78ececa3a5a26689f78e3233dc87d2e
Julen Ruiz Aizpuru <julenx@gmail.com>
2012-08-20 13:09:33 +0200

    No need to set `csrfmiddlewaretoken` here.
    An ajax prefilter is used in the base.html template so it's populated
    anywhere in Pootle.

--- pootle/static/js/common.js
+++ pootle/static/js/common.js
@@ -63,33 +63,6 @@ function zoom(v) {
  */
 
 $(document).ready(function ($) {
-  $('html').ajaxSend(function (event, xhr, settings) {
-    // FIXME: Can't we use $.cookie() here?
-    function getCookie(name) {
-      var cookieValue = null;
-
-      if (document.cookie && document.cookie != '') {
-        var cookies = document.cookie.split(';');
-
-        for (var i = 0; i < cookies.length; i++) {
-          var cookie = jQuery.trim(cookies[i]);
-
-          // Does this cookie string begin with the name we want?
-          if (cookie.substring(0, name.length + 1) == (name + '=')) {
-            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-            break;
-          }
-        }
-      }
-      return cookieValue;
-    }
-
-    if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
-      // Only send the token to relative URLs i.e. locally.
-      xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-    }
-  });
-
   /* Search input text */
   var focused = { color: "#000" },
       unfocused = { color: "#aaa" };
