translate/pootle

commit c6b5b457bffe7bc76daad2d5fd1763868e947121

Use '/' for path used in URL.

pootle/apps/pootle_store/views.py
@@ -124,7 +124,7 @@ def export_as_xliff(request, store):
             path = "%s/%s/%s" % (parts[1], parts[0], "/".join(parts[2:]))
 
     path, ext = os.path.splitext(path)
-    export_path = os.path.join('POOTLE_EXPORT', path + os.path.extsep + 'xlf')
+    export_path = "/".join(['POOTLE_EXPORT'] + path + os.path.extsep + 'xlf')
     abs_export_path = absolute_real_path(export_path)
 
     key = iri_to_uri("%s:export_as_xliff" % store.pootle_path)

commit c7ebc0041c84658637b978921b0d5d598c9b1048

Fix error introduced in c6b5b457b.

pootle/apps/pootle_store/views.py
@@ -124,7 +124,7 @@ def export_as_xliff(request, store):
             path = "%s/%s/%s" % (parts[1], parts[0], "/".join(parts[2:]))
 
     path, ext = os.path.splitext(path)
-    export_path = "/".join(['POOTLE_EXPORT'] + path + os.path.extsep + 'xlf')
+    export_path = "/".join(['POOTLE_EXPORT', path + os.path.extsep + 'xlf'])
     abs_export_path = absolute_real_path(export_path)
 
     key = iri_to_uri("%s:export_as_xliff" % store.pootle_path)