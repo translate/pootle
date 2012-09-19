898552dae8cc6bc19d916e1e86c84db0e6a525d6
Julen Ruiz Aizpuru <julenx@gmail.com>
2012-09-19 20:41:44 +0200

    Coding style corrections.

--- pootle/apps/pootle_misc/dbupdate.py
+++ pootle/apps/pootle_misc/dbupdate.py
@@ -46,20 +46,27 @@ from pootle_translationproject.models import TranslationProject
 
 
 def flush_quality_checks():
-    """reverts stores to unchecked state. if store has false positives
-    marked updates quality checks keeping false postivies intact"""
+    """Reverts stores to unchecked state.
+
+    If a store has false positives marked, quality checks will be updated
+    keeping false postivies intact."""
     for store in Store.objects.filter(state=CHECKED).iterator():
         store_checks = QualityCheck.objects.filter(unit__store=store)
         false_positives = store_checks.filter(false_positive=True).count()
+
         if false_positives:
-            logging.debug("%s has false positives, updating quality checks", store.pootle_path)
+            logging.debug("%s has false positives, updating quality checks",
+                          store.pootle_path)
+
             for unit in store.units.iterator():
                 unit.update_qualitychecks(keep_false_positives=True)
         else:
-            logging.debug("%s has no false positives, deleting checks", store.pootle_path)
+            logging.debug("%s has no false positives, deleting checks",
+                          store.pootle_path)
             store_checks.delete()
             store.state = PARSED
             store.save()
+
 
 def save_toolkit_version(build=None):
     from pootle_misc import siteconfig
@@ -69,7 +76,9 @@ def save_toolkit_version(build=None):
     config = siteconfig.load_site_config()
     config.set('TT_BUILDVERSION', build)
     config.save()
+
     logging.info("Database now at Toolkit build %d" % build)
+
 
 def save_pootle_version(build=None):
     from pootle_misc import siteconfig
@@ -79,7 +88,9 @@ def save_pootle_version(build=None):
     config = siteconfig.load_site_config()
     config.set('BUILDVERSION', build)
     config.save()
+
     logging.info("Database now at Pootle build %d" % build)
+
 
 def header(db_buildversion):
     text = """
@@ -117,41 +128,60 @@ def header(db_buildversion):
     <body>
     <h1>%(title)s</h1>
     <p class="error">%(msg)s</p>
-    """ % {'title': _('Pootle: Update'),
-           'msg': _('Database tables are currently at build version %d. Pootle will now update the database.', db_buildversion)}
+    """ % {
+        'title': _('Pootle: Update'),
+        'msg': _('Database tables are currently at build version %d. '
+                 'Pootle will now update the database.', db_buildversion)
+    }
+
     return text
+
 
 def syncdb():
     text = u"""
     <p>%s</p>
     """ % _('Creating missing database tables...')
     logging.info("Creating missing database tables")
+
     call_command('syncdb', interactive=False)
+
     return text
+
 
 def update_permissions_20030():
     text = """
     <p>%s</p>
     """ % _('Fixing permission table...')
     logging.info("Fixing permission table")
+
     from django.contrib.auth.models import Permission
     from django.contrib.contenttypes.models import ContentType
-    contenttype, created = ContentType.objects.get_or_create(app_label="pootle_app", model="directory")
-    for permission in Permission.objects.filter(content_type__name='pootle').iterator():
+
+    contenttype, created = ContentType.objects
+                                      .get_or_create(app_label="pootle_app",
+                                                     model="directory")
+
+    for permission in Permission.objects.filter(content_type__name='pootle')
+                                        .iterator():
         permission.content_type = contenttype
         permission.save()
+
     contenttype.name = 'pootle'
     contenttype.save()
+
     save_pootle_version(20030)
+
     return text
+
 
 def update_tables_21000():
     text = u"""
     <p>%s</p>
     """ % _('Updating existing database tables...')
     logging.info("Updating existing database tables")
+
     from south.db import db
-    #raise ImportError
+
     table_name = Store._meta.db_table
     field = Store._meta.get_field('state')
     db.add_column(table_name, field.name, field)
@@ -171,53 +201,74 @@ def update_tables_21000():
     try:
         en = Language.objects.get(code='en')
     except Language.DoesNotExist:
-        # we can't allow translation project detection to kick in yet so let's create en manually
-        en = Language(code='en', fullname='English', nplurals=2, pluralequation="(n != 1)")
+        # We can't allow translation project detection to kick in yet so let's
+        # create en manually
+        en = Language(code='en', fullname='English', nplurals=2,
+                      pluralequation="(n != 1)")
         en.directory = Directory.objects.root.get_or_make_subdir(en.code)
         en.save_base(raw=True)
     field.default = en.id
     db.add_column(table_name, field.name, field)
     db.create_index(table_name, (field.name + '_id',))
+
     # We shouldn't do save_pootle_version(21000) yet - more to do below
     return text
+
 
 def update_qualitychecks_21040():
     text = """
     <p>%s</p>
     """ % _('Removing quality checks, will be recalculated on demand...')
     logging.info("Fixing quality checks")
+
     flush_quality_checks()
+
     save_pootle_version(21040)
+
     return text
+
 
 def update_stats_21060():
     text = """
     <p>%s</p>
-    """ %_('Removing potentially incorrect cached stats, will be recalculated...')
-    logging.info('flushing cached stats')
-    for tp in TranslationProject.objects.filter(stores__unit__state=OBSOLETE).distinct().iterator():
-        deletefromcache(tp, ["getquickstats", "getcompletestats", "get_mtime", "has_suggestions"])
+    """ %_('Removing potentially incorrect cached stats, will be '
+           'recalculated...')
+    logging.info('Flushing cached stats')
+
+    for tp in TranslationProject.objects.filter(stores__unit__state=OBSOLETE)
+                                        .distinct().iterator():
+        deletefromcache(tp, ["getquickstats", "getcompletestats",
+                             "get_mtime", "has_suggestions"])
+
     save_pootle_version(21060)
+
     return text
+
 
 def update_ts_tt_12008():
     text = """
     <p>%s</p>
     """ %_('Reparsing Qt ts files...')
-    logging.info('reparsing qt ts')
+    logging.info('Reparsing Qt ts')
+
     for store in Store.objects.filter(state__gt=PARSED,
                                       translation_project__project__localfiletype='ts',
                                       file__iendswith='.ts').iterator():
         store.sync(update_translation=True)
-        store.update(update_structure=True, update_translation=True, conservative=False)
+        store.update(update_structure=True, update_translation=True,
+                     conservative=False)
+
     save_toolkit_version(12008)
+
     return text
+
 
 def update_tables_22000():
     text = u"""
     <p>%s</p>
     """ % _('Updating existing database tables...')
     logging.info("Updating existing database tables")
+
     from south.db import db
 
     # For the sake of South bug 313, we set the default for these fields here:
@@ -288,30 +339,39 @@ def update_tables_22000():
 
     return text
 
+
 def update_toolkit_version():
     text = """
     <p>%s</p>
     """ % _('Removing quality checks, will be recalculated on demand...')
     logging.info("New Translate Toolkit version, flushing quality checks")
+
     flush_quality_checks()
     save_toolkit_version()
+
     return text
+
 
 def parse_start():
     text = u"""
     <p>%s</p>
     <ul>
-    """ % _('Pootle will now import all the translations into the database. It could take a long time.')
+    """ % _('Pootle will now import all the translations into the database. '
+            'It could take a long time.')
+
     return text
+
 
 def import_suggestions(store):
     try:
         logging.info(u"Importing suggestions for %s if any.", store.real_path)
         store.import_pending()
+
         try:
             count = store.has_suggestions()
         except:
             count = store.get_suggestion_count()
+
         if count:
             text = u"""
             <li>%s</li>
@@ -324,7 +384,9 @@ def import_suggestions(store):
         text = u"""
         <li class="error">%s</li>
         """ % _('Failed to import suggestions from %s', store.pootle_path)
+
     return text
+
 
 def parse_store(store):
     try:
@@ -340,25 +402,33 @@ def parse_store(store):
         text = u"""
         <li class="error">%s</li>
         """ % _('Failed to import units from %s', store.pootle_path)
+
     return text
+
 
 def parse_end():
     text = u"""
     </ul>
     <p>%s</p>
     """ % _('All translations are now imported.')
+
     return text
+
 
 def footer():
     text = """
     <p>%(endmsg)s</p>
     <div><script>setTimeout("location.reload()", 10000)</script></div>
     </body></html>
-    """ % {'endmsg': _('Pootle initialized the database. You will be redirected to the front page in 10 seconds.')}
+    """ % {'endmsg': _('Pootle initialized the database. You will be '
+                       'redirected to the front page in 10 seconds.')}
+
     return text
 
+
 def staggered_update(db_buildversion, tt_buildversion):
-    """Update pootle database, while displaying progress report for each step"""
+    """Update pootle database, while displaying a progress report for each
+    step."""
     # django's syncdb command prints progress reports to stdout, but
     # mod_wsgi doesn't like stdout, so we reroute to stderr
     stdout = sys.stdout
@@ -375,31 +445,35 @@ def staggered_update(db_buildversion, tt_buildversion):
         try:
             yield update_tables_21000()
         except Exception, e:
-            logging.warning(u"something broke while upgrading database tables:\n%s", e)
+            logging.warning(u"Something broke while upgrading database "
+                            u"tables:\n%s", e)
             #TODO: should we continue?
 
-        logging.info("creating project directories")
+        logging.info("Creating project directories")
         Directory.objects.root.get_or_make_subdir('projects')
         for project in Project.objects.iterator():
             # saving should force project to update it's directory property
             try:
                 project.save()
             except Exception, e:
-                logging.warning(u"something broke while upgrading %s:\n%s", project, e)
+                logging.warning(u"Something broke while upgrading %s:\n%s",
+                                project, e)
 
-        logging.info("associating stores with translation projects")
+        logging.info("Associating stores with translation projects")
         for store in Store.objects.iterator():
             try:
                 store.translation_project = store.parent.get_translationproject()
                 store.save()
             except Exception, e:
-                logging.warning(u"something broke while upgrading %s:\n%s", store.pootle_path, e)
+                logging.warning(u"Something broke while upgrading %s:\n%s",
+                                store.pootle_path, e)
 
-    # build missing tables
+    # Build missing tables
     try:
         yield syncdb()
     except Exception, e:
-        logging.warning(u"something broke while creating new database tables:\n%s", e)
+        logging.warning(u"something broke while creating new database tables:"
+                        u"\n%s", e)
 
     if db_buildversion < 21000:
         yield parse_start()
@@ -408,7 +482,8 @@ def staggered_update(db_buildversion, tt_buildversion):
                 yield parse_store(store)
                 yield import_suggestions(store)
             except Exception, e:
-                logging.warning(u"something broke while parsing %s:\n%s", store, e)
+                logging.warning(u"Something broke while parsing %s:\n%s",
+                                store, e)
 
         yield parse_end()
         save_pootle_version(21000)
@@ -435,18 +510,23 @@ def staggered_update(db_buildversion, tt_buildversion):
         # only need to update the toolkit version, not do the upgrade
         save_toolkit_version()
 
-    # first time to visit the front page all stats for projects and
+    # First time to visit the front page all stats for projects and
     # languages will be calculated which can take forever, since users
     # don't like webpages that take forever let's precalculate the
     # stats here (copied from dbinit)
     yield stats_start()
+
     for language in Language.objects.iterator():
         yield stats_language(language)
+
     for project in Project.objects.iterator():
         yield stats_project(project)
+
     yield stats_end()
 
     yield footer()
-    # bring back stdout
+
+    # Bring back stdout
     sys.stdout = stdout
+
     return
