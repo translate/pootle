5f430168b30a68f8d99ba81bd192c0ecaa39e28e
Julen Ruiz Aizpuru <julenx@gmail.com>
2012-11-30 14:28:39 +0100

    Remove stupid comments.

--- pootle/urls.py
+++ pootle/urls.py
@@ -21,13 +21,11 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 
 urlpatterns = patterns('',
-    # Uncomment the next line to enable the admin:
     (r'^django_admin/', include(admin.site.urls)),
 
     # JavaScript i18n
