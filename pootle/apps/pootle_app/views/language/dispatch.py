c0540fbc89d9a8e9c77b6d3ff766b7cd88d6ebc1
Julen Ruiz Aizpuru <julenx@gmail.com>
2012-12-03 16:49:25 +0100

    Removed unused import.

--- pootle/apps/pootle_app/views/language/dispatch.py
+++ pootle/apps/pootle_app/views/language/dispatch.py
@@ -19,7 +19,6 @@
 # along with Pootle; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-from django.core.urlresolvers import get_script_prefix
 from pootle_misc.baseurl import l
 
 
