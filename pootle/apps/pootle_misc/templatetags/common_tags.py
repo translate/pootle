3715decd9d200dec17a227f1f105943657166fdd
Julen Ruiz Aizpuru <julenx@gmail.com>
2013-02-15 18:16:06 +0100

    Implemented a inclusion tag for displaying the tables.
    This will be necessary in order to display more than one table in a single page.

---
+++ pootle/apps/pootle_misc/templatetags/common_tags.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2013 Zuza Software Foundation
+#
+# This file is part of Pootle.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+from django import template
+
+register = template.Library()
+
+
+@register.inclusion_tag('common/table.html', takes_context=True)
+def display_table(context, table):
+    return {
+        'table': table,
+        'stats': context.get('stats', None)
+    }
