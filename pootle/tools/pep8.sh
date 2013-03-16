06b2bbd987ec0297d5b10e3db50e13ee7ece1c98
Dwayne Bailey <dwayne@translate.org.za>
2013-03-16 16:01:52 +0000

    Add help text

--- pootle/tools/pep8.sh
+++ pootle/tools/pep8.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+script_name=$(basename $0)
+if [ $1 = '--help' -o $1 = '-h' ]; then
+	echo "Usage: $script_name [test] [file(s)]"
+	echo "       $script_name - run pep8 from current directory"
+	echo "       $script_name E2 - run all E2* checks"
+	echo "       $script_name E221 dir/ - run E221 in directory dir/"
+	exit
+fi
 select=$1
 shift
 files=$*
