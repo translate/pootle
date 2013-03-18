translate/pootle

commit 4bc032daa8b6dd750650416a430c7b88174dbbc7

PEP8: E225 missing whitespace around operator

pootle/apps/pootle_misc/dbupdate.py
@@ -218,8 +218,8 @@ def update_tables_21000():
 def update_stats_21060():
     text = """
     <p>%s</p>
-    """ %_('Removing potentially incorrect cached stats, will be '
-           'recalculated...')
+    """ % _('Removing potentially incorrect cached stats, will be '
+            'recalculated...')
     logging.info('Flushing cached stats')
 
     for tp in TranslationProject.objects.filter(stores__unit__state=OBSOLETE) \
@@ -235,7 +235,7 @@ def update_stats_21060():
 def update_ts_tt_12008():
     text = """
     <p>%s</p>
-    """ %_('Reparsing Qt ts files...')
+    """ % _ ('Reparsing Qt ts files...')
     logging.info('Reparsing Qt ts')
 
     for store in Store.objects \

pootle/apps/pootle_store/forms.py
@@ -306,7 +306,7 @@ def unit_comment_form_factory(language):
         def save(self):
             """Registers the submission and saves the comment."""
             if self.has_changed():
-                creation_time=timezone.now()
+                creation_time = timezone.now()
                 translation_project = self.request.translation_project
 
                 sub = Submission(

commit 5cd061621f75f175027f1b80084202551abe0e16

Fix broken _() introduced in 4bc032d

pootle/apps/pootle_misc/dbupdate.py
@@ -235,7 +235,7 @@ def update_stats_21060():
 def update_ts_tt_12008():
     text = """
     <p>%s</p>
-    """ % _ ('Reparsing Qt ts files...')
+    """ % _('Reparsing Qt ts files...')
     logging.info('Reparsing Qt ts')
 
     for store in Store.objects \