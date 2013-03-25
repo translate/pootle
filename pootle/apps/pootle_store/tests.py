bcf4fc3d55518eeb06668868012344085299b2b6
Chris Oelmueller <chris.oelmueller@gmail.com>
2013-03-25 19:36:07 +0100

    Coding style fix

--- pootle/apps/pootle_store/tests.py
+++ pootle/apps/pootle_store/tests.py
@@ -17,6 +17,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
+
 import time
 
 from django.utils import simplejson
