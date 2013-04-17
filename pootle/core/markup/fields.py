52085af40df0f3fa21e8ddcb3a7bb890036c0533
Julen Ruiz Aizpuru <julenx@gmail.com>
2013-04-17 09:42:33 +0200

    Implemented a MarkupField.
    This field renders the page bodies using any of the existing markup
    filters and stores the result in the cache. By using this, we'll remove the
    need for extra <field>_html fields for storing the HTML contents.
    
    Part of bug 2828.

---
+++ pootle/core/markup/fields.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2013 Zuza Software Foundation
+#
+# This file is part of Pootle.
+#
+# translate is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# translate is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with translate; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+from __future__ import absolute_import
+
+import logging
+
+from django.conf import settings
+from django.core.cache import cache
+from django.db import models
+from django.utils.safestring import mark_safe
+
+from south.modelsinspector import add_introspection_rules
+
+from pootle_misc.util import apply_markup_filter
+
+from .widgets import MarkupTextarea
+
+
+__all__ = ('MarkupField',)
+
+
+logger = logging.getLogger('pootle.markup')
+
+
+_rendered_cache_key = lambda obj, pk, field: '_%s_%s_%s_rendered' % \
+        (obj, pk, field)
+
+
+class Markup(object):
+
+    def __init__(self, instance, field_name, rendered_cache_key):
+        self.instance = instance
+        self.field_name = field_name
+        self.cache_key = rendered_cache_key
+
+    def _get_raw(self):
+        return self.instance.__dict__[self.field_name]
+
+    def _set_raw(self, value):
+        setattr(self.instance, self.field_name, value)
+
+    raw = property(_get_raw, _set_raw)
+
+    def _get_rendered(self):
+        rendered = cache.get(self.cache_key)
+
+        if not rendered:
+            logger.debug(u'Caching rendered output of %r', self.cache_key)
+            rendered = apply_markup_filter(self.raw)
+            cache.set(self.cache_key, rendered,
+                      settings.OBJECT_CACHE_TIMEOUT)
+
+        return rendered
+
+    rendered = property(_get_rendered)
+
+    def __unicode__(self):
+        return mark_safe(self.rendered)
+
+    def __nonzero__(self):
+        return self.raw.strip() != '' and self.raw is not None
+
+
+class MarkupDescriptor(object):
+
+    def __init__(self, field):
+        self.field = field
+
+    def __get__(self, obj, owner):
+        if obj is None:
+            raise AttributeError('Can only be accessed via an instance.')
+
+        markup = obj.__dict__[self.field.name]
+        if markup is None:
+            return None
+
+        cache_key = _rendered_cache_key(obj.__class__.__name__,
+                                        obj.pk,
+                                        self.field.name)
+        return Markup(obj, self.field.name, cache_key)
+
+    def __set__(self, obj, value):
+        if isinstance(value, Markup):
+            obj.__dict__[self.field.name] = value.raw
+        else:
+            obj.__dict__[self.field.name] = value
+
+
+class MarkupField(models.TextField):
+
+    description = 'Text field supporting different markup formats.'
+
+    def contribute_to_class(self, cls, name):
+        super(MarkupField, self).contribute_to_class(cls, name)
+        setattr(cls, self.name, MarkupDescriptor(self))
+
+    def pre_save(self, model_instance, add):
+        value = super(MarkupField, self).pre_save(model_instance, add)
+
+        if not add:
+            # Invalidate cache to force rendering upon next retrieval
+            cache_key = _rendered_cache_key(model_instance.__class__.__name__,
+                                            model_instance.pk,
+                                            self.name)
+            logger.debug('Invalidating cache for %r', cache_key)
+            cache.delete(cache_key)
+
+        return value.raw
+
+    def get_prep_value(self, value):
+        if isinstance(value, Markup):
+            return value.raw
+
+        return value
+
+    def value_to_string(self, obj):
+        value = self._get_val_from_obj(obj)
+        return self.get_prep_value(value)
+
+    def formfield(self, **kwargs):
+        defaults = {'widget': MarkupTextarea}
+        defaults.update(kwargs)
+        return super(MarkupField, self).formfield(**defaults)
+
+
+add_introspection_rules(
+        [],
+        ["^pootle\.core\.markup\.fields\.MarkupField"],
+    )
---
+++ pootle/core/markup/widgets.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2013 Zuza Software Foundation
+#
+# This file is part of Pootle.
+#
+# translate is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# translate is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with translate; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+from django import forms
+
+
+__all__ = ('MarkupTextarea',)
+
+
+class MarkupTextarea(forms.widgets.Textarea):
+
+    def render(self, name, value, attrs=None):
+        if value is not None and not isinstance(value, unicode):
+            value = value.raw
+
+        return super(MarkupTextarea, self).render(name, value, attrs)
