ba6d1dda041569a98b3f035359fc2ab73f435641
Julen Ruiz Aizpuru <julenx@gmail.com>
2013-05-13 12:39:26 +0200

    Redirecto to the full path, which includes script_name.

--- pootle/apps/pootle_misc/baseurl.py
+++ pootle/apps/pootle_misc/baseurl.py
@@ -65,7 +65,7 @@ def get_next(request):
         next = request.GET.get(REDIRECT_FIELD_NAME, '')
 
         if not next:
-            next = request.path_info
+            next = request.path
     except AttributeError, e:
         next = ''
 
