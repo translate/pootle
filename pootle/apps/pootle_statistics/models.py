5e5ed59f0072ddc9aef12eba9993ed2bb5b2188f
Julen Ruiz Aizpuru <julenx@gmail.com>
2013-08-15 09:36:07 +0200

    Point to the editor URL

--- pootle/apps/pootle_statistics/models.py
+++ pootle/apps/pootle_statistics/models.py
@@ -130,7 +130,7 @@ class Submission(models.Model):
         if self.unit is not None:
             unit = {
                 'source': escape(truncatechars(self.unit, 50)),
-                'url': self.unit.get_absolute_url(),
+                'url': self.unit.get_translate_url(),
             }
 
         action_bundle = {
