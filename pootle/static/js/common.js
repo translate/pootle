40448366eacc1a5b2fa230d289a8f4c75c9754a4
Igor Afanasyev <igor.afanasyev@gmail.com>
2013-08-24 17:08:24 +0400

    Hide the entire breadcrumbs block by default; show it on page load
    This prevents separators between select2 controls from being shown first
    
    Conflicts:
    	pootle/static/css/style.css

--- pootle/static/js/common.js
+++ pootle/static/js/common.js
@@ -462,6 +462,9 @@ $(function ($) {
   $(".js-select2").select2({
     width: "resolve"
   });
+
+  $(".breadcrumb").css("visibility", "visible");
+
   // Hide the help messages for the Select2 multiple selects.
   $("select[multiple].js-select2").siblings("span.help_text").hide();
 
