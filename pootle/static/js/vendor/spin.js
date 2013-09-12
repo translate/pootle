66890446eaf4ebb2ed2d2c7a62d5b5a2bb6b7128
Igor Afanasyev <igor.afanasyev@gmail.com>
2013-09-12 15:52:27 +0400

    Move spinner defaults to layout.html

--- pootle/static/js/vendor/spin.js
+++ pootle/static/js/vendor/spin.js
@@ -140,16 +140,16 @@
   // Built-in defaults
 
   var defaults = {
-    lines: 11,            // The number of lines to draw
-    length: 2,            // The length of each line
+    lines: 12,            // The number of lines to draw
+    length: 7,            // The length of each line
     width: 5,             // The line thickness
-    radius: 11,           // The radius of the inner circle
+    radius: 10,           // The radius of the inner circle
     rotate: 0,            // Rotation offset
     corners: 1,           // Roundness (0..1)
     color: '#000',        // #rgb or #rrggbb
     direction: 1,         // 1: clockwise, -1: counterclockwise
     speed: 1,             // Rounds per second
-    trail: 50,            // Afterglow percentage
+    trail: 100,           // Afterglow percentage
     opacity: 1/4,         // Opacity of the lines
     fps: 20,              // Frames per second when using setTimeout()
     zIndex: 2e9,          // Use a high z-index by default
