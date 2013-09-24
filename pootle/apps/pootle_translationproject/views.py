da922d56904c34545a5a7109bc3746866d6eb083
Julen Ruiz Aizpuru <julenx@gmail.com>
2013-09-24 12:51:13 +0200

    Simplified code, use `get_for_path()`
    Conflicts:
    	pootle/apps/pootle_translationproject/views.py

--- pootle/apps/pootle_translationproject/views.py
+++ pootle/apps/pootle_translationproject/views.py
@@ -62,7 +62,7 @@ from pootle_misc.stats import get_path_summary
 from pootle_misc.util import jsonify, ajax_required
 from pootle_profile.models import get_profile
 from pootle_statistics.models import Submission, SubmissionTypes
-from pootle_store.models import Store
+from pootle_store.models import Store, Unit
 from pootle_store.util import (absolute_real_path, relative_real_path,
                                add_trailing_slash)
 from pootle_store.filetypes import factory_classes
@@ -648,22 +648,13 @@ def translate(request, translation_project, dir_path, filename):
 
 @get_path_obj
 @permission_required('view')
+@get_resource_context
 def export_view(request, translation_project, dir_path, filename=None):
     """Displays a list of units with filters applied."""
-    current_path = translation_project.directory.pootle_path + dir_path
-
-    if filename:
-        current_path = current_path + filename
-        store = get_object_or_404(Store, pootle_path=current_path)
-        units_qs = store.units
-    else:
-        store = None
-        units_qs = translation_project.units.filter(
-            store__pootle_path__startswith=current_path,
-        )
-
     filter_name, filter_extra = get_filter_name(request.GET)
 
+    units_qs = Unit.objects.get_for_path(request.pootle_path,
+                                         request.profile)
     units = get_step_query(request, units_qs)
     unit_groups = [(path, list(units)) for path, units in
                    groupby(units, lambda x: x.store.path)]
