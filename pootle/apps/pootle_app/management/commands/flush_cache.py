34b5df43f75fdbe9ef10c893b24c5edb11137cb9
Taras Semenenko <tsemenenko@evernote.com>
2013-10-02 00:11:23 -0700

    Add flush_cache PootleCommand

---
+++ pootle/apps/pootle_app/management/commands/flush_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2009 Zuza Software Foundation
+# Copyright 2013 Evernote Corporation
+#
+# This file is part of Pootle.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'pootle.settings'
+
+from pootle_app.management.commands import PootleCommand
+
+class Command(PootleCommand):
+    help = "Flush stats cache."
+
+    def handle_all_stores(self, translation_project, **options):
+        translation_project.flush_cache()
+
+    def handle_store(self, store, **options):
+        store.flush_cache()
+
