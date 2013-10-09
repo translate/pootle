translate/pootle

commit 5088f1fdf5e3d77cbf811d159f172942e031d095

Unified single- and multi-file URLs
Fixes bug 3015

pootle/apps/pootle_profile/models.py
@@ -30,7 +30,6 @@ from django.utils.translation import ugettext_lazy as _
 
 from pootle.i18n.override import lang_choices
 from pootle_language.models import Language
-from pootle_misc import dispatch
 from pootle_misc.baseurl import l
 from pootle_misc.util import cached_property
 from pootle_statistics.models import Submission, SubmissionTypes
@@ -260,36 +259,36 @@ class PootleProfile(models.Model):
                     {
                         'id': 'suggestions-pending',
                         'count': self.pending_suggestion_count(tp),
-                        'url': dispatch.translate(tp, 'user-suggestions',
-                                                  user=username),
+                        'url': tp.get_translate_url('user-suggestions',
+                                                    user=username),
                     },
                     {
                         'id': 'suggestions-accepted',
                         'count': self.accepted_suggestion_count(tp),
-                        'url': dispatch.translate(
-                            tp, 'user-suggestions-accepted',
+                        'url': tp.get_translate_url(
+                            'user-suggestions-accepted',
                             user=username,
                         ),
                     },
                     {
                         'id': 'suggestions-rejected',
                         'count': self.rejected_suggestion_count(tp),
-                        'url': dispatch.translate(
-                            tp, 'user-suggestions-rejected',
+                        'url': tp.get_translate_url(
+                            'user-suggestions-rejected',
                             user=username,
                         ),
                     },
                     {
                         'id': 'submissions-total',
                         'count': self.total_submission_count(tp),
-                        'url': dispatch.translate(tp, 'user-submissions',
-                                                  user=username),
+                        'url': tp.get_translate_url('user-submissions',
+                                                      user=username),
                     },
                     {
                         'id': 'submissions-overwritten',
                         'count': self.overwritten_submission_count(tp),
-                        'url': dispatch.translate(
-                            tp, 'user-submissions-overwritten',
+                        'url': tp.get_translate_url(
+                            'user-submissions-overwritten',
                             user=username,
                         ),
                     },

pootle/apps/pootle_language/models.py
@@ -19,6 +19,7 @@
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 from django.core.cache import cache
+from django.core.urlresolvers import reverse
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 

pootle/apps/pootle_store/models.py
@@ -30,6 +30,7 @@ from translate.storage import base
 from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.files.storage import FileSystemStorage
+from django.core.urlresolvers import reverse
 from django.db import models, DatabaseError, IntegrityError
 from django.db.models.signals import post_delete
 from django.db.transaction import commit_on_success
@@ -38,6 +39,7 @@ from django.utils.translation import ugettext_lazy as _
 
 from taggit.managers import TaggableManager
 
+from pootle.core.url_helpers import get_editor_filter, split_pootle_path
 from pootle_app.lib.util import RelatedManager
 from pootle_misc.aggregate import group_by_count_extra, max_column
 from pootle_misc.baseurl import l
@@ -309,6 +311,13 @@ class Unit(models.Model, base.TranslationUnit):
     def get_absolute_url(self):
         return u"%s/translate/#unit=%s" % (l(self.store.pootle_path),
                                            self.id)
+
+    def get_translate_url(self):
+        lang, proj, dir, fn = split_pootle_path(self.store.pootle_path)
+        return u''.join([
+            reverse('pootle-tp-translate', args=[lang, proj, dir, fn]),
+            '#unit=', unicode(self.id),
+        ])
 
     def get_mtime(self):
         return self.mtime
@@ -881,6 +890,13 @@ class Store(models.Model, base.TranslationStore):
 
     def get_absolute_url(self):
         return l(self.pootle_path + '/translate/')
+
+    def get_translate_url(self, **kwargs):
+        lang, proj, dir, fn = split_pootle_path(self.pootle_path)
+        return u''.join([
+            reverse('pootle-tp-translate', args=[lang, proj, dir, fn]),
+            get_editor_filter(**kwargs),
+        ])
 
     def delete(self, *args, **kwargs):
         super(Store, self).delete(*args, **kwargs)

pootle/apps/pootle_translationproject/models.py
@@ -26,6 +26,7 @@ from translate.storage.base import ParseError
 
 from django.conf import settings
 from django.contrib import messages
+from django.core.urlresolvers import reverse
 from django.db import models, IntegrityError
 from django.db.models.signals import post_save
 from django.utils.encoding import force_unicode
@@ -34,6 +35,7 @@ from django.utils.translation import ugettext_lazy as _
 from taggit.managers import TaggableManager
 
 from pootle.core.markup import get_markup_filter_name, MarkupField
+from pootle.core.url_helpers import get_editor_filter, split_pootle_path
 from pootle_app.lib.util import RelatedManager
 from pootle_app.models.directory import Directory
 from pootle_language.models import Language
@@ -148,6 +150,13 @@ class TranslationProject(models.Model):
 
     def get_absolute_url(self):
         return l(self.pootle_path)
+
+    def get_translate_url(self, **kwargs):
+        lang, proj, dir, fn = split_pootle_path(self.pootle_path)
+        return u''.join([
+            reverse('pootle-tp-translate', args=[lang, proj, dir, fn]),
+            get_editor_filter(**kwargs),
+        ])
 
     def natural_key(self):
         return (self.pootle_path,)

pootle/apps/pootle_app/models/directory.py
@@ -18,7 +18,9 @@
 # Pootle; if not, see <http://www.gnu.org/licenses/>.
 
 from django.db import models
+from django.core.urlresolvers import reverse
 
+from pootle.core.url_helpers import get_editor_filter, split_pootle_path
 from pootle_misc.aggregate import max_column
 from pootle_misc.baseurl import l
 from pootle_misc.util import cached_property, dictsum, getfromcache
@@ -70,8 +72,12 @@ class Directory(models.Model):
 
         super(Directory, self).save(*args, **kwargs)
 
-    def get_absolute_url(self):
-        return l(self.pootle_path)
+    def get_translate_url(self, **kwargs):
+        lang, proj, dir, fn = split_pootle_path(self.pootle_path)
+        return u''.join([
+            reverse('pootle-tp-translate', args=[lang, proj, dir, fn]),
+            get_editor_filter(**kwargs),
+        ])
 
     def get_relative(self, path):
         """Given a path of the form a/b/c, where the path is relative

pootle/apps/pootle_app/views/language/urls.py
@@ -22,10 +22,6 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('pootle_app.views.language.view',
-    # Translation
-    url(r'^(?P<language_code>[^/]*)/(?P<project_code>[^/]*)/(?P<dir_path>(.*/)*)translate.html$',
-        'translate', name='translate-wide'),
-
     # XHR
     (r'^(?P<language_code>[^/]*)/(?P<project_code>[^/]*)/(?P<dir_path>(.*/)*)checks.html$',
         'get_failing_checks_dir'),

pootle/apps/pootle_app/views/language/view.py
@@ -28,20 +28,7 @@ from pootle_app.models.directory import Directory
 from pootle_app.models.permissions import check_permission
 from pootle_misc.baseurl import redirect
 from pootle_store.models import Store
-from pootle_store.views import (translate_page, get_failing_checks,
-                                get_view_units)
-
-
-@get_translation_project
-@set_request_context
-def translate(request, translation_project, dir_path=None):
-    if dir_path:
-        pootle_path = translation_project.pootle_path + dir_path
-        request.directory = Directory.objects.get(pootle_path=pootle_path)
-    else:
-        request.directory = translation_project.directory
-
-    return translate_page(request)
+from pootle_store.views import get_failing_checks, get_view_units
 
 
 @get_translation_project

pootle/apps/pootle_language/views.py
@@ -30,7 +30,6 @@ from pootle_app.models.permissions import (get_matching_permissions,
 from pootle_app.views.admin.permissions import admin_permissions
 from pootle_app.views.top_stats import gentopstats_language
 from pootle_language.models import Language
-from pootle_misc import dispatch
 from pootle_misc.browser import get_table_headings
 from pootle_misc.stats import (get_raw_stats, stats_descriptions)
 from pootle_misc.util import nice_percentage, jsonify, ajax_required
@@ -49,8 +48,8 @@ def get_last_action(translation_project):
 def make_project_item(translation_project):
     project = translation_project.project
     href = translation_project.pootle_path
-    href_all = dispatch.translate(translation_project)
-    href_todo = dispatch.translate(translation_project, state='incomplete')
+    href_all = translation_project.get_translate_url()
+    href_todo = translation_project.get_translate_url(state='incomplete')
 
     project_stats = get_raw_stats(translation_project)
 

commit c98215e6e4a15433fd02f721b6c560042fa04c7f

Fix calls to get_translate_url()
It does not take any non-keyword arguments. It was broken in
5088f1fdf5e3d77cbf811d159f172942e031d095.

pootle/apps/pootle_profile/models.py
@@ -182,14 +182,14 @@ class PootleProfile(models.Model):
                     {
                         'id': 'suggestions-pending',
                         'count': self.pending_suggestion_count(tp),
-                        'url': tp.get_translate_url('user-suggestions',
+                        'url': tp.get_translate_url(state='user-suggestions',
                                                     user=username),
                     },
                     {
                         'id': 'suggestions-accepted',
                         'count': self.accepted_suggestion_count(tp),
                         'url': tp.get_translate_url(
-                            'user-suggestions-accepted',
+                            state='user-suggestions-accepted',
                             user=username,
                         ),
                     },
@@ -197,21 +197,21 @@ class PootleProfile(models.Model):
                         'id': 'suggestions-rejected',
                         'count': self.rejected_suggestion_count(tp),
                         'url': tp.get_translate_url(
-                            'user-suggestions-rejected',
+                            state='user-suggestions-rejected',
                             user=username,
                         ),
                     },
                     {
                         'id': 'submissions-total',
                         'count': self.total_submission_count(tp),
-                        'url': tp.get_translate_url('user-submissions',
-                                                      user=username),
+                        'url': tp.get_translate_url(state='user-submissions',
+                                                    user=username),
                     },
                     {
                         'id': 'submissions-overwritten',
                         'count': self.overwritten_submission_count(tp),
                         'url': tp.get_translate_url(
-                            'user-submissions-overwritten',
+                            state='user-submissions-overwritten',
                             user=username,
                         ),
                     },