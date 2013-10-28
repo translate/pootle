8dda404677d156e396fd2d04f745fd5124df8f61
Taras Semenenko <tsemenenko@evernote.com>
2013-10-28 20:28:30 +0400

    Implement optimized rest of refresh stats machinary

--- pootle/apps/pootle_app/management/commands/refresh_all_stats.py
+++ pootle/apps/pootle_app/management/commands/refresh_all_stats.py
@@ -29,12 +29,15 @@ import datetime
 #from django.core.management.base import NoArgsCommand
 from pootle_app.management.commands import PootleCommand
 
-from django.db.models import Max
+from django.db.models import Count, Max, Sum
 from django.utils.encoding import iri_to_uri
 from django.core.cache import cache
 from django.conf import settings
 
 from pootle_statistics.models import Submission
+from pootle_language.models import Language
+from pootle_store.models import Store, Unit, QualityCheck, Suggestion
+from pootle_store.util import OBSOLETE, UNTRANSLATED, FUZZY, TRANSLATED
 
 
 class Command(PootleCommand):
@@ -58,36 +61,120 @@ class Command(PootleCommand):
         # info start
         start = datetime.datetime.now()
         logging.info('Start running of %s', self.name)
-        timeout=settings.OBJECT_CACHE_TIMEOUT
+        timeout = settings.OBJECT_CACHE_TIMEOUT
 
-        ss = Submission.simple_objects.values('unit__store__pootle_path').annotate(max_id=Max('id'))
+        self._set_last_action_stats(timeout)
+        self._set_wordcount_stats(timeout)
+        self._set_qualitycheck_stats(timeout)
+        self._set_suggestion_stats(timeout)
+
+        logging.info('Refreshing directories stats...')
+
+        lang_query = Language.objects.all()
+        for lang in lang_query:
+            lang.get_stats()
+            lang.get_checks()
+
+        # info finish
+        end = datetime.datetime.now()
+        logging.info('All done for %s in %s', self.name, end - start)
+
+    def _set_qualitycheck_stats_cache(self, stats, key, timeout):
+        if key:
+            logging.info('Set get_checks for %s' % key)
+            cache.set(key + ':get_checks', stats, timeout)
+
+    def _set_qualitycheck_stats(self, timeout):
+        queryset = QualityCheck.objects.filter(unit__state__gt=UNTRANSLATED,
+                                               false_positive=False) \
+                                       .values('unit__store', 'name') \
+                                       .annotate(count=Count('name')) \
+                                       .order_by('unit__store')
+
+        saved = None
+        key = None
+        stats = {}
+
+        for item in queryset:
+            if item['unit__store'] != saved:
+                key = Store.objects.get(id=item['unit__store']).get_cachekey()
+                if saved:
+                    self._set_qualitycheck_stats_cache(stats, key, timeout)
+
+                saved = item['unit__store']
+                stats = {}
+
+            stats[item['name']] = item['count']
+
+        if saved:
+            self._set_qualitycheck_stats_cache(stats, key, timeout)
+
+    def _set_wordcount_stats_cache(self, stats, key, timeout):
+        if key:
+            logging.info('Set wordcount stats for %s' % key)
+            cache.set(key + ':get_total_wordcount', stats['total'], timeout)
+            cache.set(key + ':get_fuzzy_wordcount', stats[FUZZY], timeout)
+            cache.set(key + ':get_translated_wordcount', stats[TRANSLATED], timeout)
+
+    def _set_wordcount_stats(self, timeout):
+        res = Unit.objects.filter(state__gt=OBSOLETE) \
+                            .values('store', 'state') \
+                            .annotate(wordcount=Sum('source_wordcount')) \
+                            .order_by('store', 'state')
+
+        saved = None
+        stats = None
+        key = None
+
+        for item in res:
+            if saved != item['store']:
+                key = Store.objects.get(id=item['store']).get_cachekey()
+                if saved:
+                    self._set_wordcount_stats_cache(stats, key, timeout)
+
+                stats = {'total': 0, FUZZY: 0, TRANSLATED: 0}
+                saved = item['store']
+
+            stats['total'] += item['wordcount']
+
+            if item['state'] in [FUZZY, TRANSLATED]:
+                stats[item['state']] = item['wordcount']
+
+        if saved:
+            self._set_wordcount_stats_cache(stats, key, timeout)
+
+    def _set_last_action_stats(self, timeout):
+        empty_last_action = {'id': 0 , 'mtime': 0, 'snippet': ''}
+        stores = Store.objects.all()
+        for store in stores:
+            key = iri_to_uri(store.get_cachekey() + ':get_last_action')
+            cache.set(key, empty_last_action, timeout)
+        logging.info('Empty last action was set for all stores.')
+
+        ss = Submission.simple_objects.values('unit__store__pootle_path') \
+                                      .annotate(max_id=Max('id'))
         for s_id in ss:
-            sub = Submission.objects.select_related('unit__store').get(id=s_id['max_id'])
+            sub = Submission.objects.select_related('unit__store') \
+                                    .get(id=s_id['max_id'])
             if sub.unit:
-                key = iri_to_uri(sub.unit.store.get_cachekey() + ":" + 'get_last_action')
-                logging.info(key)
+                key = iri_to_uri(sub.unit.store.get_cachekey() +
+                                 ':get_last_action')
+                logging.info('Set last action stats for %s' % key)
                 res = {
                     'mtime': int(time.mktime(sub.unit.mtime.timetuple())),
                     'snippet': sub.get_submission_message()
                 }
                 cache.set(key, res, timeout)
 
-        # info finish
-        end = datetime.datetime.now()
-        logging.info('All done for %s in %s', self.name, end - start)
+        logging.info('All last actions were calculated.')
 
+    def _set_suggestion_stats(self, timeout):
+        """Check if any unit in the store has suggestions"""
+        queryset = Suggestion.objects.filter(unit__state__gt=OBSOLETE) \
+                                     .values('unit__store') \
+                                     .annotate(count=Count('id'))
 
-    def get_total_wordcount(self):
-        pass
-    def get_translated_wordcount(self):
-        pass
-    def get_fuzzy_wordcount(self):
-        pass
-    def get_suggestion_count(self):
-        pass
-    def get_last_action(self):
-        pass
-    def get_mtime(self):
-        pass
-    def get_checks(self):
-        pass
+        for item in queryset:
+            key = Store.objects.get(id=item['unit__store']).get_cachekey()
+            logging.info('Set suggestion count for %s' % key)
+            cache.set(key + ':get_suggestion_count', item['count'], timeout)
