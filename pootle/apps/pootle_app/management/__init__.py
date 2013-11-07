7b2c5896c80cdab697f8222030641c601fdd6c3b
Leandro Regueiro <leandro.regueiro@gmail.com>
2013-11-07 12:06:06 +0100

    Bugfix: Should be POOTLE_BUILDVERSION not BUILDVERSION

--- pootle/apps/pootle_app/management/__init__.py
+++ pootle/apps/pootle_app/management/__init__.py
@@ -245,8 +245,8 @@ def post_syncdb_handler(sender, created_models, **kwargs):
         create_pootle_permission_sets()
 
     config = siteconfig.load_site_config()
-    if not config.get('BUILDVERSION', None):
-        config.set('BUILDVERSION', code_buildversion)
+    if not config.get('POOTLE_BUILDVERSION', None):
+        config.set('POOTLE_BUILDVERSION', code_buildversion)
     if not config.get('TT_BUILDVERSION', None):
         config.set('TT_BUILDVERSION', code_tt_buildversion)
     config.save()
