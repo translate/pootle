f6eda611146114ac5e64c173f303ce82ac9bfd11
Leandro Regueiro <leandro.regueiro@gmail.com>
2013-11-07 10:49:12 +0100

    Partially revert last staticpages migrations hack
    Now it should be possible to deploy from scratch and upgrade an existing
    installation without any problem.

--- pootle/apps/staticpages/migrations/0001_initial.py
+++ pootle/apps/staticpages/migrations/0001_initial.py
@@ -2,36 +2,24 @@
 import datetime
 from south.db import db
 from south.v2 import SchemaMigration
-from django.db import connection, models
+from django.db import models
 
 
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
         # Adding model 'LegalPage'
-        if 'staticpages_legalpage' not in connection.introspection.table_names():
-            if 'legalpages_legalpage' in connection.introspection.table_names():
-                # Migrate to the new app name, including content types
-                db.rename_table('legalpages_legalpage', 'staticpages_legalpage')
-
-                if not db.dry_run:
-                    content_types = orm['contenttypes.ContentType'].objects \
-                    .filter(app_label='legalpages')
-                    content_types.update(app_label='staticpages')
-            else:
-                # Tables are not there, let's create them for the first time
-                db.create_table('staticpages_legalpage', (
-                    ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-                    ('active', self.gf('django.db.models.fields.BooleanField')(default=False)),
-                    ('display_on_register', self.gf('django.db.models.fields.BooleanField')(default=False)),
-                    ('title', self.gf('django.db.models.fields.CharField')(max_length=100)),
-                    ('url', self.gf('django.db.models.fields.URLField')(max_length=200, blank=True)),
-                    ('slug', self.gf('django.db.models.fields.SlugField')(max_length=50)),
-                    ('body', self.gf('django.db.models.fields.TextField')(blank=True)),
-                    ('body_html', self.gf('django.db.models.fields.TextField')(blank=True)),
-                ))
-
-                db.send_create_signal('staticpages', ['LegalPage'])
+        db.create_table('staticpages_legalpage', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('active', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('display_on_register', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('url', self.gf('django.db.models.fields.URLField')(max_length=200, blank=True)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=50)),
+            ('body', self.gf('django.db.models.fields.TextField')(blank=True)),
+            ('body_html', self.gf('django.db.models.fields.TextField')(blank=True)),
+        ))
+        db.send_create_signal('staticpages', ['LegalPage'])
 
 
     def backwards(self, orm):
