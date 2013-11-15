698aad24054a50c75b89fd1222c874f1123eb109
Taras Semenenko <tsemenenko@evernote.com>
2013-11-15 15:20:45 +0400

    Fix evernote/pootle#36. Show 100% for total and translated if total == 0
    Conflicts:
    	pootle/static/js/stats.js

--- pootle/static/js/stats.js
+++ pootle/static/js/stats.js
@@ -10,8 +10,8 @@
       $(document).on("click", "#js-path-summary", PTL.stats.toggleChecks);
     },
 
-    nicePercentage: function (part, total) {
-      var percentage = total ? part / total * 100 : 0;
+    nicePercentage: function (part, total, no_total_default) {
+      var percentage = total ? part / total * 100 : no_total_default;
       if (99 < percentage && percentage < 100) {
         return 99;
       }
@@ -27,8 +27,8 @@
         return;
       }
 
-      var translated = PTL.stats.nicePercentage(item.translated, item.total),
-          fuzzy = PTL.stats.nicePercentage(item.fuzzy, item.total),
+      var translated = PTL.stats.nicePercentage(item.translated, item.total, 100),
+          fuzzy = PTL.stats.nicePercentage(item.fuzzy, item.total, 0),
           untranslated = 100 - translated - fuzzy,
           $legend = $('<span>').html($td.find('script').text());
 
@@ -116,10 +116,10 @@
       $summary.append(interpolate(gettext(', %s% translated'), [percent]));
     },
 
-    updateTranslationStats: function ($tr, total, value) {
+    updateTranslationStats: function ($tr, total, value, no_total_default) {
       $tr.find('.stats-number a').html(value);
       $tr.find('.stats-percentage span').html(
-        PTL.stats.nicePercentage(value, total)
+        PTL.stats.nicePercentage(value, total, no_total_default)
       );
       $tr.find('.stats-percentage').show();
     },
@@ -166,14 +166,14 @@
           $('body').removeClass('js-not-loaded');
 
           PTL.stats.updateTranslationStats($('#stats-total'),
-                                           data.total, data.total);
+                                           data.total, data.total, 100);
           PTL.stats.updateTranslationStats($('#stats-translated'),
-                                           data.total, data.translated);
+                                           data.total, data.translated, 100);
           PTL.stats.updateTranslationStats($('#stats-fuzzy'),
-                                           data.total, data.fuzzy);
+                                           data.total, data.fuzzy, 0);
           var untranslated = data.total - data.translated - data.fuzzy;
           PTL.stats.updateTranslationStats($('#stats-untranslated'),
-                                           data.total, untranslated);
+                                           data.total, untranslated, 0);
 
           if ($table.length) {
             for (var name in data.children) {
