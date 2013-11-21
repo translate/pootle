5353ce18c00bcf6a16e6300bc4c47e3ec08d3af4
Taras Semenenko <tsemenenko@evernote.com>
2013-11-21 17:07:52 +0400

    Hide last updates rows if there is nothing to show. Fix evernote/pootle#61.

--- pootle/static/js/stats.js
+++ pootle/static/js/stats.js
@@ -142,8 +142,14 @@
     },
 
     updateLastUpdates: function (stats) {
-        $('#js-last-updated .last-updated').html(stats.lastupdated.snippet);
-        $('#js-last-action .last-action').html(stats.lastaction.snippet);
+        $('#js-last-updated').toggle(stats.lastupdated.snippet != '');
+        if (stats.lastupdated.snippet) {
+          $('#js-last-updated .last-updated').html(stats.lastupdated.snippet);
+        }
+        $('#js-last-action').toggle(stats.lastaction.snippet != '');
+        if (stats.lastaction.snippet) {
+          $('#js-last-action .last-action').html(stats.lastaction.snippet);
+        }
     },
 
     load: function (callback) {
