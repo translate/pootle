translate/pootle

commit 394b34571181f894c988b4174e2134257b88dfd0

Show the number of units, not the total number of checks (fix #66)

pootle/apps/pootle_app/management/commands/refresh_stats.py
@@ -135,27 +135,32 @@ class Command(PootleCommand):
     def _set_qualitycheck_stats(self, timeout):
         queryset = QualityCheck.objects.filter(unit__state__gt=UNTRANSLATED,
                                                false_positive=False) \
-                                       .values('unit__store', 'name') \
-                                       .annotate(count=Count('name')) \
-                                       .order_by('unit__store')
+                                       .values('unit', 'unit__store', 'name') \
+                                       .order_by('unit__store', 'unit')
 
-        saved = None
-        key = None
-        stats = {}
+        saved_store = None
+        saved_unit = None
+        stats = None
 
         for item in queryset.iterator():
-            if item['unit__store'] != saved:
+            if item['unit__store'] != saved_store:
                 key = Store.objects.get(id=item['unit__store']).get_cachekey()
-                if saved:
-                    self._set_qualitycheck_stats_cache(stats, key, timeout)
+                saved_store = item['unit__store']
+                stats = self.cache_values[key]['get_checks']['checks']
 
-                saved = item['unit__store']
-                stats = {}
+            if item['name'] in stats['checks']:
+                stats['checks'][item['name']] += 1
+            else:
+                stats['checks'][item['name']] = 1
 
-            stats[item['name']] = item['count']
+            if saved_unit != item['unit']:
+                saved_unit = item['unit']
+                stats['unit_count'] += 1
 
-        if saved and saved != item['unit__store']:
-            self._set_qualitycheck_stats_cache(stats, key, timeout)
+        for key in self.cache_values:
+            stats = self.cache_values['get_checks']
+            if stats['unit_count'] > 0:
+                self._set_qualitycheck_stats_cache(stats, key, timeout)
 
     def _set_wordcount_stats_cache(self, stats, key, timeout):
         if key:
@@ -205,7 +210,7 @@ class Command(PootleCommand):
             self.cache_values[store.get_cachekey()] = {
                 'get_last_action': {'id': 0, 'mtime': 0, 'snippet': ''},
                 'get_suggestion_count': 0,
-                'get_checks': {},
+                'get_checks': {'unit_count': 0, 'checks': {}},
                 'get_total_wordcount': 0,
                 'get_translated_wordcount': 0,
                 'get_fuzzy_wordcount': 0,

pootle/apps/pootle_store/models.py
@@ -1700,7 +1700,22 @@ class Store(models.Model, TreeItem, base.TranslationStore):
             queryset = QualityCheck.objects.filter(unit__store=self,
                                                    unit__state__gt=UNTRANSLATED,
                                                    false_positive=False)
-            return group_by_count(queryset, 'name')
+
+            queryset = queryset.values('unit', 'name').order_by('unit')
+
+            saved_unit = None
+            result = {'unit_count': 0, 'checks': {}}
+            for item in queryset:
+                if item['unit'] != saved_unit or saved_unit is None:
+                    saved_unit = item['unit']
+                    # assumed all checks are critical and should be counted
+                    result['unit_count'] += 1
+                if item['name'] in result['checks']:
+                    result['checks'][item['name']] += 1
+                else:
+                    result['checks'][item['name']] = 1
+
+            return result
         except Exception as e:
             logging.info(u"Error getting quality checks for %s\n%s",
                          self.name, e)

pootle/apps/pootle_store/views.py
@@ -631,7 +631,7 @@ def get_edit_unit(request, unit):
 def get_qualitycheck_stats(request, path_obj, **kwargs):
     qc_stats = request.resource_obj.get_checks()
 
-    return HttpResponse(jsonify(qc_stats), mimetype="application/json")
+    return HttpResponse(jsonify(qc_stats['checks']), mimetype="application/json")
 
 
 @ajax_required

pootle/core/mixins/treeitem.py
@@ -97,7 +97,7 @@ class TreeItem(object):
 
     def _get_checks(self):
         """This method will be overridden in descendants"""
-        return {}
+        return {'unit_count': 0, 'checks': {}}
 
     def _get_last_action(self):
         """This method will be overridden in descendants"""
@@ -204,7 +204,7 @@ class TreeItem(object):
             'fuzzy': self.get_fuzzy_wordcount(),
             'suggestions': self.get_suggestion_count(),
             'lastaction': self.get_last_action(),
-            'critical': self.get_critical(),
+            'critical': self.get_error_unit_count(),
             'lastupdated': self.get_last_updated()
         }
 
@@ -240,7 +240,9 @@ class TreeItem(object):
         result = self._get_checks()
         self.initialize_children()
         for item in self.children:
-            result = dictsum(result, item.get_checks())
+            item_res = item.get_checks()
+            result['checks'] = dictsum(result['checks'], item_res['checks'])
+            result['unit_count'] += item_res['unit_count']
 
         return result
 
@@ -258,6 +260,11 @@ class TreeItem(object):
 
         return sum(map(lambda x: check_stats[x] if x in check_stats else 0,
                        get_qualitychecks_by_category(Category.CRITICAL)))
+
+    def get_error_unit_count(self):
+        check_stats = self.get_checks()
+
+        return check_stats['unit_count']
 
     def get_critical1(self):
         """Alter implementaion (pick up every check separately)"""

pootle/static/css/style.css
@@ -28,14 +28,14 @@
 
 /*
  * TEMPORARILY HIDE CRITICAL ERRORS
- */
+ *
 #action-fix-critical,
 td.stats-number.critical,
 #js-stats-checks,
 th#js-stats-critical
 {
     display: none !important;
-}
+}*/
 
 /*
  * BASIC ELEMENT STYLING

commit a43d2d0f67cf23685f0f0c80e54eb5cf8fa66dd4

Fix bug introduced in [394b3457]

pootle/apps/pootle_app/management/commands/refresh_stats.py
@@ -146,7 +146,7 @@ class Command(PootleCommand):
             if item['unit__store'] != saved_store:
                 key = Store.objects.get(id=item['unit__store']).get_cachekey()
                 saved_store = item['unit__store']
-                stats = self.cache_values[key]['get_checks']['checks']
+                stats = self.cache_values[key]['get_checks']
 
             if item['name'] in stats['checks']:
                 stats['checks'][item['name']] += 1
@@ -158,7 +158,7 @@ class Command(PootleCommand):
                 stats['unit_count'] += 1
 
         for key in self.cache_values:
-            stats = self.cache_values['get_checks']
+            stats = self.cache_values[key]['get_checks']
             if stats['unit_count'] > 0:
                 self._set_qualitycheck_stats_cache(stats, key, timeout)
 