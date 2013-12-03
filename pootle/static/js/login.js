cfadf598c8a583a52ccc8126310da06ab7180062
Julen Ruiz Aizpuru <julenx@gmail.com>
2013-12-03 16:14:08 +0100

    Set event handlers in JavaScript
    Also there's no need for extra asset files and removed global variables.

--- pootle/static/js/login.js
+++ pootle/static/js/login.js
@@ -1,12 +1,28 @@
-// Evernote JS file login.js
-function link_with_existing() {
-	$('.form.info').removeClass('hide');
-	$('.login').removeClass('hide');
-	$('.form.question').addClass('hide');
-	$("#id_username").focus();
-}
+(function ($) {
 
-function pootle_login() {
-	$('.login').removeClass('hide');
-	$("#id_username").focus();
-}
+  window.PTL = window.PTL || {};
+
+  PTL.login = {
+
+    enLink: function (e) {
+      e.preventDefault();
+
+      $('.form.info').removeClass('hide');
+      $('.login').removeClass('hide');
+      $('.form.question').addClass('hide');
+      $("#id_username").focus();
+    },
+
+    pootleLogin: function (e) {
+      e.preventDefault();
+
+      $('.login').removeClass('hide');
+      $("#id_username").focus();
+    }
+
+  };
+
+  $(document).on('click', '.js-en-auth-link', PTL.login.enLink);
+  $(document).on('click', '.js-en-auth-login', PTL.login.pootleLogin);
+
+}(jQuery));
