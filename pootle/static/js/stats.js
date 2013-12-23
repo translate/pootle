08255565516e4f60970921e156939c6bf8dafa24
Julen Ruiz Aizpuru <julenx@gmail.com>
2013-12-23 17:28:24 +0100

    Also replace any at-signs

--- pootle/static/js/stats.js
+++ pootle/static/js/stats.js
@@ -119,7 +119,7 @@
           if ($table.length) {
             for (var name in data.children) {
               var item = data.children[name],
-                  code = name.replace(/\./g, '-'),
+                  code = name.replace(/[\.@]/g, '-'),
                   $td = $table.find('#total-words-' + code);
 
               PTL.stats.updateItemStats($td, item.total);
