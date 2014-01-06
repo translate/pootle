b60b70e8e8cf63723ab62ce819998f8b0d27d6df
Peter Bengtsson <mail@peterbe.com>
2014-01-06 11:50:29 -0500

    Explicit import of ldap.filter
    `ldap.filter` is it's own module. `ldap` is a binary module and `ldap.filter`
    is a python package. So you need both.

--- pootle/core/auth/ldap_backend.py
+++ pootle/core/auth/ldap_backend.py
@@ -19,6 +19,7 @@
 # Pootle; if not, see <http://www.gnu.org/licenses/>.
 
 import ldap
+import ldap.filter  # It is necessary to explicitly import ldap.filter.
 import logging
 
 from django.conf import settings
