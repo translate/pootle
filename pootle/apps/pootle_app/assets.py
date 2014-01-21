ca181e108701d6aa36acff40ac6d3ed370bfa219
Julen Ruiz Aizpuru <julenx@gmail.com>
2014-01-21 09:57:03 +0100

    Cleanup: removed unused script

--- pootle/apps/pootle_app/assets.py
+++ pootle/apps/pootle_app/assets.py
@@ -53,7 +53,6 @@ js_editor = Bundle(
     'js/vendor/jquery/jquery.history.js',
     'js/vendor/jquery/jquery.textarea-expander.js',
     'js/vendor/diff_match_patch.js',
-    'js/vendor/jquery/jquery.fieldselection.js',
     'js/vendor/jquery/jquery.caret.js',
     'js/vendor/jquery/jquery.highlightRegex.js',
     'js/vendor/jquery/jquery.jsonp.js',
