691cad64e3af69a73a3d23e92a7b3899877b8464
Dwayne Bailey <dwayne@translate.org.za>
2014-01-24 14:31:44 +0000

    Update copyrights to 2014

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Pootle'
-copyright = u'2004-2013, Translate'
+copyright = u'2004-2014, Translate'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
