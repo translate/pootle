translate/pootle

commit 8907a57d3055bc5b00873094fa9b6f1e296ef95e

Try a faster get_last_action calculation method

pootle/apps/pootle_store/models.py
@@ -35,6 +35,7 @@ from django.core.urlresolvers import reverse
 from django.db import models, DatabaseError, IntegrityError
 from django.db.models.signals import post_delete, post_save, pre_delete
 from django.db.transaction import commit_on_success
+from django.db.models import Max
 from django.utils import timezone, tzinfo
 from django.utils.translation import ugettext_lazy as _
 
@@ -1741,7 +1742,10 @@ class Store(models.Model, TreeItem, base.TranslationStore):
     def _get_last_action(self, submission=None):
         if submission is None:
             try:
-                sub = Submission.objects.filter(unit__store=self).latest()
+                unit = Unit.objects.filter(store=self) \
+                    .annotate(max_time=models.Max('submitted_on'))[0]
+                sub = Submission.objects.get(unit=unit, creation_time=unit.max_time)
+
             except Submission.DoesNotExist:
                 return  {'id': 0, 'mtime': 0, 'snippet': ''}
         else:

commit a6974bd4cd41017ffe5024f2fb1d98c28014a4a7

Fix bug introduced by 8907a57

pootle/apps/pootle_store/models.py
@@ -1742,9 +1742,14 @@ class Store(models.Model, TreeItem, base.TranslationStore):
     def _get_last_action(self, submission=None):
         if submission is None:
             try:
-                unit = Unit.objects.filter(store=self) \
-                    .annotate(max_time=models.Max('submitted_on'))[0]
-                sub = Submission.objects.get(unit=unit, creation_time=unit.max_time)
+                units = Unit.objects.filter(store=self) \
+                    .annotate(max_time=models.Max('submitted_on'))
+                if len(units) > 0:
+                    sub = Submission.objects.filter(unit=units[0],
+                                                    creation_time=units[0].max_time) \
+                                            .latest()
+                else:
+                    raise Submission.DoesNotExist
 
             except Submission.DoesNotExist:
                 return  {'id': 0, 'mtime': 0, 'snippet': ''}