translate/pootle

commit 67587450e47fb0d9bd2753edc2a04c5b9f93951c

Use subprocess.Popen() instead of os.system()
https://bugzilla.mozilla.org/show_bug.cgi?id=913455

pootle/apps/pootle_translationproject/models.py
@@ -861,6 +861,7 @@ class TranslationProject(models.Model):
     def get_archive(self, stores, path=None):
         """Returns an archive of the given files."""
         import shutil
+        import subprocess
         from pootle_misc import ptempfile as tempfile
 
         tempzipfile = None
@@ -876,12 +877,9 @@ class TranslationProject(models.Model):
                 store.abs_real_path[len(self.abs_real_path)+1:] \
                 for store in stores.iterator()
             )
-            cmd = u"cd %(path)s ; zip -r - %(file_list)s > %(tmpfile)s" % {
-                    'path': self.abs_real_path,
-                    'file_list': file_list,
-                    'tmpfile': tempzipfile,
-            }
-            result = os.system(cmd.encode('utf-8'))
+            result = subprocess.Popen(['zip', '-r', file_list,
+                                       '--out', tempzipfile],
+                                      cwd=self.abs_real_path)
 
             if result == 0:
                 if path is not None:

commit 2bc49e00d7485dabb5659c8104d5224ec1cad451

Fix zip export
It was broken since 67587450e47fb0d9bd2753edc2a04c5b9f93951c;

* If zip command was not found, subprocess.Popen() will raise an
  exception and the fallback path will not be reached. The old code
  worked because os.system() will just return a non-zero return status.
* If zip command was found, the call will also fail because --out does
  not do what I probably thought it does!

pootle/apps/pootle_translationproject/models.py
@@ -817,6 +817,7 @@ class TranslationProject(models.Model, TreeItem):
         from pootle_misc import ptempfile as tempfile
 
         tempzipfile = None
+        archivecontents = None
 
         try:
             # Using zip command line is fast
@@ -824,14 +825,17 @@ class TranslationProject(models.Model, TreeItem):
             # security reasons
             fd, tempzipfile = tempfile.mkstemp(prefix='pootle', suffix='.zip')
             os.close(fd)
+            archivecontents = open(tempzipfile, "wb")
 
             file_list = u" ".join(
                 store.abs_real_path[len(self.abs_real_path)+1:] \
                 for store in stores.iterator()
             )
-            result = subprocess.Popen(['zip', '-r', file_list,
-                                       '--out', tempzipfile],
-                                      cwd=self.abs_real_path)
+            process = subprocess.Popen(['zip', '-r', '-', file_list],
+                                       cwd=self.abs_real_path,
+                                       stdout=archivecontents)
+            result = process.wait()
+            os.close(fd)
 
             if result == 0:
                 if path is not None:
@@ -841,18 +845,20 @@ class TranslationProject(models.Model, TreeItem):
                     filedata = open(tempzipfile, "r").read()
                     if filedata:
                         return filedata
-        finally:
-            if tempzipfile is not None and os.path.exists(tempzipfile):
-                os.remove(tempzipfile)
-
-        # But if it doesn't work, we can do it from python
-        archivecontents = None
-        try:
+                    else:
+                        raise Exception("failed to read temporary zip file")
+            else:
+                raise Exception("zip command returned error code: %d" % result)
+        except Exception as e:
+            # But if it doesn't work, we can do it from Python.
+            logging.debug(e)
+            logging.debug("falling back to zipfile module")
             if path is not None:
-                fd, tempzipfile = tempfile.mkstemp(prefix='pootle',
-                                                   suffix='.zip')
-                os.close(fd)
-                archivecontents = open(tempzipfile, "wb")
+                if tempzipfile is None:
+                    fd, tempzipfile = tempfile.mkstemp(prefix='pootle',
+                                                       suffix='.zip')
+                    os.close(fd)
+                    archivecontents = open(tempzipfile, "wb")
             else:
                 import cStringIO
                 archivecontents = cStringIO.StringIO()