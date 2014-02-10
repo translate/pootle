translate/pootle

commit 2bc49e00d7485dabb5659c8104d5224ec1cad451

Fix zip export
It was broken since 67587450e47fb0d9bd2753edc2a04c5b9f93951c;

* If zip command was not found, subprocess.Popen() will raise an
  exception and the fallback path will not be reached. The old code
  worked because os.system() will just return a non-zero return status.
* If zip command was found, the call will also fail because --out does
  not do what I probably thought it does!

pootle/apps/pootle_translationproject/models.py
@@ -817,6 +817,7 @@ class TranslationProject(models.Model, TreeItem):
         from pootle_misc import ptempfile as tempfile
 
         tempzipfile = None
+        archivecontents = None
 
         try:
             # Using zip command line is fast
@@ -824,14 +825,17 @@ class TranslationProject(models.Model, TreeItem):
             # security reasons
             fd, tempzipfile = tempfile.mkstemp(prefix='pootle', suffix='.zip')
             os.close(fd)
+            archivecontents = open(tempzipfile, "wb")
 
             file_list = u" ".join(
                 store.abs_real_path[len(self.abs_real_path)+1:] \
                 for store in stores.iterator()
             )
-            result = subprocess.Popen(['zip', '-r', file_list,
-                                       '--out', tempzipfile],
-                                      cwd=self.abs_real_path)
+            process = subprocess.Popen(['zip', '-r', '-', file_list],
+                                       cwd=self.abs_real_path,
+                                       stdout=archivecontents)
+            result = process.wait()
+            os.close(fd)
 
             if result == 0:
                 if path is not None:
@@ -841,18 +845,20 @@ class TranslationProject(models.Model, TreeItem):
                     filedata = open(tempzipfile, "r").read()
                     if filedata:
                         return filedata
-        finally:
-            if tempzipfile is not None and os.path.exists(tempzipfile):
-                os.remove(tempzipfile)
-
-        # But if it doesn't work, we can do it from python
-        archivecontents = None
-        try:
+                    else:
+                        raise Exception("failed to read temporary zip file")
+            else:
+                raise Exception("zip command returned error code: %d" % result)
+        except Exception as e:
+            # But if it doesn't work, we can do it from Python.
+            logging.debug(e)
+            logging.debug("falling back to zipfile module")
             if path is not None:
-                fd, tempzipfile = tempfile.mkstemp(prefix='pootle',
-                                                   suffix='.zip')
-                os.close(fd)
-                archivecontents = open(tempzipfile, "wb")
+                if tempzipfile is None:
+                    fd, tempzipfile = tempfile.mkstemp(prefix='pootle',
+                                                       suffix='.zip')
+                    os.close(fd)
+                    archivecontents = open(tempzipfile, "wb")
             else:
                 import cStringIO
                 archivecontents = cStringIO.StringIO()

commit 872daced670c80f05bf807607b8127ba06590f07

Remove stray os.close(fd)
A fallout from 2bc49e00d7485dabb5659c8104d5224ec1cad451.

pootle/apps/pootle_translationproject/models.py
@@ -835,7 +835,6 @@ class TranslationProject(models.Model, TreeItem):
                                        cwd=self.abs_real_path,
                                        stdout=archivecontents)
             result = process.wait()
-            os.close(fd)
 
             if result == 0:
                 if path is not None: