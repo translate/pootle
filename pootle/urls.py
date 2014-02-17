658f5fd326b8e6bc599a39fe2b0142225195c25b
Jerome Leclanche <adys.wh@gmail.com>
2014-02-17 14:06:06 +0000

    allauth: Add django-allauth.

--- pootle/urls.py
+++ pootle/urls.py
@@ -41,6 +41,7 @@ urlpatterns = patterns('',
     # External apps.
     url(r'^contact/', include('pootle_contact.urls')),
     url(r'^accounts/', include('pootle_profile.urls')),
+    url(r'^accounts/allauth/', include('allauth.urls')),
     url(r'^profiles/', include('profiles.urls')),
 )
 
