5281de56f1912f0c151639d8dc1e7628fa735e44
Dwayne Bailey <dwayne@translate.org.za>
2014-02-20 22:07:18 +0000

    PEP8: E251 unexpected spaces around keyword / parameter equals

--- docs/_ext/pootle_docs.py
+++ docs/_ext/pootle_docs.py
@@ -24,13 +24,13 @@
 def setup(app):
     # Django :xxx: roles for intersphinx cross-references
     app.add_crossref_type(
-        directivename = "setting",
-        rolename = "setting",
-        indextemplate = "pair: %s; setting",
+        directivename="setting",
+        rolename="setting",
+        indextemplate="pair: %s; setting",
     )
     app.add_description_unit(
-        directivename = "django-admin",
-        rolename = "djadmin",
-        indextemplate = "pair: %s; django-admin command",
-        #parse_node = parse_django_admin_node,
+        directivename="django-admin",
+        rolename="djadmin",
+        indextemplate="pair: %s; django-admin command",
+        #parse_node=parse_django_admin_node,
     )
