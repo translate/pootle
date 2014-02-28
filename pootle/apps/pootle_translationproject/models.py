translate/pootle

commit a6b5dd9db6e6495444730d4e33b354315e86e967

Fix #118, #119, #120

pootle/apps/pootle_translationproject/models.py
@@ -53,12 +53,21 @@ class TranslationProjectNonDBState(object):
         self.termmatcher = None
         self.termmatchermtime = None
 
+def create_or_enable_translation_project(language, project):
+    tp = create_translation_project(language, project)
+    if tp is not None:
+        if tp.disabled:
+            tp.disabled = False
+            tp.save()
+            logging.info(u"Enabled %s", tp)
+        else:
+            logging.info(u"Created %s", tp)
 
 def create_translation_project(language, project):
     from pootle_app import project_tree
     if project_tree.translation_project_should_exist(language, project):
         try:
-            translation_project, created = TranslationProject.objects \
+            translation_project, created = TranslationProject.objects.all() \
                     .get_or_create(language=language, project=project)
             return translation_project
         except OSError:
@@ -74,6 +83,9 @@ def scan_translation_projects():
 
 
 class TranslationProjectManager(RelatedManager):
+    # disabled objects are hidden for related objects too
+    use_for_related_fields = True
+
     def get_by_natural_key(self, pootle_path):
         #FIXME: should we use Language and Project codes instead?
         return self.get(pootle_path=pootle_path)
@@ -89,6 +101,9 @@ class TranslationProjectManager(RelatedManager):
         return self.get(language=language_id,
                         project__checkstyle='terminology')
 
+    def enabled(self):
+        return self.filter(disabled=False)
+
 
 class TranslationProject(models.Model, TreeItem):
 
@@ -98,6 +113,9 @@ class TranslationProject(models.Model, TreeItem):
     directory = models.OneToOneField(Directory, db_index=True, editable=False)
     pootle_path = models.CharField(max_length=255, null=False, unique=True,
             db_index=True, editable=False)
+    creation_time = models.DateTimeField(auto_now_add=True, db_index=True,
+                                         editable=False, null=True)
+    disabled = models.BooleanField()
 
     _non_db_state_cache = LRUCachingDict(settings.PARSE_POOL_SIZE,
             settings.PARSE_POOL_CULL_FREQUENCY)
@@ -188,12 +206,14 @@ class TranslationProject(models.Model, TreeItem):
         created = self.id is None
 
         project_dir = self.project.get_real_path()
-        from pootle_app.project_tree import get_translation_project_dir
-        self.abs_real_path = get_translation_project_dir(self.language,
-                project_dir, self.file_style, make_dirs=True)
-        self.directory = self.language.directory \
-                                      .get_or_make_subdir(self.project.code)
-        self.pootle_path = self.directory.pootle_path
+        if not self.disabled:
+            from pootle_app.project_tree import get_translation_project_dir
+            self.abs_real_path = get_translation_project_dir(self.language,
+                 project_dir, self.file_style, make_dirs=not self.disabled)
+
+            self.directory = self.language.directory \
+                                          .get_or_make_subdir(self.project.code)
+            self.pootle_path = self.directory.pootle_path
 
         super(TranslationProject, self).save(*args, **kwargs)
 
@@ -202,10 +222,11 @@ class TranslationProject(models.Model, TreeItem):
 
     def delete(self, *args, **kwargs):
         directory = self.directory
+        # clear cache for translation_project parents only
+        # children cache will be cleared in directory.delete()
+        self.clear_all_cache(parents=True, children=False)
 
         super(TranslationProject, self).delete(*args, **kwargs)
-        #TODO: avoid an access to directory while flushing the cache
-        directory.flush_cache()
         directory.delete()
 
     def get_absolute_url(self):

pootle/apps/pootle_language/models.py
@@ -161,7 +161,7 @@ class Language(models.Model, TreeItem):
     ### TreeItem
 
     def get_children(self):
-        return self.translationproject_set.filter(project__disabled=False)
+        return self.translationproject_set.enabled()
 
     def get_cachekey(self):
         return self.directory.pootle_path

pootle/apps/pootle_project/models.py
@@ -308,9 +308,9 @@ class Project(models.Model, TreeItem, ProjectURLMixin):
 
         # clear stats cache
         if self.__disabled != self.disabled:
-            self.clear_cache()
+            self.clear_all_cache(parents=True, children=False)
             for tp in self.get_children():
-                tp.language.clear_cache()
+                tp.language.clear_all_cache(parents=True, children=False)
             self.__disabled = self.disabled
 
     def delete(self, *args, **kwargs):
@@ -368,13 +368,17 @@ class Project(models.Model, TreeItem, ProjectURLMixin):
     ### TreeItem
 
     def get_children(self):
-        return self.translationproject_set.all()
+        return self.translationproject_set.enabled()
 
     def get_cachekey(self):
         return self.directory.pootle_path
 
     def get_parents(self):
         return [Directory.objects.projects]
+
+    def before_delete(self):
+        # everything will be done in directory.delete()
+        pass
 
     ### /TreeItem
 

pootle/apps/pootle_store/models.py
@@ -229,6 +229,7 @@ class UnitManager(RelatedManager):
         units_qs = super(UnitManager, self).get_query_set().filter(
             state__gt=OBSOLETE,
             store__translation_project__project__disabled=False,
+            store__translation_project__disabled=False,
         )
 
         # /projects/<project_code>/translate/*
@@ -459,7 +460,7 @@ class Unit(models.Model, base.TranslationUnit):
         # update cache only if we are updating a single unit
         if self.store.state >= PARSED:
             self.store.flag_for_deletion(CachedMethods.MTIME)
-            self.store.update_cache()
+            self.store.clear_flagged_cache()
 
     def get_absolute_url(self):
         return l(self.store.pootle_path)
@@ -1232,15 +1233,14 @@ class Store(models.Model, TreeItem, base.TranslationStore):
                 unit.index = index + i
                 unit.save()
         if self.state >= PARSED:
-            self.update_cache()
+            self.clear_flagged_cache()
 
     def delete(self, *args, **kwargs):
         self.before_delete()
-        self.clear_cache()
-
         super(Store, self).delete(*args, **kwargs)
 
     def before_delete(self):
+        super(Store, self).before_delete()
         store_log(user='system', action=STORE_DELETED,
                   path=self.pootle_path, store=self.id)
 

pootle/apps/pootle_app/management/commands/flush_cache.py
@@ -29,13 +29,13 @@ class Command(PootleCommand):
     help = "Flush stats cache."
 
     def handle_all_stores(self, translation_project, **options):
-        translation_project.flush_cache()
+        translation_project.clear_all_cache()
 
     def handle_store(self, store, **options):
-        store.flush_cache()
+        store.clear_all_cache()
 
     def handle_language(self, lang, **options):
-        lang.flush_cache(False)
+        lang.clear_all_cache(children=False)
 
     def handle_project(self, prj, **options):
-        prj.flush_cache(False)
+        prj.clear_all_cache(children=False)

pootle/apps/pootle_app/management/commands/refresh_stats.py
@@ -73,25 +73,25 @@ class Command(PootleCommand):
 
     def handle_all_stores(self, translation_project, **options):
         # TODO use the faster method
-        translation_project.flush_cache()
+        translation_project.clear_all_cache(parents=False)
         translation_project.get_stats()
         translation_project.get_mtime()
 
     def handle_store(self, store, **options):
         # TODO use the faster method
-        store.flush_cache()
+        store.clear_all_cache(parents=False)
         store.get_stats()
         store.get_mtime()
 
     def handle_language(self, lang, **options):
         # TODO use the faster method
-        lang.flush_cache(False)
+        lang.clear_all_cache(children=False)
         lang.get_stats()
         lang.get_mtime()
 
     def handle_project(self, prj, **options):
         # TODO use the faster method
-        prj.flush_cache(False)
+        prj.clear_all_cache(children=False)
         prj.get_stats()
         prj.get_mtime()
 

pootle/apps/pootle_app/management/commands/update_translation_projects.py
@@ -27,7 +27,7 @@ import errno
 
 from pootle_app.management.commands import PootleCommand
 
-from pootle_translationproject.models import create_translation_project
+from pootle_translationproject.models import create_or_enable_translation_project
 from pootle_language.models import Language
 from pootle_project.models import Project
 
@@ -53,27 +53,34 @@ class Command(PootleCommand):
            "adds them to database."
 
     def handle_project(self, project, **options):
+        clean = options.get('clean', False)
+        if clean and does_not_exist(project.get_real_path()):
+            logging.info(u"Disabling %s", project)
+            project.disabled = True
+            project.save()
+            project.clear_all_cache(parents=True, children=False)
+            return
+
         lang_query = Language.objects.exclude(
-                id__in=project.translationproject_set \
+                id__in=project.translationproject_set.enabled() \
                               .values_list('language', flat=True)
             )
         for language in lang_query.iterator():
-            tp = create_translation_project(language, project)
-            if tp:
-                logging.info(u"Created %s", tp)
+            create_or_enable_translation_project(language, project)
 
     def handle_language(self, language, **options):
         project_query = Project.objects.exclude(
-                id__in=language.translationproject_set \
+                id__in=language.translationproject_set.enabled() \
                                .values_list('project', flat=True)
             )
         for project in project_query.iterator():
-            tp = create_translation_project(language, project)
-            if tp:
-                logging.info(u"Created %s", tp)
+            create_or_enable_translation_project(language, project)
 
     def handle_translation_project(self, translation_project, **options):
         clean = options.get('clean', False)
-        if clean and does_not_exist(translation_project.abs_real_path):
-            logging.info(u"Deleting %s", translation_project)
-            translation_project.delete()
+        if not translation_project.disabled and \
+                clean and does_not_exist(translation_project.abs_real_path):
+            logging.info(u"Disabling %s", translation_project)
+            translation_project.disabled = True
+            translation_project.save()
+            translation_project.clear_all_cache(parents=True, children=False)

pootle/apps/pootle_app/models/directory.py
@@ -238,10 +238,5 @@ class Directory(models.Model, TreeItem):
 
     def delete(self, *args, **kwargs):
         self.before_delete()
-        self.clear_cache()
         super(Directory, self).delete(*args, **kwargs)
 
-    def before_delete(self):
-        self.initialize_children()
-        for item in self.children:
-            item.before_delete()

commit f208be8f66dcc9f5cabec548caa1e9f4baf0ed42

Fix TranslationProjectManager.enabled() getter
If Project is disabled all TranslationProjects are disabled
fix bug introduced in a6b5dd9 (#118)

pootle/apps/pootle_translationproject/models.py
@@ -102,7 +102,7 @@ class TranslationProjectManager(RelatedManager):
                         project__checkstyle='terminology')
 
     def enabled(self):
-        return self.filter(disabled=False)
+        return self.filter(disabled=False, project__disabled=False)
 
 
 class TranslationProject(models.Model, TreeItem):