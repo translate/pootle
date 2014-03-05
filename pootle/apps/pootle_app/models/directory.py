translate/pootle

commit 70dd59606aa619038d23dffe86b514598032c31f

Remove dirty before_delete method (fix #131)

pootle/apps/pootle_app/models/directory.py
@@ -237,6 +237,10 @@ class Directory(models.Model, TreeItem):
             return translation_project.real_path + path_prefix
 
     def delete(self, *args, **kwargs):
-        self.before_delete()
+        # cache will be cleared from child stores
+        self.initialize_children()
+        for item in self.children:
+            item.detele()
+
         super(Directory, self).delete(*args, **kwargs)
 

pootle/apps/pootle_project/models.py
@@ -376,10 +376,6 @@ class Project(models.Model, TreeItem, ProjectURLMixin):
     def get_parents(self):
         return [Directory.objects.projects]
 
-    def before_delete(self):
-        # everything will be done in directory.delete()
-        pass
-
     ### /TreeItem
 
     def translated_percentage(self):

pootle/apps/pootle_store/models.py
@@ -1236,11 +1236,8 @@ class Store(models.Model, TreeItem, base.TranslationStore):
             self.clear_flagged_cache()
 
     def delete(self, *args, **kwargs):
-        self.before_delete()
-        super(Store, self).delete(*args, **kwargs)
+        self.clear_all_cache(parents=True, children=False)
 
-    def before_delete(self):
-        super(Store, self).before_delete()
         store_log(user='system', action=STORE_DELETED,
                   path=self.pootle_path, store=self.id)
 
@@ -1248,6 +1245,8 @@ class Store(models.Model, TreeItem, base.TranslationStore):
         for unit in self.unit_set.iterator():
             action_log(user='system', action=UNIT_DELETED, lang=lang,
                        unit=unit.id, Translation='', path=self.pootle_path)
+
+        super(Store, self).delete(*args, **kwargs)
 
     def get_absolute_url(self):
         return l(self.pootle_path)

pootle/apps/pootle_translationproject/models.py
@@ -223,6 +223,7 @@ class TranslationProject(models.Model, TreeItem):
     def delete(self, *args, **kwargs):
         directory = self.directory
         # clear cache for translation_project parents only
+        # (they differ from directory ones)
         # children cache will be cleared in directory.delete()
         self.clear_all_cache(parents=True, children=False)
 

pootle/core/mixins/treeitem.py
@@ -306,16 +306,6 @@ class TreeItem(object):
             if pla and pla['mtime'] < last_action['mtime']:
                 p.set_last_action(last_action)
 
-    def before_delete(self):
-        self.initialize_children()
-        for item in self.children:
-            item.detele()
-        else:
-            # clear cache form leaf to the root
-            self.clear_all_cache(parents=True, children=False)
-
-        super(TreeItem, self).before_delete()
-
     def _clear_cache(self, keys, parents=True, children=False):
         itemkey = self.get_cachekey()
         for key in keys:

commit e6e81699c35542558ba85373c8e57ce617819ed8

Fix typo introduced in [70dd596]

pootle/apps/pootle_app/models/directory.py
@@ -240,7 +240,7 @@ class Directory(models.Model, TreeItem):
         # cache will be cleared from child stores
         self.initialize_children()
         for item in self.children:
-            item.detele()
+            item.delete()
 
         super(Directory, self).delete(*args, **kwargs)
 