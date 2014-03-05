translate/pootle

commit c209b1ee6359b33c51e3f72c94434394b0a8f274

Cleanups: multiple minor cleanups
ctx, form and model fields, imports order, shorten calls, blank lines,
remove try block, unused imports, model order, use single quotes on URL...

pootle/apps/pootle_app/views/admin/adminroot.py
@@ -22,12 +22,10 @@ from pootle.core.decorators import admin_required
 from pootle_app.models.directory import Directory
 from pootle_app.views.admin.permissions import admin_permissions
 
+
 @admin_required
 def view(request):
-
-    directory = Directory.objects.root
-    template_vars = {
-        'directory': directory,
+    ctx = {
+        'directory': Directory.objects.root,
     }
-    return admin_permissions(request, directory, "admin/permissions.html",
-                             template_vars)
+    return admin_permissions(request, directory, "admin/permissions.html", ctx)

pootle/apps/pootle_app/models/directory.py
@@ -83,6 +83,10 @@ class Directory(models.Model, TreeItem):
     def is_template_project(self):
         return self.pootle_path.startswith('/templates/')
 
+    @property
+    def code(self):
+        return self.name.replace('.', '-')
+
     ############################ Cached properties ############################
 
     @cached_property
@@ -114,10 +118,6 @@ class Directory(models.Model, TreeItem):
 
     def __unicode__(self):
         return self.pootle_path
-
-    @property
-    def code(self):
-        return self.name.replace('.', '-')
 
     def __init__(self, *args, **kwargs):
         super(Directory, self).__init__(*args, **kwargs)

pootle/apps/pootle_app/views/admin/adminlanguages.py
@@ -19,9 +19,10 @@
 # Pootle; if not, see <http://www.gnu.org/licenses/>.
 
 from pootle.core.decorators import admin_required
+from pootle_app.admin import MyLanguageAdminForm
 from pootle_app.views.admin import util
 from pootle_language.models import Language
-from pootle_app.admin import MyLanguageAdminForm
+
 
 @admin_required
 def view(request):

pootle/apps/pootle_app/views/admin/adminprojects.py
@@ -39,11 +39,15 @@ def view(request):
 
 
     class ProjectForm(forms.ModelForm):
+
+        source_language = forms.ModelChoiceField(
+            label=_('Source Language'),
+            initial=default_lang.pk,
+            queryset=queryset,
+        )
+
         class Meta:
             model = Project
-
-        source_language = forms.ModelChoiceField(label=_('Source Language'),
-                initial=default_lang.pk, queryset=queryset)
 
         def __init__(self, *args, **kwargs):
             super(ProjectForm, self).__init__(*args, **kwargs)
@@ -84,9 +88,8 @@ def view(request):
         def clean_code(self):
             value = self.cleaned_data['code']
             if value in RESERVED_PROJECT_CODES:
-                raise ValidationError(
-                    _('"%s" cannot be used as a project code' % (value,))
-                )
+                raise ValidationError(_('"%s" cannot be used as a project '
+                                        'code' % value))
             return value
 
     return util.edit(

pootle/apps/pootle_app/views/admin/dashboard.py
@@ -305,11 +305,11 @@ def server_stats_more(request):
 
 @admin_required
 def view(request):
-    template_vars = {
+    ctx = {
         'server_stats': server_stats(),
         'required': required_depcheck(),
         'optional': optional_depcheck(),
         'optimal': optimal_depcheck(),
     }
-    return render_to_response("admin/dashboard.html", template_vars,
+    return render_to_response("admin/dashboard.html", ctx,
                               context_instance=RequestContext(request))

pootle/apps/pootle_app/views/admin/util.py
@@ -101,12 +101,11 @@ def form_set_as_table(formset, link=None, linkfield='code'):
             if field == linkfield and linkfield in form.initial and link:
                 if callable(link):
                     result.append(link(form.instance))
-                    result.append(form[field].as_hidden())
                 else:
                     link = l(link % form.initial[linkfield])
-                    result.append("<a href='" + link + "'>" +
-                                  form.initial[linkfield] + "</a>")
-                    result.append(form[field].as_hidden())
+                    result.append('<a href="%s">%s</a>' %
+                                  (link, form.initial[linkfield]))
+                result.append(form[field].as_hidden())
             else:
                 result.append(form[field].as_widget())
 

pootle/apps/pootle_app/views/index/contributors.py
@@ -136,7 +136,9 @@ def view(request):
             contributors.append((language, projects))
     contributors.sort()
 
-    data = {'contributors': contributors}
+    ctx = {
+        'contributors': contributors,
+    }
 
-    return render_to_response('about/contributors.html', data,
+    return render_to_response('about/contributors.html', ctx,
                               context_instance=RequestContext(request))

pootle/apps/pootle_app/views/index/index.py
@@ -107,7 +107,7 @@ def view(request, root_dir):
     }
 
     siteconfig = load_site_config()
-    templatevars = {
+    ctx = {
         'description': _(siteconfig.get('DESCRIPTION')),
         'keywords': [
             'Pootle',
@@ -125,13 +125,14 @@ def view(request, root_dir):
         'languages_table': languages_table,
         'projects_table': projects_table,
         'resource_obj': request.resource_obj,
+        'moreprojects': (len(projects) > len(languages))
     }
-    templatevars['moreprojects'] = (len(projects) > len(languages))
 
     if can_edit:
         from pootle_app.forms import GeneralSettingsForm
-        setting_form = GeneralSettingsForm(siteconfig)
-        templatevars['form'] = setting_form
+        ctx.update({
+            'form': GeneralSettingsForm(siteconfig),
+        })
 
-    return render_to_response('home/home.html', templatevars,
+    return render_to_response('home/home.html', ctx,
                               RequestContext(request))

commit ef50adbb9e0f585698e9face946e9b963e72a575

Fix bug introduced in c209b1ee

pootle/apps/pootle_app/views/admin/adminroot.py
@@ -25,7 +25,8 @@ from pootle_app.views.admin.permissions import admin_permissions
 
 @admin_required
 def view(request):
+    directory = Directory.objects.root
     ctx = {
-        'directory': Directory.objects.root,
+        'directory': directory,
     }
     return admin_permissions(request, directory, "admin/permissions.html", ctx)