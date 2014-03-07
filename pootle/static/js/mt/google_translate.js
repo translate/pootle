54a2e33590140d6830b9b3c66978fcf8bcfd56dd
Igor Afanasyev <igor.afanasyev@gmail.com>
2014-03-07 21:39:01 +0400

    List all languages Google Translate supports (there were missing ones), and in a more compact manner

--- pootle/static/js/mt/google_translate.js
+++ pootle/static/js/mt/google_translate.js
@@ -12,66 +12,31 @@
 
     /* For a list of currently supported languages:
      * https://developers.google.com/translate/v2/using_rest#language-params
-     * FIXME note that we seem not to use this list, but an API does exist to
-     * query if Google supports a given language */
-    pairs: [{"source":"af","target":"af"},
-            {"source":"sq","target":"sq"},
-            {"source":"ar","target":"ar"},
-            {"source":"be","target":"be"},
-            {"source":"bg","target":"bg"},
-            {"source":"ca","target":"ca"},
-            {"source":"zh","target":"zh"},
-            {"source":"zh-CN","target":"zh-CN"},
-            {"source":"zh-TW","target":"zh-TW"},
-            {"source":"hr","target":"hr"},
-            {"source":"cs","target":"cs"},
-            {"source":"da","target":"da"},
-            {"source":"nl","target":"nl"},
-            {"source":"en","target":"en"},
-            {"source":"et","target":"et"},
-            {"source":"tl","target":"tl"},
-            {"source":"fi","target":"fi"},
-            {"source":"fr","target":"fr"},
-            {"source":"gl","target":"gl"},
-            {"source":"de","target":"de"},
-            {"source":"el","target":"el"},
-            {"source":"ht","target":"ht"},
-            {"source":"iw","target":"iw"},
-            {"source":"hi","target":"hi"},
-            {"source":"hu","target":"hu"},
-            {"source":"is","target":"is"},
-            {"source":"id","target":"id"},
-            {"source":"ga","target":"ga"},
-            {"source":"it","target":"it"},
-            {"source":"ja","target":"ja"},
-            {"source":"ko","target":"ko"},
-            {"source":"lv","target":"lv"},
-            {"source":"lt","target":"lt"},
-            {"source":"mk","target":"mk"},
-            {"source":"ms","target":"ms"},
-            {"source":"mt","target":"mt"},
-            {"source":"no","target":"no"},
-            {"source":"fa","target":"fa"},
-            {"source":"pl","target":"pl"},
-            {"source":"pt","target":"pt"},
-            {"source":"pt-PT","target":"pt-PT"},
-            {"source":"ro","target":"ro"},
-            {"source":"ru","target":"ru"},
-            {"source":"sr","target":"sr"},
-            {"source":"sk","target":"sk"},
-            {"source":"sl","target":"sl"},
-            {"source":"es","target":"es"},
-            {"source":"sw","target":"sw"},
-            {"source":"sv","target":"sv"},
-            {"source":"tl","target":"tl"},
-            {"source":"th","target":"th"},
-            {"source":"tr","target":"tr"},
-            {"source":"uk","target":"uk"},
-            {"source":"vi","target":"vi"},
-            {"source":"cy","target":"cy"},
-            {"source":"yi","target":"yi"}],
+     *
+     * Google supports translations between any of the supported languages
+     * (any combination is acceptable)
+     *
+     * FIXME Note that an API does exist to query if Google supports
+     * a given language */
+
+    supported_languages: [
+      'af','sq','ar','az','eu','bn','be','bg','ca','zh-CN','zh-TW','hr',
+      'cs','da','nl','en','eo','et','tl','fi','fr','gl','ka','de','el',
+      'gu','ht','iw','hi','hu','is','id','ga','it','ja','kn','ko','la',
+      'lv','lt','mk','ms','mt','no','fa','pl','pt','ro','ru','sr','sk',
+      'sl','es','sw','sv','ta','te','th','tr','uk','ur','vi','cy','yi'
+    ],
 
     init: function (apiKey) {
+      /* Init variables */
+      this.pairs = [];
+      for (var i = 0; i < this.supported_languages.length; i++) {
+        this.pairs.push({
+          'source': this.supported_languages[i],
+          'target': this.supported_languages[i]
+        });
+      };
+
       /* Prepare URL for requests. */
       this.url += "?callback=?";
       /* Set API key */
