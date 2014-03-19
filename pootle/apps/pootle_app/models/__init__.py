75ac8105ed3a8bd9daacbd3a124fbc16d6f68f47
Taras Semenenko <tsemenenko@evernote.com>
2014-03-19 17:24:00 +0400

    Remove pootle_app.Suggestion model
    remove from_suggestion field from Submission model
    (last part of #163)

--- pootle/apps/pootle_app/models/__init__.py
+++ pootle/apps/pootle_app/models/__init__.py
@@ -20,10 +20,9 @@
 # along with translate; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-from pootle_app.models.suggestion import Suggestion
 from pootle_app.models.directory import Directory
 from pootle_app.models.permissions import PermissionSet
 from pootle_app.models.revision import Revision
 
 
-__all__ = ["Suggestion", "Directory", "PermissionSet", "Revision"]
+__all__ = ["Directory", "PermissionSet", "Revision"]
