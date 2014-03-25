5b0be513b453e5451c65700c60f7591d7580be24
Julen Ruiz Aizpuru <julenx@gmail.com>
2014-03-25 10:47:58 +0100

    Tests: updated suggestion test
    The return values of `add_suggestion()` were changed in 203baf17.

--- tests/models/suggestion.py
+++ tests/models/suggestion.py
@@ -26,7 +26,7 @@ import pytest
 def test_hash(af_tutorial_po, system):
     """Tests that target hash changes when suggestion is modified"""
     unit = af_tutorial_po.getitem(0)
-    suggestion = unit.add_suggestion("gras")
+    suggestion, created = unit.add_suggestion("gras")
 
     first_hash = suggestion.target_hash
     suggestion.translator_comment = "my nice comment"
