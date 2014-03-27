1dcb42ba108b75f986745df0834680600191fdc6
Julen Ruiz Aizpuru <julenx@gmail.com>
2014-03-27 13:55:41 +0100

    Tests: added disabled TP

--- tests/fixtures/models/translation_project.py
+++ tests/fixtures/models/translation_project.py
@@ -28,7 +28,24 @@ def _require_tp(language, project):
     return create_translation_project(language, project)
 
 
+def _require_disabled_tp(language, project):
+    """Helper to get/create a new translation project in disabled state."""
+    from pootle_translationproject.models import create_translation_project
+
+    tp = create_translation_project(language, project)
+    tp.disabled = True
+    tp.save()
+
+    return tp
+
+
 @pytest.fixture
 def afrikaans_tutorial(afrikaans, tutorial):
     """Require Afrikaans Tutorial."""
     return _require_tp(afrikaans, tutorial)
+
+
+@pytest.fixture
+def arabic_tutorial_disabled(arabic, tutorial):
+    """Require Arabic Tutorial in disabled state."""
+    return _require_disabled_tp(arabic, tutorial)
