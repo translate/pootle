56721e4e601485482bf8a3440ee8f6f6ec666a55
Taras Semenenko <tsemenenko@evernote.com>
2014-04-15 20:30:12 +0400

    Add pootle_misc/checks tests

---
+++ tests/misc/checks.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2014 Evernote Corporation
+#
+# This file is part of Pootle.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+import pytest
+
+from translate.filters.checks import FilterFailure
+from pootle_misc.checks import ENChecker
+
+checker = ENChecker()
+
+def test_dollar_sign_check():
+    check = checker.dollar_sign_placeholders
+
+    tests = [
+        (u'$1 aa $2', u'$1 dd $2', True),
+        (u'$1 aa $2', u'$1dd$2', True),
+    ]
+
+    for str1, str2, state in tests:
+        info = "check('%s', '%s') == %s" % (str1, str2, state)
+        try:
+            assert (state == check(str1, str2)), info
+
+        except FilterFailure as e:
+
+            assert (not state), info
