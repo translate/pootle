translate/pootle

commit d4f60c762d1960fc00ee4e49b705665f5fe99ed0

Display current project's files/dirs in a third drop-down
Some redirects might be incomplete but they'll be part of evernote/pootle#75.

Fixes evernote/pootle#74.

Pick notes: Had to fix conflicts due to code divergence, but didn't
    alter any of the changes.

Conflicts:
	pootle/apps/pootle_project/models.py
	pootle/core/url_helpers.py
	pootle/static/css/style.css
	pootle/templates/browser/overview.html

pootle/apps/pootle_project/models.py
@@ -37,7 +37,7 @@ from translate.lang.data import langcode_re
 from pootle.core.managers import RelatedManager
 from pootle.core.markup import get_markup_filter_name, MarkupField
 from pootle.core.mixins import TreeItem, CachedMethods
-from pootle.core.url_helpers import get_editor_filter
+from pootle.core.url_helpers import get_editor_filter, get_path_sortkey
 from pootle_app.models.directory import Directory
 from pootle_app.models.permissions import PermissionSet
 from pootle_misc.util import cached_property
@@ -181,6 +181,39 @@ class Project(models.Model, TreeItem):
         # built-in invalidation -- did I hear django-cache-machine?
         return Language.objects.filter(Q(translationproject__project=self),
                                        ~Q(code='templates'))
+
+    @cached_property
+    def resources(self):
+        """Returns a list of :cls:`~pootle_app.models.Directory` and
+        :cls:`~pootle_store.models.Store` objects available for this
+        :cls:`~pootle_project.models.Project` across all languages.
+        """
+        from pootle_store.models import Store
+
+        resources_path = ''.join(['/%/', self.code, '/%'])
+
+        store_objs = Store.objects.extra(
+            where=[
+                '`pootle_store_store`.`pootle_path` LIKE %s',
+                '`pootle_store_store`.`pootle_path` NOT LIKE %s',
+            ], params=[resources_path, '/templates/%']
+        ).select_related('parent').distinct()
+
+        # Populate with stores and their parent directories, avoiding any
+        # duplicates
+        resources = []
+        for store in store_objs.iterator():
+            directory = store.parent
+            if (not directory.is_translationproject() and
+                all(directory.path != r.path for r in resources)):
+                resources.append(directory)
+
+            if all(store.path != r.path for r in resources):
+                resources.append(store)
+
+        resources.sort(key=get_path_sortkey)
+
+        return resources
 
     ############################ Methods ######################################
 

pootle/core/url_helpers.py
@@ -54,6 +54,15 @@ def split_pootle_path(pootle_path):
     return (language_code, project_code, dir_path, filename)
 
 
+def get_path_sortkey(path_obj):
+    """Returns the sortkey to use in a path-like `path_obj` object."""
+    if path_obj.is_dir:
+        return path_obj.path
+
+    (head, tail) = os.path.split(path_obj.path)
+    return u'~'.join([head, path_obj.path.replace(u'.', u'_')])
+
+
 def get_editor_filter(state=None, check=None, user=None, goal=None):
     """Return a filter string to be appended to a translation URL."""
     filter_string = ''

pootle/static/css/style.css
@@ -2281,18 +2281,6 @@ html[dir="rtl"] .breadcrumb li
     content: normal;
 }
 
-.breadcrumb li.file-path
-{
-    font-weight: normal;
-    font-size: 90%;
-    color: #999;
-}
-
-.breadcrumb li.file-path span
-{
-    color: #000;
-}
-
 /* Tabs */
 
 #tabs,

pootle/static/js/common.js
@@ -18,7 +18,8 @@
         function (e) {
           var langCode = $(this).val(),
               projectCode = $('#js-select-project').val();
-          PTL.common.navigateTo(langCode, projectCode);
+              resource = $('#js-select-resource').val() || '';
+          PTL.common.navigateTo(langCode, projectCode, resource);
       });
       PTL.utils.makeSelectableInput('#js-select-project',
         {
@@ -30,9 +31,25 @@
         },
         function (e) {
           var projectCode = $(this).val(),
-              langCode = $('#js-select-language').val();
-          PTL.common.navigateTo(langCode, projectCode);
+              langCode = $('#js-select-language').val(),
+              resource = $('#js-select-resource').val() || '';
+          PTL.common.navigateTo(langCode, projectCode, resource);
       });
+      PTL.utils.makeSelectableInput('#js-select-resource',
+        {
+          allowClear: true,
+          dropdownAutoWidth: true,
+          dropdownCssClass: 'breadcrumb-dropdown',
+          placeholder: gettext("All Resources"),
+          width: 'off'
+        },
+        function (e) {
+          var resource = $(this).val(),
+              projectCode = $('#js-select-project').val();
+              langCode = $('#js-select-language').val();
+          PTL.common.navigateTo(langCode, projectCode, resource);
+        }
+      );
 
       // Append fragment identifiers for login redirects
       $('#navbar').on('focus click', '#js-login', function (e) {
@@ -387,16 +404,18 @@
       });
     },
 
-    /* Navigates to `languageCode`, `projectCode` while retaining the
-     * current context when applicable */
-    navigateTo: function (languageCode, projectCode) {
+    /* Navigates to `languageCode`, `projectCode`, `resource` while
+     * retaining the current context when applicable */
+    navigateTo: function (languageCode, projectCode, resource) {
       var curProject = $('#js-select-project').data('initial-code'),
           curLanguage = $('#js-select-language').data('initial-code'),
+          curResource = $('#js-select-resource').data('initial-code'),
           curUrl = window.location.toString(),
           newUrl = curUrl,
           langChanged = languageCode !== curLanguage,
           projChanged = projectCode !== curProject,
-          hasChanged = langChanged || projChanged;
+          resChanged = resource !== curResource,
+          hasChanged = langChanged || projChanged || resChanged;
 
       if (!hasChanged) {
         return;
@@ -405,7 +424,7 @@
       if (languageCode === '' && projectCode === '') {
         newUrl = l('/projects/');
       } else if (languageCode === '' && projectCode !== '') {
-        newUrl = l(['', 'projects', projectCode].join('/'));
+        newUrl = l(['', 'projects', projectCode, resource].join('/'));
       } else if (languageCode !== '' && projectCode === '') {
         newUrl = l(['', languageCode].join('/'));
       } else if (languageCode !== '' && projectCode !== '') {
@@ -420,6 +439,18 @@
                                     languageCode + '/' + curProject)
                            .replace(/(\#|&)unit=\d+/, '');
           }
+        } else if (resChanged) {
+          if (curResource) {
+            newUrl = curUrl.replace(curResource, resource);
+          } else {
+            var pattern = [
+              '(', curLanguage, '/', curProject, '/', '([^/]+/)?)', curResource
+            ].join(''),
+                regex = new RegExp(pattern);
+
+            newUrl = newUrl.replace(regex, ['$1', resource].join(''));
+          }
+          newUrl = newUrl.replace(/(\#|&)unit=\d+/, '');
         }
         var changed = projChanged ? 'project' : 'language';
         $.cookie('user-choice', changed, {path: '/'});

pootle/templates/browser/overview.html
@@ -7,13 +7,6 @@
 {% block body_id %}{{ browser_body_id }}{% endblock %}
 {% block body_class %}{{ block.super }} js-not-loaded{% endblock %}
 
-{% block breadcrumbs %}
-  {{ block.super }}
-  {% if resource_path %}
-    <li class="file-path"><a href="{% url 'pootle-tp-overview' language.code project.code '' %}">&lt; {% trans "root folder" %} &gt;</a> / {{ resource_obj|trail:"/" }}</li>
-  {% endif %}
-{% endblock %}
-
 {% block header_content_block %}
 <div id="top-stats" class="header">
   {% block header_content %}

pootle/templates/core/_breadcrumbs.html
@@ -20,3 +20,18 @@
     {% endfor %}
   </select>
 </li>
+{% if project %}
+<li>
+  <select id="js-select-resource"
+          data-initial-code="{{ resource_path }}"
+    style="visibility: hidden;">
+    <option value=""></option>
+    {% for res in project.resources %}
+    <option value="{{ res.path }}"
+      {% if res.path == resource_path %} selected="selected"{% endif %}>
+      {{ res.path }}
+    </option>
+    {% endfor %}
+  </select>
+</li>
+{% endif %}

commit 8a325b1bea76ee7e717ba6e3466457c3fcf48c98

pootle_project: Fix a SQL syntax issue introduced in d4f60c76

pootle/apps/pootle_project/models.py
@@ -194,8 +194,8 @@ class Project(models.Model, TreeItem):
 
         store_objs = Store.objects.extra(
             where=[
-                '`pootle_store_store`.`pootle_path` LIKE %s',
-                '`pootle_store_store`.`pootle_path` NOT LIKE %s',
+                'pootle_store_store.pootle_path LIKE %s',
+                'pootle_store_store.pootle_path NOT LIKE %s',
             ], params=[resources_path, '/templates/%']
         ).select_related('parent').distinct()
 