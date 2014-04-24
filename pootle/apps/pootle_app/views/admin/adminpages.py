df59248895cc60f03c84f38252c5e8043b68b3e7
Leandro Regueiro <leandro.regueiro@gmail.com>
2014-04-24 13:04:53 +0200

    Cleanup: Remove legacy comment

--- pootle/apps/pootle_app/views/admin/adminpages.py
+++ pootle/apps/pootle_app/views/admin/adminpages.py
@@ -5,8 +5,6 @@
 #
 # This file is part of Pootle.
 #
-# This file is somewhat based on the older Pootle/translatepage.py
-#
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
 # Foundation; either version 2 of the License, or (at your option) any later
