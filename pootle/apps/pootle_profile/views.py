cf22fc51cc8ba88abdbb6ec9782f57ba04a6f9f4
Julen Ruiz Aizpuru <julenx@gmail.com>
2014-04-28 15:20:08 +0200

    Added missing import

--- pootle/apps/pootle_profile/views.py
+++ pootle/apps/pootle_profile/views.py
@@ -19,6 +19,7 @@
 
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.utils.encoding import iri_to_uri
