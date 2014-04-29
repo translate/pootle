ded69a99a8569dd124d6bde12649e4d2ce8cb36f
Leandro Regueiro <leandro.regueiro@gmail.com>
2014-04-29 19:22:33 +0200

    More cleanups

--- pootle/apps/pootle_misc/stats.py
+++ pootle/apps/pootle_misc/stats.py
@@ -49,12 +49,11 @@ def get_translate_actions(path_obj):
         # import issues.
         from pootle_tagging.models import Goal
 
-        pootle_path = path_obj.pootle_path
         goal = Goal.get_most_important_incomplete_for_path(path_obj)
 
         if goal is not None:
             goal_words = goal.get_incomplete_words_in_path(path_obj)
-            goal_url = goal.get_translate_url_for_path(pootle_path,
+            goal_url = goal.get_translate_url_for_path(path_obj.pootle_path,
                                                        state='incomplete')
             if goal_words > 0:
                 goals_summary.extend([
@@ -71,12 +70,13 @@ def get_translate_actions(path_obj):
                               {'num': goal_words, }),
                 ])
 
-    return {'is_dir': path_obj.is_dir,
-            'goals_summary': u''.join(goals_summary),
-            'translate_url': path_obj.get_translate_url(state='all'),
-            'incomplete_url': path_obj.get_translate_url(state='incomplete'),
-            'suggestions_url': path_obj.get_translate_url(state='suggestions'),
-            'critical_url': path_obj.get_critical_url(),
+    return {
+        'is_dir': path_obj.is_dir,
+        'goals_summary': u''.join(goals_summary),
+        'translate_url': path_obj.get_translate_url(state='all'),
+        'incomplete_url': path_obj.get_translate_url(state='incomplete'),
+        'suggestions_url': path_obj.get_translate_url(state='suggestions'),
+        'critical_url': path_obj.get_critical_url(),
     }
 
 
