fd275f55e4b44c1d8df08e15d2345ad64a104f8c
Julen Ruiz Aizpuru <julenx@gmail.com>
2014-06-05 11:45:20 +0200

    Tests: don't use mutable type as a default argument

--- tests/forms/unit.py
+++ tests/forms/unit.py
@@ -27,8 +27,11 @@ from pootle_store.util import FUZZY, TRANSLATED, UNTRANSLATED
 from pootle_store.forms import unit_form_factory, UnitStateField
 
 
-def _create_post_request(rf, directory, user, url='/', data={}):
+def _create_post_request(rf, directory, user, url='/', data=None):
     """Convenience function to create and setup fake POST requests."""
+    if data is None:
+        data = {}
+
     User = get_user_model()
 
     request = rf.post(url, data=data)
