ba6b340737d5465b7d2d5e526e947d06ed105e9a
Leandro Regueiro <leandro.regueiro@gmail.com>
2014-06-13 22:00:25 +0200

    Bump up Django requirement: Bit missed in c17660f8

--- pootle/depcheck.py
+++ pootle/depcheck.py
@@ -28,7 +28,7 @@ from django.conf import settings
 TTK_MINIMUM_REQUIRED_VERSION = (1, 11, 0)
 
 # Minimum Django version required for Pootle to run.
-DJANGO_MINIMUM_REQUIRED_VERSION = (1, 5, 6)
+DJANGO_MINIMUM_REQUIRED_VERSION = (1, 5, 8)
 
 # Minimum lxml version required for Pootle to run.
 LXML_MINIMUM_REQUIRED_VERSION = (2, 1, 4, 0)
