10a5caf9ba25ef3a1587ea88aba0a723b28263dd
Jerome Leclanche <jerome@leclan.ch>
2014-07-21 23:09:57 +0100

    Accounts: Do not run constraint-related migrations on sqlite3
    See SOUTH-775: http://south.aeracode.org/ticket/775

--- pootle/apps/pootle_store/migrations/0011_update_user_to_profile.py
+++ pootle/apps/pootle_store/migrations/0011_update_user_to_profile.py
@@ -17,6 +17,12 @@ class Migration(SchemaMigration):
             # 4. Update all Unit.submitted_by fields
             # 5. Update all Unit.commented_by fields
 
+            def clean_constraints(t1, c1, t2, c2):
+                # Workaround for http://south.aeracode.org/ticket/775
+                if db.backend_name != "sqlite3":
+                    db.delete_foreign_key(t1, c1)
+                    db.execute(db.foreign_key_sql(t1, c1, t2, c2))
+
             print("Starting migration from Profile to User.")
             print("This may take anywhere between a few minutes to several hours, depending on the size of your projects.")
 
@@ -25,48 +31,42 @@ class Migration(SchemaMigration):
                           (select pootle_app_pootleprofile.user_id FROM pootle_app_pootleprofile
                            WHERE pootle_app_pootleprofile.id = pootle_store_suggestion.user_id)
                        """)
-            db.delete_foreign_key("pootle_store_suggestion", "user_id")
-            db.execute(db.foreign_key_sql("pootle_store_suggestion", "user_id", "accounts_user", "id"))
+            clean_constraints("pootle_store_suggestion", "user_id", "accounts_user", "id")
 
             print("Migrating Suggestion.reviewer (Step 2 of 6)")
             db.execute("""UPDATE pootle_store_suggestion SET reviewer_id =
                           (select pootle_app_pootleprofile.user_id FROM pootle_app_pootleprofile
                            WHERE pootle_app_pootleprofile.id = pootle_store_suggestion.reviewer_id)
                        """)
-            db.delete_foreign_key("pootle_store_suggestion", "reviewer_id")
-            db.execute(db.foreign_key_sql("pootle_store_unit", "submitted_by_id", "accounts_user", "id"))
+            clean_constraints("pootle_store_suggestion", "reviewer_id", "accounts_user", "id")
 
             print("Migrating TMUnit.submitted_by (Step 3 of 6)")
             db.execute("""UPDATE pootle_store_tmunit SET submitted_by_id =
                           (select pootle_app_pootleprofile.user_id FROM pootle_app_pootleprofile
                            WHERE pootle_app_pootleprofile.id = pootle_store_tmunit.submitted_by_id)
                        """)
-            db.delete_foreign_key("pootle_store_tmunit", "submitted_by_id")
-            db.execute(db.foreign_key_sql("pootle_store_tmunit", "submitted_by_id", "accounts_user", "id"))
+            clean_constraints("pootle_store_tmunit", "submitted_by_id", "accounts_user", "id")
 
             print("Migrating Unit.submitted_by (Step 4 of 6)")
             db.execute("""UPDATE pootle_store_unit SET submitted_by_id =
                           (select pootle_app_pootleprofile.user_id FROM pootle_app_pootleprofile
                            WHERE pootle_app_pootleprofile.id = pootle_store_unit.submitted_by_id)
                        """)
-            db.delete_foreign_key("pootle_store_unit", "submitted_by_id")
-            db.execute(db.foreign_key_sql("pootle_store_unit", "submitted_by_id", "accounts_user", "id"))
+            clean_constraints("pootle_store_unit", "submitted_by_id", "accounts_user", "id")
 
             print("Migrating Unit.commented_by (Step 5 of 6)")
             db.execute("""UPDATE pootle_store_unit SET commented_by_id =
                           (select pootle_app_pootleprofile.user_id FROM pootle_app_pootleprofile
                            WHERE pootle_app_pootleprofile.id = pootle_store_unit.commented_by_id)
                        """)
-            db.delete_foreign_key("pootle_store_unit", "commented_by_id")
-            db.execute(db.foreign_key_sql("pootle_store_unit", "commented_by_id", "accounts_user", "id"))
+            clean_constraints("pootle_store_unit", "commented_by_id", "accounts_user", "id")
 
             print("Migrating Submission.submitter_id (Step 6 of 6)")
             db.execute("""UPDATE pootle_app_submission SET submitter_id =
                           (select pootle_app_pootleprofile.user_id FROM pootle_app_pootleprofile
                            WHERE pootle_app_pootleprofile.id = pootle_app_submission.submitter_id)
                        """)
-            db.delete_foreign_key("pootle_app_submission", "submitter_id")
-            db.execute(db.foreign_key_sql("pootle_app_submission", "submitter_id", "accounts_user", "id"))
+            clean_constraints("pootle_app_submission", "submitter_id", "accounts_user", "id")
 
     def backwards(self, orm):
         raise NotImplementedError
