74dec60480024329c893432338f04f4b67a6783d
Taras Semenenko <taras.semenenko@gmail.com>
2014-09-05 19:48:40 +0400

    Set no parents for any Project
    children() method of `/projects/` Directory children is not used when
    project list is rendered (ProjectSet is used instead).
    There is no need to update (cached) stats for `/projects/` Directory.

--- pootle/apps/pootle_project/models.py
+++ pootle/apps/pootle_project/models.py
@@ -370,9 +370,6 @@ class Project(models.Model, TreeItem, ProjectURLMixin):
     def get_cachekey(self):
         return self.directory.pootle_path
 
-    def get_parents(self):
-        return [Directory.objects.projects]
-
     ### /TreeItem
 
     def get_real_path(self):
