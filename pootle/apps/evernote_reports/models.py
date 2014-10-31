5d0e2449ea0d88c62f48cdfd5d9c02126233a2cd
Taras Semenenko <taras.semenenko@gmail.com>
2014-10-31 15:06:45 +0300

    Add task month info to paid task logging

--- pootle/apps/evernote_reports/models.py
+++ pootle/apps/evernote_reports/models.py
@@ -57,6 +57,6 @@ class PaidTask(models.Model):
         return dict(self.type_choices).get(task_type, '')
 
     def __unicode__(self):
-        return u'Task: [id=%s, user=%s, type=%s, amount=%s, comment=%s]' % \
+        return u'Task: [id=%s, user=%s, type=%s, amount=%s, comment=%s, month=%s]' % \
             (self.id, self.user.username, PaidTask.get_task_type_title(self.task_type),
-             self.amount, self.description)
+             self.amount, self.description, self.date.strftime('%Y-%m'))
