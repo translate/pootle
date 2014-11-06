5443ea81fc956f5cdf5b24c3b788086daefcff27
Julen Ruiz Aizpuru <julenx@gmail.com>
2014-11-06 13:10:28 +0100

    Messages: converted code into a CommonJS module
    Part of #3316.

--- pootle/static/js/msg.js
+++ pootle/static/js/msg.js
@@ -1,72 +1,79 @@
-window.PTL = window.PTL || {};
+'use strict';
 
-PTL.msg = PTL.msg || {};
-
-(function (ns, $) {
-
-  var Message = Backbone.Model.extend({
-    defaults: {
-      text: '',
-      level: 'info',
-      language: 'en'
-    }
-  });
-
-  var MessageList = Backbone.Collection.extend({
-    model: Message
-  });
-
-  var MessageView = Backbone.View.extend({
-    className: 'alert alert-block',
-
-    template: _.template('<%= text %>'),
-
-    render: function () {
-      this.$el.addClass(['alert', this.model.get('level')].join('-'));
-      this.$el.attr('lang', this.model.get('language'));
-
-      this.$el.html(this.template(this.model.toJSON())).hide().fadeIn(300);
-
-      return this;
-    },
-
-  });
-
-  var MessageListView = Backbone.View.extend({
-    el: '.js-alerts',
-
-    initialize: function () {
-      this.subViews = [];
-
-      this.listenTo(this.collection, 'add', this.add);
-      this.listenTo(this.collection, 'remove', this.remove);
-    },
-
-    add: function (msg) {
-      var msgView = new MessageView({model: msg});
-      this.subViews.push(msgView);
-
-      this.$el.prepend(msgView.render().el);
-    },
-
-    remove: function (msg) {
-      var currentView = _.find(this.subViews, function (view) {
-        return view.model === msg;
-      });
-      this.subViews = _(this.subViews).without(currentView);
-
-      currentView.$el.fadeOut(3500, function () {
-        currentView.remove();
-      });
-    }
-
-  });
+var $ = require('jquery');
+var _ = require('underscore');
+var Backbone = require('backbone');
 
 
-  var messages = new MessageList(),
-      messagesView;
+var Message = Backbone.Model.extend({
+  defaults: {
+    text: '',
+    level: 'info',
+    language: 'en'
+  }
+});
 
-  ns.show = function (opts) {
+
+var MessageList = Backbone.Collection.extend({
+  model: Message
+});
+
+
+var MessageView = Backbone.View.extend({
+  className: 'alert alert-block',
+
+  template: _.template('<%= text %>'),
+
+  render: function () {
+    this.$el.addClass(['alert', this.model.get('level')].join('-'));
+    this.$el.attr('lang', this.model.get('language'));
+
+    this.$el.html(this.template(this.model.toJSON())).hide().fadeIn(300);
+
+    return this;
+  },
+
+});
+
+
+var MessageListView = Backbone.View.extend({
+  el: '.js-alerts',
+
+  initialize: function () {
+    this.subViews = [];
+
+    this.listenTo(this.collection, 'add', this.add);
+    this.listenTo(this.collection, 'remove', this.remove);
+  },
+
+  add: function (msg) {
+    var msgView = new MessageView({model: msg});
+    this.subViews.push(msgView);
+
+    this.$el.prepend(msgView.render().el);
+  },
+
+  remove: function (msg) {
+    var currentView = _.find(this.subViews, function (view) {
+      return view.model === msg;
+    });
+    this.subViews = _(this.subViews).without(currentView);
+
+    currentView.$el.fadeOut(3500, function () {
+      currentView.remove();
+    });
+  }
+
+});
+
+
+var messages = new MessageList(),
+    messagesView;
+
+
+var msg = {
+
+  show: function (opts) {
     if (!messagesView) {
       messagesView = new MessageListView({collection: messages});
     }
@@ -77,6 +84,9 @@ PTL.msg = PTL.msg || {};
     window.setTimeout(function () {
       messages.remove(msg);
     }, 2000);
-  };
+  }
 
-}(PTL.msg, jQuery));
+};
+
+
+module.exports = msg;
