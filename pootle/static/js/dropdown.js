759ccc03d0420271b130522c8a042cd5b4d25fea
Julen Ruiz Aizpuru <julenx@gmail.com>
2014-11-06 13:09:42 +0100

    Dropdown: converted code into a CommonJS module
    Part of #3316.

--- pootle/static/js/dropdown.js
+++ pootle/static/js/dropdown.js
@@ -1,113 +1,118 @@
-window.PTL = window.PTL || {};
+'use strict';
 
-PTL.dropdown = PTL.dropdown || {};
-
-(function (ns, $) {
-
-  var sel = {
-    data: {
-      toggle: '[data-action="toggle"]'
-    },
-    target: '.dropdown-menu',
-    targetItems : 'li:not(.menu-divider) a',
-    open: 'show-dropdown'
-  };
-
-  var keys = {
-    ESC: 27,
-    UP: 38,
-    DOWN: 40
-  };
+var $ = require('jquery');
+var Backbone = require('backbone');
 
 
-  var DropdownView = Backbone.View.extend({
+var sel = {
+  data: {
+    toggle: '[data-action="toggle"]'
+  },
+  target: '.dropdown-menu',
+  targetItems : 'li:not(.menu-divider) a',
+  open: 'show-dropdown'
+};
 
-    events: function () {
-      var events = {
-        'keydown': 'onKey'
-      };
-      events['click ' + sel.data.toggle] = 'toggle';
+var keys = {
+  ESC: 27,
+  UP: 38,
+  DOWN: 40
+};
 
-      return events;
-    },
 
-    initialize: function () {
-      this.$toggle = this.$(sel.data.toggle);
-      this.$target = this.$(sel.target);
+var DropdownView = Backbone.View.extend({
 
-      this.$target.css('width', this.$el.width());
+  events: function () {
+    var events = {
+      'keydown': 'onKey'
+    };
+    events['click ' + sel.data.toggle] = 'toggle';
 
-      $(document).on('click.PTL.dropdown', this.hide.bind(this));
-    },
+    return events;
+  },
 
-    onKey: function (e) {
-      // Avoid hijacking browser keyboard shortcuts when not shown
-      if (!this.isVisible()) {
-        return true;
-      }
+  initialize: function () {
+    this.$toggle = this.$(sel.data.toggle);
+    this.$target = this.$(sel.target);
 
-      if ([keys.ESC, keys.UP, keys.DOWN].indexOf(e.which) !== -1) {
-        e.preventDefault();
-      }
+    this.$target.css('width', this.$el.width());
 
-      if (e.which === keys.ESC) {
-        return this.$toggle.click();
-      }
+    $(document).on('click.PTL.dropdown', this.hide.bind(this));
+  },
 
-      if ([keys.UP, keys.DOWN].indexOf(e.which) !== -1) {
-        var $items = this.$target.find(sel.targetItems);
-
-        if (!$items.length) {
-          return false;
-        }
-
-        var index = $items.index($items.filter(':focus'));
-
-        if (e.which === keys.UP && index > 0) {
-          index--;
-        }
-        if (e.which === keys.DOWN && index < $items.length - 1) {
-          index++;
-        }
-        if (!index) {
-          index = 0;
-        }
-
-        $items.eq(index).trigger('focus');
-      }
-    },
-
-    isVisible: function () {
-      return this.$el.hasClass(sel.open);
-    },
-
-    show: function () {
-      !this.isVisible() && this.$el.addClass(sel.open);
-    },
-
-    hide: function () {
-      this.isVisible() && this.$el.removeClass(sel.open);
-    },
-
-    toggle: function (e) {
-      e.preventDefault();
-      e.stopPropagation();
-
-      this.$el.toggleClass(sel.open);
-
-      if (this.isVisible()) {
-        this.$toggle.focus();
-      } else {
-        this.$toggle.blur();
-      }
+  onKey: function (e) {
+    // Avoid hijacking browser keyboard shortcuts when not shown
+    if (!this.isVisible()) {
+      return true;
     }
 
-  });
+    if ([keys.ESC, keys.UP, keys.DOWN].indexOf(e.which) !== -1) {
+      e.preventDefault();
+    }
+
+    if (e.which === keys.ESC) {
+      return this.$toggle.click();
+    }
+
+    if ([keys.UP, keys.DOWN].indexOf(e.which) !== -1) {
+      var $items = this.$target.find(sel.targetItems);
+
+      if (!$items.length) {
+        return false;
+      }
+
+      var index = $items.index($items.filter(':focus'));
+
+      if (e.which === keys.UP && index > 0) {
+        index--;
+      }
+      if (e.which === keys.DOWN && index < $items.length - 1) {
+        index++;
+      }
+      if (!index) {
+        index = 0;
+      }
+
+      $items.eq(index).trigger('focus');
+    }
+  },
+
+  isVisible: function () {
+    return this.$el.hasClass(sel.open);
+  },
+
+  show: function () {
+    !this.isVisible() && this.$el.addClass(sel.open);
+  },
+
+  hide: function () {
+    this.isVisible() && this.$el.removeClass(sel.open);
+  },
+
+  toggle: function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    this.$el.toggleClass(sel.open);
+
+    if (this.isVisible()) {
+      this.$toggle.focus();
+    } else {
+      this.$toggle.blur();
+    }
+  }
+
+});
 
 
-  ns.init = function (el) {
+var dropdown = {
+
+  init: function (el) {
     el = el instanceof $ ? el : $(el);
     return new DropdownView({el: el});
-  };
+  }
 
-}(PTL.dropdown, jQuery));
+};
+
+
+module.exports = dropdown;
