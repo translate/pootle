d8057f9af986598b0fd13377c1ea84019879d74d
Dwayne Bailey <dwayne@translate.org.za>
2014-11-26 17:20:19 +0000

    setup: install_requires can process --allow-external and --allow-unverified

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def parse_requirements(file_name):
     requirements = []
     for line in open(file_name, 'r').read().split('\n'):
         # Ignore comments, blank lines and included requirements files
-        if re.match(r'(\s*#)|(\s*$)|(-r .*$)', line):
+        if re.match(r'(\s*#)|(\s*$)|((-r|--allow-external|--allow-unverified) .*$)', line):
             continue
 
         if re.match(r'\s*-e\s+', line):
