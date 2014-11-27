b42ca6a3dc777d4fb9bbc0d64f64a3afadad3c32
Dwayne Bailey <dwayne@translate.org.za>
2014-11-27 17:26:38 +0000

    pep8: port script from master

--- pootle/tools/pep8.sh
+++ pootle/tools/pep8.sh
@@ -15,8 +15,13 @@ if [ "$files" == ""  ]; then
 	files="."
 fi
 
+if [ "$select" == "travis" ]; then
+	# See http://pep8.readthedocs.org/en/latest/intro.html#error-codes
+	select="E10,E11,E251,E261,E262,E27,E401,E70,E711,E721,W191,W291,W292,W293,W391,W60"
+fi
+
 pep8 \
---exclude=djblets,registration,assets,profiles \
+--exclude=djblets,assets,migrations \
 --select=$select \
 --statistics \
 $files
