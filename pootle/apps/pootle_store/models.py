translate/pootle

commit fb3738f5bd62b48ecfa03cf4e99543ae4d6d0239

Record submissions for comments added from update_store command
Fix #3376

pootle/apps/pootle_store/models.py
@@ -1720,6 +1720,7 @@ class Store(models.Model, CachedTreeItem, base.TranslationStore):
                 if changed:
                     changes['updated'] += 1
                     create_subs = {}
+                    current_time = timezone.now()
 
                     if unit._target_updated:
                         create_subs[SubmissionFields.TARGET] = \
@@ -1729,7 +1730,12 @@ class Store(models.Model, CachedTreeItem, base.TranslationStore):
                         create_subs[SubmissionFields.STATE] = \
                             [old_unit_state, unit.state]
 
-                    current_time = timezone.now()
+                    if unit._comment_updated:
+                        unit.commented_by = system
+                        unit.commented_on = current_time
+                        create_subs[SubmissionFields.COMMENT] = \
+                            ['', unit.translator_comment]
+
                     # Create Submission after unit saved
                     for field in create_subs:
                         sub = Submission(

commit 333c75cc360ffcc6d5b153c28e5877dff4f797b0

Fix bug introduced in fb3738f
`new_value` field is not null, save empty string instead.
Related to #3376

pootle/apps/pootle_store/models.py
@@ -1735,7 +1735,7 @@ class Store(models.Model, CachedTreeItem, base.TranslationStore):
                         unit.commented_by = system
                         unit.commented_on = current_time
                         create_subs[SubmissionFields.COMMENT] = \
-                            ['', unit.translator_comment]
+                            ['', unit.translator_comment or '']
 
                     # Create Submission after unit saved
                     for field in create_subs: