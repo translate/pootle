ad87a83354b2072d972e4e5c22e52f181dde5190
Julen Ruiz Aizpuru <julenx@gmail.com>
2014-12-30 18:09:33 +0100

    Cleanup: made admin API collection logic reusable

---
+++ pootle/static/js/mixins/admin_api.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var _ = require('underscore');
+
+
+var AdminAPIMixin = {
+
+  initialize: function (opts) {
+    this.count = 0;
+    this.page = 0;
+    this.keywords = '';
+
+    this.on('add', this.incrCount);
+    this.on('remove', this.decrCount);
+  },
+
+  parse: function (response, options) {
+    this.count = response.count;
+
+    return response.models;
+  },
+
+  comparator: '-id',
+
+
+  /* Methods */
+
+  incrCount: function () {
+    this.count++;
+  },
+
+  decrCount: function () {
+    this.count--;
+  },
+
+  fetchNextPage: function (opts) {
+    var newPage = this.page + 1,
+        pageData = newPage === 1 ? {} : {p: newPage},
+        keywordsData = this.keywords === '' ? {} : {q: this.keywords},
+        reqData = _.extend({}, pageData, keywordsData),
+        fetchOpts = {remove: false, silent: true, data: reqData};
+
+    _.defaults(fetchOpts, opts);
+
+    return this.fetch(fetchOpts).done(function () {
+      this.page = newPage;
+    }.bind(this));
+  },
+
+  search: function (keywords) {
+    var opts = {};
+    if (keywords !== this.keywords) {
+      this.setSearch(keywords);
+      opts = {reset: true};
+    }
+    return this.fetchNextPage(opts);
+  },
+
+  setSearch: function (keywords) {
+    this.keywords = keywords;
+    this.page = 0;
+  },
+
+};
+
+
+module.exports = AdminAPIMixin;
--- pootle/static/js/models/user.js
+++ pootle/static/js/models/user.js
@@ -4,6 +4,8 @@ var _ = require('underscore');
 var Backbone = require('backbone');
 var md5 = require('md5').digest_s;
 
+var AdminAPIMixin = require('../mixins/admin_api');
+
 
 var metaUsers = ['nobody', 'default', 'system'];
 
@@ -62,68 +64,14 @@ var User = Backbone.Model.extend({
 });
 
 
-var UserSet = Backbone.Collection.extend({
+var UserSet = Backbone.Collection.extend(
+  _.extend({}, AdminAPIMixin, {
+
   model: User,
 
   url: l('/xhr/admin/users/'),
 
-  initialize: function (opts) {
-    this.count = 0;
-    this.page = 0;
-    this.keywords = '';
-
-    this.on('add', this.incrCount);
-    this.on('remove', this.decrCount);
-  },
-
-  parse: function (response, options) {
-    this.count = response.count;
-
-    return response.models;
-  },
-
-  comparator: '-id',
-
-
-  /* Methods */
-
-  incrCount: function () {
-    this.count++;
-  },
-
-  decrCount: function () {
-    this.count--;
-  },
-
-  fetchNextPage: function (opts) {
-    var newPage = this.page + 1,
-        pageData = newPage === 1 ? {} : {p: newPage},
-        keywordsData = this.keywords === '' ? {} : {q: this.keywords},
-        reqData = _.extend({}, pageData, keywordsData),
-        fetchOpts = {remove: false, silent: true, data: reqData};
-
-    _.defaults(fetchOpts, opts);
-
-    return this.fetch(fetchOpts).done(function () {
-      this.page = newPage;
-    }.bind(this));
-  },
-
-  search: function (keywords) {
-    var opts = {};
-    if (keywords !== this.keywords) {
-      this.setSearch(keywords);
-      opts = {reset: true};
-    }
-    return this.fetchNextPage(opts);
-  },
-
-  setSearch: function (keywords) {
-    this.keywords = keywords;
-    this.page = 0;
-  }
-
-});
+}));
 
 
 module.exports = {
