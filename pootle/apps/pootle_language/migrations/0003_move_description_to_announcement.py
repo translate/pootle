db439bf65baac7716da65445a29b7f7be6cebf78
Leandro Regueiro <leandro.regueiro@gmail.com>
2015-01-15 21:05:14 +0100

    Datamigration: Move Language descriptions to announcements
    Part of #3458.

---
+++ pootle/apps/pootle_language/migrations/0003_move_description_to_announcement.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+
+class Migration(DataMigration):
+    depends_on = (
+        ("staticpages", "0012_data__adjust_pks"),
+    )
+
+    def forwards(self, orm):
+        Announcement = orm['staticpages.StaticPage']
+
+        for language in orm.Language.objects.all():
+            if language.description.raw:
+                ann = Announcement(active=True, title="Language instructions",
+                                   body=language.description.raw,
+                                   virtual_path="announcements/"+language.code)
+                ann.save()
+
+    def backwards(self, orm):
+        pass
+
+    models = {
+        'pootle_app.directory': {
+            'Meta': {'ordering': "['name']", 'object_name': 'Directory'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'obsolete': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'child_dirs'", 'null': 'True', 'to': "orm['pootle_app.Directory']"}),
+            'pootle_path': ('django.db.models.fields.CharField', [], {'max_length': '255', 'db_index': 'True'})
+        },
+        u'pootle_language.language': {
+            'Meta': {'ordering': "['code']", 'object_name': 'Language', 'db_table': "'pootle_app_language'"},
+            'code': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50', 'db_index': 'True'}),
+            'description': ('pootle.core.markup.fields.MarkupField', [], {'blank': 'True'}),
+            'directory': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['pootle_app.Directory']", 'unique': 'True'}),
+            'fullname': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'nplurals': ('django.db.models.fields.SmallIntegerField', [], {'default': '0'}),
+            'pluralequation': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+            'specialchars': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'})
+        },
+        'staticpages.staticpage': {
+            'Meta': {'object_name': 'StaticPage'},
+            'active': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'body': ('pootle.core.markup.fields.MarkupField', [], {'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified_on': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'auto_now_add': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'blank': 'True'}),
+            'virtual_path': ('django.db.models.fields.CharField', [], {'default': "''", 'unique': 'True', 'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['pootle_language']
+    symmetrical = True
