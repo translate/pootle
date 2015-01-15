e686fbefa0b8c97e82ca5764638bdf14cb80cd64
Leandro Regueiro <leandro.regueiro@gmail.com>
2015-01-15 21:23:02 +0100

    Remove remnant of dropping Project description
    Not necessary, but a cleaner API code might help when bringing it back.

--- pootle/apps/pootle_project/api.py
+++ pootle/apps/pootle_project/api.py
@@ -44,7 +44,6 @@ class ProjectResource(StatisticsModelResource):
         fields = [
             'checkstyle',
             'code',
-            'description',
             'fullname',
             'ignoredfiles',
             'localfiletype',
