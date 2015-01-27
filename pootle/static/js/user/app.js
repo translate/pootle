ebbcd1215d30169fe6728830f63779faa46fbb8f
Julen Ruiz Aizpuru <julenx@gmail.com>
2015-01-27 14:13:02 +0100

    Cleanup: no need for react/addons

--- pootle/static/js/user/app.js
+++ pootle/static/js/user/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var React = require('react/addons');
+var React = require('react');
 
 var User = require('../models/user').User;
 var UserProfileEditClass = require('./components/edit');
