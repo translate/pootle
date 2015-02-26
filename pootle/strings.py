75146602fb4149a23be52bfd8b94393023651958
Dwayne Bailey <dwayne@translate.org.za>
2015-02-26 13:48:56 +0000

    l10n: gut strings.py
    Remove all but the static pages strings.  We don't seem to need any of
    these at the moment so lets rather start building these up from scratch
    as needed.

--- pootle/strings.py
+++ pootle/strings.py
@@ -15,47 +15,13 @@
 # 3. If we ever rely on large parts of text from an upstream app, rather
 #    consider having translators work on the upstream translations.
 
+#########
+# Django
+#########
 
-
-_('Hold down "Control", or "Command" on a Mac, to select more than one.')
-_("Enter a valid e-mail address.")
-_("That e-mail address doesn't have an associated user account. Are you sure you've registered?")
-_("This field is required.")
-_("Ensure this value has at most %(max)d characters (it has %(length)d).")
-_("Save and add another")
-_("Save and continue editing")
-_("Add")
-_("Change")
-_("Please enter a correct username and password. Note that both fields are case-sensitive.")
-# from Django/django/contrib/auth/forms.py:
-# Translators: Email subject line. The variable is the server name shown
-# on the top of each page
-_("Password reset on %s")
-
-# Translators: This is a constructed string that might end up saying
-# "Add language" or "Add project". If you need to translate that differently,
-# you might want to try to translate it as "Add an entry to the %(name)s table"
-# or "Add an object of type "%(name)s".
-_("Add %(name)s")
-
-# Translators: This link takes the user to the page where the currently
-# configured option is displayed (such as a project or language)
-_("View on site")
-
-_("first name")
-_("last name")
-_("e-mail address")
-_("active")
-
-# Translators: Please translate this simply as "Administrator". This string is
-# needed by Django, but 'superuser' is very technical and should be avoided.
-_("superuser status")
-
-# Translators: The language from which translation will happen
-_("Source Language")
-
-_("Android strings")
-
+#######
+# Apps
+#######
 
 ###############
 # Static pages
