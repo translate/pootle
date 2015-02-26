0519ff88c04dad9e03d5e48b4eedcf7b9424220c
Dwayne Bailey <dwayne@translate.org.za>
2015-02-26 14:35:26 +0000

    l10n: ensure correct Plural headers in pseudolocalised files

--- pootle/tools/test_localisation.sh
+++ pootle/tools/test_localisation.sh
@@ -24,8 +24,13 @@ mkdir -p locale/$lang
 podebug --rewrite=$style --progress=none locale/templates/pootle.pot locale/$lang/pootle.po
 podebug --rewrite=$style --progress=none locale/templates/pootle_js.pot locale/$lang/pootle_js.po
 
-sed -i .bak "s/INTEGER/2/g;s/EXPRESSION/(n!=1)/g" locale/$lang/pootle.po
-sed -i .bak "s/INTEGER/2/g;s/EXPRESSION/(n!=1)/g" locale/$lang/pootle_js.po
+for pofile in locale/$lang/pootle.po locale/$lang/pootle_js.po
+do
+	[[ $(egrep "INTEGER" $pofile) ]] || sed -i .bak '/^"Content-Transfer-Encoding: /a\
+"Plural-Forms: nplurals=2; plural=(n!=1);\\n"
+' $pofile
+	sed -i .bak "s/INTEGER/2/g;s/EXPRESSION/(n!=1)/g" $pofile
+done
 
 cd ..
 ./setup.py build_mo --lang=$lang
