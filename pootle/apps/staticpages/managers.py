e11ce129f4ae97e9bcf7c0a8b9ebd6294410cc15
Julen Ruiz Aizpuru <julenx@gmail.com>
2015-03-03 13:37:47 +0100

    Cleanup: removed unused import

--- pootle/apps/staticpages/managers.py
+++ pootle/apps/staticpages/managers.py
@@ -20,7 +20,7 @@
 # along with translate; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-from django.db.models import F, Manager, Q
+from django.db.models import F, Manager
 
 
 class PageManager(Manager):
