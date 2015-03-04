9a85460e1c5e7e4b306c186667cf5eba3fcdb3d6
Leandro Regueiro <leandro.regueiro@gmail.com>
2015-03-04 18:22:17 +0100

    VFolder: Relate new units with existing matching virtual folders
    Fixes #3635.

--- pootle/apps/virtualfolder/models.py
+++ pootle/apps/virtualfolder/models.py
@@ -9,6 +9,8 @@
 
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils.translation import ugettext_lazy as _
 
 from pootle.core.markup import get_markup_filter_name, MarkupField
@@ -156,3 +158,28 @@ class VirtualFolder(models.Model):
                     for proj in projects]
 
         return [self.location]
+
+
+@receiver(post_save, sender=Unit)
+def relate_unit(sender, instance, created=False, **kwargs):
+    """Add newly created units to the virtual folders they belong, if any.
+
+    When a new store or translation project, or even a full project is added,
+    some of their units might be matched by the filters of any of the
+    previously existing virtual folders, so this signal handler relates those
+    new units to the virtual folders they belong to, if any.
+    """
+    if not created:
+        return
+
+    pootle_path = instance.store.pootle_path
+
+    for vf in VirtualFolder.objects.iterator():
+        for location in vf.get_all_pootle_paths():
+            if not pootle_path.startswith(location):
+                continue
+
+            for filename in vf.filter_rules.split(","):
+                if pootle_path == "".join([location, filename]):
+                    vf.units.add(instance)
+                    break
