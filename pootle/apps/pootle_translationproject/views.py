translate/pootle

commit 4859e54c19d475d1c08b166a1351ee1f98e18bcb

Offline: Display form for uploading files

pootle/apps/pootle_translationproject/views.py
@@ -101,14 +101,19 @@ def overview(request, translation_project, dir_path, filename=None):
 
     ctx = get_overview_context(request)
 
-    if settings.POOTLE_ENABLE_OFFLINE and request.user.is_authenticated():
+    # TODO improve plugin logic
+    if "import_export" in settings.INSTALLED_APPS and request.user.is_authenticated():
+        from import_export.views import handle_upload_form
         from pootle_app.models.permissions import check_permission
+
+        ctx.update(handle_upload_form(request))
 
         if (check_permission("translate", request) or
             check_permission("suggest", request)):
 
             ctx.update({
-                'display_download': True,
+                "display_download": True,
+                "display_sidebar": True,
             })
 
     ctx.update({
@@ -121,6 +126,7 @@ def overview(request, translation_project, dir_path, filename=None):
 
         'announcement': announcement,
         'announcement_displayed': display_announcement,
+        'display_sidebar': True,
     })
 
     if store is None:

pootle/apps/import_export/views.py
@@ -19,11 +19,12 @@
 
 import os
 from io import BytesIO
-from zipfile import ZipFile
+from zipfile import ZipFile, is_zipfile
 from django.core.servers.basehttp import FileWrapper
 from django.http import Http404, HttpResponse
-from pootle_store.models import Store
 from translate.storage import po
+from pootle_store.models import Store
+from .forms import UploadForm
 
 
 def download(contents, name, content_type):
@@ -69,6 +70,8 @@ def _import_file(file):
     if not pootle_path:
         raise ValueError("File %r missing X-Pootle-Path header\n" % (file.name))
 
+    # TODO: assert pofile.mtime >= whatever_is_in_the_db.mtime
+
     try:
         store, created = Store.objects.get_or_create(pootle_path=pootle_path)
     except Exception as e:
@@ -77,14 +80,33 @@ def _import_file(file):
     store.update(overwrite=True, store=pofile)
 
 
-def import_(request):
-    for file in request.FILES.values():
-        if is_zipfile(file):
-            with ZipFile(filename, "r") as zf:
-                for path in zf.namelist():
-                    _import_file(f)
-        else:
-            with open(file) as f:
-                _import_file(f)
+def handle_upload_form(request):
+    """Process the upload form."""
+    if request.method == "POST" and "file" in request.FILES:
+        upload_form = UploadForm(request.POST, request.FILES)
 
-    return HttpResponse("OK")
+        if upload_form.is_valid():
+            django_file = request.FILES["file"]
+            try:
+                if is_zipfile(django_file):
+                    with ZipFile(django_file, "r") as zf:
+                        for path in zf.namelist():
+                            with zf.open(path, "r") as f:
+                                _import_file(f)
+                else:
+                    # It is necessary to seek to the beginning because
+                    # is_zipfile fucks the file, and thus cannot be read.
+                    django_file.seek(0)
+                    _import_file(django_file)
+            except Exception as e:
+                upload_form.add_error("file", e.message)
+                return {
+                    "upload_form": upload_form,
+                    "display_sidebar": True,
+                }
+
+    # Always return a blank upload form unless the upload form is not valid.
+    return {
+        "upload_form": UploadForm(),
+        "display_sidebar": True,
+    }

pootle/apps/import_export/forms.py
@@ -1,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2015 Zuza Software Foundation
+#
+# This file is part of Pootle.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+from django import forms
+from django.utils.translation import ugettext as _
+
+
+class UploadForm(forms.Form):
+    file = forms.FileField(required=True, label=_('File'))

pootle/templates/browser/overview.html
@@ -75,7 +75,7 @@
 {% endblock %}
 
 {% block pre_content %}
-{% if announcement or display_download %}
+{% if display_sidebar %}
 <div id="sidebar"
   class="js-sidebar{% if announcement_displayed %} sidebar-open{% endif %}">
 
@@ -84,6 +84,17 @@
     title="{% trans 'Download translation files' %}">
     <i class="icon-download"></i> <span>{% trans 'Download' %}</span>
   </a>
+  {% endif %}
+
+  {% if upload_form %}
+  <form action="" method="post" enctype="multipart/form-data">
+    {% csrf_token %}
+    {% for field in upload_form %}
+      {{ field }}
+      {{ field.errors }}
+    {% endfor %}
+    <input type="submit" value="{% trans 'Upload' %}" />
+  </form>
   {% endif %}
 
   {% if announcement %}

commit 2e2739b2789a4c0990826a7e78018c2130874219

Overview: don't always display sidebar
Fixes regression introduced in 4859e54c.

pootle/apps/pootle_translationproject/views.py
@@ -126,7 +126,6 @@ def overview(request, translation_project, dir_path, filename=None):
 
         'announcement': announcement,
         'announcement_displayed': display_announcement,
-        'display_sidebar': True,
     })
 
     if store is None: