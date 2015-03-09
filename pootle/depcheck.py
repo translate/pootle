c3e83750a09c322db1e705098c5e706e0cb00378
Julen Ruiz Aizpuru <julenx@gmail.com>
2015-03-09 22:33:17 +0100

    Requirements: Django 1.7.6

--- pootle/depcheck.py
+++ pootle/depcheck.py
@@ -27,7 +27,7 @@ from django.conf import settings
 TTK_MINIMUM_REQUIRED_VERSION = (1, 11, 0)
 
 # Minimum Django version required for Pootle to run.
-DJANGO_MINIMUM_REQUIRED_VERSION = (1, 7, 5)
+DJANGO_MINIMUM_REQUIRED_VERSION = (1, 7, 6)
 
 # Minimum lxml version required for Pootle to run.
 LXML_MINIMUM_REQUIRED_VERSION = (2, 2, 2, 0)
