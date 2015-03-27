14d4e9e2c06ce2a8f4f3541399c3c4f93545ba4f
Jerome Leclanche <jerome@leclan.ch>
2015-03-27 14:01:04 +0100

    Checks: Minor style cleanups

--- pootle/checks.py
+++ pootle/checks.py
@@ -47,19 +47,22 @@ def check_library_versions(app_configs=None, **kwargs):
     errors = []
 
     if django_version < DJANGO_MINIMUM_REQUIRED_VERSION:
-        errors.append(checks.Critical(_("Your version of Django is too old."),
+        errors.append(checks.Critical(
+            _("Your version of Django is too old."),
             hint=_("Try pip install --upgrade Django"),
             id="pootle.C002",
         ))
 
     if lxml_version < LXML_MINIMUM_REQUIRED_VERSION:
-        errors.append(checks.Warning(_("Your version of lxml is too old."),
+        errors.append(checks.Warning(
+            _("Your version of lxml is too old."),
             hint=_("Try pip install --upgrade lxml"),
             id="pootle.W003",
         ))
 
     if ttk_version < TTK_MINIMUM_REQUIRED_VERSION:
-        errors.append(checks.Critical(_("Your version of Translate Toolkit is too old."),
+        errors.append(checks.Critical(
+            _("Your version of Translate Toolkit is too old."),
             hint=_("Try pip install --upgrade translate-toolkit"),
             id="pootle.C003",
         ))
@@ -74,8 +77,9 @@ def check_optional_dependencies(app_configs=None, **kwargs):
     try:
         import Levenshtein
     except ImportError:
-        errors.append(checks.Warning(_("Can't find python-levenshtein package. Updating against "
-            "templates is faster with python-levenshtein."),
+        errors.append(checks.Warning(
+            _("Can't find python-levenshtein package. "
+              "Updating against templates is faster with python-levenshtein."),
             hint=_("Try pip install python-levenshtein"),
             id="pootle.W010",
         ))
@@ -104,14 +108,16 @@ def check_redis(app_configs=None, **kwargs):
             # We need to check we're not running manage.py rqworker right now..
             import sys
             if len(sys.argv) > 1 and sys.argv[1] in RQWORKER_WHITELIST:
-                errors.append(checks.Warning(_("No RQ Worker running."),
+                errors.append(checks.Warning(
+                    _("No RQ Worker running."),
                     hint=_("Run new workers with manage.py rqworker"),
                     id="pootle.W001",
                 ))
 
         redis_version = queue.connection.info()["redis_version"].split(".")
         if tuple(int(x) for x in redis_version) < REDIS_MINIMUM_REQUIRED_VERSION:
-            errors.append(checks.Warning(_("Your version of Redis is too old."),
+            errors.append(checks.Warning(
+                _("Your version of Redis is too old."),
                 hint=_("Update your system's Redis server package"),
                 id="pootle.W002",
             ))
@@ -126,7 +132,8 @@ def check_settings(app_configs=None, **kwargs):
     errors = []
 
     if "RedisCache" not in settings.CACHES.get("default", {}).get("BACKEND"):
-        errors.append(checks.Critical(_("Cache backend is not set to Redis."),
+        errors.append(checks.Critical(
+            _("Cache backend is not set to Redis."),
             hint=_("Set default cache backend to django_redis.cache.RedisCache\n"
                    "Current settings: %r") % (settings.CACHES.get("default")),
             id="pootle.C005",
@@ -135,39 +142,45 @@ def check_settings(app_configs=None, **kwargs):
         from django_redis import get_redis_connection
 
         if not get_redis_connection():
-            errors.append(checks.Critical(_("Could not initiate a Redis cache connection"),
+            errors.append(checks.Critical(
+                _("Could not initiate a Redis cache connection"),
                 hint=_("Double-check your CACHES settings"),
                 id="pootle.C004",
             ))
 
     if settings.DEBUG:
-        errors.append(checks.Warning(_("DEBUG mode is on. Do not do this in production!"),
+        errors.append(checks.Warning(
+            _("DEBUG mode is on. Do not do this in production!"),
             hint=_("Set DEBUG = False in Pootle settings"),
             id="pootle.W005"
         ))
     elif "sqlite" in settings.DATABASES.get("default", {}).get("ENGINE"):
         # We don't bother warning about sqlite in DEBUG mode.
-        errors.append(checks.Warning(_("The sqlite database backend is unsupported"),
+        errors.append(checks.Warning(
+            _("The sqlite database backend is unsupported"),
             hint=_("Set your default database engine to postgresql_psycopg2 or mysql"),
             id="pootle.W006",
         ))
 
     if settings.SESSION_ENGINE.split(".")[-1] not in ("cache", "cached_db"):
-        errors.append(checks.Warning(_("Not using cached_db as session engine"),
+        errors.append(checks.Warning(
+            _("Not using cached_db as session engine"),
             hint=_("Set SESSION_ENGINE to django.contrib.sessions.backend.cached_db\n"
                    "Current settings: %r") % (setting.SESSION_ENGINE),
             id="pootle.W007",
         ))
 
     if not settings.CONTACT_EMAIL:
-        errors.append(checks.Warning(_("settings.CONTACT_EMAIL is not set."),
+        errors.append(checks.Warning(
+            _("settings.CONTACT_EMAIL is not set."),
             hint=_("Set CONTACT_EMAIL to allow users to contact administrators"
                    "through the Pootle contact form."),
             id="pootle.W008",
         ))
 
     if not settings.DEFAULT_FROM_EMAIL:
-        errors.append(checks.Warning(_("settings.DEFAULT_FROM_EMAIL is not set."),
+        errors.append(checks.Warning(
+            _("settings.DEFAULT_FROM_EMAIL is not set."),
             hint=_("DEFAULT_FROM_EMAIL is used in all outgoing Pootle email.\n"
                    "Don't forget to review your mail server settings."),
             id="pootle.W009",
