27f13aa28eab413aa9bf5e2ba66bf6daccf9cd82
Dwayne Bailey <dwayne@translate.org.za>
2015-04-30 23:07:41 +0100

    localtm: create elasticsearch search backend

--- pootle/core/search/backends/__init__.py
+++ pootle/core/search/backends/__init__.py
@@ -6,6 +6,3 @@
 # This file is a part of the Pootle project. It is distributed under the GPL3
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
-
-from .base import *
-from .backends import *
