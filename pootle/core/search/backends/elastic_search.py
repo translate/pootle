2038a5a2c420d89e1f145bde17bcf7211f530464
Dwayne Bailey <dwayne@translate.org.za>
2015-04-30 23:07:41 +0100

    localtm: create elasticsearch search backend
    Part of #3610

--- pootle/core/search/backends/elastic_search.py
+++ pootle/core/search/backends/elastic_search.py
@@ -7,86 +7,79 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
+__all__ = ('ElasticSearchBackend',)
+
 try:
     from elasticsearch import Elasticsearch
 except:
     Elasticsearch = None
 
-from django.conf import settings
+from ..base import SearchBackend
 
 
-def get_params():
-    params = getattr(settings, 'POOTLE_TM_SERVER', None)
+class ElasticSearchBackend(SearchBackend):
+    def __init__(self, config_name):
+        super(ElasticSearchBackend, self).__init__(config_name)
+        self.es = None
+        if self._settings is not None and Elasticsearch is not None:
+            self.es = Elasticsearch([{'host': self._settings['HOST'], 'port': self._settings['PORT']}, ])
+        if self.es.ping():
+            if not self.es.indices.exists(self._settings['INDEX_NAME']):
+                self.es.indices.create(self._settings['INDEX_NAME'])
 
-    if params is not None:
-        return params['default']
+    def _is_valuable_hit(self, unit, hit):
+        if str(unit.id) == hit['_id']:
+            return False
 
-    return None
+        return True
 
+    def search(self, unit):
+        if self.es is None or not self.es.ping():
+            return []
 
-es_params = get_params()
-# TODO support ENGINE param
-# Elasticsearch is the only supported engine now
-es = None
-if es_params is not None and Elasticsearch is not None:
-    es = Elasticsearch([{'host': es_params['HOST'], 'port': es_params['PORT']}, ])
-    if not es.indices.exists(es_params['INDEX_NAME']):
-        es.indices.create(es_params['INDEX_NAME'])
+        counter = {}
+        res = []
+        language = unit.store.translation_project.language.code
+        es_res = self.es.search(index=self._settings['INDEX_NAME'],
+                                doc_type=language,
+                                body={
+                                    "query": {
+                                        "match": {
+                                            'source': {
+                                                'query': unit.source,
+                                                'fuzziness': self._settings['MIN_SCORE'],
+                                            }
+                                        }
+                                    }
+                                })
 
+        for hit in es_res['hits']['hits']:
+            if self._is_valuable_hit(unit, hit):
+                if hit['_source']['target'] not in counter:
+                    counter[hit['_source']['target']] = 1
+                    res.append({
+                        'unit_id': hit['_id'],
+                        'source': hit['_source']['source'],
+                        'target': hit['_source']['target'],
+                        'project': hit['_source']['project'],
+                        'path': hit['_source']['path'],
+                        'username': hit['_source']['username'],
+                        'fullname': hit['_source']['fullname'],
+                        'email_md5': hit['_source']['email_md5'],
+                    })
+                else:
+                    counter[hit['_source']['target']] += 1
 
-def update(language, obj):
-    if es is not None:
-        es.index(index=es_params['INDEX_NAME'],
-                 doc_type=language,
-                 body=obj,
-                 id=obj['id'])
+        for item in res:
+            item['count'] = counter[item['target']]
 
+        return res
 
-def is_valuable_hit(unit, hit):
-    if str(unit.id) == hit['_id']:
-        return False
-
-    return True
-
-
-def search(unit):
-    if es is None:
-        return None
-
-    counter = {}
-    res = []
-    language = unit.store.translation_project.language.code
-    es_res = es.search(index=es_params['INDEX_NAME'],
-                       doc_type=language,
-                       body={
-                           "query": {
-                               "match": {
-                                   'source': {
-                                       'query': unit.source,
-                                       'fuzziness': es_params['MIN_SCORE'],
-                                   }
-                               }
-                           }
-                       })
-
-    for hit in es_res['hits']['hits']:
-        if is_valuable_hit(unit, hit):
-            if hit['_source']['target'] not in counter:
-                counter[hit['_source']['target']] = 1
-                res.append({
-                    'unit_id': hit['_id'],
-                    'source': hit['_source']['source'],
-                    'target': hit['_source']['target'],
-                    'project': hit['_source']['project'],
-                    'path': hit['_source']['path'],
-                    'username': hit['_source']['username'],
-                    'fullname': hit['_source']['fullname'],
-                    'email_md5': hit['_source']['email_md5'],
-                })
-            else:
-                counter[hit['_source']['target']] += 1
-
-    for item in res:
-        item['count'] = counter[item['target']]
-
-    return res
+    def update(self, language, obj):
+        if self.es is not None and self.es.ping():
+            self.es.index(
+                index=self._settings['INDEX_NAME'],
+                doc_type=language,
+                body=obj,
+                id=obj['id']
+            )
