f079e1101ed4b433e8a4ed22be94e19db1cc246f
Dwayne Bailey <dwayne@translate.org.za>
2015-05-01 10:53:53 +0100

    localtm: create and use a search broker for TM
    The broker will initialise any configured TM servers and pass all
    .search() and .update() requests to the backend.
    
    Fixes evernote/pootleXX244

--- pootle/core/search/__init__.py
+++ pootle/core/search/__init__.py
@@ -7,5 +7,6 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
-from .base import *
 from .backends import *
+from .base import *
+from .broker import *
