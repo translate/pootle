8fea909f1a03ddf154d25f7a0e016eb7eac9353d
Dwayne Bailey <dwayne@translate.org.za>
2015-05-23 23:55:22 +0100

    tools: improve doc checker output and check settings

--- pootle/tools/docs_check_commands.sh
+++ pootle/tools/docs_check_commands.sh
@@ -5,6 +5,9 @@
 
 BASE_DIR=$(dirname $0)/../..
 DOCS_DIR=$BASE_DIR/docs
+EGREP_EXCLUDE_DIR="--exclude-dir=$DOCS_DIR/_build"
+
+# ./manage.py $command
 
 pootle_commands=$(
 for app in pootle_app import_export virtualfolder
@@ -23,19 +26,64 @@ django_commands=$(
     sort -u
 )
 
+echo
+echo "Missing '.. django-admin::' markers for the following commands"
 for command in $pootle_commands
 do
-        egrep -rq "^.. django-admin:: $command" $DOCS_DIR
+        egrep -rq $EGREP_EXCLUDE_DIR "^.. django-admin:: $command" $DOCS_DIR
         if [ $? -eq 1 ]; then
-            echo "Missing Pootle docs for $command"
+            echo ".. django-admin:: $command"
         fi
 done
 
+echo
+echo "No command references ':djadmin:' for the following commands"
+for command in $pootle_commands
+do
+        egrep -rq $EGREP_EXCLUDE_DIR ":djadmin:\`$command\`" $DOCS_DIR
+        if [ $? -eq 1 ]; then
+            echo ":djadmin:\`$command\`"
+        fi
+done
+
+echo
+echo "Potential missing links to Django management commands need :djadmin:"
 for command in $django_commands
 do
     if [[ ! $(echo "$pootle_commands" | egrep $command) ]]; then
-        egrep -r --exclude-dir=$DOCS_DIR/_build "manage.py $command" $DOCS_DIR
-        egrep -r --exclude-dir=$DOCS_DIR/_build "pootle $command" $DOCS_DIR
-        egrep -r --exclude-dir=$DOCS_DIR/_build "\`\`$command\`\`" $DOCS_DIR
+        egrep -r $EGREP_EXCLUDE_DIR "manage.py $command ?[^<]" $DOCS_DIR
+        egrep -r $EGREP_EXCLUDE_DIR  "pootle $command" $DOCS_DIR
+        egrep -r $EGREP_EXCLUDE_DIR  "\`\`$command\`\`" $DOCS_DIR
     fi
 done
+
+
+# POOTLE_* settings
+
+settings=$(egrep -h $EGREP_EXCLUDE_DIR "^POOTLE_" $(ls $BASE_DIR/pootle/settings/*.conf | egrep -v $EGREP_EXCLUDE_DIR "90-.*.conf$") | sed "s/\w*=.*$//g")
+
+echo
+echo "Missing '.. setting::' markers for the following settings"
+for setting in $settings
+do
+    egrep -rq $EGREP_EXCLUDE_DIR "^.. setting:: $setting" $DOCS_DIR
+    if [ $? -eq 1 ]; then
+        echo "..setting:: $setting"
+    fi
+done
+
+echo
+echo "No setting references ':setting:' for the following settings"
+for setting in $settings
+do
+    egrep -rq $EGREP_EXCLUDE_DIR ":setting:\`$setting\`" $DOCS_DIR
+    if [ $? -eq 1 ]; then
+        echo ":setting:\`$setting\`"
+    fi
+done
+
+IGNORABLE_ALLCAPS_ENTRIES="(HEAD|PYTHONPATH|UTC|POOTLE_TOP_STATS_CACHE_TIMEOUT|ENVIRONMENT|POOTLE_ENABLE_API|DEPRECATIONS)"
+
+echo
+echo "ALL_CAPS that could be :envvar: or :setting:"
+egrep -rh $EGREP_EXCLUDE_DIR --exclude=$DOCS_DIR/server/settings.rst "\`\`[A-Z][A-Z_]{1,}\`\`" $DOCS_DIR | egrep -v $IGNORABLE_ALLCAPS_ENTRIES
