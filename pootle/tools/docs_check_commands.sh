db319580a84b078d8ad4e8cc8e94fdbc1ad4ed3a
Dwayne Bailey <dwayne@translate.org.za>
2015-05-24 21:18:34 +0100

    docs: detect django admin command references
    Check for admin commands so that we can correct them to point to Django
    docs.

--- pootle/tools/docs_check_commands.sh
+++ pootle/tools/docs_check_commands.sh
@@ -6,7 +6,7 @@
 BASE_DIR=$(dirname $0)/../..
 DOCS_DIR=$BASE_DIR/docs
 
-commands=$(
+pootle_commands=$(
 for app in pootle_app import_export virtualfolder
 do
     $BASE_DIR/manage.py --help | \
@@ -15,10 +15,27 @@ do
     sed "s/^[ ]*//g"
 done)
 
-for command in $commands
+django_commands=$(
+    $BASE_DIR/manage.py --help | \
+    sed -E -n "/^\[.*\]$/,$ p" | \
+    sed "/^\[/d" | \
+    sed "s/^[ ]*//g" | \
+    sort -u
+)
+
+for command in $pootle_commands
 do
         egrep -rq "^.. django-admin:: $command" $DOCS_DIR
         if [ $? -eq 1 ]; then
-            echo "Missing docs for $command"
+            echo "Missing Pootle docs for $command"
         fi
+done
+
+for command in $django_commands
+do
+    if [[ ! $(echo "$pootle_commands" | egrep $command) ]]; then
+        egrep -r --exclude-dir=$DOCS_DIR/_build "manage.py $command" $DOCS_DIR
+        egrep -r --exclude-dir=$DOCS_DIR/_build "pootle $command" $DOCS_DIR
+        egrep -r --exclude-dir=$DOCS_DIR/_build "\`\`$command\`\`" $DOCS_DIR
+    fi
 done
