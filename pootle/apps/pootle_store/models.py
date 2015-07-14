7f4571f268bc11ec9cbd6b15df28916e692f278c
Julen Ruiz Aizpuru <julenx@gmail.com>
2015-07-14 13:33:35 +0200

    Stats: let's add a comment to prevent accidents

--- pootle/apps/pootle_store/models.py
+++ pootle/apps/pootle_store/models.py
@@ -2019,6 +2019,8 @@ class Store(models.Model, CachedTreeItem, base.TranslationStore):
             'translated': 0,
             'fuzzy': 0
         }
+        # XXX: `order_by()` here is important as it removes the default
+        # ordering for units. See #3897 for reference.
         res = self.units.order_by().values('state') \
                         .annotate(wordcount=models.Sum('source_wordcount'))
         for item in res:
