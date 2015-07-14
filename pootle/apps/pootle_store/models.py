ff2bee6e91f02ed02024ccd546a46691fc036619
Taras Semenenko <taras.semenenko@gmail.com>
2015-07-14 13:08:03 +0300

    Stats: remove default ordering explicitly
    Fixes #3897

--- pootle/apps/pootle_store/models.py
+++ pootle/apps/pootle_store/models.py
@@ -2019,7 +2019,7 @@ class Store(models.Model, CachedTreeItem, base.TranslationStore):
             'translated': 0,
             'fuzzy': 0
         }
-        res = self.units.values('state') \
+        res = self.units.order_by().values('state') \
                         .annotate(wordcount=models.Sum('source_wordcount'))
         for item in res:
             ret['total'] += item['wordcount']
