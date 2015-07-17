2520fdd27acdbda3e99379ad5979c40d194dbfda
Julen Ruiz Aizpuru <julenx@gmail.com>
2015-07-17 13:08:08 +0200

    Cleanup: removed unused import

--- tests/models/submission.py
+++ tests/models/submission.py
@@ -7,8 +7,6 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
-import pytest
-
 from pootle_statistics.models import SubmissionTypes, SubmissionFields
 from pootle_store.util import TRANSLATED, UNTRANSLATED
 
