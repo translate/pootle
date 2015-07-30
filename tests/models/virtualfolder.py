12457e87f9a82a5edc75071e9a4ba51ef7777554
Leandro Regueiro <leandro.regueiro@gmail.com>
2015-07-30 10:38:08 +0200

    Test virtual folder priority must be greater than zero

--- tests/models/virtualfolder.py
+++ tests/models/virtualfolder.py
@@ -38,3 +38,37 @@ def test_vfolder_directory_clash(af_vfolder_test_browser_defines_po):
     assert (u"Problem adding virtual folder 'browser' with location "
             u"'/af/vfolder_test/': VirtualFolderTreeItem clashes with "
             u"Directory /af/vfolder_test/browser/") in str(excinfo.value)
+
+
+@pytest.mark.django_db
+def test_vfolder_priority_not_greater_than_zero(af_vfolder_test_browser_defines_po):
+    """Tests that the creation of a virtual folder fails if the provided
+    priority is not greater than zero.
+    """
+    from django.core.exceptions import ValidationError
+
+    from virtualfolder.models import VirtualFolder
+
+    # Test priority less than zero.
+    vfolder_item = {
+        'name': "whatever",
+        'location': "/af/vfolder_test/",
+        'priority': -3,
+        'is_public': True,
+        'filter_rules': "browser/defines.po",
+    }
+    vfolder = VirtualFolder(**vfolder_item)
+
+    with pytest.raises(ValidationError) as excinfo:
+        vfolder.save()
+
+    assert u'Priority must be greater than zero.' in str(excinfo.value)
+
+    # Test zero priority.
+    vfolder_item['priority'] = 0
+    vfolder = VirtualFolder(**vfolder_item)
+
+    with pytest.raises(ValidationError) as excinfo:
+        vfolder.save()
+
+    assert u'Priority must be greater than zero.' in str(excinfo.value)
