translate/pootle

commit 50fc74ede3c0017dbc03b0b7e54dc1063aac2a4c

Check for permissions by using the decorator

pootle/apps/pootle_store/views.py
@@ -709,7 +709,7 @@ def get_failing_checks(request, path_obj):
 
 
 @ajax_required
-@get_unit_context('')
+@get_unit_context('translate')
 def submit(request, unit):
     """Processes translation submissions and stores them in the database.
 
@@ -717,11 +717,6 @@ def submit(request, unit):
              units for the unit next to unit ``uid``.
     """
     json = {}
-
-    cantranslate = check_permission("translate", request)
-    if not cantranslate:
-        raise PermissionDenied(_("You do not have rights to access "
-                                 "translation mode."))
 
     translation_project = request.translation_project
     language = translation_project.language
@@ -774,7 +769,7 @@ def submit(request, unit):
 
 
 @ajax_required
-@get_unit_context('')
+@get_unit_context('suggest')
 def suggest(request, unit):
     """Processes translation suggestions and stores them in the database.
 
@@ -782,11 +777,6 @@ def suggest(request, unit):
              units for the unit next to unit ``uid``.
     """
     json = {}
-
-    cansuggest = check_permission("suggest", request)
-    if not cansuggest:
-        raise PermissionDenied(_("You do not have rights to access "
-                                 "translation mode."))
 
     translation_project = request.translation_project
     language = translation_project.language
@@ -823,7 +813,7 @@ def suggest(request, unit):
 
 
 @ajax_required
-@get_unit_context('')
+@get_unit_context('review')
 def reject_suggestion(request, unit, suggid):
     json = {}
     translation_project = request.translation_project
@@ -836,9 +826,8 @@ def reject_suggestion(request, unit, suggid):
         except ObjectDoesNotExist:
             raise Http404
 
-        if (not check_permission('review', request) and
-            (not request.user.is_authenticated() or sugg and
-                 sugg.user != request.profile)):
+        if (not request.user.is_authenticated() or sugg and
+            sugg.user != request.profile):
             raise PermissionDenied(_("You do not have rights to access "
                                      "review mode."))
 

pootle/apps/pootle_store/decorators.py
@@ -37,6 +37,19 @@ from pootle_profile.models import get_profile
 from .models import Store, Unit
 
 
+def get_permission_message(permission_code):
+    """Returns a human-readable message when `permission_code` is not met
+    by the current context.
+    """
+    default_message = _("Insufficient rights to access this directory.")
+
+    return {
+        'suggest': _('Insufficient rights to access suggestion mode.'),
+        'translate': _('Insufficient rights to access translation mode.'),
+        'review': _('Insufficient rights to access review mode.'),
+    }.get(permission_code, default_message)
+
+
 def _common_context(request, translation_project, permission_codes):
     """Adds common context to request object and checks permissions."""
     request.translation_project = translation_project
@@ -59,9 +72,7 @@ def _check_permissions(request, directory, permission_codes):
         permission_codes = [permission_codes]
     for permission_code in permission_codes:
         if not check_permission(permission_code, request):
-            raise PermissionDenied(
-                _("Insufficient rights to access this directory."),
-            )
+            raise PermissionDenied(get_permission_message(permission_code))
 
 
 def get_store_context(permission_codes):

commit 65ac374c0dfb0006ac4146d048355f3e1328c71c

Suggestions: ensure users can reject their own suggestions
This fixes a regression introduced in 50fc74e which prevented users without
`review` rights to reject their own suggestions.

Fixes #4021.

pootle/apps/pootle_store/views.py
@@ -33,7 +33,7 @@ from pootle.core.decorators import (get_path_obj, get_resource,
 from pootle.core.exceptions import Http400
 from pootle.core.http import JsonResponse, JsonResponseBadRequest
 from pootle_app.models.directory import Directory
-from pootle_app.models.permissions import check_user_permission
+from pootle_app.models.permissions import check_permission, check_user_permission
 from pootle_misc.checks import get_category_id, check_names
 from pootle_misc.forms import make_search_form
 from pootle_misc.util import ajax_required, to_int, get_date_interval
@@ -984,15 +984,15 @@ def suggest(request, unit):
 
 @ajax_required
 @require_http_methods(['POST', 'DELETE'])
-@get_unit_context('review')
-def manage_suggestion(request, unit, sugg_id):
+def manage_suggestion(request, uid, sugg_id):
     """Dispatches the suggestion action according to the HTTP verb."""
     if request.method == 'DELETE':
-        return reject_suggestion(request, unit, sugg_id)
+        return reject_suggestion(request, uid, sugg_id)
     elif request.method == 'POST':
-        return accept_suggestion(request, unit, sugg_id)
+        return accept_suggestion(request, uid, sugg_id)
 
 
+@get_unit_context()
 def reject_suggestion(request, unit, suggid):
     json = {
         'udbid': unit.id,
@@ -1004,6 +1004,13 @@ def reject_suggestion(request, unit, suggid):
     except ObjectDoesNotExist:
         raise Http404
 
+    # In order to be able to reject a suggestion, users have to either:
+    # 1. Have `review` rights, or
+    # 2. Be the author of the suggestion being rejected
+    if (not check_permission('review', request) and
+        (request.user.is_anonymous() or request.user != sugg.user)):
+        raise PermissionDenied(_('Insufficient rights to access review mode.'))
+
     unit.reject_suggestion(sugg, request.translation_project,
                            request.profile)
 
@@ -1012,6 +1019,7 @@ def reject_suggestion(request, unit, suggid):
     return JsonResponse(json)
 
 
+@get_unit_context('review')
 def accept_suggestion(request, unit, suggid):
     json = {
         'udbid': unit.id,