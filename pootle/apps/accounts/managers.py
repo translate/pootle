44d8116632eff93fdf4c749d9a33000357292c52
Ryan Northey <ryan@space23.net>
2015-09-09 07:50:44 +0100

    User manager validates email uniqueness on create_user
    This prevents an admin adding further duplicate emails using
    createsuperuser

--- pootle/apps/accounts/managers.py
+++ pootle/apps/accounts/managers.py
@@ -12,6 +12,8 @@ __all__ = ('UserManager', )
 from django.contrib.auth.models import BaseUserManager
 from django.utils import timezone
 
+import accounts
+
 
 class UserManager(BaseUserManager):
     """Pootle User manager.
@@ -36,6 +38,7 @@ class UserManager(BaseUserManager):
             raise ValueError('The given username must be set')
 
         email = self.normalize_email(email)
+        accounts.utils.validate_email_unique(email)
         user = self.model(username=username, email=email,
                           is_active=True, is_superuser=is_superuser,
                           last_login=now, date_joined=now, **extra_fields)
