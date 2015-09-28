e6e0de48b9ddac93a511aece2cb791384daa371a
Ryan Northey <ryan@space23.net>
2015-09-28 22:35:29 +0100

    Add pootle_fs app

--- pootle/apps/pootle_fs/apps.py
+++ pootle/apps/pootle_fs/apps.py
@@ -6,4 +6,10 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
-default_app_config = 'pootle_fs.apps.PootleFSConfig'
+from django.apps import AppConfig
+
+
+class PootleFSConfig(AppConfig):
+
+    name = "pootle_fs"
+    verbose_name = "Pootle Filesystem synchronisation"
