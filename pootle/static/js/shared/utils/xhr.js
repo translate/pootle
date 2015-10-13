178cfd571609dc3b76ea5213ba1432d610d1feeb
Julen Ruiz Aizpuru <julenx@gmail.com>
2015-10-14 00:19:21 +0200

    Added generic XHR resource fetcher
    Built on top of jQuery, auto-aborts ongoing requests when retrying to fetch
    identical resources again.
    
    It also mimics to some extent the WHATWG Fetch API to require as less changes as
    possible in the future.

---
+++ pootle/static/js/shared/utils/xhr.js
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) Pootle contributors.
+ *
+ * This file is a part of the Pootle project. It is distributed under the GPL3
+ * or later license. See the LICENSE file for a copy of the license and the
+ * AUTHORS file for copyright and authorship information.
+ */
+
+import $ from 'jquery';
+
+
+let requests = {};
+
+
+export default function fetch({url, body, method='POST', dataType='json'}) {
+  if (requests[url]) {
+    requests[url].abort();
+  }
+
+  requests[url] = (
+    $.ajax({
+      url: l(url),
+      type: method,
+      data: body,
+      dataType: dataType,
+    })
+  );
+
+  return requests[url];
+}
