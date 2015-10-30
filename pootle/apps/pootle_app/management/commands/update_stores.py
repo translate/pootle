f4d8569488bf1ebae92c21ee8b5a513d299f2786
Ryan Northey <ryan@space23.net>
2015-10-30 12:17:33 +0000

    Moving update_store specific code to mgmt command
    - Only_newer check is now done in update_stores mgmt command.
    - Only increment revision in Store.update if changes occur

--- pootle/apps/pootle_app/management/commands/update_stores.py
+++ pootle/apps/pootle_app/management/commands/update_stores.py
@@ -53,15 +53,34 @@ class Command(PootleCommand):
     def handle_store(self, store, **options):
         if not store.file:
             return
+        disk_mtime = store.get_file_mtime()
+        if not options["force"] and disk_mtime == store.file_mtime:
+            # The file on disk wasn't changed since the last sync
+            logging.debug(u"File didn't change since last sync, skipping "
+                          u"%s" % store.pootle_path)
+            return
         if options["overwrite"]:
             store_revision = store.get_max_unit_revision()
         else:
-            store_revision = store.last_sync_revision
-        store.update(
+            store_revision = store.last_sync_revision or 0
+
+        update_revision, changes = store.update(
             store.file.store,
             store_revision=store_revision,
-            only_newer=not options['force']
         )
+        store.file_mtime = disk_mtime
+        if changes and any(x > 0 for x in changes.values()):
+            update_unsynced = None
+            if store.last_sync_revision is not None:
+                updated_unsynced = store.increment_unsynced_unit_revision(
+                    update_revision
+                )
+            store.last_sync_revision = update_revision
+            if update_unsynced:
+                logging.info(u"[update] unsynced %d units in %s [revision: %d]"
+                             % (update_unsynced, store.pootle_path,
+                                update_revision))
+        store.save()
 
     def handle_all(self, **options):
         scan_translation_projects(languages=self.languages,
