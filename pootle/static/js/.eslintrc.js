21691ad6477d4898349e3df82ef76005d5b5e6e2
Julen Ruiz Aizpuru <julenx@gmail.com>
2015-11-23 00:54:06 +0100

    Enable running ESLint via `npm run lint`
    Fixes #4268.

---
+++ pootle/static/js/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  ecmaFeatures: {
+    jsx: true,
+    modules: true,
+  },
+  globals: {
+    gettext: false,
+    ngettext: false,
+    interpolate: false,
+    l: false,
+    PTL: false,
+  },
+  plugins: [
+    'react',
+  ],
+};
