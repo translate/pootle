translate/pootle

commit 1abe45b716c28e174dced4bd9d16387fafcfacb8

PEP8: E501 line too long

pootle/core/log.py
@@ -48,7 +48,8 @@ def action_log(*args, **kwargs):
     tr = tr.replace("\n", "\\\n")
     d['translation'] = tr
 
-    msg = u"%(user)s\t%(action)s\t%(lang)s\t%(unit)s\t%(path)s\t%(translation)s" % d
+    msg = u"%(user)s\t%(action)s\t%(lang)s\t" + \
+          u"%(unit)s\t%(path)s\t%(translation)s" % d
 
     logger.info(msg)
 

pootle/apps/accounts/models.py
@@ -150,7 +150,8 @@ class User(AbstractBaseUser):
     @cached_property
     def is_meta(self):
         """Returns `True` if this is a special fake user."""
-        return self.username in UserManager.META_USERS + settings.POOTLE_META_USERS
+        return self.username in \
+            UserManager.META_USERS + settings.POOTLE_META_USERS
 
     @cached_property
     def email_hash(self):

pootle/apps/accounts/utils.py
@@ -282,7 +282,8 @@ class UserPurger(object):
             review.delete()
 
         for unit in self.user.reviewed.iterator():
-            reviews = unit.get_suggestion_reviews().exclude(submitter=self.user)
+            reviews = unit.get_suggestion_reviews().exclude(
+                submitter=self.user)
             if reviews.exists():
                 previous_review = reviews.latest('pk')
                 unit.reviewed_by = previous_review.submitter

pootle/apps/import_export/management/commands/export.py
@@ -78,13 +78,15 @@ class Command(PootleCommand):
         self._create_zip(stores, prefix)
 
     def handle_project(self, project, **options):
-        stores = Store.objects.live().filter(translation_project__project=project)
+        stores = Store.objects.live().filter(
+            translation_project__project=project)
         if not stores:
             raise CommandError("No matches for project %r" % (project))
         self._create_zip(stores, prefix=project.code)
 
     def handle_language(self, language, **options):
-        stores = Store.objects.live().filter(translation_project__language=language)
+        stores = Store.objects.live().filter(
+            translation_project__language=language)
         self._create_zip(stores, prefix=language.code)
 
     def handle_path(self, path, **options):

pootle/apps/import_export/views.py
@@ -57,7 +57,8 @@ def export(request):
                 try:
                     data = store.serialize()
                 except Exception as e:
-                    logging.error("Could not serialize %r: %s", store.pootle_path, e)
+                    logging.error("Could not serialize %r: %s",
+                                  store.pootle_path, e)
                     continue
                 zf.writestr(prefix + store.pootle_path, data)
 

pootle/apps/pootle_app/management/__init__.py
@@ -24,7 +24,8 @@ def fix_permission_content_type_pre(sender, instance, **kwargs):
         logging.debug("Fixing permissions content types")
         global permission_queryset
         permission_queryset = [permission for permission in
-                               Permission.objects.filter(content_type=instance)]
+                               Permission.objects.filter(
+                                   content_type=instance)]
 
 
 @receiver(post_delete, sender=ContentType)

pootle/apps/pootle_app/management/commands/calculate_checks.py
@@ -78,7 +78,8 @@ def calculate_checks(check_names=None, translation_project=None):
                            'category', 'false_positive')
     all_units_checks = {}
     for check in checks:
-        all_units_checks.setdefault(check['unit_id'], {})[check['name']] = check
+        all_units_checks.setdefault(
+            check['unit_id'], {})[check['name']] = check
 
     unit_filter = {
         'state__gt': OBSOLETE

pootle/apps/pootle_app/management/commands/dump.py
@@ -84,7 +84,8 @@ class Command(PootleCommand):
     def handle_translation_project(self, tp, **options):
         if options['stats']:
             res = {}
-            self._dump_stats(tp.directory, res, stop_level=options['stop_level'])
+            self._dump_stats(tp.directory, res,
+                             stop_level=options['stop_level'])
             return
 
         if options['data']:

commit b9359464589382dca399be98d7538c37c01eeb45

Logger: fix logline regression
Regression from 1abe45b716c28e174dced4bd9d16387fafcfacb8
Used string continuation instead of just \

pootle/core/log.py
@@ -48,8 +48,8 @@ def action_log(*args, **kwargs):
     tr = tr.replace("\n", "\\\n")
     d['translation'] = tr
 
-    msg = u"%(user)s\t%(action)s\t%(lang)s\t" + \
-          u"%(unit)s\t%(path)s\t%(translation)s" % d
+    msg = (u"%(user)s\t%(action)s\t%(lang)s\t"
+           "%(unit)s\t%(path)s\t%(translation)s" % d)
 
     logger.info(msg)
 