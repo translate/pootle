96b607f3d63b8e044e81fe25ea24d395ba79f0d5
Ryan Northey <ryan@space23.net>
2015-12-06 20:11:57 +0000

    Add signal receivers to set Unit priority
    This merges the original relate_unit handler from virtualfolder.models

--- pootle/apps/virtualfolder/receivers.py
+++ pootle/apps/virtualfolder/receivers.py
@@ -6,44 +6,116 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 
 from pootle_store.models import Unit
+from pootle_store.util import OBSOLETE
+
+from .models import VirtualFolder, VirtualFolderTreeItem
+from .signals import vfolder_post_save
 
 
-@receiver(post_save, sender=Unit)
-def relate_unit(sender, instance, created=False, **kwargs):
-    """Add newly created units to the virtual folders they belong, if any.
-
-    When a new store or translation project, or even a full project is added,
-    some of their units might be matched by the filters of any of the
-    previously existing virtual folders, so this signal handler relates those
-    new units to the virtual folders they belong to, if any.
+def update_vfolder_tree(vf, store):
+    """For a given VirtualFolder and Store update the VirtualFolderTreeItem
     """
-    if not created:
-        return
+    # Create missing VirtualFolderTreeItem tree structure for affected Stores
+    # after adding or unobsoleting Units.
+    vfolder_treeitem, created = (
+        VirtualFolderTreeItem.objects.get_or_create(
+            directory=store.parent, vfolder=vf))
 
-    pootle_path = instance.store.pootle_path
+    if not created:
+        # The VirtualFolderTreeItem already existed, so
+        # calculate again the stats up to the root.
+        vfolder_treeitem.update_all_cache()
+
+
+def add_unit_to_vfolders(unit):
+    """For a given Unit check for membership of any VirtualFolders
+    """
+    pootle_path = unit.store.pootle_path
 
     for vf in VirtualFolder.objects.iterator():
+        unit_added = False
         for location in vf.all_locations:
             if not pootle_path.startswith(location):
                 continue
 
             for filename in vf.filter_rules.split(","):
                 if pootle_path == "".join([location, filename]):
-                    vf.units.add(instance)
-
-                    # Create missing VirtualFolderTreeItem tree structure after
-                    # adding this new unit.
-                    vfolder_treeitem, created = \
-                        VirtualFolderTreeItem.objects.get_or_create(
-                            directory=instance.store.parent, vfolder=vf)
-
-                    if not created:
-                        # The VirtualFolderTreeItem already existed, so
-                        # calculate again the stats up to the root.
-                        vfolder_treeitem.update_all_cache()
-
+                    vf.units.add(unit)
+                    unit_added = True
                     break
+
+            if unit_added:
+                break
+
+        if unit_added:
+            update_vfolder_tree(vf, unit.store)
+
+
+@receiver(pre_save, sender=VirtualFolder)
+def vfolder_unit_priority_presave_handler(sender, instance, **kwargs):
+    """Remove Units from VirtualFolder when vfolder changes
+
+    - Check the original VirtualFolder object's locations
+    - Check the new VirtualFolders object's locations
+    - Remove any units from the VirtualFolder that are only in original's
+      locations and reset Unit priority
+    - Update VirtualFolderTree for any Stores that may have been affected
+    """
+
+    if instance.id is None:
+        return
+
+    original = VirtualFolder.objects.get(pk=instance.pk)
+
+    original_locations = set()
+    new_locations = set()
+
+    for location in original.all_locations:
+        for filename in original.filter_rules.split(","):
+            original_locations.add("".join([location, filename]))
+
+    for location in instance.all_locations:
+        for filename in instance.filter_rules.split(","):
+            new_locations.add("".join([location, filename]))
+
+    removed_locations = original_locations - new_locations
+
+    stores_affected = set()
+    for location in removed_locations:
+        # reindex these units without this vfolder
+        removed_units = (
+            Unit.objects.filter(store__pootle_path__startswith=location,
+                                vfolders=original))
+        for unit in removed_units.iterator():
+            unit.vfolders.remove(original)
+            unit.set_priority()
+            stores_affected.add(unit.store)
+
+    for store in stores_affected:
+        update_vfolder_tree(original, store)
+
+
+@receiver(vfolder_post_save, sender=VirtualFolder)
+def vfolder_unit_priority_handler(sender, instance, **kwargs):
+    """Set Unit priorities for VirtualFolder members on change
+    """
+    for unit in instance.units.all():
+        unit.set_priority()
+
+
+@receiver(post_save, sender=Unit)
+def vfolder_unit_postsave_handler(sender, instance, created=False, **kwargs):
+    """Match VirtualFolders to Unit and update Unit.priority
+
+    - If unit was newly created, then check vfolders for membership
+    - Update Unit priority from vfolder membership on Unit.save or create
+
+    """
+
+    if created:
+        add_unit_to_vfolders(instance)
+    instance.set_priority()
