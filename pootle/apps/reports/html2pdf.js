e000301039f4b2794b0e05b7d5d51a403d725c2d
Julen Ruiz Aizpuru <julenx@gmail.com>
2015-12-09 11:42:13 +0100

    Cleanup: missing semicolon

--- pootle/apps/reports/html2pdf.js
+++ pootle/apps/reports/html2pdf.js
@@ -6,7 +6,7 @@ var infile = system.args[1];
 var outfile = system.args[2];
 
 page.viewportSize = { width: 600, height: 600 };
-page.paperSize = { format: 'Letter', orientation: 'portrait', margin: '1cm' }
+page.paperSize = { format: 'Letter', orientation: 'portrait', margin: '1cm' };
 page.zoomFactor = 1.3;
 
 page.open(infile, function (status) {
