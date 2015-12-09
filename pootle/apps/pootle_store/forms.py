112c5b8cef820978cc6f3aba41a9684db74ed90d
Ryan Northey <ryan@space23.net>
2015-12-09 22:26:45 +0000

    Add a SearchForm for get_units

--- pootle/apps/pootle_store/forms.py
+++ pootle/apps/pootle_store/forms.py
@@ -9,25 +9,59 @@
 
 """Form fields required for handling translation files."""
 
+from importlib import import_module
 import re
 
 from translate.misc.multistring import multistring
 
 from django import forms
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.utils import timezone
 from django.utils.translation import get_language, ugettext as _
 
 from pootle.core.log import (TRANSLATION_ADDED, TRANSLATION_CHANGED,
                              TRANSLATION_DELETED)
 from pootle.core.mixins import CachedMethods
+from pootle.core.url_helpers import split_pootle_path
 from pootle_app.models.permissions import check_permission
+from pootle_misc.checks import CATEGORY_IDS
+from pootle_misc.util import get_date_interval
 from pootle_statistics.models import (Submission, SubmissionFields,
                                       SubmissionTypes)
+from virtualfolder.helpers import extract_vfolder_from_path
 
 from .fields import to_db
+from .form_fields import (
+    ISODateTimeField, MultipleArgsField, SFieldsCheckboxSelectMultiple)
 from .models import Unit
+from .unit.search import UnitSearch
 from .util import FUZZY, OBSOLETE, TRANSLATED, UNTRANSLATED
 
+
+UNIT_SEARCH_FILTER_CHOICES = (
+    ("all", "all"),
+    ("translated", "translated"),
+    ("untranslated", "untranslated"),
+    ("fuzzy", "fuzzy"),
+    ("incomplete", "incomplete"),
+    ("suggestions", "suggestions"),
+    ("my-suggestions", "my-suggestions"),
+    ("user-suggestions", "user-suggestions"),
+    ("user-suggestions-accepted", "user-suggestions-accepted"),
+    ("user-suggestions-rejected", "user-suggestions-rejected"),
+    ("my-submissions", "my-submissions"),
+    ("user-submissions", "user-submissions"),
+    ("my-submissions-overwritten", "my-submissions-overwritten"),
+    ("user-submissions-overwritten", "user-submissions-overwritten"),
+    ("checks", "checks"))
+
+UNIT_SEARCH_SORT_CHOICES = (
+    ('priority', 'priority'),
+    ('oldest', 'oldest'),
+    ('newest', 'newest'))
+
+# # # # # # #  text cleanup and highlighting # # # # # # # # # # # # #
 
 FORM_RE = re.compile('\r\n|\r|\n|\t|\\\\')
 
@@ -374,3 +408,126 @@ def unit_comment_form_factory(language):
             super(UnitCommentForm, self).save(**kwargs)
 
     return UnitCommentForm
+
+
+class UnitSearchForm(forms.Form):
+
+    unit_search_class = UnitSearch
+
+    initial = forms.BooleanField(required=False)
+    count = forms.IntegerField(required=False)
+    path = forms.CharField(max_length=100, required=False)
+    uids = MultipleArgsField(
+        field=forms.IntegerField(),
+        required=False)
+    filter = forms.ChoiceField(
+        required=False,
+        choices=UNIT_SEARCH_FILTER_CHOICES)
+    category = forms.ChoiceField(
+        required=False,
+        choices=CATEGORY_IDS.items())
+    modified_since = ISODateTimeField(required=False)
+    month = forms.DateField(required=False)
+    sort_by_param = forms.ChoiceField(
+        required=False,
+        choices=UNIT_SEARCH_SORT_CHOICES)
+
+    user = forms.ModelChoiceField(
+        queryset=get_user_model().objects.all(), required=False)
+
+    search = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={
+            'size': '15',
+            'placeholder': _('Search'),
+            'title': _("Search (Ctrl+Shift+S)<br/>Type and press Enter to "
+                       "search")}))
+
+    soptions = forms.MultipleChoiceField(
+        required=False,
+        widget=forms.CheckboxSelectMultiple,
+        choices=(
+            ('exact', _('Exact Match'))))
+
+    sfields = forms.MultipleChoiceField(
+        required=False,
+        widget=SFieldsCheckboxSelectMultiple,
+        choices=(
+            ('source', _('Source Text')),
+            ('target', _('Target Text')),
+            ('notes', _('Comments')),
+            ('locations', _('Locations'))),
+        initial=['source', 'target'])
+
+    def __init__(self, *la, **kwa):
+        self.request_user = kwa.pop("user")
+        super(UnitSearchForm, self).__init__(*la, **kwa)
+
+    def clean_user(self):
+        if self.cleaned_data["user"] is None:
+            return get_user_model().get(self.request_user)
+
+    def clean(self):
+        if self.cleaned_data['count'] is None:
+            self.cleaned_data["count"] = (
+                self.cleaned_data["user"].get_unit_rows())
+
+        pootle_path = self.cleaned_data.get("path", None)
+        if pootle_path is None:
+            lang = proj = dir_path = filename = None
+        else:
+            lang, proj, dir_path, filename = split_pootle_path(pootle_path)
+            if 'virtualfolder' in settings.INSTALLED_APPS:
+                vfolder, pootle_path = extract_vfolder_from_path(pootle_path)
+                self.cleaned_data["vfolder"] = vfolder
+                self.cleaned_data["pootle_path"] = pootle_path
+            else:
+                self.cleaned_data["vfolder"] = None
+        self.cleaned_data.update(
+            dict(language=lang, project=proj,
+                 dir_path=dir_path, filename=filename))
+
+        sort_on = "units"
+        if "filter" in self.cleaned_data:
+            unit_filter = self.cleaned_data["filter"]
+            if unit_filter in ('suggestions', 'user-suggestions'):
+                sort_on = 'suggestions'
+            elif unit_filter in ('user-submissions', ):
+                sort_on = 'submissions'
+        sort_by_param = self.cleaned_data["sort_by_param"]
+
+        if sort_by_param:
+            try:
+                from .views import ALLOWED_SORTS
+                self.cleaned_data["sort_by"] = (
+                    ALLOWED_SORTS[sort_on].get(sort_by_param, None))
+            except KeyError:
+                raise forms.ValidationError(
+                    "Invalid sort by param: '%s'" % sort_by_param)
+        else:
+            self.cleaned_data["sort_by"] = None
+
+        self.cleaned_data["sort_on"] = sort_on
+
+    def clean_checks(self):
+        return self.cleaned_data['checks'].split(',')
+
+    def clean_month(self):
+        if self.cleaned_data["month"]:
+            return get_date_interval(self.cleaned_data["month"])
+
+    def search_units(self, limit=True):
+        from django.conf import settings
+
+        klass = self.unit_search_class
+        if settings.POOTLE_SEARCH_BACKEND:
+            parts = settings.POOTLE_SEARCH_BACKEND.split(".")
+            module = ".".join(parts[:-1])
+            klass = parts[-1]
+            module = import_module(module)
+            klass = getattr(module, klass)
+
+        return klass(
+            request_user=self.request_user,
+            limit=limit,
+            **self.cleaned_data).grouped_search()
