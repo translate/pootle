547ba7544a52dd4e44e09e0c379774fe0b36ec28
Taras Semenenko <taras.semenenko@gmail.com>
2015-12-18 11:43:44 +0300

    Create Scorelog object for UPLOAD submissions
    - add UPLOAD submission type to EDIT_TYPES
    - get rid of EDITING_TYPES

--- pootle/apps/pootle_statistics/models.py
+++ pootle/apps/pootle_statistics/models.py
@@ -49,11 +49,10 @@ class SubmissionTypes(object):
     # Combined types that rely on other types (useful for querying)
     # Please use the `_TYPES` suffix to make it clear they're not core
     # types that are stored in the DB
-    EDIT_TYPES = [NORMAL, SYSTEM]
+    EDIT_TYPES = [NORMAL, SYSTEM, UPLOAD]
     CONTRIBUTION_TYPES = [NORMAL, SYSTEM, SUGG_ADD]
     SUGGESTION_TYPES = [SUGG_ACCEPT, SUGG_ADD, SUGG_REJECT]
     REVIEW_TYPES = [SUGG_ACCEPT, SUGG_REJECT]
-    EDITING_TYPES = [NORMAL, SYSTEM, UNIT_CREATE, UPLOAD]
 
 
 #: Values for the 'field' field of Submission
@@ -142,7 +141,7 @@ class SubmissionManager(BaseSubmissionManager):
         return (
             self.get_queryset().exclude(new_value__isnull=True).filter(
                 field__in=SubmissionFields.TRANSLATION_FIELDS,
-                type__in=SubmissionTypes.EDITING_TYPES,
+                type__in=SubmissionTypes.EDIT_TYPES,
             )
         )
 
