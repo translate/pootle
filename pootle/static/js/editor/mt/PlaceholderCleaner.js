translate/pootle

commit 1d8bf75a430cbea5045574a93d676e3b2ececc93

MT: refactor supporting code
This includes:

  * A new base class that implements most of the shared logic. Providers will
    only need to define their own details; no need to write imperative code.
  * Placeholder-handling code has been moved to a separate module.

The providers haven't been migrated yet, and there's some glue code missing.

pootle/static/js/editor/mt/PlaceholderCleaner.js
@@ -1,0 +1,80 @@
+/*
+ * Copyright (C) Pootle contributors.
+ *
+ * This file is a part of the Pootle project. It is distributed under the GPL3
+ * or later license. See the LICENSE file for a copy of the license and the
+ * AUTHORS file for copyright and authorship information.
+ */
+
+const HTML_PAT = /<[\/]?\w+.*?>/g;
+// The printf regex based on http://phpjs.org/functions/sprintf:522
+const C_PRINTF_PAT = /%%|%(\d+\$)?([-+\'#0 ]*)(\*\d+\$|\*|\d+)?(\.(\*\d+\$|\*|\d+))?([scboxXuidfegEG])/g;
+const C_SHARP_STR_PAT = /{\d+(,\d+)?(:[a-zA-Z ]+)?}/g;
+const PERCENT_NUMBER_PAT = /%\d+/g;
+
+
+class PlaceholderCleaner {
+
+  constructor() {
+    this.resetState();
+  }
+
+  resetState() {
+    this.argSubs = [];
+    this.argPos = 0;
+    this.sourceText = null;
+  }
+
+  collectArguments(s) {
+    this.argSubs[this.argPos] = s;
+    return `[${this.argPos++}]`;
+  }
+
+  replace(text) {
+    this.sourceText = text;
+
+    // Walk through known patterns and replace them with [N] placeholders
+    return text.replace(HTML_PAT, (s) => this.collectArguments(s))
+               .replace(C_PRINTF_PAT, (s) => this.collectArguments(s))
+               .replace(C_SHARP_STR_PAT, (s) => this.collectArguments(s))
+               .replace(PERCENT_NUMBER_PAT, (s) => this.collectArguments(s));
+  }
+
+  recover(translation) {
+    const { argSubs, sourceText } = this;
+
+    if (sourceText === null) {
+      throw new Error('Attempted to recover without calling `replace()` first');
+    }
+
+    // Fix whitespace which may have been added around [N] blocks
+    for (let i=0; i<argSubs.length; i++) {
+      if (sourceText.match(new RegExp('\\[' + i + '\\][^\\s]'))) {
+        translation = translation.replace(
+          new RegExp('\\[' + i + '\\]\\s+'), '[' + i + ']'
+        );
+      }
+      if (sourceText.match(new RegExp('[^\\s]\\[' + i + '\\]'))) {
+        translation = translation.replace(
+          new RegExp('\\s+\\[' + i + '\\]'), '[' + i + ']'
+        );
+      }
+    }
+
+    // Replace temporary [N] placeholders back to their real values
+    for (let i=0; i<argSubs.length; i++) {
+      const value = argSubs[i].replace(/\&/g, '&amp;')
+                              .replace(/\</g, '&lt;')
+                              .replace(/\>/g, '&gt;');
+      translation = translation.replace('[' + i + ']', value);
+    }
+
+    this.resetState();
+
+    return translation;
+  }
+
+}
+
+
+export default PlaceholderCleaner;

pootle/static/js/editor/app.js
@@ -40,9 +40,7 @@ import msg from '../msg';
 import score from '../score';
 import search from '../search';
 import utils from '../utils';
-import {
-  escapeUnsafeRegexSymbols, makeRegexForMultipleWords, normalizeCode
-} from './utils';
+import { escapeUnsafeRegexSymbols, makeRegexForMultipleWords } from './utils';
 
 
 const CTX_STEP = 1;
@@ -518,8 +516,7 @@ PTL.editor = {
       $('#extras-container').append(tmContent);
     }
 
-    // All is ready, let's call the ready functions of the MT backends
-    mtBackends.forEach((backend) => backend.ready());
+    this.runHooks();
 
     this.isUnitDirty = false;
     this.keepState = false;
@@ -2213,142 +2210,35 @@ PTL.editor = {
    * Machine Translation
    */
 
-  /* Checks whether the provided source is supported */
-  isSupportedSource: function (pairs, source) {
-    for (var i in pairs) {
-      if (source === pairs[i].source) {
-        return true;
-      }
+  runHooks: function () {
+    mtBackends.forEach((provider) => provider.init({
+      unit: this.units.getCurrent().toJSON(),
+    }));
+  },
+
+  /* FIXME: provide an alternative to such an ad-hoc entry point */
+  setTranslation: function(opts) {
+    const { translation } = opts;
+    if (translation === undefined && opts.msg) {
+      that.displayError(opts.msg);
+      return false;
     }
-    return false;
+
+    const area = document.querySelector('.js-translation-area');
+
+    area.value = translation;
+    autosize.update(area);
+
+    // Save a copy of the resulting text in the DOM for further
+    // similarity comparisons
+    area.dataset.translationAidMt = translation;
+
+    area.dispatchEvent((new Event('input')));
+    area.focus();
+
+    this.goFuzzy();
+
+    return true;
   },
-
-
-  /* Checks whether the provided target is supported */
-  isSupportedTarget: function (pairs, target) {
-    for (var i in pairs) {
-      if (target === pairs[i].target) {
-        return true;
-      }
-    }
-    return false;
-  },
-
-
-  /* Adds a new MT service button in the editor toolbar */
-  addMTButton: function (container, aClass, tooltip) {
-      var btn = '<a class="translate-mt ' + aClass + '">';
-      btn += '<i class="icon-' + aClass+ '" title="' + tooltip + '"><i/></a>';
-      $(container).first().prepend(btn);
-  },
-
-  /* Goes through all source languages and adds a new MT service button
-   * in the editor toolbar if the language is supported
-   */
-  addMTButtons: function (provider) {
-    const targetLang = this.units.getCurrent().get('store').get('target_lang');
-    if (this.isSupportedTarget(provider.pairs, targetLang)) {
-      var that = this,
-          $sources = $(".translate-toolbar");
-
-      $sources.each(function () {
-        var source = normalizeCode(
-          $(this).parents('.source-language').find('.translation-text').attr('lang')
-        );
-
-        if (that.isSupportedSource(provider.pairs, source)) {
-          that.addMTButton($(this).find('.js-toolbar-buttons'),
-            provider.buttonClassName,
-            provider.hint + ' (' + source.toUpperCase() + '&rarr;' + targetLang.toUpperCase() + ')');
-        }
-      });
-    }
-  },
-
-  collectArguments: function (s) {
-    this.argSubs[this.argPos] = s;
-    return "[" + (this.argPos++) + "]";
-  },
-
-  translate: function (linkObject, providerCallback) {
-    var that = this,
-        $areas = $('.js-translation-area'),
-        $sources = $(linkObject).parents('.source-language').find('.translation-text'),
-        langFrom = normalizeCode($sources[0].lang),
-        langTo = normalizeCode($areas[0].lang);
-
-    var htmlPat = /<[\/]?\w+.*?>/g,
-    // The printf regex based on http://phpjs.org/functions/sprintf:522
-        cPrintfPat = /%%|%(\d+\$)?([-+\'#0 ]*)(\*\d+\$|\*|\d+)?(\.(\*\d+\$|\*|\d+))?([scboxXuidfegEG])/g,
-        csharpStrPat = /{\d+(,\d+)?(:[a-zA-Z ]+)?}/g,
-        percentNumberPat = /%\d+/g,
-        pos = 0;
-
-    $sources.each(function (j) {
-      // Reset collected arguments array and counter
-      that.argSubs = [];
-      that.argPos = 0;
-
-      // Walk through known patterns and replace them with [N] placeholders
-      var sourceText = $(this).text()
-                              .replace(htmlPat,
-                                       that.collectArguments.bind(that))
-                              .replace(cPrintfPat,
-                                       that.collectArguments.bind(that))
-                              .replace(csharpStrPat,
-                                       that.collectArguments.bind(that))
-                              .replace(percentNumberPat,
-                                       that.collectArguments.bind(that));
-
-      providerCallback(sourceText, langFrom, langTo, function (opts) {
-        var translation = opts.translation,
-            msg = opts.msg,
-            area = $areas[j],
-            $area = $areas.eq(j),
-            i, value;
-
-        if (translation === undefined && msg) {
-          that.displayError(msg);
-          return;
-        }
-
-        // Fix whitespace which may have been added around [N] blocks
-        for (i=0; i<that.argSubs.length; i++) {
-          if (sourceText.match(new RegExp("\\[" + i + "\\][^\\s]"))) {
-            translation = translation.replace(
-              new RegExp("\\[" + i + "\\]\\s+"), "[" + i + "]"
-            );
-          }
-          if (sourceText.match(new RegExp("[^\\s]\\[" + i + "\\]"))) {
-            translation = translation.replace(
-              new RegExp("\\s+\\[" + i + "\\]"), "[" + i + "]"
-            );
-          }
-        }
-
-        // Replace temporary [N] placeholders back to their real values
-        for (i=0; i<that.argSubs.length; i++) {
-          value = that.argSubs[i].replace(/\&/g, '&amp;')
-                                 .replace(/\</g, '&lt;')
-                                 .replace(/\>/g, '&gt;');
-          translation = translation.replace("[" + i + "]", value);
-        }
-
-        $area.val($('<div />').html(translation).text());
-        autosize.update(area);
-
-        // Save a copy of the resulting text in the DOM for further
-        // similarity comparisons
-        if (opts.storeResult) {
-          $area.attr('data-translation-aid-mt', translation);
-        }
-      });
-    });
-
-    $areas.eq(0).trigger('input').focus();
-
-    that.goFuzzy();
-    return false;
-  }
 
 };

pootle/static/js/editor/mt/MTProvider.js
@@ -1,0 +1,134 @@
+/*
+ * Copyright (C) Pootle contributors.
+ *
+ * This file is a part of the Pootle project. It is distributed under the GPL3
+ * or later license. See the LICENSE file for a copy of the license and the
+ * AUTHORS file for copyright and authorship information.
+ */
+
+import $ from 'jquery';
+import assign from 'object-assign';
+
+import fetch from 'utils/fetch';
+import { normalizeCode } from '../utils';
+import PlaceholderCleaner from './PlaceholderCleaner';
+
+
+class MTProvider {
+
+  constructor(opts) {
+    assign(this, opts);
+
+    $(document).on('click', `.js-${opts.name}`, (e) => {
+      const sourceLang = e.currentTarget.dataset.sourceLang;
+      this.translate(this.unit.sources[sourceLang][0], sourceLang,
+                     this.unit.store.target_lang)
+          .then(
+            (result) => PTL.editor.setTranslation(result)
+          );
+    });
+  }
+
+  init(props) {
+    this.unit = props.unit;
+
+    this.injectUI(props);
+  }
+
+  /**
+   * Checks whether the provided source language is supported
+   * @param {string} target - language code
+   */
+  isSupportedSource(source) {
+    for (let i in this.pairs) {
+      if (source === this.pairs[i].source) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Checks whether the provided target language is supported
+   * @param {string} target - language code
+   */
+  isSupportedTarget(target) {
+    for (let i in this.pairs) {
+      if (target === this.pairs[i].target) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Translates a text into another language
+   * @param {string} text - text to be translated
+   * @param {string} sourceLanguage - language code
+   * @param {string} targetLanguage - language code
+   */
+  translate(sourceText, sourceLanguage, targetLanguage) {
+    const placeholderCleaner = new PlaceholderCleaner();
+    const bodyOpts =  {
+      sourceLanguage,
+      targetLanguage,
+      text: placeholderCleaner.replace(sourceText),
+    };
+
+    return fetch({
+      crossDomain: true,
+      url: this.url,
+      body: this.getRequestBody(bodyOpts)
+    }).then(
+      (response) => this.handleSuccess(response)
+    ).then(
+      (result) => {
+        if (!'translation' in result) {
+          return result;
+        }
+        result.translation = placeholderCleaner.recover(result.translation);
+        return result;
+      }
+    );
+  }
+
+  handleSuccess(response) {
+    throw new Error('Not Implemented');
+  }
+
+  /**
+   * Injects the controls in the UI.
+   * @param {object} props - the current unit
+   */
+  injectUI(props) {
+    const { unit } = props;
+    const targetLang = normalizeCode(unit.store.target_lang);
+
+    if (this.isSupportedTarget(targetLang)) {
+
+      Object.keys(unit.sources).forEach((sourceLanguage) => {
+        const sourceLang = normalizeCode(sourceLanguage);
+
+        if (this.isSupportedSource(sourceLang)) {
+          $(`.js-mt-${sourceLang}`).append(
+            this.renderIcon(sourceLang, targetLang)
+          );
+        }
+      });
+    }
+  }
+
+  renderIcon(sourceLang, targetLang) {
+    const hint = `${this.displayName} (${sourceLang.toUpperCase()} &rarr; ` +
+                 `${targetLang.toUpperCase()})`;
+    return (
+      `<a class="translate-mt js-${this.name}" data-source-lang="${sourceLang}">` +
+        `<i class="icon-${this.name}" title="${hint}">` +
+      `</a>`
+    );
+  }
+
+}
+
+
+export default MTProvider;

pootle/templates/editor/units/edit.html
@@ -106,10 +106,9 @@
                 <div class="language-name">{{ altunit.store.translation_project.language.name }}</div>
                 {% if cansuggest or cantranslate %}
                 <div class="translate-toolbar">
-                  <span class="js-toolbar-buttons">
-                    <a class="icon-copy js-copyoriginal" data-uid="{{ altunit.id }}"
-                       title="{% trans 'Copy into translation' %}" accesskey="c"></a>
-                  </span>
+                  <span class="js-mt-{{ altunit.store.translation_project.language.code }}"></span>
+                  <a class="icon-copy js-copyoriginal" data-uid="{{ altunit.id }}"
+                     title="{% trans 'Copy into translation' %}" accesskey="c"></a>
                 </div>
                 {% endif %}
               </div>
@@ -134,10 +133,9 @@
                       {% trans "Report a problem with this string" %}
                     </a>
                   </span>
-                  <span class="js-toolbar-buttons">
-                    <a class="icon-copy js-copyoriginal" data-uid="{{ unit.id }}"
-                       title="{% trans 'Copy into translation' %}" accesskey="c"></a>
-                  </span>
+                  <span class="js-mt-{{ source_language.code }}"></span>
+                  <a class="icon-copy js-copyoriginal" data-uid="{{ unit.id }}"
+                     title="{% trans 'Copy into translation' %}" accesskey="c"></a>
                 </div>
                 {% endif %}
               </div>

commit 093a024bda16c84dddb7f3aa37def7f99d37f363

MT: use replaced source text when recovering
Using the original source text had no sense, and this was a regression
introduced in 1d8bf75 when refactoring the old code.

Fixes #4306.

pootle/static/js/editor/mt/PlaceholderCleaner.js
@@ -22,7 +22,7 @@ class PlaceholderCleaner {
   resetState() {
     this.argSubs = [];
     this.argPos = 0;
-    this.sourceText = null;
+    this.replacedSourceText = null;
   }
 
   collectArguments(s) {
@@ -31,32 +31,32 @@ class PlaceholderCleaner {
   }
 
   replace(text) {
-    this.sourceText = text;
-
     // Walk through known patterns and replace them with [N] placeholders
-    return text
+    this.replacedSourceText = text
       .replace(HTML_PAT, (s) => this.collectArguments(s))
       .replace(C_PRINTF_PAT, (s) => this.collectArguments(s))
       .replace(C_SHARP_STR_PAT, (s) => this.collectArguments(s))
       .replace(PERCENT_NUMBER_PAT, (s) => this.collectArguments(s));
+
+    return this.replacedSourceText;
   }
 
   recover(translation) {
-    const { argSubs, sourceText } = this;
+    const { argSubs, replacedSourceText } = this;
 
-    if (sourceText === null) {
+    if (replacedSourceText === null) {
       throw new Error('Attempted to recover without calling `replace()` first');
     }
 
     let recoveredTranslation = translation;
     // Fix whitespace which may have been added around [N] blocks
     for (let i = 0; i < argSubs.length; i++) {
-      if (sourceText.match(new RegExp('\\[' + i + '\\][^\\s]'))) {
+      if (replacedSourceText.match(new RegExp('\\[' + i + '\\][^\\s]'))) {
         recoveredTranslation = recoveredTranslation.replace(
           new RegExp('\\[' + i + '\\]\\s+'), '[' + i + ']'
         );
       }
-      if (sourceText.match(new RegExp('[^\\s]\\[' + i + '\\]'))) {
+      if (replacedSourceText.match(new RegExp('[^\\s]\\[' + i + '\\]'))) {
         recoveredTranslation = recoveredTranslation.replace(
           new RegExp('\\s+\\[' + i + '\\]'), '[' + i + ']'
         );