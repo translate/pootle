92f591019fe120cd7e83843c86bba66c5b329322
Ryan Northey <ryan@space23.net>
2015-12-31 22:06:03 +0000

    Add test for view context

---
+++ pootle_pytest/suite.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+from json import loads
+
+
+def view_context_test(ctx, **assertions):
+    for k, v in assertions.items():
+        if k == "stats":
+            assert loads(ctx[k]) == loads(v)
+        elif k == "check_categories":
+            for i, cat in enumerate(ctx[k]):
+                assert v[i] == cat
+        elif k == "search_form":
+            assert ctx[k].as_p() == v.as_p()
+        elif k == "table":
+            for tk in ["id", "fields", "headings"]:
+                assert ctx[k][tk] == v[tk]
+            assert list(ctx[k]["items"]) == list(v["items"])
+        else:
+            assert ctx[k] == v
