63e0c1b44dec5a2cfa9d61000831e34c31919d75
Leandro Regueiro <leandro.regueiro@gmail.com>
2016-01-11 13:09:17 +0100

    Further simplify vfolder extraction algorithm
    A given path can match either a single Directory or a single
    VirtualFolderTreeItem.

--- pootle/apps/virtualfolder/helpers.py
+++ pootle/apps/virtualfolder/helpers.py
@@ -7,8 +7,6 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
-from pootle.core.url_helpers import get_all_pootle_paths
-
 from .models import VirtualFolderTreeItem
 
 
@@ -39,14 +37,12 @@ def extract_vfolder_from_path(request_path):
     :param request_path: a path that may contain a virtual folder
     :return: (`VirtualFolder`, path)
     """
-    all_dir_paths = [
-        dir_path for dir_path in get_all_pootle_paths(request_path)
-        if dir_path.count('/') > 3 and dir_path.endswith('/')]
-    vftis = VirtualFolderTreeItem.objects.filter(pootle_path__in=all_dir_paths)
-    if vftis.exists():
-        # There may be more than one vfti with matching pootle_path, so we get
-        # the one with the shortest path or highest priority.
-        vfti = (vftis.select_related("vfolder", "directory")
-                     .order_by("pootle_path").first())
+    if not (request_path.count('/') > 3 and request_path.endswith('/')):
+        return None, request_path
+
+    try:
+        vfti = VirtualFolderTreeItem.objects.get(pootle_path=request_path)
+    except VirtualFolderTreeItem.DoesNotExist:
+        return None, request_path
+    else:
         return vfti.vfolder, vfti.directory.pootle_path
-    return None, request_path
