3d2e0398d49470aa37b6491fba5582268f8916c3
Ryan Northey <ryan@space23.net>
2016-01-16 19:53:07 +0000

    Optimization: move select_relateds to creation of ProjectResource
    this reduces expensive queries

--- pootle/apps/pootle_project/views.py
+++ pootle/apps/pootle_project/views.py
@@ -71,7 +71,12 @@ class ProjectMixin(object):
                self.kwargs['filename']))
         regex = r"^/[^/]*%s$" % project_path
         if not self.kwargs["filename"]:
-            dirs = Directory.objects.live().select_related("parent__pootle_path")
+            dirs = Directory.objects.live()
+            if self.kwargs['dir_path'].count("/"):
+                tp_prefix = "parent__" * self.kwargs['dir_path'].count("/")
+                dirs = dirs.select_related(
+                    "%stranslationproject" % tp_prefix,
+                    "%stranslationproject__language" % tp_prefix)
             resources = (
                 dirs.exclude(pootle_path__startswith="/templates")
                     .filter(pootle_path__endswith=project_path)
@@ -131,25 +136,16 @@ class ProjectBrowseView(ProjectMixin, PootleBrowseView):
             if (self.kwargs['dir_path']
                 or self.kwargs['filename'])
             else make_language_item)
-        dir_path, filename = split_pootle_path(self.pootle_path)[2:]
-        if dir_path.count("/"):
-            tp_prefix = "parent__" * dir_path.count("/")
-            selects = [
-                "%stranslationproject" % tp_prefix,
-                "%stranslationproject" % tp_prefix,
-                "%stranslationproject__language" % tp_prefix]
-        elif not filename:
-            selects = ["language", "directory"]
-        else:
-            selects = None
+
         items = [
             item_func(item)
             for item
             in self.object.get_children_for_user(
-                self.request.profile,
-                select_related=selects)]
+                self.request.profile)]
+
         items.sort(
             lambda x, y: locale.strcoll(x['title'], y['title']))
+
         return items
 
 
