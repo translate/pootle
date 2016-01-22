21060187b5f58f4ffb11f629ed61dc1175eea813
Dwayne Bailey <dwayne@translate.org.za>
2016-01-22 17:17:10 +0000

    argparse: Test 'pootle' command and subcommands.

---
+++ tests/commands/pootle_runner.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+import pytest
+
+from subprocess import call
+
+
+@pytest.mark.cmd
+def test_pootle_noargs(capfd):
+    """Pootle no args should give help"""
+    call(['pootle'])
+    out, err = capfd.readouterr()
+    assert "Type 'pootle help <subcommand>'" in out
+
+
+@pytest.mark.cmd
+def test_pootle_version(capfd):
+    """Display Pootle version info"""
+    call(['pootle', '--version'])
+    out, err = capfd.readouterr()
+    assert 'Pootle' in err
+    assert 'Django' in err
+    assert 'Translate Toolkit' in err
+
+
+@pytest.mark.cmd
+def test_pootle_init(capfd):
+    """pootle init --help"""
+    call(['pootle', 'init', '--help'])
+    out, err = capfd.readouterr()
+    assert "--db" in out
+
+
+@pytest.mark.cmd
+def test_pootle_init_db_sqlite(capfd):
+    """pootle init --help"""
+    call(['pootle', 'init', '--db=sqlite'])
+    out, err = capfd.readouterr()
+    assert "Configuration file created" in out
