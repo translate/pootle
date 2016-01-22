bc57283cfeb57cde6d143af466102bbf837e6385
Dwayne Bailey <dwayne@translate.org.za>
2016-01-22 23:49:35 +0000

    argparse: Test contributors

---
+++ tests/commands/contributors.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+import pytest
+
+from django.core.management import call_command
+
+
+@pytest.mark.cmd
+@pytest.mark.django_db
+def test_contributors_noargs(capfd, en_tutorial_po_member_updated):
+    """Contributors across the site."""
+    call_command('contributors')
+    out, err = capfd.readouterr()
+    assert "Member (1 contributions)" in out
+
+
+@pytest.mark.cmd
+@pytest.mark.django_db
+def test_contributors_revision(capfd, en_tutorial_po_member_updated):
+    """Contributors since a given revision."""
+    call_command('contributors', '--from-revision=1')
+    out, err = capfd.readouterr()
+    assert "Member (1 contributions)" in out
+
+
+@pytest.mark.cmd
+@pytest.mark.django_db
+def test_contributors_project(capfd, en_tutorial_po_member_updated):
+    """Contributors in a given project."""
+    call_command('contributors', '--project=tutorial')
+    out, err = capfd.readouterr()
+    assert "Member (1 contributions)" in out
+
+
+@pytest.mark.cmd
+@pytest.mark.django_db
+def test_contributors_language(capfd, en_tutorial_po_member_updated):
+    """Contributors in a given language."""
+    call_command('contributors', '--language=en')
+    out, err = capfd.readouterr()
+    assert "Member (1 contributions)" in out
+
+
+@pytest.mark.cmd
+@pytest.mark.django_db
+def test_contributors_sort(capfd, en_tutorial_po_member_updated, member):
+    """Contributors in contribution order."""
+    member.email = "member@test.email"
+    member.save()
+    call_command('contributors', '--sort-by=contributions')
+    out, err = capfd.readouterr()
+    assert "Member <member@test.email> (1 contributions)" in out
