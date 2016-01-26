a7fe6709dfb937a16fc2cbf6c7fae2301ebbd654
Taras Semenenko <taras.semenenko@gmail.com>
2016-01-26 15:41:16 +0300

    Pass through all users and refresh their score
    Refs. #4418

--- pootle/apps/pootle_app/management/commands/refresh_scores.py
+++ pootle/apps/pootle_app/management/commands/refresh_scores.py
@@ -8,6 +8,7 @@
 # AUTHORS file for copyright and authorship information.
 
 
+import datetime
 import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'pootle.settings'
 
@@ -28,10 +29,45 @@ class Command(BaseCommand):
             default=False,
             help='Reset all scores to zero',
         )
+        parser.add_argument(
+            '--user',
+            action='append',
+            dest='users',
+            help='User to refresh',
+        )
 
     def handle(self, **options):
+        self.stdout.write('Start running of refresh_scores command...')
+
+        User = get_user_model()
+        users = User.objects.all()
+        if options['users']:
+            users = users.filter(username__in=options['users'])
 
         if options['reset']:
-            User = get_user_model()
-            User.objects.all().update(score=0)
-            ScoreLog.objects.all().delete()
+            users.update(score=0)
+            scorelogs = ScoreLog.objects.all()
+            if options['users']:
+                scorelogs = scorelogs.filter(user__in=users)
+            scorelogs.delete()
+            return
+
+        start = datetime.datetime.now()
+        for user_pk, username in users.values_list("pk", "username"):
+            self.stdout.write("Processing user %s..." % username)
+            scorelog_qs = ScoreLog.objects.filter(user=user_pk) \
+                .select_related(
+                    'submission',
+                    'submission__suggestion',
+                    'submission__unit')
+            user_score = 0
+            for scorelog in scorelog_qs.iterator():
+                score_delta = scorelog.get_score_delta()
+                user_score += score_delta
+                ScoreLog.objects.filter(id=scorelog.id) \
+                    .update(score_delta=score_delta)
+            self.stdout.write("Score for user %s set to %.3f" %
+                              (username, user_score))
+            User.objects.filter(id=user_pk).update(score=user_score)
+        end = datetime.datetime.now()
+        self.stdout.write('All done in %s.' % (end - start))
