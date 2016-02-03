translate/pootle

commit 810c49e03cca9e429ba8459abd440a902ee4b954

Don't change an user score when old actions are processed
fix #247

pootle/apps/pootle_statistics/models.py
@@ -492,16 +492,20 @@ class ScoreLog(models.Model):
         analyzeCost = ns * ANALYZE_COEF
 
         def get_sugg_rejected():
+            result = 0
             try:
                 s = self.submission.suggestion.submission_set \
                         .get(type=SubmissionTypes.SUGG_ADD) \
                         .similarity
-                self.similarity = 0 if s is None else s
+                if s is None:
+                    s = 0
+                self.similarity = s
                 rawTranslationCost = ns * EDIT_COEF * (1 - s)
-            except:
-                rawTranslationCost = 0
+                result = (-1) * (rawTranslationCost * SUGG_COEF + analyzeCost)
+            except Submission.DoesNotExist:
+                pass
 
-            return (-1) * (rawTranslationCost * SUGG_COEF + analyzeCost)
+            return result
 
         def get_edit_penalty():
             try:
@@ -512,9 +516,11 @@ class ScoreLog(models.Model):
                     field=SubmissionFields.TARGET,
                     type=SubmissionTypes.NORMAL
                 ).similarity
-                self.similarity = 0 if s is None else s
+                if s is None:
+                    s = 0
+                self.similarity = s
                 rawTranslationCost = ns * EDIT_COEF * (1 - s)
-            except:
+            except Submission.DoesNotExist:
                 rawTranslationCost = 0
 
             return (-1) * rawTranslationCost
@@ -524,9 +530,11 @@ class ScoreLog(models.Model):
                 s = self.submission.suggestion.submission_set \
                         .get(type=SubmissionTypes.SUGG_ADD) \
                         .similarity
-                self.similarity = 0 if s is None else s
+                if s is None:
+                    s = 0
+                self.similarity = 0
                 rawTranslationCost = ns * EDIT_COEF * (1 - s)
-            except:
+            except Submission.DoesNotExist:
                 rawTranslationCost = 0
 
             return rawTranslationCost * (1 - SUGG_COEF)

commit 0e08a1af47897bb4b0f3c02b9ce5eaa195e405bf

Use real value of similarity when suggestion was added instead of 0
Fixes a bug caused by typo in 810c49e03cc. It is necessary to recalculate
to fix user scores data.

pootle/apps/pootle_statistics/models.py
@@ -604,7 +604,7 @@ class ScoreLog(models.Model):
                         .similarity
                 if s is None:
                     s = 0
-                self.similarity = 0
+                self.similarity = s
                 rawTranslationCost = ns * EDIT_COEF * (1 - s)
             except Submission.DoesNotExist:
                 rawTranslationCost = 0