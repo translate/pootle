8e70166b8034cb27861e912fcb439e52906e18bd
Ryan Northey <ryan@space23.net>
2016-02-12 07:21:06 +0000

    Add fixtures for unit_filter tests

--- pytest_pootle/fixtures/search.py
+++ pytest_pootle/fixtures/search.py
@@ -53,6 +53,71 @@ UNITS_TEXT_SEARCH_TESTS["suggestion for translated FOO (target)"] = {
     "sfields": ["target"],
     "empty": True}
 
+UNITS_CONTRIB_SEARCH_TESTS = [
+    "suggestions",
+    "FOO",
+    "member:my_suggestions",
+    "my_suggestions",
+    "member:user_suggestions",
+    "user_suggestions",
+    "member:user_suggestions_accepted",
+    "user_suggestions_accepted",
+    "member:user_suggestions_rejected",
+    "user_suggestions_rejected",
+    "member:my_submissions",
+    "my_submissions",
+    "member:user_submissions",
+    "user_submissions",
+    "member:my_submissions_overwritten",
+    "my_submissions_overwritten",
+    "member:user_submissions_overwritten",
+    "user_submissions_overwritten"]
+
+UNITS_STATE_SEARCH_TESTS = [
+    "all",
+    "translated",
+    "untranslated",
+    "fuzzy",
+    "incomplete",
+    "FOO"]
+
+UNITS_CHECKS_SEARCH_TESTS = [
+    "checks:foo",
+    "category:foo",
+    "category:critical",
+    "checks:endpunc",
+    "checks:endpunc,printf",
+    "checks:endpunc,foo"]
+
+
+@pytest.fixture(params=UNITS_STATE_SEARCH_TESTS)
+def units_state_searches(request):
+    return request.param
+
+
+@pytest.fixture(params=UNITS_CHECKS_SEARCH_TESTS)
+def units_checks_searches(request):
+    from pootle_misc.checks import get_category_id
+
+    check_type, check_data = request.param.split(":")
+    if check_type == "category":
+        return check_type, get_category_id(check_data)
+    return check_type, check_data.split(",")
+
+
+@pytest.fixture(params=UNITS_CONTRIB_SEARCH_TESTS)
+def units_contributor_searches(request):
+    from django.contrib.auth import get_user_model
+
+    User = get_user_model()
+    if ":" in request.param:
+        username, unit_filter = request.param.split(":")
+        user = User.objects.get(username=username)
+    else:
+        user = None
+        unit_filter = request.param
+    return unit_filter, user
+
 
 @pytest.fixture(params=UNITS_TEXT_SEARCH_TESTS.keys())
 def units_text_searches(request):
