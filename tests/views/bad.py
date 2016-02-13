a51902866ddb7bcab8065d6689f4706edfaa3c43
Ryan Northey <ryan@space23.net>
2016-02-13 20:00:38 +0000

    Cleanup: remove unused code from bad views test

--- tests/views/bad.py
+++ tests/views/bad.py
@@ -13,14 +13,6 @@ import pytest
 def test_views_bad(bad_views):
     path, response, test = bad_views
     assert response.status_code == test["code"]
-    for k, v in test.items():
-        if k == "code":
-            continue
-        if k == "location":
-            if test['location'] is None:
-                location = None
-            else:
-                location = "http://testserver/%s" % test["location"].lstrip("/")
-            assert response.get("location") == location
-        else:
-            assert response.get(k) == v
+    if test.get("location"):
+        location = "http://testserver/%s" % test["location"].lstrip("/")
+        assert response.get("location") == location
