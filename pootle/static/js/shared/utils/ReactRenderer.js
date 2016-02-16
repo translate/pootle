a66d0c5ccb3a12bdd5685c5416058be15ce4866e
Julen Ruiz Aizpuru <julenx@gmail.com>
2016-02-16 16:01:04 +0100

    Add ReactRenderer wrapper
    The wrapper is (temporarily) necessary so that component changes within a single
    page can be handled gracefully without leaking memory. Refs.
    http://facebook.github.io/react/blog/2015/10/01/react-render-and-top-level-api.html

---
+++ pootle/static/js/shared/utils/ReactRenderer.js
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) Pootle contributors.
+ *
+ * This file is a part of the Pootle project. It is distributed under the GPL3
+ * or later license. See the LICENSE file for a copy of the license and the
+ * AUTHORS file for copyright and authorship information.
+ */
+
+import ReactDOM from 'react-dom';
+
+
+const ReactRenderer = {
+
+  nodes: [],
+
+  render(component, node) {
+    ReactDOM.render(component, node);
+    this.nodes.push(node);
+  },
+
+  unmountComponents() {
+    this.nodes.forEach((node) => ReactDOM.unmountComponentAtNode(node));
+    this.nodes = [];
+  },
+
+};
+
+
+export default ReactRenderer;
