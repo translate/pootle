915db9edb092926679aa554e5657f8774631d1ba
Ryan Northey <ryan@space23.net>
2016-02-16 07:28:51 +0000

    Add get_units view tests

---
+++ tests/views/get_units.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+import json
+
+import pytest
+
+from pytest_pootle.search import calculate_search_results
+
+
+@pytest.mark.django_db
+def test_get_units(get_units_views):
+    (user, search_params, url_params, response) = get_units_views
+    result = json.loads(response.content)
+
+    assert "unitGroups" in result
+    assert isinstance(result["unitGroups"], list)
+    if result["unitGroups"]:
+        assert "uIds" in result
+
+        expected_uids, expected_units = calculate_search_results(
+            search_params, user)
+
+        assert list(result["uIds"]) == list(expected_uids)
+
+        for i, group in enumerate(expected_units):
+            result_group = result["unitGroups"][i]
+            for store, data in group.items():
+                result_data = result_group[store]
+                assert (
+                    [u["url"] for u in result_data["units"]]
+                    == [u["url"] for u in data["units"]])
