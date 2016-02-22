e37fa75a955ec248696c21a24224513b58058de4
Ryan Northey <ryan@space23.net>
2016-02-22 23:51:48 +0000

    Add signal receiver to update Units if Store pootle_path changes

--- pootle/apps/pootle_store/apps.py
+++ pootle/apps/pootle_store/apps.py
@@ -6,4 +6,15 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
-default_app_config = 'pootle_store.apps.PootleStoreConfig'
+import importlib
+
+from django.apps import AppConfig
+
+
+class PootleStoreConfig(AppConfig):
+
+    name = "pootle_store"
+    verbose_name = "Pootle Store"
+
+    def ready(self):
+        importlib.import_module("pootle_store.receivers")
