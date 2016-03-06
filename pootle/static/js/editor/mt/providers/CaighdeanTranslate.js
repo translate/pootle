0fb87b0866155e61b99700e8b131546056a951b2
Ryan Northey <ryan@space23.net>
2016-03-06 12:05:46 +0000

    Add Gaelic machine translation provider
    https://github.com/kscanne/caighdean/blob/master/API.md
    
    Fixes #4258

---
+++ pootle/static/js/editor/mt/providers/CaighdeanTranslate.js
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) Pootle contributors.
+ *
+ * This file is a part of the Pootle project. It is distributed under the GPL3
+ * or later license. See the LICENSE file for a copy of the license and the
+ * AUTHORS file for copyright and authorship information.
+ */
+
+import MTProvider from '../MTProvider';
+
+
+class CaighdeanTranslate extends MTProvider {
+
+  constructor(apiKey) {
+    super({
+      apiKey,
+      name: 'caighdean-translate',
+      displayName: 'Caighdeán Translate',
+      url: 'http://borel.slu.edu/cgi-bin/seirbhis3.cgi',
+      supportedLanguages: ['gd', 'gv', 'ga_IE', 'ga-IE'],
+    });
+  }
+
+  getRequestBody(opts) {
+    return {
+      teacs: opts.text,
+      foinse: opts.sourceLanguage,
+    };
+  }
+
+  handleSuccess(response) {
+    const trimmed = /^([.,\/;”:!?%})]|<[^>]+>)$/;
+    let text = '';
+    for (let i = 0; i < response.length; i++) {
+      let token = response[i][1];
+      if (!token.match(trimmed) && text !== '') {
+        token = ' ' + token;
+      }
+      text = text + token;
+    }
+    return {
+      translation: text.trim(),
+    };
+  }
+}
+
+
+export default CaighdeanTranslate;
