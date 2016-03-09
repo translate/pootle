ef469b4e81bc5ee48a07c8a1c2090f7c8cdbcae4
Leandro Regueiro <leandro.regueiro@gmail.com>
2016-03-09 23:07:41 +0100

    Add ability to only list user names and emails
    Useful when you just want to email the users. The anonymous
    contributions are therefore skipped.

--- pootle/apps/pootle_app/management/commands/contributors.py
+++ pootle/apps/pootle_app/management/commands/contributors.py
@@ -42,11 +42,22 @@ class Command(PootleCommand):
             help="Sort by specified item. Accepts: %(choices)s. "
                  "Default: %(default)s",
         )
+        parser.add_argument(
+            "--only-emails",
+            action="store_true",
+            dest="only_emails",
+            help="Output only names and email addresses. Contribution counts "
+                 "are excluded.",
+        )
 
     def handle_all(self, **options):
         system_user = User.objects.get_system_user()
         units = Unit.objects.exclude(submitted_by=system_user) \
                             .exclude(submitted_by=None)
+
+        if options["only_emails"]:
+            nobody_user = User.objects.get_nobody_user()
+            units = units.exclude(submitted_by=nobody_user)
 
         if options["revision"]:
             units = units.filter(revision__gte=options["revision"])
@@ -65,9 +76,10 @@ class Command(PootleCommand):
         for v in units.values("submitted_by"):
             contribs.update((v["submitted_by"], ))
 
-        self.list_contributions(contribs, options["sort_by"])
+        self.list_contributions(contribs, options["sort_by"],
+                                options["only_emails"])
 
-    def list_contributions(self, contribs, sort_by):
+    def list_contributions(self, contribs, sort_by, only_emails):
         if sort_by == "name":
             contributions = contribs.items()
         else:
@@ -79,7 +91,11 @@ class Command(PootleCommand):
             name = user.display_name
             if user.email:
                 name += " <%s>" % (user.email)
-            out.append("%s (%i contributions)" % (name, count))
+            elif only_emails:
+                continue
+            if not only_emails:
+                name = "%s (%i contributions)" % (name, count)
+            out.append(name)
 
         if sort_by == "name":
             # Sort users alphabetically
