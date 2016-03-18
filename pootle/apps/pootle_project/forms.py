14e2326fed88a7d3c315b08062bea26e8a2c11e0
Ryan Northey <ryan@space23.net>
2016-03-18 10:23:07 +0000

    Move TranslationProjectForm out of factory function

--- pootle/apps/pootle_project/forms.py
+++ pootle/apps/pootle_project/forms.py
@@ -16,28 +16,34 @@ from pootle_project.models import Project
 from pootle_translationproject.models import TranslationProject
 
 
-def tp_form_factory(current_project):
+class TranslationProjectForm(forms.ModelForm):
 
-    class TranslationProjectForm(forms.ModelForm):
+    language = LiberalModelChoiceField(
+        label=_("Language"),
+        queryset=Language.objects.all(),
+        widget=forms.Select(
+            attrs={
+                'class': 'js-select2 select2-language'}))
+    project = forms.ModelChoiceField(
+        queryset=Project.objects.all(),
+        widget=forms.HiddenInput())
 
-        project = forms.ModelChoiceField(
-            queryset=Project.objects.filter(pk=current_project.pk),
-            initial=current_project.pk,
-            widget=forms.HiddenInput(),
-        )
-        language = LiberalModelChoiceField(
-            label=_("Language"),
-            queryset=Language.objects.exclude(
-                translationproject__project=current_project
-            ),
-            widget=forms.Select(attrs={
-                'class': 'js-select2 select2-language',
-            }),
-        )
+    class Meta(object):
+        prefix = "existing_language"
+        model = TranslationProject
+        fields = ('language', 'project')
 
-        class Meta(object):
-            prefix = "existing_language"
-            model = TranslationProject
-            fields = ('language', 'project')
-
-    return TranslationProjectForm
+    def __init__(self, *args, **kwargs):
+        """If this form is not bound, it must be called with an initial value
+        for Project.
+        """
+        super(TranslationProjectForm, self).__init__(*args, **kwargs)
+        if kwargs.get("instance"):
+            project_id = kwargs["instance"].project.pk
+        else:
+            project_id = kwargs["initial"]["project"]
+            self.fields["language"].queryset = (
+                self.fields["language"].queryset.exclude(
+                    translationproject__project_id=project_id))
+        self.fields["project"].queryset = self.fields[
+            "project"].queryset.filter(pk=project_id)
