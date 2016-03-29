04b8f0c6008a84dccdc6c90ecbb666595d5c953b
Ryan Northey <ryan@space23.net>
2016-03-29 10:38:18 +0100

    Add getters/providers to vfolder app config

--- pootle/apps/virtualfolder/apps.py
+++ pootle/apps/virtualfolder/apps.py
@@ -18,3 +18,5 @@ class PootleVirtualFolderConfig(AppConfig):
 
     def ready(self):
         importlib.import_module("virtualfolder.receivers")
+        importlib.import_module("virtualfolder.providers")
+        importlib.import_module("virtualfolder.getters")
