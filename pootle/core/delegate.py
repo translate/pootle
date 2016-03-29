29e3906e8b084b6787f61a9252a82a7fa5320ce9
Ryan Northey <ryan@space23.net>
2016-03-29 10:32:06 +0100

    Add pootle.core.delegate

--- pootle/core/delegate.py
+++ pootle/core/delegate.py
@@ -6,11 +6,13 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
-
 from pootle.core.plugin.delegate import Getter, Provider
+from pootle.core.plugin.results import GatheredList
 
 
 config = Getter(providing_args=["instance"])
+
+# search_backend
 search_backend = Getter(providing_args=["instance"])
 lang_mapper = Getter(providing_args=["instance"])
 state = Getter()
@@ -22,3 +24,31 @@ subcommands = Provider()
 
 # view.context_data
 context_data = Provider(providing_args=["view", "context"])
+
+# custom path mangling
+# returns:
+#   pootle_path
+#   path_other: bit in between ctx_path and resource_path
+#   path_extra: dict containing any parsed vars/objects
+extracted_path = Getter(providing_args=["view", "instance"])
+
+# unit.priority
+unit_priority = Getter(providing_args=["instance", "stats", "user"])
+
+# object.stats
+object_stats = Provider(
+    providing_args=["instance", "stats", "user"])
+
+# object.parents
+object_parents = Provider(
+    providing_args=["instance", "parents"],
+    result_class=GatheredList)
+
+# child pootle_paths
+pootle_paths = Provider(
+    providing_args=["instance", "pootle_paths"],
+    result_class=GatheredList)
+
+# custom search filters
+search_filters = Provider(
+    providing_args=["search_backend", "filter_kwargs"])
