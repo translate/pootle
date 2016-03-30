04e1efe0ec35a7c2ba48f7ecdaade71b42ab8d1b
Taras Semenenko <taras.semenenko@gmail.com>
2016-03-30 13:46:11 +0300

    Make send_mail accept CC and BCC params

--- pootle/core/mail.py
+++ pootle/core/mail.py
@@ -11,7 +11,8 @@ from django.core.mail import EmailMultiAlternatives, get_connection
 
 def send_mail(subject, message, from_email, recipient_list,
               fail_silently=False, auth_user=None, auth_password=None,
-              connection=None, html_message=None, headers=None):
+              connection=None, html_message=None, headers=None,
+              cc=None, bcc=None):
     """Override django send_mail function to allow use of custom email headers.
     """
 
@@ -21,7 +22,8 @@ def send_mail(subject, message, from_email, recipient_list,
 
     mail = EmailMultiAlternatives(subject, message,
                                   from_email, recipient_list,
-                                  connection=connection, headers=headers)
+                                  connection=connection, headers=headers,
+                                  cc=cc, bcc=bcc)
 
     if html_message:
         mail.attach_alternative(html_message, 'text/html')
