4a2f9404b3226b4afcda045ecffd65c49f0e0bf4
Ryan Northey <ryan@space23.net>
2016-04-15 07:45:20 +0100

    Add pootle.core.plugin results

---
+++ pootle/core/plugin/results.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+from collections import OrderedDict
+
+
+class Gathered(object):
+
+    def __init__(self, provider):
+        self.provider = provider
+        self.__results__ = []
+
+    def add_result(self, func, gathered):
+        self.__results__.append((func, gathered))
+
+
+class GatheredDict(Gathered):
+
+    @property
+    def results(self):
+        gathered = OrderedDict()
+        for func, result in self.__results__:
+            if result:
+                try:
+                    gathered.update(result)
+                except TypeError:
+                    # Result is ignored if you cant update a dict with it
+                    pass
+        return gathered
+
+    def keys(self):
+        return self.results.keys()
+
+    def values(self):
+        return [self[k] for k in self.results]
+
+    def items(self):
+        return [(k, self[k]) for k in self.results]
+
+    def __contains__(self, k):
+        return k in self.results
+
+    def __getitem__(self, k):
+        return self.results[k]
+
+    def __iter__(self):
+        for k in self.results.keys():
+            yield k
+
+
+class GatheredList(Gathered):
+
+    @property
+    def results(self):
+        gathered = []
+        for func, result in self.__results__:
+            if isinstance(result, (list, tuple)):
+                gathered.extend(result)
+        return gathered
+
+    def __iter__(self):
+        for item in self.results:
+            yield item
