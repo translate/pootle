c1f6d6a4804c43e8e69c8248265ac2a02bdc2fa3
Ryan Northey <ryan@space23.net>
2016-04-25 21:28:33 +0100

    Add StoreDeserialization class

---
+++ pootle/apps/pootle_store/store/deserialize.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+import io
+
+from translate.storage.factory import getclass
+
+from django.utils.functional import cached_property
+
+from pootle.core.delegate import config, deserializers
+
+
+class StoreDeserialization(object):
+    """Calls configured deserializers for Store"""
+
+    def __init__(self, store):
+        self.store = store
+
+    @property
+    def project_deserializers(self):
+        project = self.store.translation_project.project
+        return (
+            config.get(
+                project.__class__,
+                instance=project,
+                key="pootle.core.deserializers")
+            or [])
+
+    @cached_property
+    def deserializers(self):
+        available_deserializers = deserializers.gather(
+            self.store.__class__, instance=self.store)
+        found_deserializers = []
+        for deserializer in self.project_deserializers:
+            found_deserializers.append(available_deserializers[deserializer])
+        return found_deserializers
+
+    def pipeline(self, data):
+        if not self.deserializers:
+            return data
+        for deserializer in self.deserializers:
+            data = deserializer(self, data).output
+        return data
+
+    def fromstring(self, data):
+        return getclass(io.BytesIO(data))(data)
+
+    def deserialize(self, data):
+        return self.fromstring(self.pipeline(data))
