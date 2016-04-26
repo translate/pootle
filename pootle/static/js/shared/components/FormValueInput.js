09775dfb602bcf41678d4175386771b2674a7cf7
Julen Ruiz Aizpuru <julenx@gmail.com>
2016-04-26 23:07:23 +0200

    FormValueInput: add support for autosize textareas

--- pootle/static/js/shared/components/FormValueInput.js
+++ pootle/static/js/shared/components/FormValueInput.js
@@ -6,6 +6,7 @@
  * AUTHORS file for copyright and authorship information.
  */
 
+import autosize from 'autosize';
 import React from 'react';
 
 
@@ -13,8 +14,27 @@ const FormValueInput = React.createClass({
 
   propTypes: {
     handleChange: React.PropTypes.func.isRequired,
+    autosize: React.PropTypes.bool,
     type: React.PropTypes.string,
     value: React.PropTypes.string,
+  },
+
+  getDefaultProps() {
+    return {
+      autosize: true,
+    };
+  },
+
+  componentDidMount() {
+    if (this.props.type === 'textarea' && this.props.autosize) {
+      autosize(this.refs.textarea);
+    }
+  },
+
+  componentWillUnmount() {
+    if (this.props.type === 'textarea' && this.props.autosize) {
+      autosize.destroy(this.refs.textarea);
+    }
   },
 
   handleChange(e) {
@@ -24,7 +44,13 @@ const FormValueInput = React.createClass({
 
   render() {
     if (this.props.type === 'textarea') {
-      return <textarea onChange={this.handleChange} {...this.props} />;
+      return (
+        <textarea
+          onChange={this.handleChange}
+          ref="textarea"
+          {...this.props}
+        />
+      );
     }
 
     return <input onChange={this.handleChange} {...this.props} />;
