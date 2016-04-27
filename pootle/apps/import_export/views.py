16c9ca9346b13fe22e17dbc7d8dcc6dd9e39b292
Ryan Northey <ryan@space23.net>
2016-04-27 07:09:05 +0100

    Remove store.is_terminology conditions on im/export

--- pootle/apps/import_export/views.py
+++ pootle/apps/import_export/views.py
@@ -52,8 +52,6 @@ def export(request):
     with BytesIO() as f:
         with ZipFile(f, "w") as zf:
             for store in stores:
-                if store.is_terminology:
-                    continue
                 try:
                     data = store.serialize()
                 except Exception as e:
