36cbc21ea6dfdeda055c37bf2b01bf936bdae6d0
Ryan Northey <ryan@space23.net>
2016-05-04 10:20:23 +0100

    Add pootle_config delegation/getter

---
+++ pootle/apps/pootle_config/getters.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+from django.utils.translation import ugettext_lazy as _
+
+from pootle.core.delegate import config
+from pootle.core.plugin import getter
+
+from .exceptions import ConfigurationError
+from .models import Config
+
+
+@getter(config)
+def config_getter(sender=None, instance=None, key=None, **kwargs):
+
+    if sender:
+        if instance is not None and not isinstance(instance, sender):
+            raise ConfigurationError(
+                _("'instance' must be an instance of sender, when specified"))
+        conf = Config.objects.get_config_queryset(instance or sender)
+    elif instance:
+        raise ConfigurationError(
+            _("'sender' must be defined when 'instance' is specified"))
+    else:
+        conf = Config.objects.site_config()
+
+    if key is not None:
+        if isinstance(key, (list, tuple)):
+            return conf.list_config(key)
+        try:
+            return conf.get_config(key)
+        except Config.DoesNotExist:
+            return None
+        except Config.MultipleObjectsReturned as e:
+            raise ConfigurationError(e)
+    return conf
