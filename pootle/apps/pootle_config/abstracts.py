7c9d5e3a313d678671f6825018c602ace4366bf2
Ryan Northey <ryan@space23.net>
2016-05-04 10:19:00 +0100

    Add pootle_config models

---
+++ pootle/apps/pootle_config/abstracts.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+import collections
+
+from jsonfield.fields import JSONField
+
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ValidationError
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from .managers import ConfigManager, ConfigQuerySet
+
+
+class AbstractConfig(models.Model):
+    content_type = models.ForeignKey(
+        ContentType,
+        blank=True,
+        null=True,
+        db_index=True,
+        verbose_name=_('content type'),
+        related_name="content_type_set_for_%(class)s",
+        on_delete=models.CASCADE)
+    object_pk = models.CharField(
+        _('object ID'),
+        max_length=255,
+        db_index=True,
+        blank=True,
+        null=True)
+    content_object = GenericForeignKey(
+        ct_field="content_type",
+        fk_field="object_pk")
+    key = models.CharField(
+        _("Configuration key"),
+        max_length=255,
+        blank=False,
+        null=False,
+        db_index=True)
+    value = JSONField(
+        _("Configuration value"),
+        default="",
+        blank=True,
+        null=False,
+        load_kwargs={'object_pairs_hook': collections.OrderedDict})
+
+    objects = ConfigManager.from_queryset(ConfigQuerySet)()
+
+    class Meta(object):
+        abstract = True
+        ordering = ['pk']
+        index_together = ["content_type", "object_pk"]
+
+    def save(self, **kwargs):
+        if not self.key:
+            raise ValidationError("Config object must have a key")
+        if self.object_pk and not self.content_type:
+            raise ValidationError(
+                "Config object must have content_type when object_pk is set")
+        super(AbstractConfig, self).save(**kwargs)
---
+++ pootle/apps/pootle_config/migrations/0001_initial.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import jsonfield.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('contenttypes', '0002_remove_content_type_name'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Config',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('object_pk', models.CharField(db_index=True, max_length=255, null=True, verbose_name='object ID', blank=True)),
+                ('key', models.CharField(max_length=255, verbose_name='Configuration key', db_index=True)),
+                ('value', jsonfield.fields.JSONField(default=b'', verbose_name='Configuration value', blank=True)),
+                ('content_type', models.ForeignKey(related_name='content_type_set_for_config', verbose_name='content type', blank=True, to='contenttypes.ContentType', null=True)),
+            ],
+            options={
+                'ordering': ['pk'],
+                'abstract': False,
+                'db_table': 'pootle_config',
+            },
+        ),
+        migrations.AlterIndexTogether(
+            name='config',
+            index_together=set([('content_type', 'object_pk')]),
+        ),
+    ]
