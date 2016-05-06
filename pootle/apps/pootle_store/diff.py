translate/pootle

commit 80d35df2b2c3987470873c541c288dc95c0ec79e

Only update units that have changed

pootle/apps/pootle_store/diff.py
@@ -15,6 +15,55 @@ from .util import OBSOLETE
 from .fields import to_python as multistring_to_python
 
 
+class UnitProxy(object):
+    """Wraps File/DB Unit dicts used by StoreDiff for equality comparison"""
+
+    match_attrs = ["context", "developer_comment", "locations",
+                   "source", "target", "translator_comment"]
+
+    def __init__(self, unit):
+        self.unit = unit
+
+    def __eq__(self, other):
+        return all(getattr(self, k) == getattr(other, k)
+                   for k in self.match_attrs)
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __getattr__(self, k):
+        try:
+            return self.__dict__["unit"][k] or ""
+        except KeyError:
+            return self.__getattribute__(k)
+
+
+class DBUnit(UnitProxy):
+
+    @property
+    def source(self):
+        return multistring_to_python(self.unit["source_f"])
+
+    @property
+    def target(self):
+        return multistring_to_python(self.unit["target_f"])
+
+
+class FileUnit(UnitProxy):
+
+    @property
+    def locations(self):
+        return "\n".join(self.unit["locations"])
+
+    @property
+    def source(self):
+        return multistring_to_python(self.unit["source"])
+
+    @property
+    def target(self):
+        return multistring_to_python(self.unit["target"])
+
+
 class StoreDiff(object):
 
     def __init__(self, db_store, file_store, file_revision):
@@ -201,7 +250,9 @@ class StoreDiff(object):
                 update_dbids.update(
                     set(self.db_units[uid]['id']
                         for uid in self.active_units[i1:i2]
-                        if uid in self.file_units))
+                        if (uid in self.file_units
+                            and (DBUnit(self.db_units[uid])
+                                 != FileUnit(self.file_units[uid])))))
         return update_dbids
 
     def has_changes(self, diff):

commit bb6a5c3a61e1c85fc9b952b2a8831dff16d48268

fix update_stores not changing fuzzy state
regression from 80d35df2b2c3987470873c541c288dc95c0ec79e
ignoring change in state makes it impossible to update the fuzzy state
of a translatin unit by modifying the files on disk, and running
update_stores. With state ignored in the comparison, those changes are
silently ignored (and thenget overridden again on next sync_stores)

pootle/apps/pootle_store/diff.py
@@ -20,7 +20,7 @@ class UnitDiffProxy(UnitProxy):
     """Wraps File/DB Unit dicts used by StoreDiff for equality comparison"""
 
     match_attrs = ["context", "developer_comment", "locations",
-                   "source", "target", "translator_comment"]
+                   "source", "state", "target", "translator_comment"]
 
     def __eq__(self, other):
         return all(getattr(self, k) == getattr(other, k)