4e644f89d1f6d6e49f020f61da4cc3173a87123c
Ryan Northey <ryan@space23.net>
2016-05-06 17:16:02 +0100

    Add app_label for pootle_project

--- pootle/apps/pootle_project/apps.py
+++ pootle/apps/pootle_project/apps.py
@@ -6,4 +6,10 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
-default_app_config = 'pootle_project.apps.PootleProjectConfig'
+from django.apps import AppConfig
+
+
+class PootleProjectConfig(AppConfig):
+
+    name = "pootle_project"
+    verbose_name = "Pootle Project"
