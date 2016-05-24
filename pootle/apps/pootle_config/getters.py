5556b70b7f6164e4d588535af5f0beb9f2466bb6
Ryan Northey <ryan@synca.io>
2016-05-24 12:45:05 +0100

    flake8: remove ununsed import

--- pootle/apps/pootle_config/getters.py
+++ pootle/apps/pootle_config/getters.py
@@ -6,8 +6,6 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
-from django.utils.translation import ugettext_lazy as _
-
 from pootle.core.delegate import config
 from pootle.core.plugin import getter
 
