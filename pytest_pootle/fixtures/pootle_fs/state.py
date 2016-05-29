2ead6b607812e0e3c471b5188d60b79d7ed99cf8
Ryan Northey <ryan@synca.io>
2016-05-29 11:55:04 +0100

    Add fixtures for pootle_fs.StateResources tests

---
+++ pytest_pootle/fixtures/pootle_fs/state.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+from collections import OrderedDict
+
+import pytest
+
+from django.db.models import Q
+from django.utils.functional import cached_property
+
+
+FS_PATH_QS = OrderedDict((
+    ("all", (
+        (None, None, None))),
+    ("language0", (
+        (Q(pootle_path__startswith="/language0"),
+         "/language0/*", None))),
+    ("fs/language1", (
+        (Q(pootle_path__startswith="/language1"),
+         None, "/fs/language1/*"))),
+    ("store0.po", (
+        (Q(pootle_path__endswith="store0.po"),
+         "*/store0.po", "*/store0.po"))),
+    ("none", (
+        (False, "/language0/*", "/fs/language1/*")))))
+
+
+class DummyPlugin(object):
+
+    def __init__(self, project):
+        self.project = project
+
+    def find_translations(self, fs_path=None, pootle_path=None):
+        return [
+            (pp, hash("%s::%s::/fs%s" % (fs_path, pootle_path, pp)))
+            for pp
+            in self.resources.stores.values_list(
+                "pootle_path", flat=True)]
+
+    @cached_property
+    def resources(self):
+        from pootle_fs.resources import FSProjectResources
+
+        return FSProjectResources(self.project)
+
+    def get_fs_path(self, pootle_path):
+        return "/fs%s" % pootle_path
+
+
+@pytest.fixture
+def project0_dummy_plugin(settings, request):
+    from pootle.core.plugin import provider
+    from pootle_fs.delegate import fs_plugins
+    from pootle_fs.utils import FSPlugin
+    from pootle_project.models import Project
+
+    @provider(fs_plugins, weak=False)
+    def plugin_provider(**kwargs):
+        return dict(dummyfs=DummyPlugin)
+
+    project = Project.objects.get(code="project0")
+    settings.POOTLE_FS_PATH = "/tmp/foo/"
+    project.config["pootle_fs.fs_type"] = "dummyfs"
+    project.config["pootle_fs.fs_url"] = "/foo/bar"
+
+    request.addfinalizer(
+        lambda: fs_plugins.disconnect(plugin_provider))
+    return FSPlugin(project)
+
+
+@pytest.fixture(params=FS_PATH_QS.keys())
+def fs_path_queries(project0_dummy_plugin, request):
+    return project0_dummy_plugin, FS_PATH_QS[request.param]
