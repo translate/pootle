ae35edc08a574ca2706ded3009e8dadd1c8b631b
Ryan Northey <ryan@synca.io>
2016-05-30 16:39:44 +0100

    Add tests for PathFilter utils

--- tests/pootle_fs/utils.py
+++ tests/pootle_fs/utils.py
@@ -7,13 +7,18 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
+from fnmatch import translate
+
 import pytest
 
 from pootle.core.exceptions import MissingPluginError, NotConfiguredError
 from pootle.core.plugin import provider
 from pootle_fs.delegate import fs_plugins
-from pootle_fs.utils import FSPlugin
+from pootle_fs.models import StoreFS
+from pootle_fs.utils import (
+    PathFilter, StoreFSPathFilter, StorePathFilter, FSPlugin)
 from pootle_project.models import Project
+from pootle_store.models import Store
 
 
 class DummyFSPlugin(object):
@@ -32,6 +37,96 @@ class DummyFSPlugin(object):
 
     def plugin_method(self, foo):
         return "Plugin method called with: %s" % foo
+
+
+@pytest.mark.django_db
+def test_store_fs_path_filter_instance():
+    path_filter = StoreFSPathFilter()
+    assert path_filter.pootle_path is None
+    assert path_filter.pootle_regex is None
+    assert path_filter.fs_path is None
+    assert path_filter.fs_regex is None
+    # regexes are cached
+    path_filter.pootle_path = "/foo"
+    path_filter.fs_path = "/bar"
+    assert path_filter.pootle_regex is None
+    assert path_filter.fs_regex is None
+    del path_filter.__dict__["pootle_regex"]
+    del path_filter.__dict__["fs_regex"]
+    assert (
+        path_filter.pootle_regex
+        == path_filter.path_regex(path_filter.pootle_path))
+    assert (
+        path_filter.fs_regex
+        == path_filter.path_regex(path_filter.fs_path))
+
+
+@pytest.mark.django_db
+def test_store_path_filter_instance():
+    path_filter = StorePathFilter()
+    assert path_filter.pootle_path is None
+    assert path_filter.pootle_regex is None
+    assert not hasattr(path_filter, "fs_path")
+    assert not hasattr(path_filter, "fs_regex")
+    # regexes are cached
+    path_filter.pootle_path = "/foo"
+    path_filter.fs_path = "/bar"
+    assert path_filter.pootle_regex is None
+    del path_filter.__dict__["pootle_regex"]
+    assert (
+        path_filter.pootle_regex
+        == path_filter.path_regex(path_filter.pootle_path))
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "glob", [None, "/language0", "/language0/*", "*.po",
+             "/language[01]*", "/language[!1]/*", "/language?/*"])
+def test_store_path_filtered(glob):
+    project = Project.objects.get(code="project0")
+    stores = Store.objects.filter(
+        translation_project__project=project)
+    path_filter = StorePathFilter(glob)
+    if not glob:
+        assert path_filter.filtered(stores) is stores
+    else:
+        assert (
+            list(path_filter.filtered(stores))
+            == list(stores.filter(pootle_path__regex=path_filter.pootle_regex)))
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "glob", [None, "/language0", "/language0/*", "*.po",
+             "/language[01]*", "/language[!1]/*", "/language?/*"])
+def test_store_fs_path_filtered(glob):
+    project = Project.objects.get(code="project0")
+    stores = Store.objects.filter(
+        translation_project__project=project)
+    for store in stores:
+        StoreFS.objects.create(
+            store=store,
+            path="/fs%s" % store.pootle_path)
+    stores_fs = StoreFS.objects.filter(project=project)
+    if not glob:
+        assert StoreFSPathFilter().filtered(stores_fs) is stores_fs
+    else:
+        path_filter = StoreFSPathFilter(pootle_path=glob)
+        assert (
+            list(path_filter.filtered(stores_fs))
+            == list(stores_fs.filter(pootle_path__regex=path_filter.pootle_regex)))
+        path_filter = StoreFSPathFilter(fs_path="/fs%s" % glob)
+        assert (
+            list(path_filter.filtered(stores_fs))
+            == list(stores_fs.filter(path__regex=path_filter.fs_regex)))
+
+
+@pytest.mark.parametrize(
+    "glob", ["/foo", "/bar/*", "*.baz", "[abc]", "[!xyz]", "language?"])
+def test_fs_path_filter_path_regex(glob):
+    assert (
+        PathFilter().path_regex(glob)
+        == translate(glob).replace("\Z(?ms)", "$"))
 
 
 @pytest.mark.django_db
