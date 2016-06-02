cb0497c621a869074458cf430c1fb9899fab1912
Ryan Northey <ryan@space23.net>
2016-06-02 19:51:23 +0100

    Add pootle_fs delegates

--- pootle/apps/pootle_fs/delegate.py
+++ pootle/apps/pootle_fs/delegate.py
@@ -6,8 +6,12 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
-from pootle.core.plugin.delegate import Provider
+from pootle.core.plugin.delegate import Getter, Provider
 
+
+fs_file = Getter()
+fs_finder = Getter()
+fs_resources = Getter()
 
 # File system plugins such as git/mercurial/localfs
 fs_plugins = Provider()
