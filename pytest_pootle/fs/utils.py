0da8e951f66920653dbe9fa4898f98856a0fa935
Ryan Northey <ryan@space23.net>
2016-06-14 19:53:10 +0100

    Cleaup: fix pyflakes fail

--- pytest_pootle/fs/utils.py
+++ pytest_pootle/fs/utils.py
@@ -6,8 +6,6 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
-from fnmatch import fnmatch
-
 
 def parse_fs_action_args(action):
     command_args = []
