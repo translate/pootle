868fb547c1c23866d591106411f4b3451ff5c72c
Dwayne Bailey <dwayne@translate.org.za>
2016-06-16 21:31:17 +0100

    Docs: remove tastypie interwiki
    We don't use tastypie

--- docs/conf.py
+++ docs/conf.py
@@ -413,7 +413,6 @@ intersphinx_mapping = {
     'django': ('https://docs.djangoproject.com/en/%s' % django_ver,
                'https://docs.djangoproject.com/en/%s/_objects' % django_ver),
     'toolkit': ('http://docs.translatehouse.org/projects/translate-toolkit/en/latest/', None),
-    'tastypie': ('https://django-tastypie.readthedocs.io/en/latest/', None),
 }
 
 
