e8cd1d9850a3f3b3199b686c1316fe36c899cbbd
Leandro Regueiro <leandro.regueiro@gmail.com>
2016-07-04 21:39:32 +0200

    Drop previous contributors command tests

--- tests/commands/contributors.py
+++ tests/commands/contributors.py
@@ -9,13 +9,8 @@
 from argparse import ArgumentTypeError
 from dateutil.parser import parse as parse_datetime
 
-import pytest
-
-from django.core.management import call_command
-
 from pootle.core.utils.timezone import make_aware
 from pootle_app.management.commands.contributors import get_aware_datetime
-from pootle_store.models import Unit
 
 
 def test_contributors_get_aware_datetime():
@@ -45,51 +40,3 @@ def test_contributors_get_aware_datetime():
     # Test blank string.
     with pytest.raises(ArgumentTypeError):
         get_aware_datetime(" ")
-
-
-@pytest.mark.cmd
-@pytest.mark.django_db
-def test_contributors_noargs(capfd, en_tutorial_po_member_updated):
-    """Contributors across the site."""
-    call_command('contributors')
-    out, err = capfd.readouterr()
-    contribs = Unit.objects.filter(submitted_by__username="member")
-    assert ("Member (%d contributions)" % contribs.count()) in out
-
-
-@pytest.mark.cmd
-@pytest.mark.django_db
-def test_contributors_project(capfd, en_tutorial_po_member_updated):
-    """Contributors in a given project."""
-    call_command('contributors', '--project=tutorial')
-    out, err = capfd.readouterr()
-    contribs = Unit.objects.filter(
-        submitted_by__username="member",
-        store__translation_project__project__code="tutorial")
-    assert ("Member (%d contributions)" % contribs.count()) in out
-
-
-@pytest.mark.cmd
-@pytest.mark.django_db
-def test_contributors_language(capfd, en_tutorial_po_member_updated):
-    """Contributors in a given language."""
-    call_command('contributors', '--language=en')
-    out, err = capfd.readouterr()
-    contribs = Unit.objects.filter(
-        submitted_by__username="member",
-        store__translation_project__language__code="en")
-    assert ("Member (%d contributions)" % contribs.count()) in out
-
-
-@pytest.mark.cmd
-@pytest.mark.django_db
-def test_contributors_sort(capfd, en_tutorial_po_member_updated, member):
-    """Contributors in contribution order."""
-    member.email = "member@test.email"
-    member.save()
-    contribs = Unit.objects.filter(submitted_by__username="member")
-    call_command('contributors', '--sort-by=contributions')
-    out, err = capfd.readouterr()
-    assert (
-        ("Member <member@test.email> (%d contributions)" % contribs.count())
-        in out)
