e1ca0957eef99c0bda87fae85841f80678bbe7f7
Ryan Northey <ryan@space23.net>
2016-07-24 15:32:32 +0100

    Add pootle_format.providers

---
+++ pootle/apps/pootle_format/providers.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+from collections import OrderedDict
+
+from pootle.core.delegate import format_registration
+from pootle.core.plugin import provider
+
+from .default import POOTLE_FORMATS
+
+
+@provider(format_registration)
+def register_formats(**kwargs):
+    return OrderedDict(POOTLE_FORMATS)
