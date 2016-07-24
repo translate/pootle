3d23798801805fa72de231856456f2f00bf9a313
Ryan Northey <ryan@space23.net>
2016-07-24 16:44:23 +0100

    Switch Project admin form/view to filetypes

--- pootle/apps/pootle_app/forms.py
+++ pootle/apps/pootle_app/forms.py
@@ -15,7 +15,6 @@ from django.utils.translation import ugettext_lazy as _
 
 from pootle_language.models import Language
 from pootle_project.models import RESERVED_PROJECT_CODES, Project
-from pootle_store.filetypes import filetype_choices
 from pootle_store.models import Store
 
 
@@ -47,8 +46,8 @@ class ProjectForm(forms.ModelForm):
 
     class Meta(object):
         model = Project
-        fields = ('id', 'code', 'fullname', 'checkstyle', 'localfiletype',
-                  'treestyle', 'source_language', 'ignoredfiles',
+        fields = ('id', 'code', 'fullname', 'checkstyle',
+                  'filetypes', 'treestyle', 'source_language', 'ignoredfiles',
                   'report_email', 'screenshot_search_prefix', 'disabled',)
 
     def __init__(self, *args, **kwargs):
@@ -57,28 +56,28 @@ class ProjectForm(forms.ModelForm):
         queryset = Language.objects.exclude(code='templates')
         self.fields['source_language'].queryset = queryset
 
+        self.fields["filetypes"].initial = [
+            self.fields["filetypes"].queryset.get(name="po")]
+
         if self.instance.id:
-            has_stores = Store.objects.filter(
-                translation_project__project=self.instance
-            ).count
-
-            if has_stores:
-                self.fields['localfiletype'].required = False
-
             if (self.instance.treestyle != 'auto' and
                 self.instance.translationproject_set.count() and
                 self.instance.treestyle == self.instance._detect_treestyle()):
                 self.fields['treestyle'].required = False
 
-    def clean_localfiletype(self):
-        value = self.cleaned_data.get('localfiletype', None)
-        if not value:
-            value = self.instance.localfiletype
-        filetypes = [x[0] for x in filetype_choices]
-        if value not in filetypes:
-            raise forms.ValidationError(
-                "Unrecognised project filetype: %s" % value
-            )
+    def clean_filetypes(self):
+        value = self.cleaned_data.get('filetypes', [])
+        if not self.instance.pk:
+            return value
+        for filetype in self.instance.filetypes.all():
+            if filetype not in value:
+                has_stores = Store.objects.filter(
+                    translation_project__project=self.instance, filetype=filetype)
+                if has_stores.exists():
+                    raise forms.ValidationError(
+                        _("You cannot remove a file type from a Project, "
+                          "if there are Stores of that file type ('%s')"
+                          % filetype))
         return value
 
     def clean_treestyle(self):
--- pootle/apps/pootle_app/views/admin/projects.py
+++ pootle/apps/pootle_app/views/admin/projects.py
@@ -8,11 +8,11 @@
 
 from django.views.generic import TemplateView
 
+from pootle.core.delegate import formats
 from pootle.core.views import APIView, SuperuserRequiredMixin
 from pootle_app.forms import ProjectForm
 from pootle_language.models import Language
 from pootle_project.models import Project
-from pootle_store.filetypes import filetype_choices
 
 
 __all__ = ('ProjectAdminView', 'ProjectAPIView')
@@ -30,11 +30,16 @@ class ProjectAdminView(SuperuserRequiredMixin, TemplateView):
         except Language.DoesNotExist:
             default_language = languages[0].id
 
+        filetypes = []
+        for info in formats.get().values():
+            filetypes.append(
+                [info["pk"], info["display_title"]])
+
         return {
             'page': 'admin-projects',
             'form_choices': {
                 'checkstyle': Project.checker_choices,
-                'localfiletype': filetype_choices,
+                'filetypes': filetypes,
                 'source_language': language_choices,
                 'treestyle': Project.treestyle_choices,
                 'defaults': {
@@ -51,3 +56,4 @@ class ProjectAPIView(SuperuserRequiredMixin, APIView):
     edit_form_class = ProjectForm
     page_size = 10
     search_fields = ('code', 'fullname', 'disabled')
+    m2m = ("filetypes", )
