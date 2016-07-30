3f377046ee5d6d3ece93595271b1e66d90194e46
Ryan Northey <ryan@space23.net>
2016-07-30 14:40:45 +0100

    Add migration to correctly set Store.filetypes

---
+++ pootle/apps/pootle_store/migrations/0013_set_store_filetype_again.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import logging
+
+from django.db import migrations, models
+
+from pootle.core.delegate import formats
+from pootle_format.exceptions import UnrecognizedFiletype
+from pootle_format.utils import ProjectFiletypes
+
+
+logger = logging.getLogger(__name__)
+
+
+def migrate_store_filetypes(apps, schema_editor):
+    format_registry = formats.get()
+
+    projects = apps.get_model("pootle_project.Project").objects.all()
+
+    for project in projects:
+        for filetype in project.filetypes.all():
+            (apps.get_model("pootle_store.Store").objects.all()
+                 .filter(translation_project__project=project)
+                 .filter(name__endswith=".%s" % filetype.extension.name)
+                 .update(filetype=filetype))
+            if filetype.extension != filetype.template_extension:
+                (apps.get_model("pootle_store.Store").objects.all()
+                     .filter(translation_project__project=project)
+                     .filter(name__endswith=".%s" % filetype.template_extension.name)
+                     .update(filetype=filetype))
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('pootle_store', '0012_set_is_template'),
+    ]
+
+    operations = [
+        migrations.RunPython(migrate_store_filetypes),
+    ]
