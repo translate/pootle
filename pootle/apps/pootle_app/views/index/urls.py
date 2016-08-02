8160574edc6cf7e69c08d8349f8f6d9e52d1a857
Dwayne Bailey <dwayne@translate.org.za>
2016-08-02 15:01:48 +0100

    Use templates for robots.txt
    This drops the autogenerated robots.txt and replaces it with a default
    and customisable templated file.
    
    The rules ensure:
    * Allow the whole site by default
    * Disallow any of the editor and export views
    * Allows access to the browse views
    * Allows access to static pages
    * Allows access to user pages but not to user stats

--- pootle/apps/pootle_app/views/index/urls.py
+++ pootle/apps/pootle_app/views/index/urls.py
@@ -7,15 +7,17 @@
 # AUTHORS file for copyright and authorship information.
 
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
-from . import index, robots
+from . import index
 from .index import AboutView
 
 
 urlpatterns = [
-    url(r'^robots.txt$',
-        robots.view,
-        name='pootle-robots'),
+    url(r'^robots\.txt$',
+        TemplateView.as_view(template_name='robots.txt',
+                             content_type='text/plain'),
+        name="pootle-robots"),
 
     url(r'^/?$',
         index.view,
