c5533de352e4086aba45fef6873aadff99721f44
Ryan Northey <ryan@space23.net>
2016-08-11 21:58:32 +0100

    Add pootle_format.model fixtures

---
+++ pytest_pootle/fixtures/models/formats.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+import pytest
+
+
+@pytest.fixture
+def po():
+    from pootle_format.models import Format
+
+    return Format.objects.get(name="po")
+
+
+@pytest.fixture
+def po2():
+    from pootle.core.delegate import formats
+
+    registry = formats.get()
+
+    # register po2
+    return registry.register(
+        "special_po_2", "po2", template_extension="pot2")
