88df1645e5fb8329c160804e25fc9fd7209e6be5
Ryan Northey <ryan@space23.net>
2016-08-11 16:10:37 +0100

    Add pootle.core.delegate.filetype_tool

--- pootle/core/delegate.py
+++ pootle/core/delegate.py
@@ -19,6 +19,7 @@ contributors = Getter()
 formats = Getter()
 format_registration = Provider()
 format_classes = Provider()
+filetype_tool = Getter()
 
 serializers = Provider(providing_args=["instance"])
 deserializers = Provider(providing_args=["instance"])
