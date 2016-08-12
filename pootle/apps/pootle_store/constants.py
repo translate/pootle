9b3fad3924d3d528f3fd97ff9da7d103e1eaed1f
Ryan Northey <ryan@space23.net>
2016-08-12 08:47:56 +0100

    Add pootle_store.constants module

---
+++ pootle/apps/pootle_store/constants.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+#: Mapping of allowed sorting criteria.
+#: Keys are supported query strings, values are the field + order that
+#: will be used against the DB.
+ALLOWED_SORTS = {
+    'units': {
+        'priority': '-priority',
+        'oldest': 'submitted_on',
+        'newest': '-submitted_on',
+    },
+    'suggestions': {
+        'oldest': 'suggestion__creation_time',
+        'newest': '-suggestion__creation_time',
+    },
+    'submissions': {
+        'oldest': 'submission__creation_time',
+        'newest': '-submission__creation_time',
+    },
+}
+
+#: List of fields from `ALLOWED_SORTS` that can be sorted by simply using
+#: `order_by(field)`
+SIMPLY_SORTED = ['units']
