c44256add2824107d2dd6ba868a7014204f9f7ed
Ryan Northey <ryan@space23.net>
2016-08-18 00:10:02 +0100

    Add pootle_format.formats.providers

--- pootle/apps/pootle_format/apps.py
+++ pootle/apps/pootle_format/apps.py
@@ -20,3 +20,4 @@ class PootleFormatConfig(AppConfig):
         importlib.import_module("pootle_format.models")
         importlib.import_module("pootle_format.getters")
         importlib.import_module("pootle_format.providers")
+        importlib.import_module("pootle_format.formats.providers")
