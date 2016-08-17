04fd347a75755e00d059906de6c0bced26db3906
Ryan Northey <ryan@space23.net>
2016-08-17 14:23:31 +0100

    Add default pootle.core.delegate.format_syncers provider

--- pootle/apps/pootle_store/providers.py
+++ pootle/apps/pootle_store/providers.py
@@ -6,16 +6,23 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
-from pootle.core.delegate import format_diffs, format_updaters
+from pootle.core.delegate import format_diffs, format_syncers, format_updaters
 from pootle.core.plugin import provider
 
 from .diff import DiffableStore
+from .syncer import StoreSyncer
 from .updater import StoreUpdater
 
 
 @provider(format_diffs)
 def register_format_diffs(**kwargs_):
     return dict(default=DiffableStore)
+
+
+@provider(format_syncers)
+def register_format_syncers(**kwargs_):
+    return dict(
+        default=StoreSyncer)
 
 
 @provider(format_updaters)
