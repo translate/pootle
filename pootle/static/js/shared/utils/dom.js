5990356913086dee6e27f6aef0b34f5245a76b12
Julen Ruiz Aizpuru <julenx@gmail.com>
2016-08-18 15:30:34 +0200

    DOM: add utility wrapper around `querySelectorAll`
    `document.querySelectorAll` will always return a `NodeList`, which albeit it is
    array-like, it doesn't implement all of `Array.prototype`'s interface. This
    effectively avoids working with such `NodeList`s using regular array methods
    (`forEach` and friends). This wrapper makes it easy to work with it.

---
+++ pootle/static/js/shared/utils/dom.js
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) Pootle contributors.
+ *
+ * This file is a part of the Pootle project. It is distributed under the GPL3
+ * or later license. See the LICENSE file for a copy of the license and the
+ * AUTHORS file for copyright and authorship information.
+ */
+
+
+/**
+ * Queries the DOM for a collection matching `selector`.
+ *
+ * @param {String} selector - The CSS selector
+ * @return {Array} - Array of matched `Node`s
+ */
+export function qAll(selector) {
+  const nodes = document.querySelectorAll(selector);
+  // XXX: not using array spread because this requires an extra polyfill to
+  // support IE 11
+  return Array.prototype.slice.call(nodes);
+}
