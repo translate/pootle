8dfd95971ce4b765492f53943dc2633d7f627806
Ryan Northey <ryan@space23.net>
2016-08-21 16:41:23 +0100

    Cleanup: remove old fixtures from test

--- tests/models/directory.py
+++ tests/models/directory.py
@@ -7,34 +7,35 @@
 # AUTHORS file for copyright and authorship information.
 
 import os
+import shutil
 
 import pytest
 
+from pootle_app.models.directory import Directory
+from pootle_store.models import Store, Unit
+
 
 @pytest.mark.django_db
-def test_delete_mark_obsolete_resurrect_sync(fr_tutorial_subdir_to_remove_po):
+def test_delete_mark_obsolete_resurrect_sync(subdir0):
     """Tests that the in-DB Directory are marked as obsolete
     after the on-disk file ceased to exist and that the on-disk file and
     directory are recovered after syncing.
 
     Refs. #269.
     """
-    from pootle_app.models.directory import Directory
-    from pootle_store.models import Store, Unit
+    store = subdir0.child_stores.first()
+    store.sync()
+    tp = store.translation_project
+    dir_pootle_path = store.parent.pootle_path
+    store_pootle_path = store.pootle_path
 
-    tp = fr_tutorial_subdir_to_remove_po.translation_project
-    dir_pootle_path = fr_tutorial_subdir_to_remove_po.parent.pootle_path
-    store_pootle_path = fr_tutorial_subdir_to_remove_po.pootle_path
-
-    # Parse stores
-    for store in tp.stores.all():
-        store.update(store.file.store)
+    # Sync stores
+    for _store in tp.stores.all():
+        _store.sync()
 
     # Remove on-disk directory
-    os.remove(fr_tutorial_subdir_to_remove_po.file.path)
-    af_tutorial_subdir_to_remove = \
-        os.path.dirname(fr_tutorial_subdir_to_remove_po.file.path)
-    os.rmdir(af_tutorial_subdir_to_remove)
+    os.remove(store.file.path)
+    shutil.rmtree(os.path.dirname(store.file.path))
 
     # Update stores by rescanning TP
     tp.scan_files()
@@ -70,15 +71,15 @@ def test_delete_mark_obsolete_resurrect_sync(fr_tutorial_subdir_to_remove_po):
 
 
 @pytest.mark.django_db
-def test_scan_empty_project_obsolete_dirs(es_tutorial_subdir_remove_po):
+def test_scan_empty_project_obsolete_dirs(store0):
     """Tests that the in-DB Directories are marked as obsolete
     if the on-disk directories are empty.
     """
-    spanish_tutorial = es_tutorial_subdir_remove_po.translation_project
-    os.remove(es_tutorial_subdir_remove_po.file.path)
+    tp = store0.translation_project
+    os.remove(store0.file.path)
 
-    spanish_tutorial.scan_files()
-    for item in spanish_tutorial.directory.child_dirs.all():
+    tp.scan_files()
+    for item in tp.directory.child_dirs.all():
         assert item.obsolete
 
-    assert spanish_tutorial.directory.obsolete
+    assert tp.directory.obsolete
