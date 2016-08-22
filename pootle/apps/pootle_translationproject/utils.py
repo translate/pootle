8955458a068eb12d0152743857dc70f56e1be975
Ryan Northey <ryan@space23.net>
2016-08-22 19:56:09 +0100

    Cleanup: reorder tp_tool methods

--- pootle/apps/pootle_translationproject/utils.py
+++ pootle/apps/pootle_translationproject/utils.py
@@ -26,6 +26,54 @@ class TPTool(object):
     def tp_qs(self):
         return self.project.translationproject_set
 
+    def check_no_tp(self, language):
+        if self.get_tp(language):
+            raise ValueError(
+                "TranslationProject '%s' already exists"
+                % self.get_path(language.code))
+
+    def check_tp(self, tp):
+        if tp.project != self.project:
+            raise ValueError(
+                "TP '%s' is not part of project '%s'"
+                % (tp, self.project.code))
+
+    def clone(self, tp, language):
+        self.check_tp(tp)
+        self.check_no_tp(language)
+        new_tp = self.create_tp(language)
+        self.clone_children(
+            tp.directory,
+            new_tp.directory)
+        return new_tp
+
+    def clone_children(self, source_dir, target_parent):
+        for store in source_dir.child_stores.live():
+            self.clone_store(store, target_parent)
+        for subdir in source_dir.child_dirs.live():
+            self.clone_directory(subdir, target_parent)
+
+    def clone_directory(self, source_dir, target_parent):
+        target_dir = target_parent.child_dirs.create(
+            name=source_dir.name)
+        self.clone_children(
+            source_dir,
+            target_dir)
+        return target_dir
+
+    def clone_store(self, store, target_dir):
+        cloned = target_dir.child_stores.create(
+            name=store.name,
+            translation_project=target_dir.translation_project)
+        cloned.mark_all_dirty()
+        cloned.update(cloned.deserialize(store.serialize()))
+        cloned.state = store.state
+        cloned.save()
+        return cloned
+
+    def create_tp(self, language):
+        return self.tp_qs.create(language=language)
+
     def get_path(self, language_code):
         return "/%s/%s/" % (language_code, self.project.code)
 
@@ -35,37 +83,34 @@ class TPTool(object):
         except self.tp_qs.model.DoesNotExist:
             pass
 
-    def check_tp(self, tp):
-        if tp.project != self.project:
-            raise ValueError(
-                "TP '%s' is not part of project '%s'"
-                % (tp, self.project.code))
-
-    def create_tp(self, language):
-        return self.tp_qs.create(language=language)
-
-    def update_from_tp(self, source, target):
-        self.check_tp(source)
-        self.check_tp(target)
-        self.update_children(source.directory, target.directory)
-
-    def update_store(self, source, target):
-        source_revision = target.get_max_unit_revision() + 1
-        differ = StoreDiff(target, source, source_revision)
-        diff = differ.diff()
-        if diff is None:
+    def move(self, tp, language):
+        """Re-assign a tp to a different language"""
+        self.check_tp(tp)
+        if tp.language == language:
             return
-        system = User.objects.get_system_user()
-        update_revision = Revision.incr()
-        return target.update_from_diff(
-            source,
-            source_revision,
-            diff,
-            update_revision,
-            system,
-            SubmissionTypes.SYSTEM,
-            SOURCE_WINS,
-            True)
+        self.check_no_tp(language)
+        pootle_path = self.get_path(language.code)
+        directory = tp.directory
+        tp.language = language
+        tp.pootle_path = pootle_path
+        tp.save()
+        self.set_parents(directory, self.get_tp(language).directory)
+        directory.delete()
+
+    def set_parents(self, directory, parent):
+        """Recursively sets the parent for children of a directory"""
+        self.check_tp(directory.translation_project)
+        self.check_tp(parent.translation_project)
+        for store in directory.child_stores.all():
+            store.clear_all_cache(parents=False, children=False)
+            store.parent = parent
+            store.mark_all_dirty()
+            store.save()
+        for subdir in directory.child_dirs.all():
+            subdir.clear_all_cache(parents=False, children=False)
+            subdir.parent = parent
+            subdir.save()
+            self.set_parents(subdir, subdir)
 
     def update_children(self, source_dir, target_dir):
         stores = []
@@ -90,70 +135,25 @@ class TPTool(object):
         for store in target_dir.child_stores.exclude(name__in=stores):
             store.makeobsolete()
 
-    def clone_store(self, store, target_dir):
-        cloned = target_dir.child_stores.create(
-            name=store.name,
-            translation_project=target_dir.translation_project)
-        cloned.mark_all_dirty()
-        cloned.update(cloned.deserialize(store.serialize()))
-        cloned.state = store.state
-        cloned.save()
-        return cloned
+    def update_from_tp(self, source, target):
+        self.check_tp(source)
+        self.check_tp(target)
+        self.update_children(source.directory, target.directory)
 
-    def clone(self, tp, language):
-        self.check_tp(tp)
-        self.check_no_tp(language)
-        new_tp = self.create_tp(language)
-        self.clone_children(
-            tp.directory,
-            new_tp.directory)
-        return new_tp
-
-    def clone_directory(self, source_dir, target_parent):
-        target_dir = target_parent.child_dirs.create(
-            name=source_dir.name)
-        self.clone_children(
-            source_dir,
-            target_dir)
-        return target_dir
-
-    def clone_children(self, source_dir, target_parent):
-        for store in source_dir.child_stores.live():
-            self.clone_store(store, target_parent)
-        for subdir in source_dir.child_dirs.live():
-            self.clone_directory(subdir, target_parent)
-
-    def check_no_tp(self, language):
-        if self.get_tp(language):
-            raise ValueError(
-                "TranslationProject '%s' already exists"
-                % self.get_path(language.code))
-
-    def set_parents(self, directory, parent):
-        """Recursively sets the parent for children of a directory"""
-        self.check_tp(directory.translation_project)
-        self.check_tp(parent.translation_project)
-        for store in directory.child_stores.all():
-            store.clear_all_cache(parents=False, children=False)
-            store.parent = parent
-            store.mark_all_dirty()
-            store.save()
-        for subdir in directory.child_dirs.all():
-            subdir.clear_all_cache(parents=False, children=False)
-            subdir.parent = parent
-            subdir.save()
-            self.set_parents(subdir, subdir)
-
-    def move(self, tp, language):
-        """Re-assign a tp to a different language"""
-        self.check_tp(tp)
-        if tp.language == language:
+    def update_store(self, source, target):
+        source_revision = target.get_max_unit_revision() + 1
+        differ = StoreDiff(target, source, source_revision)
+        diff = differ.diff()
+        if diff is None:
             return
-        self.check_no_tp(language)
-        pootle_path = self.get_path(language.code)
-        directory = tp.directory
-        tp.language = language
-        tp.pootle_path = pootle_path
-        tp.save()
-        self.set_parents(directory, self.get_tp(language).directory)
-        directory.delete()
+        system = User.objects.get_system_user()
+        update_revision = Revision.incr()
+        return target.update_from_diff(
+            source,
+            source_revision,
+            diff,
+            update_revision,
+            system,
+            SubmissionTypes.SYSTEM,
+            SOURCE_WINS,
+            True)
