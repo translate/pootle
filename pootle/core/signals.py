b7cb908d2314e413092f10aa2ec4ff1ca470c840
Ryan Northey <ryan@space23.net>
2016-08-26 08:16:57 +0100

    Add pootle.core.signals.update_data

---
+++ pootle/core/signals.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+from django.dispatch import Signal
+
+
+update_data = Signal(providing_args=["instance"])
