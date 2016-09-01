25429aeb6ab207be5008b811ec5d98ee1ad89cb8
Ryan Northey <ryan@space23.net>
2016-09-01 12:29:57 +0100

    Add test debugging fixtures

---
+++ pytest_pootle/fixtures/debug.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+import functools
+import logging
+import time
+from datetime import datetime
+
+import pytest
+
+from pytest_pootle import utils
+
+
+logger = logging.getLogger("POOTLE_DEBUG")
+
+
+@pytest.fixture(scope="session")
+def log_timings(request, timings):
+    logger.debug("TESTS START: %s", datetime.now())
+    return functools.partial(
+        utils.log_test_timing,
+        logger,
+        timings)
+
+
+@pytest.fixture(scope="session")
+def timings(request):
+    debug_tests = request.config.getoption("--debug-tests")
+
+    if not debug_tests:
+        return
+    if debug_tests != "-":
+        logger.addHandler(logging.FileHandler(debug_tests))
+    timings = dict(start=time.time(), tests={})
+    request.addfinalizer(
+        functools.partial(
+            utils.log_test_report,
+            logger,
+            timings))
+    return timings
