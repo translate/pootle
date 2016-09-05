f49e4dbf5b053aad036fa83830e6ee15e2d72a82
Ryan Northey <ryan@space23.net>
2016-09-05 17:49:54 +0100

    vftests

--- pytest_pootle/env.py
+++ pytest_pootle/env.py
@@ -475,17 +475,10 @@ class PootleTestEnv(object):
         from django.apps import apps
 
         from pootle.core.utils.db import set_mysql_collation_for_column
+        from pootle_language.models import Language
+        from pootle_project.models import Project
 
         cursor = connection.cursor()
-
-        # VirtualFolderTreeItem
-        set_mysql_collation_for_column(
-            apps,
-            cursor,
-            "virtualfolder.VirtualFolderTreeItem",
-            "pootle_path",
-            "utf8_bin",
-            "varchar(255)")
 
         # VirtualFolder
         set_mysql_collation_for_column(
@@ -495,24 +488,21 @@ class PootleTestEnv(object):
             "name",
             "utf8_bin",
             "varchar(70)")
-        set_mysql_collation_for_column(
-            apps,
-            cursor,
-            "virtualfolder.VirtualFolder",
-            "location",
-            "utf8_bin",
-            "varchar(255)")
-
-        VirtualFolderDBFactory(filter_rules="store0.po")
-        VirtualFolderDBFactory(filter_rules="store1.po")
+        project0 = Project.objects.get(code="project0")
+        language0 = Language.objects.get(code="language0")
+        VirtualFolderDBFactory(filter_rules="/store0.po")
+        VirtualFolderDBFactory(filter_rules="/store1.po")
         VirtualFolderDBFactory(
-            location='/{LANG}/project0/',
+            language=None,
+            project=project0,
             is_public=False,
-            filter_rules="store0.po")
+            filter_rules="/store0.po")
         VirtualFolderDBFactory(
-            location='/{LANG}/project0/',
+            language=None,
+            project=project0,
             is_public=False,
-            filter_rules="store1.po")
+            filter_rules="/store1.po")
         VirtualFolderDBFactory(
-            location='/language0/project0/',
-            filter_rules="subdir0/store4.po")
+            language=language0,
+            project=project0,
+            filter_rules="/subdir0/store4.po")
