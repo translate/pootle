eed8d42016b0ae427de3af052fe5474efbc8fef4
Taras Semenenko <taras.semenenko@gmail.com>
2016-09-08 01:27:17 +0300

    Add AltSrcUnit proxy
    AltSrcUnit proxy class is used for getting alternative sources when editing a unit.
    It looks clearer and it is more efficint to use such classes to prerare data for
    JSON views.

---
+++ pootle/apps/pootle_store/unit/altsrc.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+from pootle.i18n.gettext import language_dir
+from pootle_store.templatetags.store_tags import pluralize_target
+
+from .proxy import UnitProxy
+
+
+class AltSrcUnitProxy(UnitProxy):
+    @property
+    def store_path(self):
+        """Returns just the path part omitting language and project codes."""
+        return self.unit["store__pootle_path"].split(u'/', 2)[-1]
+
+    @property
+    def language_code(self):
+        return self.unit["store__translation_project__language__code"]
+
+    @property
+    def language_direction(self):
+        return language_dir(self.language_code)
+
+    @property
+    def language_name(self):
+        return self.unit["store__translation_project__language__fullname"]
+
+    @property
+    def nplurals(self):
+        return self.unit["store__translation_project__language__nplurals"] or 0
+
+    @property
+    def data(self):
+        return dict(
+            id=self.id,
+            store_path=self.store_path,
+            language_code=self.language_code,
+            language_name=self.language_name,
+            language_direction=self.language_direction,
+            nplurals=self.nplurals,
+            has_plurals=self.hasplural(),
+            target=[target[1]
+                    for target
+                    in pluralize_target(self, self.nplurals)],
+        )
+
+
+class AltSrcUnits(object):
+    fields = {
+        "id",
+        "source_f",
+        "target_f",
+        "store__pootle_path",
+        "store__translation_project__language__code",
+        "store__translation_project__language__fullname",
+        "store__translation_project__language__nplurals",
+    }
+
+    def __init__(self, qs):
+        self.qs = qs
+
+    @property
+    def units(self):
+        return [AltSrcUnitProxy(x) for x in self.qs.values(*self.fields)]
