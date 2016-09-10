c593a18abf2a548968e468ee1261e96a7b199cd5
Ryan Northey <ryan@space23.net>
2016-09-10 15:41:02 +0100

    Remove unnecessary vfolder validation test

--- tests/models/virtualfolder.py
+++ tests/models/virtualfolder.py
@@ -19,31 +19,6 @@ from virtualfolder.models import VirtualFolder, VirtualFolderTreeItem
 
 
 @pytest.mark.django_db
-def test_vfolder_directory_clash(af_vfolder_test_browser_defines_po):
-    """Tests that the creation of a virtual folder fails if it clashes with
-    some already existing directory.
-
-    References #3905.
-    """
-
-    vfolder_item = {
-        'name': "browser",
-        'location': "/af/vfolder_test/",
-        'priority': 4,
-        'is_public': True,
-        'filter_rules': "browser/defines.po",
-    }
-    vfolder = VirtualFolder(**vfolder_item)
-
-    with pytest.raises(ValidationError) as excinfo:
-        vfolder.save()
-
-    assert (u"Problem adding virtual folder 'browser' with location "
-            u"'/af/vfolder_test/': VirtualFolderTreeItem clashes with "
-            u"Directory /af/vfolder_test/browser/") in str(excinfo.value)
-
-
-@pytest.mark.django_db
 def test_vfolder_priority_not_greater_than_zero():
     """Tests that the creation of a virtual folder fails if the provided
     priority is not greater than zero.
