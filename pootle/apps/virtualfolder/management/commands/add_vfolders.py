6dc9490825cdebfe790d647593c25fe53714ad96
Leandro Regueiro <leandro.regueiro@gmail.com>
2016-09-15 14:04:22 +0200

    Adjust add_vfolders command to new reality

--- pootle/apps/virtualfolder/management/commands/add_vfolders.py
+++ pootle/apps/virtualfolder/management/commands/add_vfolders.py
@@ -16,6 +16,9 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'pootle.settings'
 from django.core.exceptions import ValidationError
 from django.core.management.base import BaseCommand, CommandError
 
+from pootle.core.url_helpers import split_pootle_path
+from pootle_language.models import Language
+from pootle_project.models import Project
 from virtualfolder.models import VirtualFolder
 
 
@@ -28,6 +31,33 @@ class Command(BaseCommand):
             nargs=1,
             help="JSON vfolder configuration file",
         )
+
+    def parse_vfolder_rules(self, location, old_rules):
+        """Extract languages, projects and new rules from location and rules."""
+        languages = set()
+        projects = set()
+        new_rules = set()
+
+        full_rules = [location + old_rule.strip() for old_rule in old_rules]
+
+        for full_rule in full_rules:
+            lang_code, proj_code, dir_path, fname = split_pootle_path(full_rule)
+            if fname:
+                new_rules.add(dir_path + fname)
+            else:
+                new_rules.add(dir_path + "*")
+            languages.add(lang_code)
+            projects.add(proj_code)
+
+        if "{LANG}" in languages:
+            languages = set()
+
+        if "{PROJ}" in projects:
+            projects = set()
+
+        new_rules = ",".join(new_rules)
+
+        return languages, projects, new_rules
 
     def handle(self, **options):
         """Add virtual folders from file."""
@@ -57,12 +87,16 @@ class Command(BaseCommand):
         errored_count = 0
 
         for vfolder_item in vfolders:
-            vfolder_item['name'] = vfolder_item['name'].lower()
+            vfolder_item['name'] = vfolder_item['name'].strip().lower()
 
             # Put all the files for each virtual folder as a list and save it
             # as its filter rules.
-            vfolder_item['filter_rules'] = ','.join(
-                vfolder_item['filters']['files'])
+            languages, projects, new_rules = self.parse_vfolder_rules(
+                vfolder_item['location'].strip(),
+                vfolder_item['filters']['files']
+            )
+
+            vfolder_item['filter_rules'] = new_rules
 
             if 'filters' in vfolder_item:
                 del vfolder_item['filters']
@@ -70,15 +104,14 @@ class Command(BaseCommand):
             # Now create or update the virtual folder.
             try:
                 # Retrieve the virtual folder if it exists.
-                vfolder = VirtualFolder.objects.get(
-                    name=vfolder_item['name'],
-                    location=vfolder_item['location'],
-                )
+                vfolder = VirtualFolder.objects.get(name=vfolder_item['name'])
             except VirtualFolder.DoesNotExist:
                 # If the virtual folder doesn't exist yet then create it.
                 try:
                     self.stdout.write(u'Adding new virtual folder %s...' %
                                       vfolder_item['name'])
+                    vfolder_item['all_projects'] = not projects
+                    vfolder_item['all_languages'] = not languages
                     vfolder = VirtualFolder(**vfolder_item)
                     vfolder.save()
                 except ValidationError as e:
@@ -86,11 +119,40 @@ class Command(BaseCommand):
                     self.stdout.write('FAILED')
                     self.stderr.write(e)
                 else:
+                    if projects:
+                        vfolder.projects.add(
+                            *Project.objects.filter(code__in=projects)
+                        )
+                    if languages:
+                        vfolder.languages.add(
+                            *Language.objects.filter(code__in=languages)
+                        )
                     self.stdout.write('DONE')
                     added_count += 1
             else:
                 # Update the already existing virtual folder.
                 changed = False
+
+                if not projects:
+                    vfolder.all_projects = True
+                    changed = True
+                    logging.debug("'All projects' for virtual folder '%s' "
+                                  "will be changed.", vfolder.name)
+
+                if not languages:
+                    vfolder.all_languages = True
+                    changed = True
+                    logging.debug("'All languages' for virtual folder '%s' "
+                                  "will be changed.", vfolder.name)
+
+                if projects:
+                    vfolder.projects.set(
+                        *Project.objects.filter(code__in=projects)
+                    )
+                if languages:
+                    vfolder.languages.set(
+                        *Language.objects.filter(code__in=languages)
+                    )
 
                 if vfolder.filter_rules != vfolder_item['filter_rules']:
                     vfolder.filter_rules = vfolder_item['filter_rules']
