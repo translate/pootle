96609c7539f23e4fb0db84f1a17c5c77fa1b60cb
Ryan Northey <ryan@space23.net>
2016-09-15 22:11:35 +0100

    Use env fixtures in tm server tests

--- tests/commands/update_tmserver.py
+++ tests/commands/update_tmserver.py
@@ -16,7 +16,7 @@ from django.core.management.base import CommandError
 
 @pytest.mark.cmd
 @pytest.mark.django_db
-def test_update_tmserver_nosetting(capfd, po_directory, afrikaans_tutorial):
+def test_update_tmserver_nosetting(capfd, po_directory, tp0):
     """We need configured TM for anything to work"""
     with pytest.raises(CommandError) as e:
         call_command('update_tmserver')
@@ -25,7 +25,7 @@ def test_update_tmserver_nosetting(capfd, po_directory, afrikaans_tutorial):
 
 @pytest.mark.cmd
 @pytest.mark.django_db
-def __test_update_tmserver_noargs(capfd, afrikaans_tutorial, settings):
+def __test_update_tmserver_noargs(capfd, tp0, settings):
     """Load TM from the database"""
 
     from pootle_store.models import Unit
