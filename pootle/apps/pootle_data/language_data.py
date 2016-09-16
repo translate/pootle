5c40ec885d4b1d2aa15d7eede1e8eec6284d36ea
Ryan Northey <ryan@space23.net>
2016-09-16 11:29:22 +0100

    Add LanguageData tool

---
+++ pootle/apps/pootle_data/language_data.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+from .utils import RelatedTPsDataTool
+
+
+class LanguageDataTool(RelatedTPsDataTool):
+    """Retrieves aggregate stats for a Language"""
+
+    def filter_data(self, qs):
+        return qs.filter(tp__language=self.context)
