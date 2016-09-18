d607b4371aec12abca574f04661361933d6488cd
Ryan Northey <ryan@space23.net>
2016-09-18 14:09:09 +0100

    Add tests for TPDataTool.get_stats

--- tests/pootle_data/data_tool.py
+++ tests/pootle_data/data_tool.py
@@ -8,7 +8,10 @@
 
 import pytest
 
+from django.db.models import Sum
+
 from pootle.core.delegate import data_tool
+from pootle_data.models import StoreData
 from pootle_data.store_data import StoreDataTool
 from pootle_data.utils import DataTool
 
@@ -79,3 +82,78 @@ def test_data_tool_store_get_checks(store0):
     assert (
         sorted(checks.items())
         == sorted(store0.check_data.values_list("name", "count")))
+
+
+@pytest.mark.django_db
+def test_data_tool_tp_get_stats(tp0):
+    stats = tp0.data_tool.get_stats(include_children=False)
+
+    # TODO: remove when old stats are removed
+    old_stats = tp0.get_stats(include_children=False)
+    assert (
+        sorted(old_stats.keys())
+        == sorted(stats.keys()))
+
+    assert "children" not in stats
+    assert stats["translated"] == tp0.data.translated_words
+    assert stats["fuzzy"] == tp0.data.fuzzy_words
+    assert stats["total"] == tp0.data.total_words
+    assert stats["critical"] == tp0.data.critical_checks
+    assert stats["suggestions"] == tp0.data.pending_suggestions
+    assert stats["is_dirty"] is False
+    # this is the actually last updated unit - called "lastaction"
+    last_submission_info = (
+        tp0.data.last_submission.get_submission_info())
+    assert (
+        sorted(stats["lastaction"].items())
+        == sorted(last_submission_info.items()))
+    # apparently "updated" means created
+    last_updated_info = (
+        tp0.data.last_created_unit.get_last_updated_info())
+    assert (
+        sorted(stats["lastupdated"].items())
+        == sorted(last_updated_info.items()))
+
+
+@pytest.mark.django_db
+def test_data_tool_tp_get_stats_with_children(tp0):
+    stats = tp0.data_tool.get_stats()
+
+    # TODO: remove when old stats are removed
+    old_stats = tp0.get_stats()
+    assert (
+        sorted(old_stats.keys())
+        == sorted(stats.keys()))
+
+    assert stats["translated"] == tp0.data.translated_words
+    assert stats["fuzzy"] == tp0.data.fuzzy_words
+    assert stats["total"] == tp0.data.total_words
+    assert stats["critical"] == tp0.data.critical_checks
+    assert stats["suggestions"] == tp0.data.pending_suggestions
+    # this is the actually last updated unit - called "lastaction"
+    last_submission_info = (
+        tp0.data.last_submission.get_submission_info())
+    assert (
+        sorted(stats["lastaction"].items())
+        == sorted(last_submission_info.items()))
+    # apparently "updated" means created
+    last_updated_info = (
+        tp0.data.last_created_unit.get_last_updated_info())
+    assert (
+        sorted(stats["lastupdated"].items())
+        == sorted(last_updated_info.items()))
+
+    for directory in tp0.directory.child_dirs.all():
+        dir_stats = stats["children"][directory.name]
+        store_data = StoreData.objects.filter(
+            store__pootle_path__startswith=directory.pootle_path)
+        aggregate_mapping = dict(
+            total="total_words",
+            fuzzy="fuzzy_words",
+            translated="translated_words",
+            suggestions="pending_suggestions",
+            critical="critical_checks")
+        for k, v in aggregate_mapping.items():
+            assert (
+                dir_stats[k]
+                == store_data.aggregate(**{k: Sum(v)})[k])
