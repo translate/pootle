448115e5cfae8be186d5bea776f43884d5552852
Julen Ruiz Aizpuru <julenx@gmail.com>
2016-09-22 10:34:18 +0200

    Undoable: only pass down current value
    There's not much use in providing the entire list of modifications down the tree
    and it can end up slowing things down by passing around relatively big amounts
    of data arrays.

--- pootle/static/js/shared/components/Undoable.js
+++ pootle/static/js/shared/components/Undoable.js
@@ -93,7 +93,7 @@ const Undoable = (Component) => React.createClass({
   render() {
     return (
       <Component
-        {...this.state}
+        value={this.state.value}
         {...this.props}
         onChange={this.handleChange}
         onUndo={this.handleUndo}
