4f1b403e0a15ad74b8d628dc0c121880b056f111
Taras Semenenko <taras.semenenko@gmail.com>
2016-09-23 17:02:29 +0300

    Adjust l20nsource component for richmode support

--- pootle/static/js/editor/formats/l20n/L20nSource.js
+++ pootle/static/js/editor/formats/l20n/L20nSource.js
@@ -11,7 +11,20 @@ import React from 'react';
 import { t } from 'utils/i18n';
 
 import UnitSource from '../../components/UnitSource';
-import { getL20nPlurals } from './utils';
+import { getL20nData } from './utils';
+
+
+const InnerPre = ({ sourceValue }) => (
+  <pre
+    dangerouslySetInnerHTML={
+      { __html: sourceValue }
+    }
+  />
+);
+
+InnerPre.propTypes = {
+  sourceValue: React.PropTypes.string.isRequired,
+};
 
 
 const L20nSource = React.createClass({
@@ -20,21 +33,27 @@ const L20nSource = React.createClass({
     values: React.PropTypes.array.isRequired,
     isRichModeEnabled: React.PropTypes.bool,
     sourceLocaleCode: React.PropTypes.string,
-    },
+  },
 
   getInitialState() {
     return {
       values: this.props.values,
+      isRichModeEnabled: false,
     };
   },
 
   componentWillMount() {
-    const l20nPlurals = getL20nPlurals(this.props.values, 1);
-    if (l20nPlurals) {
+    const l20nData = getL20nData(this.props.values);
+
+    if (l20nData.hasL20nPlurals) {
       this.setState({
-        values: l20nPlurals.unitValues,
-        pluralForms: l20nPlurals.pluralForms,
+        values: l20nData.unitValues,
+        pluralForms: l20nData.pluralForms,
         hasPlurals: true,
+      });
+    } else if (!l20nData.hasSimpleValue) {
+      this.setState({
+        applyFontFilterEnabled: false,
       });
     }
   },
@@ -44,14 +63,20 @@ const L20nSource = React.createClass({
       this.setState({
         values: nextProps.values,
         hasPlurals: nextProps.values.length > 1,
+        isRichModeEnabled: true,
       });
     } else {
-      const l20nPlurals = getL20nPlurals(nextProps.values, 1);
-      if (l20nPlurals) {
+      const l20nData = getL20nData(nextProps.values);
+      if (l20nData.hasL20nPlurals) {
         this.setState({
-          values: l20nPlurals.unitValues,
-          pluralForms: l20nPlurals.pluralForms,
+          values: l20nData.unitValues,
+          pluralForms: l20nData.pluralForms,
           hasPlurals: true,
+          isRichModeEnabled: false,
+        });
+      } else if (!l20nData.hasSimpleValue) {
+        this.setState({
+          isRichModeEnabled: true,
         });
       }
     }
@@ -62,13 +87,19 @@ const L20nSource = React.createClass({
         this.state.pluralForms.length === this.state.values.length) {
       return t('Plural form [%(key)s]', { key: this.state.pluralForms[index] });
     }
+    return '';
   },
 
   render() {
+    const extraProps = {};
+    if (this.state.isRichModeEnabled) {
+      extraProps.innerComponent = InnerPre;
+    }
     return (
       <UnitSource
         {...this.props}
         {...this.state }
+        {...extraProps}
         getPluralFormName={this.getPluralFormName}
       />
     );
