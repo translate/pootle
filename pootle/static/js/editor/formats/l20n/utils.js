bb4c2bd6701f1d849e005d17081fea736e580773
Taras Semenenko <taras.semenenko@gmail.com>
2016-09-23 17:03:22 +0300

    Adjust l20n format utils module for richmode support

--- pootle/static/js/editor/formats/l20n/utils.js
+++ pootle/static/js/editor/formats/l20n/utils.js
@@ -6,35 +6,87 @@
  * AUTHORS file for copyright and authorship information.
  */
 
+import assign from 'object-assign';
+
 import { FTLASTParser, FTLASTSerializer, getPluralForms } from 'l20n';
 
 
-export function getL20nPlurals(values, nplurals) {
-  if (nplurals !== 1 || values.length !== 1 || values[0] === '') {
-    return false;
+class L20nEditorError extends Error {
+  constructor(message, id) {
+    super();
+    this.name = 'L20nEditorError';
+    this.message = message;
+    this.id = id;
   }
-  const unitEntity = FTLASTParser.parseResource('unit = ' + values[0])[0].body[0];
-  const value = unitEntity.value;
+}
+
+
+function parseL20nValue(value) {
+  try {
+    const unitEntity = FTLASTParser.parseResource(`unit = ${value}`)[0].body[0];
+    return { unitEntity, error: null };
+  } catch (e) {
+    if (e.name === 'L10nError') {
+      return { unitEntity: null, error: e };
+    }
+    throw e;
+  }
+}
+
+
+function setL20nPlurals(data) {
+  const value = data.unitEntity.value;
   const hasL20nPlurals = (value !== undefined &&
                           value.elements.length === 1 &&
                           value.elements[0].type === 'Placeable' &&
                           value.elements[0].expressions[0].type === 'SelectExpression' &&
                           value.elements[0].expressions[0].expression.callee.name === 'PLURAL');
+  assign(data, { hasL20nPlurals });
   if (hasL20nPlurals) {
     const unitValues = [];
     const pluralForms = [];
-    const variants = unitEntity.value.elements[0].expressions[0].variants;
-    for (let i in variants) {
-      unitValues.push(FTLASTSerializer.dumpPattern(variants[i].value));
-      let key = FTLASTSerializer.dumpKeyword(variants[i].key);
+    const variants = data.unitEntity.value.elements[0].expressions[0].variants;
+    for (let i = 0; i < variants.length; i++) {
+      unitValues.push(variants[i].value.source);
+      let key = variants[i].key.name;
       if (variants[i].default) {
         key = `${key}, default`;
       }
       pluralForms.push(key);
     }
-    return {unitValues, pluralForms, unitEntity};
+    assign(data, { unitValues, pluralForms });
   }
-  return false;
+  return hasL20nPlurals;
+}
+
+
+function setSimpleValue(data) {
+  const value = data.unitEntity.value;
+  const hasSimpleValue = (value !== undefined &&
+                    value.elements.length === 1 &&
+                    value.elements[0].type === 'TextElement');
+
+  assign(data, { hasSimpleValue });
+  return hasSimpleValue;
+}
+
+
+export function getL20nData(values, nplurals) {
+  if (nplurals !== undefined && nplurals !== 1 || values.length !== 1 || values[0] === '') {
+    return { isEmpty: true };
+  }
+  const result = parseL20nValue(values[0]);
+  if (result.error !== null) {
+    return result;
+  }
+
+  if (setL20nPlurals(result)) {
+    return result;
+  }
+
+  setSimpleValue(result);
+
+  return result;
 }
 
 
@@ -44,9 +96,9 @@ export function dumpL20nPlurals(values, l20nUnitEntity) {
   if (values.every(value => value === '')) {
     return '';
   } else if (values.some(value => value === '')) {
-    throw new L20nEditorError('All plural forms should be filled.')
+    throw new L20nEditorError('All plural forms should be filled.');
   }
-  for (let i in values) {
+  for (let i = 0; i < values.length; i++) {
     const pfEntity = FTLASTParser.parseResource('unit = val');
     pfEntity[0].body[0].value.elements[0].value = values[i];
     variants[i].value = pfEntity[0].body[0].value;
@@ -55,7 +107,7 @@ export function dumpL20nPlurals(values, l20nUnitEntity) {
   try {
     return [FTLASTSerializer.dumpPattern(l20nUnitEntity.value)];
   } catch (e) {
-    throw new L20nEditorError(e.message)
+    throw new L20nEditorError(e.message);
   }
 }
 
@@ -69,14 +121,4 @@ export function getL20nEmptyPluralsEntity(localeCode) {
 }
 
 
-class L20nEditorError extends Error {
-   constructor(message, id) {
-     super();
-     this.name = 'L20nEditorError';
-     this.message = message;
-     this.id = id;
-   }
-}
-
-
-export { L20nEditorError as L20nEditorError }
+export { L20nEditorError as L20nEditorError };
