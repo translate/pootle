c6716997b386035662689183f40f6110e4ad67b9
Ryan Northey <ryan@space23.net>
2016-10-08 09:53:44 +0100

    Remove unused vfolder helper

--- pootle/apps/virtualfolder/helpers.py
+++ pootle/apps/virtualfolder/helpers.py
@@ -11,23 +11,6 @@ from pootle_app.models import Directory
 from .models import VirtualFolderTreeItem
 
 
-def make_vfolder_treeitem_dict(vfolder_treeitem):
-    return {
-        'href_all': vfolder_treeitem.get_translate_url(),
-        'href_todo': vfolder_treeitem.get_translate_url(
-            state='incomplete'),
-        'href_sugg': vfolder_treeitem.get_translate_url(
-            state='suggestions'),
-        'href_critical': vfolder_treeitem.get_critical_url(),
-        'title': vfolder_treeitem.vfolder.name,
-        'code': vfolder_treeitem.code,
-        'priority': vfolder_treeitem.vfolder.priority,
-        'is_grayed': not vfolder_treeitem.is_visible,
-        'stats': vfolder_treeitem.get_stats(
-            include_children=False),
-        'icon': 'vfolder'}
-
-
 def vftis_for_child_dirs(directory):
     """
     Returns the vfoldertreeitems for a directory's child directories
