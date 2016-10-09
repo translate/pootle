cc864825673e7165536034080e3d2eb2294c2101
Ryan Northey <ryan@space23.net>
2016-10-09 20:37:11 +0100

    Add pootle_langage.getter for language_team

--- pootle/apps/pootle_language/apps.py
+++ pootle/apps/pootle_language/apps.py
@@ -6,6 +6,8 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
+import importlib
+
 from django.apps import AppConfig
 
 
@@ -13,3 +15,6 @@ class PootleLanguageConfig(AppConfig):
 
     name = "pootle_language"
     verbose_name = "Pootle Language"
+
+    def ready(self):
+        importlib.import_module("pootle_language.getters")
