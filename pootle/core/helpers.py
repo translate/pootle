f0ee9557479d3171ead158dc6d6fdda15d36d35e
Ryan Northey <ryan@space23.net>
2016-10-12 20:24:24 +0100

    Remove unused helper function

--- pootle/core/helpers.py
+++ pootle/core/helpers.py
@@ -11,53 +11,8 @@ from urllib import quote, unquote
 
 from django.utils import dateformat
 
-from pootle.i18n.gettext import ugettext as _
-from pootle_misc.checks import check_names
-
 
 SIDEBAR_COOKIE_NAME = 'pootle-browser-sidebar'
-
-
-def get_filter_name(GET):
-    """Gets current filter's human-readable name.
-
-    :param GET: A copy of ``request.GET``.
-    :return: Two-tuple with the filter name, and a list of extra arguments
-        passed to the current filter.
-    """
-    filter = extra = None
-
-    if 'filter' in GET:
-        filter = GET['filter']
-
-        if filter.startswith('user-'):
-            extra = [GET.get('user', _('User missing'))]
-        elif filter == 'checks' and 'checks' in GET:
-            extra = map(lambda check: check_names.get(check, check),
-                        GET['checks'].split(','))
-    elif 'search' in GET:
-        filter = 'search'
-
-        extra = [GET['search']]
-        if 'sfields' in GET:
-            extra.extend(GET['sfields'].split(','))
-
-    filter_name = {
-        'all': _('All'),
-        'translated': _('Translated'),
-        'untranslated': _('Untranslated'),
-        'fuzzy': _('Needs work'),
-        'incomplete': _('Incomplete'),
-        # Translators: This is the name of a filter
-        'search': _('Search'),
-        'checks': _('Checks'),
-        'my-submissions': _('My submissions'),
-        'user-submissions': _('Submissions'),
-        'my-submissions-overwritten': _('My overwritten submissions'),
-        'user-submissions-overwritten': _('Overwritten submissions'),
-    }.get(filter)
-
-    return (filter_name, extra)
 
 
 def get_sidebar_announcements_context(request, objects):
