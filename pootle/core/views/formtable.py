fac4a860170af44b0dff4733301f3e5752999b51
Ryan Northey <ryan@space23.net>
2016-10-14 20:53:45 +0100

    Add pootle.core.views.formtable form wrapper

---
+++ pootle/core/views/formtable.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+from django.template.loader import render_to_string
+
+
+class Formtable(object):
+    row_field = ""
+    filters_template = ""
+    actions_field = "actions"
+    form_method = "POST"
+    form_action = ""
+    form_css_class = "formtable"
+
+    def __init__(self, form, **kwargs):
+        self.form = form
+        self.kwargs = kwargs
+
+    @property
+    def form_attrs(self):
+        return {
+            "method": self.form_method,
+            "class": self.form_css_class,
+            "action": self.form_action}
+
+    @property
+    def table_attrs(self):
+        return dict({"class": "pootle-table centered"})
+
+    @property
+    def actions(self):
+        return self.form[self.actions_field]
+
+    @property
+    def columns(self):
+        return self.kwargs.get("columns", ())
+
+    @property
+    def colspan(self):
+        return len(self.columns)
+
+    @property
+    def page(self):
+        return self.kwargs.get("page", ())
+
+    @property
+    def rows(self):
+        return (
+            self.form[self.row_field]
+            if (self.row_field in self.form.fields
+                and self.form.fields[self.row_field].choices)
+            else [])
+
+    @property
+    def filters(self):
+        return (
+            render_to_string(
+                self.filters_template,
+                dict(formtable=self))
+            if self.filters_template
+            else "")
+
+    @property
+    def form_method(self):
+        return self.kwargs.get("method", "POST")
+
+    @property
+    def table_css_class(self):
+        return 'pootle-table centered'
