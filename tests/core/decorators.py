08c1fb35a8f0d6fa570b6c8464487fd9e4acb7bf
Ryan Northey <ryan@space23.net>
2016-10-15 09:02:07 +0100

    Add tests for pootle.core.decorators.persistent_property

--- tests/core/decorators.py
+++ tests/core/decorators.py
@@ -10,7 +10,9 @@ import pytest
 
 from django.http import Http404
 
-from pootle.core.decorators import get_path_obj, get_resource
+from pootle.core.cache import get_cache
+from pootle.core.decorators import (
+    get_path_obj, get_resource, persistent_property)
 from pootle_app.models import Directory
 from pootle_language.models import Language
 from pootle_project.models import Project, ProjectResource
@@ -210,3 +212,80 @@ def test_get_resource_project(rf, default, project0, tp0):
                 name=subdir_name.rstrip("/"),
                 parent__translationproject__project=project0)))
     assert isinstance(request.resource_obj.resources[0], Directory)
+
+
+@pytest.mark.django
+def test_deco_persistent_property_no_cache_key():
+
+    # no cache key set - uses instance caching
+    class Foo(object):
+
+        @persistent_property
+        def bar(self):
+            return "Baz"
+    foo = Foo()
+    assert foo.bar == "Baz"
+    assert foo.__dict__ == dict(bar="Baz")
+
+    # no cache key set and not always_cache - no caching
+    class Foo(object):
+
+        def _bar(self):
+            return "Baz"
+        bar = persistent_property(_bar, always_cache=False)
+    foo = Foo()
+    assert foo.bar == "Baz"
+    assert foo.__dict__ == {}
+
+    # no cache key set - uses instance caching with custom name
+    class Foo(object):
+
+        def _bar(self):
+            return "Baz"
+        bar = persistent_property(_bar, name="special_bar")
+    foo = Foo()
+    assert foo.bar == "Baz"
+    assert foo.__dict__ == dict(special_bar="Baz")
+
+
+@pytest.mark.django
+def test_deco_persistent_property():
+
+    # cache_key set - cached with it
+    class Foo(object):
+        cache_key = "foo-cache"
+
+        @persistent_property
+        def bar(self):
+            """Get a bar man"""
+            return "Baz"
+    assert isinstance(Foo.bar, persistent_property)
+    assert Foo.bar.__doc__ == "Get a bar man"
+    foo = Foo()
+    assert foo.bar == "Baz"
+    assert get_cache().get('foo-cache/bar') == "Baz"
+    # cached version this time
+    assert foo.bar == "Baz"
+
+    # cache_key set with custom key attr - cached with it
+    class Foo(object):
+        special_key = "special-foo-cache"
+
+        def _bar(self):
+            return "Baz"
+        bar = persistent_property(_bar, key_attr="special_key")
+    foo = Foo()
+    assert foo.bar == "Baz"
+    assert get_cache().get('special-foo-cache/_bar') == "Baz"
+
+    # cache_key set with custom key attr and name - cached with it
+    class Foo(object):
+        special_key = "special-foo-cache"
+
+        def _bar(self):
+            return "Baz"
+        bar = persistent_property(
+            _bar, name="bar", key_attr="special_key")
+    foo = Foo()
+    assert foo.bar == "Baz"
+    assert get_cache().get('special-foo-cache/bar') == "Baz"
