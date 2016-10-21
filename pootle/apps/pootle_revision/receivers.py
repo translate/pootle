c77409486ca882fe17bac4c671666795baf5ea42
Ryan Northey <ryan@space23.net>
2016-10-21 20:19:12 +0100

    Add pootle_revision receivers

---
+++ pootle/apps/pootle_revision/receivers.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from pootle.core.delegate import revision_updater
+from pootle_data.models import StoreData
+from pootle_store.models import Store
+
+
+@receiver(post_save, sender=StoreData)
+def handle_storedata_save(**kwargs):
+    revision_updater.get(Store)(
+        context=kwargs["instance"].store).update(keys=["stats", "checks"])
