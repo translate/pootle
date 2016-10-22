8d7d9b2fe5a038268b4f64693199e19c8d5be7ae
Ryan Northey <ryan@space23.net>
2016-10-22 05:35:26 +0100

    Add cache_keys to ProjectResource and ProjectSet

--- pootle/apps/pootle_data/project_data.py
+++ pootle/apps/pootle_data/project_data.py
@@ -20,22 +20,35 @@ class ProjectDataTool(RelatedTPsDataTool):
 
 class ProjectResourceDataTool(RelatedStoresDataTool):
     group_by = ("store__translation_project__language__code", )
+    cache_key_name = "project_resource"
 
-    def filter_data(self, qs):
-        project_path = (
+    @property
+    def project_path(self):
+        return (
             "/%s/%s%s"
             % (self.project_code,
                self.dir_path,
                self.filename))
-        regex = r"^/[^/]*%s" % project_path
+
+    def filter_data(self, qs):
+        regex = r"^/[^/]*%s" % self.project_path
         return (
             qs.filter(store__translation_project__project__code=self.project_code)
-              .filter(store__pootle_path__contains=project_path)
+              .filter(store__pootle_path__contains=self.project_path)
               .filter(store__pootle_path__regex=regex))
+
+    @property
+    def context_name(self):
+        return "/projects%s" % self.project_path
 
 
 class ProjectSetDataTool(RelatedTPsDataTool):
     group_by = ("tp__project__code", )
+    cache_key_name = "projects"
 
     def get_root_child_path(self, child):
         return child[self.group_by[0]]
+
+    @property
+    def context_name(self):
+        return "ALL"
