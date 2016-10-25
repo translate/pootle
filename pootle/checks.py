ff2f71e8f01baa1671ada4fa37ee06e9d150d3ee
Dwayne Bailey <dwayne@translate.org.za>
2016-10-25 01:14:10 +0100

    Checks: adjust rqworker system check
    * Reduce the whitelist for command that no longer exist or need async
      stats
    * Fix comments to reflect the logic, WHITELIST being the commands that
      should be checked (probably could do with a better name)

--- pootle/checks.py
+++ pootle/checks.py
@@ -37,12 +37,10 @@ LXML_MINIMUM_REQUIRED_VERSION = (3, 5, 0, 0)
 REDIS_MINIMUM_REQUIRED_VERSION = (2, 8, 4)
 
 
-# XXX List of manage.py commands not to run the rqworker check on.
-# Maybe tagging can improve this?
+# List of pootle commands that need a running rqworker.
+# FIXME Maybe tagging can improve this?
 RQWORKER_WHITELIST = [
-    "start", "initdb", "revision", "sync_stores", "refresh_stats",
-    "update_stores", "calculate_checks", "retry_failed_jobs", "check",
-    "runserver",
+    "revision", "retry_failed_jobs", "check", "runserver",
 ]
 
 
@@ -140,7 +138,8 @@ def check_redis(app_configs=None, **kwargs):
             ))
 
         if len(queue.connection.smembers(Worker.redis_workers_keys)) == 0:
-            # We need to check we're not running manage.py rqworker right now..
+            # If we're not running 'pootle rqworker' report for whitelisted
+            # commands
             import sys
             if len(sys.argv) > 1 and sys.argv[1] in RQWORKER_WHITELIST:
                 errors.append(checks.Warning(
