42012f8c76f702e5eff8237344a380d2dae6038c
Ryan Northey <ryan@synca.io>
2016-10-31 13:07:41 +0000

    Add js for pootle fs ui

--- pootle/static/js/common.js
+++ pootle/static/js/common.js
@@ -22,6 +22,7 @@ import browser from './browser';
 import captcha from './captcha';
 import contact from './contact';
 import dropdown from './dropdown';
+import fs from './fs';
 import helpers from './helpers';
 import score from './score';
 import search from './search';
@@ -65,6 +66,7 @@ PTL.captcha = captcha;
 PTL.cookie = cookie;
 PTL.contact = contact;
 PTL.dropdown = dropdown;
+PTL.fs = fs;
 PTL.score = score;
 PTL.search = search;
 PTL.stats = stats;
@@ -80,6 +82,7 @@ PTL.common = {
   init(opts) {
     PTL.auth.init();
     PTL.browser.init();
+    PTL.fs.init();
 
     $(window).load(() => {
       $('body').removeClass('preload');
---
+++ pootle/static/js/fs.js
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) Pootle contributors.
+ *
+ * This file is a part of the Pootle project. It is distributed under the GPL3
+ * or later license. See the LICENSE file for a copy of the license and the
+ * AUTHORS file for copyright and authorship information.
+ */
+
+import $ from 'jquery';
+
+
+const fs = {
+  init() {
+    const choices = {};
+    $.each($('.js-fs-preselect-choices dt'), function () {
+      const k = $(this).text();
+      const v = $(this).next('dd').text();
+      if (v) {
+        choices[k] = v;
+      }
+    });
+    $('body').on('select2:select', '.js-select-fs-mapping', (e) => {
+      const mapping = choices[$(e.target).val()];
+      $('.js-select-fs-mapping-target').val(mapping);
+    });
+  },
+};
+
+export default fs;
