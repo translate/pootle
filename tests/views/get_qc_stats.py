fc9f39552f9904622d221ca5389400e0b2b7688c
Ryan Northey <ryan@synca.io>
2016-11-05 08:51:25 +0000

    Add xhr qc checks for other objects

--- tests/views/get_qc_stats.py
+++ tests/views/get_qc_stats.py
@@ -10,7 +10,9 @@ import json
 
 import pytest
 
-from pootle_store.models import Unit
+from pootle_app.models import Directory
+from pootle_project.models import Project, ProjectResource, ProjectSet
+from pootle_store.models import Store, Unit
 
 
 @pytest.mark.django_db
@@ -68,5 +70,113 @@ def test_get_qc_stats_tp(client, request_users, settings):
         HTTP_X_REQUESTED_WITH='XMLHttpRequest')
     result = json.loads(response.content)
     checks = tp.data_tool.get_checks() or {}
+    for k, v in result.items():
+        assert checks[k] == v
+
+
+@pytest.mark.django_db
+def test_get_qc_stats_language(client, request_users, settings):
+    user = request_users["user"]
+    if user.username != "nobody":
+        client.login(
+            username=user.username,
+            password=request_users["password"])
+    unit = (
+        Unit.objects.get_translatable(user)
+                    .first())
+    language = unit.store.translation_project.language
+    response = client.get(
+        "/xhr/stats/checks/?path=%s" % language.pootle_path,
+        HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+    result = json.loads(response.content)
+    checks = language.data_tool.get_checks() or {}
+    for k, v in result.items():
+        assert checks[k] == v
+
+
+@pytest.mark.django_db
+def test_get_qc_stats_projects(client, request_users, settings):
+    user = request_users["user"]
+    if user.username != "nobody":
+        client.login(
+            username=user.username,
+            password=request_users["password"])
+    user_projects = (
+        Project.objects.for_user(user)
+                       .select_related("directory"))
+    project_set = ProjectSet(user_projects)
+    response = client.get(
+        "/xhr/stats/checks/?path=/projects/",
+        HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+    result = json.loads(response.content)
+    checks = project_set.data_tool.get_checks() or {}
+    for k, v in result.items():
+        assert checks[k] == v
+
+
+@pytest.mark.django_db
+def test_get_qc_stats_project(client, request_users, settings):
+    user = request_users["user"]
+    if user.username != "nobody":
+        client.login(
+            username=user.username,
+            password=request_users["password"])
+    unit = (
+        Unit.objects.get_translatable(user)
+                    .first())
+    project = unit.store.translation_project.project
+    response = client.get(
+        "/xhr/stats/checks/?path=/projects/%s/" % project.code,
+        HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+    result = json.loads(response.content)
+    checks = project.data_tool.get_checks() or {}
+    for k, v in result.items():
+        assert checks[k] == v
+
+
+@pytest.mark.django_db
+def test_get_qc_stats_project_dir(client, request_users, settings, subdir0):
+    user = request_users["user"]
+    if user.username != "nobody":
+        client.login(
+            username=user.username,
+            password=request_users["password"])
+    project = subdir0.tp.project
+    dirs = Directory.objects.live().filter(tp__project=project)
+    resources = (
+        dirs.exclude(pootle_path__startswith="/templates")
+            .filter(tp_path=subdir0.tp_path))
+    resource_path = "/projects/%s%s" % (project.code, subdir0.tp_path)
+    projectres = ProjectResource(resources, resource_path)
+    response = client.get(
+        "/xhr/stats/checks/?path=%s" % resource_path,
+        HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+    result = json.loads(response.content)
+    checks = projectres.data_tool.get_checks() or {}
+    for k, v in result.items():
+        assert checks[k] == v
+
+
+@pytest.mark.django_db
+def test_get_qc_stats_project_store(client, request_users, settings, subdir0):
+    user = request_users["user"]
+    if user.username != "nobody":
+        client.login(
+            username=user.username,
+            password=request_users["password"])
+    store = subdir0.child_stores.first()
+    project = subdir0.tp.project
+    resources = (
+        Store.objects.live()
+                     .select_related("translation_project__language")
+                     .filter(translation_project__project=project)
+                     .filter(tp_path=store.tp_path))
+    resource_path = "/projects/%s%s" % (project.code, store.tp_path)
+    projectres = ProjectResource(resources, resource_path)
+    response = client.get(
+        "/xhr/stats/checks/?path=%s" % resource_path,
+        HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+    result = json.loads(response.content)
+    checks = projectres.data_tool.get_checks() or {}
     for k, v in result.items():
         assert checks[k] == v
