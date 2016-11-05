941a753813558fed3eeafe33cd6f304be96a07a0
Ryan Northey <ryan@synca.io>
2016-11-05 08:54:11 +0000

    Remove get_resource tests

--- tests/core/decorators.py
+++ tests/core/decorators.py
@@ -11,12 +11,9 @@ import pytest
 from django.http import Http404
 
 from pootle.core.cache import get_cache
-from pootle.core.decorators import (
-    get_path_obj, get_resource, persistent_property)
-from pootle_app.models import Directory
+from pootle.core.decorators import get_path_obj, persistent_property
 from pootle_language.models import Language
-from pootle_project.models import Project, ProjectResource
-from pootle_store.models import Store
+from pootle_project.models import Project
 from pootle_translationproject.models import TranslationProject
 
 
@@ -121,97 +118,6 @@ def test_get_path_obj_disabled(rf, default, admin,
     with pytest.raises(Http404):
         func(request, language_code=language_code_obsolete,
              project_code=project_code_obsolete)
-
-
-@pytest.mark.django_db
-def test_get_resource_tp(rf, default, tp0):
-    """Tests that the correct resources are set for the given TP contexts."""
-    store_name = 'store0.po'
-    subdir_name = 'subdir0/'
-
-    subdir_name_fake = 'fake_subdir/'
-    store_name_fake = 'fake_store.po'
-
-    request = rf.get('/')
-    request.user = default
-
-    # Fake decorated function
-    func = get_resource(lambda x, y, s, t: (x, y, s, t))
-
-    # TP, no resource
-    func(request, tp0, '', '')
-    assert isinstance(request.resource_obj, TranslationProject)
-
-    # TP, file resource
-    func(request, tp0, '', store_name)
-    assert isinstance(request.resource_obj, Store)
-
-    # TP, directory resource
-    func(request, tp0, subdir_name, '')
-    assert isinstance(request.resource_obj, Directory)
-
-    # TP, missing file/dir resource, redirects to parent resource
-    response = func(request, tp0, '', store_name_fake)
-    assert response.status_code == 302
-    assert tp0.pootle_path in response.get('location')
-
-    response = func(request, tp0, subdir_name, store_name_fake)
-    assert response.status_code == 302
-    assert (''.join([tp0.pootle_path, subdir_name]) in
-            response.get('location'))
-
-    response = func(request, tp0, subdir_name_fake, '')
-    assert response.status_code == 302
-    assert tp0.pootle_path in response.get('location')
-
-
-@pytest.mark.django_db
-def test_get_resource_project(rf, default, project0, tp0):
-    """Tests that the correct resources are set for the given Project
-    contexts.
-    """
-    store_name = 'store0.po'
-    subdir_name = 'subdir0/'
-
-    request = rf.get('/')
-    request.user = default
-
-    # Fake decorated function
-    func = get_resource(lambda x, y, s, t: (x, y, s, t))
-
-    # Project, no resource
-    func(request, project0, '', '')
-    assert isinstance(request.resource_obj, Project)
-
-    # Project, cross-language file resource
-    func(request, project0, '', store_name)
-    assert isinstance(request.resource_obj, ProjectResource)
-
-    # Two languages had this file, but it was marked as obsolete for the Arabic
-    # language!
-    # Should only contain a single file resource
-    assert (
-        len(request.resource_obj.resources)
-        == len(
-            Store.objects.filter(
-                translation_project__project__code=project0.code,
-                name="store0.po")))
-    assert isinstance(request.resource_obj.resources[0], Store)
-
-    # Project, cross-language directory resource
-    func(request, project0, subdir_name, '')
-    assert isinstance(request.resource_obj, ProjectResource)
-
-    # Two languages have this dir, but it was marked as obsolete for the Arabic
-    # language!
-    # Should only contain a single dir resource
-    assert (
-        len(request.resource_obj.resources)
-        == len(
-            Directory.objects.filter(
-                name=subdir_name.rstrip("/"),
-                parent__translationproject__project=project0)))
-    assert isinstance(request.resource_obj.resources[0], Directory)
 
 
 @pytest.mark.django
