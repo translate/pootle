a06336ae13d88310e4b9fbb624e353446de1699f
Ryan Northey <ryan@synca.io>
2016-11-09 09:30:56 +0000

    Add tests for progress_bar inclusion_tag

---
+++ tests/pootle_misc/templatetags.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+import pytest
+
+from django.template import Context, Template
+
+
+def _render_str(string, context=None):
+    context = context or {}
+    context = Context(context)
+    return Template(string).render(context)
+
+
+@pytest.mark.django
+def test_templatetag_progress_bar():
+    rendered = _render_str("{% load common_tags %}{% progress_bar 0 0 0 %}")
+    assert "<span class=\'value translated\'>0</span>" in rendered
+    assert '<span class=\'value fuzzy\'>0</span>%' in rendered
+    assert '<span class=\'value untranslated\'>0</span>%' in rendered
+    rendered = _render_str(
+        "{% load common_tags %}{% progress_bar 123 23 73 %}")
+    assert "<span class=\'value translated\'>59.0</span>%" in rendered
+    assert "<span class=\'value fuzzy\'>19.0</span>%" in rendered
+    assert "<span class=\'value untranslated\'>22.0</span>%" in rendered
+    assert '<td class="translated" style="width: 59.0%">' in rendered
+    assert '<td class="untranslated" style="width: 22.0%">' in rendered
+    assert '<td class="fuzzy" style="width: 19.0%">' in rendered
