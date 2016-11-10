827e63177f6cb837cad27fb62af0182cb7214537
Ryan Northey <ryan@synca.io>
2016-11-10 07:59:07 +0000

    Remove autorefresh from browse view

--- pootle/core/views/browse.py
+++ pootle/core/views/browse.py
@@ -6,7 +6,6 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
-from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.utils.functional import cached_property
 
@@ -116,8 +115,6 @@ class PootleBrowseView(PootleDetailView):
 
         ctx.update(
             {'page': 'browse',
-             'stats_refresh_attempts_count':
-                 settings.POOTLE_STATS_REFRESH_ATTEMPTS_COUNT,
              'stats': self.stats,
              'translation_states': get_translation_states(self.object),
              'checks': self.checks,
