98690ba133a23e8180ceea36a870d0110cd1a73c
Ryan Northey <ryan@synca.io>
2016-11-13 16:47:01 +0000

    Add base Panel and TablePanel implementations

---
+++ pootle/core/views/panels.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+from django.template import loader
+
+from pootle.core.decorators import persistent_property
+
+
+class Panel(object):
+    template_name = None
+    panel_name = None
+
+    def __init__(self, view):
+        self.view = view
+
+    def get_context_data(self):
+        return {}
+
+    def render(self):
+        if not self.template_name:
+            return ""
+        return loader.render_to_string(
+            self.template_name, self.get_context_data())
+
+    @property
+    def cache_key(self):
+        return (
+            "panel.%s.%s"
+            % (self.panel_name, self.view.cache_key))
+
+    @persistent_property
+    def content(self):
+        return self.render()
+
+
+class TablePanel(Panel):
+    template_name = "browser/includes/table_panel.html"
+    table = None
+
+    def get_context_data(self):
+        return dict(table=self.table)
