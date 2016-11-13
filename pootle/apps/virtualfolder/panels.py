214a575498dee4807c1fb35c66f5507885a59914
Ryan Northey <ryan@synca.io>
2016-11-13 16:51:38 +0000

    Add vfolders panel

---
+++ pootle/apps/virtualfolder/panels.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+from pootle.core.views.panels import TablePanel
+
+
+class VFolderPanel(TablePanel):
+    panel_name = "vfolder"
+    table_fields = [
+        'name', 'progress', 'total', 'need-translation',
+        'suggestions', 'critical', 'last-updated', 'activity']
+
+    @property
+    def table(self):
+        if not self.view.vfolders_data_view:
+            return ""
+        vfdata = self.view.vfolders_data_view.table_data
+        if not vfdata:
+            return ""
+        return vfdata["children"]
--- pootle/apps/virtualfolder/providers.py
+++ pootle/apps/virtualfolder/providers.py
@@ -6,12 +6,19 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
-from pootle.core.delegate import url_patterns
+from pootle.core.delegate import panels, url_patterns
 from pootle.core.plugin import provider
+from pootle_translationproject.views import TPBrowseView
 
+from .panels import VFolderPanel
 from .urls import urlpatterns
 
 
 @provider(url_patterns)
 def vf_url_provider(**kwargs_):
     return dict(vfolders=urlpatterns)
+
+
+@provider(panels, sender=TPBrowseView)
+def vf_panel_provider(**kwargs_):
+    return dict(vfolders=VFolderPanel)
