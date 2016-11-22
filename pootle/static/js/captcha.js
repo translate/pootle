c20d8f8c76c91f0b695a0b836f1a34edc0e4f76e
Taras Semenenko <taras.semenenko@gmail.com>
2016-11-22 16:30:47 +0300

    Use JS object to keep POST data from ajax requests instead if fillin captcha form

--- pootle/static/js/captcha.js
+++ pootle/static/js/captcha.js
@@ -10,6 +10,8 @@ import $ from 'jquery';
 import 'jquery-magnific-popup';
 import 'jquery-serializeObject';
 
+import assign from 'object-assign';
+
 import utils from './utils';
 
 
@@ -25,7 +27,7 @@ function onError(xhr, errorFn) {
 function onSubmit(e) {
   e.preventDefault();
   const $form = $(this);
-  const reqData = $form.serializeObject();
+  const reqData = assign(PTL.captcha.postData, $form.serializeObject());
   const successFn = reqData.sfn;
   const errorFn = reqData.efn;
   const url = $form.attr('action');
@@ -60,4 +62,5 @@ function display(html) {
 
 export default {
   onError,
+  postData: {},
 };
