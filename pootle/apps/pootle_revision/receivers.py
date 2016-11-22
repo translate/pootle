1a3607309e14fce8c3bad75ca03a4d9a4832e1de
Ryan Northey <ryan@synca.io>
2016-11-22 11:48:14 +0000

    Fix: add receivers to expire directory caches, fixes #5591

--- pootle/apps/pootle_revision/receivers.py
+++ pootle/apps/pootle_revision/receivers.py
@@ -6,10 +6,11 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 
 from pootle.core.delegate import revision_updater
+from pootle_app.models import Directory
 from pootle_data.models import StoreData
 from pootle_store.models import Store
 
@@ -18,3 +19,17 @@ from pootle_store.models import Store
 def handle_storedata_save(**kwargs):
     revision_updater.get(Store)(
         context=kwargs["instance"].store).update(keys=["stats", "checks"])
+
+
+@receiver(post_save, sender=Directory)
+def handle_directory_save(**kwargs):
+    if kwargs.get("created"):
+        return
+    revision_updater.get(Directory)(
+        context=kwargs["instance"]).update(keys=["stats", "checks"])
+
+
+@receiver(pre_delete, sender=Directory)
+def handle_directory_delete(**kwargs):
+    revision_updater.get(Directory)(
+        context=kwargs["instance"].parent).update(keys=["stats", "checks"])
