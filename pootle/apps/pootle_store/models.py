ce8eb07918ee889a1e846dee9948014664495d1f
Dwayne Bailey <dwayne@translate.org.za>
2016-11-23 16:18:47 +0000

    Make Submission.creation_time timezone aware

--- pootle/apps/pootle_store/models.py
+++ pootle/apps/pootle_store/models.py
@@ -901,7 +901,7 @@ class Unit(models.Model, base.TranslationUnit):
         else:
             sub_type = SubmissionTypes.UNMUTE_CHECK
 
-        sub = Submission(creation_time=timezone.now(),
+        sub = Submission(creation_time=make_aware(timezone.now()),
                          translation_project=self.store.translation_project,
                          submitter=user, field=SubmissionFields.NONE,
                          unit=self, store=self.store, type=sub_type,
