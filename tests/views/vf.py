e760346d01fe116ff7016351d5a922e43c98a7c8
Ryan Northey <ryan@synca.io>
2016-11-23 12:04:02 +0000

    Adjust vfolder translate view tests to properly check check_data

--- tests/views/vf.py
+++ tests/views/vf.py
@@ -7,12 +7,90 @@
 # AUTHORS file for copyright and authorship information.
 
 import json
+from collections import OrderedDict
 
 import pytest
 
 from pytest_pootle.search import calculate_search_results
 
 from django.urls import resolve, reverse
+
+from pootle_app.models.permissions import check_permission
+from pootle.core.url_helpers import get_previous_url, get_path_parts
+from pootle_misc.checks import (
+    CATEGORY_IDS, check_names,
+    get_qualitychecks, get_qualitycheck_schema)
+from pootle_misc.forms import make_search_form
+from virtualfolder.models import VirtualFolder
+from virtualfolder.utils import DirectoryVFDataTool
+
+
+def _test_vf_translate_view(tp, request, response, kwargs, settings):
+    from .tp import view_context_test
+
+    ctx = response.context
+    obj = ctx["object"]
+    kwargs["project_code"] = tp.project.code
+    kwargs["language_code"] = tp.language.code
+    resource_path = "%(dir_path)s%(filename)s" % kwargs
+    request_path = "%s%s" % (tp.pootle_path, resource_path)
+
+    checks = get_qualitychecks()
+    schema = {sc["code"]: sc for sc in get_qualitycheck_schema()}
+    vfolder_pk = response.context["current_vfolder_pk"]
+    check_data = DirectoryVFDataTool(obj).get_checks(
+        user=request.user).get(vfolder_pk, {})
+    _checks = {}
+    for check, checkid in checks.items():
+        if check not in check_data:
+            continue
+        _checkid = schema[checkid]["name"]
+        _checks[_checkid] = _checks.get(
+            _checkid, dict(checks=[], title=schema[checkid]["title"]))
+        _checks[_checkid]["checks"].append(
+            dict(
+                code=check,
+                title=check_names[check],
+                count=check_data[check]))
+    _checks = OrderedDict(
+        (k, _checks[k])
+        for k in CATEGORY_IDS.keys()
+        if _checks.get(k))
+    vfolder = VirtualFolder.objects.get(
+        name=request.resolver_match.kwargs["vfolder_name"])
+    current_vfolder_pk = vfolder.pk
+    display_priority = False
+    unit_api_root = reverse(
+        "vfolder-pootle-xhr-units",
+        kwargs=dict(vfolder_name=vfolder.name))
+    resource_path = (
+        "/".join(
+            ["++vfolder",
+             vfolder.name,
+             ctx['object'].pootle_path.replace(tp.pootle_path, "")]))
+    assertions = dict(
+        page="translate",
+        translation_project=tp,
+        language=tp.language,
+        project=tp.project,
+        has_admin_access=check_permission('administrate', request),
+        ctx_path=tp.pootle_path,
+        pootle_path=request_path,
+        resource_path=resource_path,
+        resource_path_parts=get_path_parts(resource_path),
+        editor_extends="translation_projects/base.html",
+        checks=_checks,
+        previous_url=get_previous_url(request),
+        current_vfolder_pk=current_vfolder_pk,
+        display_priority=display_priority,
+        cantranslate=check_permission("translate", request),
+        cansuggest=check_permission("suggest", request),
+        canreview=check_permission("review", request),
+        search_form=make_search_form(request=request),
+        unit_api_root=unit_api_root,
+        POOTLE_MT_BACKENDS=settings.POOTLE_MT_BACKENDS,
+        AMAGAMA_URL=settings.AMAGAMA_URL)
+    view_context_test(ctx, **assertions)
 
 
 @pytest.mark.django_db
@@ -65,10 +143,8 @@ def test_view_vf_xhr_edit_unit():
 @pytest.mark.pootle_vfolders
 @pytest.mark.django_db
 def test_views_vf_translate(vfolder_views, settings):
-    from tp import _test_translate_view
-
     test_type, tp, request, response, kwargs = vfolder_views
-    _test_translate_view(tp, request, response, kwargs, settings)
+    _test_vf_translate_view(tp, request, response, kwargs, settings)
 
 
 @pytest.mark.pootle_vfolders
