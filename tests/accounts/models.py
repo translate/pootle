38642ea662017d1480b89955e1bf8552565b0d3a
Ryan Northey <ryan@synca.io>
2016-11-24 18:37:56 +0000

    Add tests for user.last_event

---
+++ tests/accounts/models.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+import pytest
+
+from pytest_pootle.factories import UserFactory
+
+from pootle.core.views.display import ActionDisplay
+from pootle_statistics.models import Submission
+
+
+@pytest.mark.django_db
+def test_model_user_last_event(member):
+    last_submission = Submission.objects.filter(submitter=member).last()
+    last_event = member.last_event()
+    assert isinstance(last_event, ActionDisplay)
+    assert last_event.action == last_submission.get_submission_info()
+    user = UserFactory()
+    assert not user.last_event()
