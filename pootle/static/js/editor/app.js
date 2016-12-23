translate/pootle

commit 0ca12793b43931c95064794d7d98429b40acc866

Use ViewUnit component for editor context rows

pootle/static/js/editor/app.js
@@ -1245,6 +1245,7 @@ PTL.editor = {
 
     const rows = [];
     this.viewUnits = {};
+    this.viewCtxUnits = {};
 
     unitGroups.forEach((unitGroup) => {
       // Don't display a delimiter row if all units have the same origin
@@ -1279,12 +1280,9 @@ PTL.editor = {
 
     for (let i = 0; i < units.length; i++) {
       // FIXME: Please let's use proper models for context units
-      let unit = units[i];
-      unit = assign({}, currentUnit.toJSON(), unit);
-
-      rows += `<tr id="ctx${unit.id}" class="ctx-row ${extraCls}">`;
-      rows += this.tmpl.vUnit({ unit });
-      rows += '</tr>';
+      const unit = assign({}, currentUnit.toJSON(), units[i]);
+      this.viewCtxUnits[unit.id] = unit;
+      rows += `<tr data-id="${unit.id}" class="ctx-row ${extraCls}"><td></td><td></td></tr>`;
     }
 
     return rows;
@@ -1348,38 +1346,41 @@ PTL.editor = {
 
     if (newTbody !== undefined) {
       this.$editorBody.html(newTbody);
-      const rows = document.querySelectorAll('.view-row');
-      for (let i = 0; i < rows.length; i++) {
-        const unit = this.viewUnits[rows[i].dataset.id];
-        const sourceProps = {
-          id: unit.id,
-          url: unit.url,
-          dir: unit.store.source_dir,
-          language: unit.store.source_lang,
-          values: unit.source,
-          type: 'original',
-          fileType: unit.store.filetype,
-          hasPlurals: unit.source.length > 1,
-        };
-        const targetProps = {
-          id: unit.id,
-          url: unit.url,
-          isFuzzy: unit.isfuzzy,
-          dir: unit.store.target_dir,
-          language: unit.store.target_lang,
-          values: unit.target,
-          fileType: unit.store.filetype,
-          type: 'translation',
-          hasPlurals: unit.target.length > 1,
-        };
-
-        ReactEditor.renderViewUnitComponent(sourceProps, rows[i].cells[0]);
-        ReactEditor.renderViewUnitComponent(targetProps, rows[i].cells[1]);
-      }
+      this.renderViewRowValues('.view-row', this.viewUnits);
       this.ready();
     }
   },
 
+  renderViewRowValues(selector, units) {
+    const rows = document.querySelectorAll(selector);
+    for (let i = 0; i < rows.length; i++) {
+      const unit = units[rows[i].dataset.id];
+      const sourceProps = {
+        id: unit.id,
+        url: unit.url,
+        dir: unit.store.source_dir,
+        language: unit.store.source_lang,
+        values: unit.source,
+        type: 'original',
+        fileType: unit.store.filetype,
+        hasPlurals: unit.source.length > 1,
+      };
+      const targetProps = {
+        id: unit.id,
+        url: unit.url,
+        isFuzzy: unit.isfuzzy,
+        dir: unit.store.target_dir,
+        language: unit.store.target_lang,
+        values: unit.target,
+        fileType: unit.store.filetype,
+        type: 'translation',
+        hasPlurals: unit.target.length > 1,
+      };
+
+      ReactEditor.renderViewUnitComponent(sourceProps, rows[i].cells[0]);
+      ReactEditor.renderViewUnitComponent(targetProps, rows[i].cells[1]);
+    }
+  },
 
   /* Updates a button in `selector` to the `disable` state */
   updateNavButton($button, disable) {
@@ -1938,6 +1939,8 @@ PTL.editor = {
     const editCtxRows = $('tr.edit-ctx');
     editCtxRows.first().after(before);
     editCtxRows.last().before(after);
+    this.renderViewRowValues('.ctx-row', this.viewCtxUnits);
+
     return undefined;
   },
 

pootle/static/css/editor.css
@@ -1067,7 +1067,8 @@ html[dir="rtl"] .suggestion-original
 }
 
 #suggestions .plural-form-label,
-tr.view-row .plural-form-label
+tr.view-row .plural-form-label,
+tr.ctx-row .plural-form-label
 {
     margin-right: 5px;
     vertical-align: top;
@@ -1076,7 +1077,9 @@ tr.view-row .plural-form-label
 #suggestions .plural-form-label,
 #suggestions .suggestion-translation,
 tr.view-row .plural-form-label,
-tr.view-row .view-unit-value
+tr.view-row .view-unit-value,
+tr.ctx-row .plural-form-label,
+tr.ctx-row .view-unit-value
 {
     display: inline-block;
 }

commit 939c552e9decc72b0ec32994e48e6c3140b9604e

Fix view and ctx rows click handler behaviour
- allow to select text
- allow right click
which was broken in 0ca12793

pootle/static/js/editor/app.js
@@ -1212,7 +1212,15 @@ PTL.editor = {
 
   /* Renders a single row */
   renderRow(unitId) {
-    return (`<tr id="row${unitId}" class="view-row" data-id="${unitId}"><td></td><td></td></tr>`);
+    const unit = this.viewUnits[unitId];
+    return (`
+      <tr
+        id="row${unitId}"
+        class="view-row"
+        data-id="${unitId}"
+        data-target="${unit.url}"
+      ><td></td><td></td></tr>
+    `);
   },
 
   renderEditorRow(unit) {
@@ -1276,7 +1284,14 @@ PTL.editor = {
       // FIXME: Please let's use proper models for context units
       const unit = assign({}, currentUnit.toJSON(), units[i]);
       this.viewCtxUnits[unit.id] = unit;
-      rows += `<tr data-id="${unit.id}" class="ctx-row ${extraCls}"><td></td><td></td></tr>`;
+      rows += (`
+        <tr
+          id="ctx${unit.id}"
+          data-id="${unit.id}"
+          data-target="${unit.url}"
+          class="ctx-row ${extraCls}"
+        ><td></td><td></td></tr>
+      `);
     }
 
     return rows;
@@ -1350,8 +1365,6 @@ PTL.editor = {
     for (let i = 0; i < rows.length; i++) {
       const unit = units[rows[i].dataset.id];
       const sourceProps = {
-        id: unit.id,
-        url: unit.url,
         dir: unit.store.source_dir,
         language: unit.store.source_lang,
         values: unit.source,
@@ -1360,8 +1373,6 @@ PTL.editor = {
         hasPlurals: unit.source.length > 1,
       };
       const targetProps = {
-        id: unit.id,
-        url: unit.url,
         isFuzzy: unit.isfuzzy,
         dir: unit.store.target_dir,
         language: unit.store.target_lang,
@@ -1705,8 +1716,8 @@ PTL.editor = {
 
     // Ctrl + click / Alt + click / Cmd + click / Middle click opens a new tab
     if (e.ctrlKey || e.altKey || e.metaKey || e.which === 2) {
-      const $el = e.target.nodeName !== 'TD' ?
-                  $(e.target).parents('td') :
+      const $el = e.target.nodeName !== 'TR' ?
+                  $(e.target).parents('tr') :
                   $(e.target);
       window.open($el.data('target'), '_blank');
       return false;

pootle/static/js/editor/components/ViewUnit.js
@@ -29,8 +29,6 @@ InnerDiv.propTypes = {
 const ViewUnit = React.createClass({
 
   propTypes: {
-    id: PropTypes.number.isRequired,
-    url: PropTypes.string.isRequired,
     dir: PropTypes.string.isRequired,
     isFuzzy: PropTypes.bool,
     language: PropTypes.string.isRequired,
@@ -83,8 +81,6 @@ const ViewUnit = React.createClass({
       <div
         dir={this.props.dir}
         className={classNames}
-        id={`${this.props.type}${this.props.id}`}
-        data-target={this.props.url}
       >
         {this.props.values.map(this.createValue)}
       </div>