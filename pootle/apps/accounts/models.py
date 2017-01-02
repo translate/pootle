7ddbd5d0b9e0a6aab8dfc86645347f92b6b9f2a4
Ryan Northey <ryan@synca.io>
2017-01-02 13:15:43 +0000

    Cleanup: remove unused user property

--- pootle/apps/accounts/models.py
+++ pootle/apps/accounts/models.py
@@ -119,14 +119,6 @@ class User(AbstractBaseUser):
                 else self.get_short_name())
 
     @property
-    def formatted_name(self):
-        """Full name (username)"""
-        if self.get_full_name():
-            return "%s (%s)" % (self.get_full_name(), self.get_short_name())
-
-        return self.get_short_name()
-
-    @property
     def public_score(self):
         return formatter.number(round(self.score))
 
