30bbabc076851dd7cd2b324a51a648f290cc859f
Ryan Northey <ryan@synca.io>
2017-01-02 15:19:03 +0000

    Add UserTPScore model tests

---
+++ tests/pootle_score/models.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+import pytest
+
+from django.db.utils import IntegrityError
+from django.utils import timezone
+
+from pootle_score.models import UserTPScore
+
+
+@pytest.mark.django_db
+def test_user_tp_score_repr(tp0, member):
+    score = UserTPScore.objects.create(
+        user=member, tp=tp0, score=2, date=timezone.now().date())
+    score_info = (
+        "%s(%s) %s score: %s, suggested: %s, translated: %s, reviewed: %s"
+        % (score.user.username,
+           score.tp.pootle_path,
+           score.date,
+           score.score,
+           score.suggested,
+           score.translated,
+           score.reviewed))
+    assert str(score) == score_info
+    assert repr(score) == u"<UserTPScore: %s>" % score_info
+
+
+@pytest.mark.django_db
+def test_user_tp_score_instance(tp0, member):
+    theday = timezone.now().date()
+    score = UserTPScore.objects.create(
+        user=member, tp=tp0, score=2, date=theday)
+    assert score.user == member
+    assert score.tp == tp0
+    assert score.date == theday
+    assert score.score is 2
+    assert score.suggested is 0
+    assert score.translated is 0
+    assert score.reviewed is 0
+    score.score = -3
+    score.suggested = 1
+    score.translated = 4
+    score.reviewed = 10005
+    score.save()
+
+
+@pytest.mark.django_db
+def test_user_tp_score_bad_no_user(tp0, member):
+    with pytest.raises(IntegrityError):
+        UserTPScore.objects.create(tp=tp0, score=2, date=timezone.now().date())
+
+
+@pytest.mark.django_db
+def test_user_tp_score_bad_no_tp(tp0, member):
+    with pytest.raises(IntegrityError):
+        UserTPScore.objects.create(
+            user=member, score=2, date=timezone.now().date())
+
+
+@pytest.mark.django_db
+def test_user_tp_score_bad_no_date(tp0, member):
+    with pytest.raises(IntegrityError):
+        UserTPScore.objects.create(
+            user=member, tp=tp0, score=2)
+
+
+@pytest.mark.django_db
+def test_user_tp_score_bad_dupe(tp0, member):
+    theday = timezone.now().date()
+    UserTPScore.objects.create(
+        user=member, tp=tp0, score=2, date=theday)
+    with pytest.raises(IntegrityError):
+        UserTPScore.objects.create(
+            user=member, tp=tp0, score=3, date=theday)
