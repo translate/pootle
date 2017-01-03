685d77d1df2c5eae8762d78ba8554bb38f29b5a2
Ryan Northey <ryan@synca.io>
2017-01-03 09:55:46 +0000

    Add welcome view with persistent stats caching

--- pootle/apps/pootle_app/views/index/index.py
+++ pootle/apps/pootle_app/views/index/index.py
@@ -7,15 +7,60 @@
 # AUTHORS file for copyright and authorship information.
 
 from django.contrib.auth import REDIRECT_FIELD_NAME
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect
 from django.urls import reverse
+from django.utils.functional import cached_property
+from django.utils.translation import get_language
 from django.views.generic import TemplateView
 
+from pootle.core.decorators import persistent_property
+from pootle.core.delegate import revision, scores
 from pootle.i18n.override import get_lang_from_http_header
 from pootle_language.models import Language
+from pootle_project.models import Project, ProjectSet
 
 
 COOKIE_NAME = 'pootle-language'
+
+
+class WelcomeView(TemplateView):
+    ns = "pootle.web.welcome"
+    template_name = "welcome.html"
+
+    @property
+    def revision(self):
+        return revision.get(self.project_set.directory.__class__)(
+            self.project_set.directory).get(key="stats")
+
+    @property
+    def cache_key(self):
+        return (
+            "%s.%s.%s"
+            % (self.request.user.username,
+               self.revision,
+               self.request_lang))
+
+    @cached_property
+    def project_set(self):
+        user_projects = Project.accessible_by_user(self.request.user)
+        user_projects = (
+            Project.objects.for_user(self.request.user)
+                           .filter(code__in=user_projects))
+        return ProjectSet(user_projects)
+
+    @property
+    def request_lang(self):
+        return get_language()
+
+    @persistent_property
+    def score_data(self):
+        return scores.get(ProjectSet)(
+            self.project_set).display(language=self.request_lang)
+
+    def get_context_data(self, **kwargs):
+        context = super(WelcomeView, self).get_context_data(**kwargs)
+        context.update(dict(score_data=self.score_data))
+        return context
 
 
 def view(request):
@@ -23,7 +68,7 @@ def view(request):
         ctx = {
             'next': request.GET.get(REDIRECT_FIELD_NAME, ''),
         }
-        return render(request, 'welcome.html', ctx)
+        return WelcomeView.as_view()(request, ctx)
 
     lang = request.COOKIES.get(COOKIE_NAME, None)
 
