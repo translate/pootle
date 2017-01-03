2314ea0380b150366b802cce24ecef10bad6aa09
Ovidiu-Florin BOGDAN <ovidiu.b13@gmail.com>
2017-01-03 21:27:43 +0200

    Enable support for sqlite

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# Default to MySQL
-: ${DATABASE:=mysql}
+# Default to SQLite3
+: ${DATABASE:=sqlite}
 : ${INSTALL_DIR:=/pootle}
 : ${CONFIG_DIR:=/config}
+
+DB_INIT_PARAMS="--db $DATABASE"
 
 if [[ "$DATABASE" == "mysql" ]]; then
     : ${POOTLE_DB_USER:=${DBSERVER_ENV_MYSQL_USER:-root}}
@@ -13,26 +15,32 @@ if [[ "$DATABASE" == "mysql" ]]; then
     fi
     # Default to "POOTLE"
     : ${POOTLE_DB_NAME:=${DBSERVER_ENV_MYSQL_DATABASE:-pootledb}}
+
+    if [ -z "$POOTLE_DB_PASSWORD" ]; then
+        echo >&2 'error: missing required POOTLE_DB_PASSWORD environment variable'
+        echo >&2 '  Did you forget to -e POOTLE_DB_PASSWORD=... ?'
+        echo >&2
+        echo >&2 '  (Also of interest might be POOTLE_DB_USER and POOTLE_DB_NAME.)'
+        exit 1
+    fi
+
+    : "${POOTLE_DB_HOSTNAME:=dbserver}"
+
+    DB_INIT_PARAMS="$DB_INIT_PARAMS \
+        --db-name $POOTLE_DB_NAME \
+        --db-user $POOTLE_DB_USER \
+        --db-host $POOTLE_DB_HOSTNAME"
+
 elif [[ "$DATABASE" == "postgresql" ]]; then
     echo 'PostgreSQL was chosen as database backend.'
     echo >&2 'ERROR: PostgreSQL support not implemented yet in Docker image.'
     exit 2
-elif [[ "$DATABASE" == "sqlite3" ]]; then
+elif [[ "$DATABASE" == "sqlite" ]]; then
     echo 'SQLite3 was chosen as database backend.'
-    echo >&2 'ERROR: SQLite3 support not implemented yet in Docker image.'
-    exit 2
 fi
 
-if [ -z "$POOTLE_DB_PASSWORD" ]; then
-    echo >&2 'error: missing required POOTLE_DB_PASSWORD environment variable'
-    echo >&2 '  Did you forget to -e POOTLE_DB_PASSWORD=... ?'
-    echo >&2
-    echo >&2 '  (Also of interest might be POOTLE_DB_USER and POOTLE_DB_NAME.)'
-    exit 1
-fi
 
-: "${POOTLE_DB_HOSTNAME:=dbserver}"
-: "${DOMAIN:=""}" # By defaulting this empty we set ALLOWED_HOSTS to '*', thus avoiding "Bad request 400" when accessed form non "localhost"
+: "${DOMAIN:="*"}" # set ALLOWED_HOSTS to '*'
 
 CONFIG_FILE="$CONFIG_DIR/pootle.conf"
 
@@ -40,11 +48,7 @@ if [ ! -f $CONFIG_FILE ]; then
     cd $INSTALL_DIR \
     && . bin/activate || exit 3
     echo "Creating initial Pootle configuration..."
-    pootle init --config $CONFIG_FILE \
-        --db $DATABASE \
-        --db-name $POOTLE_DB_NAME \
-        --db-user $POOTLE_DB_USER \
-        --db-host $POOTLE_DB_HOSTNAME || exit 3
+    pootle init --config $CONFIG_FILE $DB_INIT_PARAMS || exit 3
     echo "Created initial Pootle configuration. See documentation for help on customising the settings: http://docs.translatehouse.org/projects/pootle/en/stable-2.7.6/server/settings.html"
     # exit virtualenv
     deactivate || exit 3
