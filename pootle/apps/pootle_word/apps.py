462e80fe110eb8fee0572eb362b2dc3909aa12de
Ryan Northey <ryan@synca.io>
2017-01-06 13:58:31 +0000

    Add pootle_word boilerplate

---
+++ pootle/apps/pootle_word/apps.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+import importlib
+
+from django.apps import AppConfig
+
+
+class PootleWordConfig(AppConfig):
+
+    name = "pootle_word"
+    verbose_name = "Pootle Word"
+    version = "0.0.1"
+
+    def ready(self):
+        importlib.import_module("pootle_word.models")
