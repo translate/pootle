5cb1cf02662fdfd1eaa08ad5af2da9e81a23353c
Ryan Northey <ryan@synca.io>
2017-01-06 15:26:44 +0000

    Cleanup: move import

--- pootle/apps/pootle_word/getters.py
+++ pootle/apps/pootle_word/getters.py
@@ -6,10 +6,10 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
+from stemming.porter2 import stem
+
 from pootle.core.delegate import stemmer
 from pootle.core.plugin import getter
-
-from stemming.porter2 import stem
 
 
 @getter(stemmer)
