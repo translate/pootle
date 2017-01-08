9867ec8abcfd1290e726f539b0b296fc4b756da3
Ryan Northey <ryan@synca.io>
2017-01-08 07:35:11 +0000

    Add tests for stopwords util

--- tests/pootle_word/utils.py
+++ tests/pootle_word/utils.py
@@ -6,10 +6,27 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
+import os
+
 from stemming.porter2 import stem
 
-from pootle.core.delegate import stemmer
+import translate
+
+from pootle.core.delegate import stemmer, stopwords
 
 
 def test_stemmer():
     assert stemmer.get() is stem
+
+
+def test_stopwords():
+    fpath = os.path.join(translate.__path__[0], "share", "stoplist-en")
+    words = set()
+    with open(fpath) as f:
+        for line in f.read().split("\n"):
+            if not line:
+                continue
+            if line[0] in "<>=@":
+                words.add(line[1:].strip().lower())
+    stops = stopwords.get()
+    assert stops.words == words
