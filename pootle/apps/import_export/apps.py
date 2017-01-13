89b883654f7fe9a2ab77678d9c418d0876d27083
Taras Semenenko <taras.semenenko@gmail.com>
2017-01-13 09:31:18 +0300

    Add import_export app config

--- pootle/apps/import_export/apps.py
+++ pootle/apps/import_export/apps.py
@@ -6,4 +6,15 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
-default_app_config = 'import_export.apps.PootleImportExportConfig'
+import importlib
+
+from django.apps import AppConfig
+
+
+class PootleImportExportConfig(AppConfig):
+
+    name = "import_export"
+    verbose_name = "Pootle Import Export"
+
+    def ready(self):
+        importlib.import_module("import_export.providers")
