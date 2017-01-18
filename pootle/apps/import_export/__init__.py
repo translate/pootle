8eb83260e0a3c478451710c8cc8057208551f8e8
Taras Semenenko <taras.semenenko@gmail.com>
2017-01-19 02:17:09 +0300

    Revert "Add import_export app config"
    This reverts commit 89b883654f7fe9a2ab77678d9c418d0876d27083.

--- pootle/apps/import_export/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-#
-# Copyright (C) Pootle contributors.
-#
-# This file is a part of the Pootle project. It is distributed under the GPL3
-# or later license. See the LICENSE file for a copy of the license and the
-# AUTHORS file for copyright and authorship information.
-
-default_app_config = 'import_export.apps.PootleImportExportConfig'
