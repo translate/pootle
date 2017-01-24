63bc0341961a2b9fa8f276b34f9479c8544035b2
Taras Semenenko <taras.semenenko@gmail.com>
2017-01-24 19:24:34 +0300

    Tests: adjust export tmx --rotate test to check unnecessary deletions

--- tests/commands/export.py
+++ tests/commands/export.py
@@ -117,23 +117,24 @@ def test_export_tmx_with_wrong_options(capfd):
 
 @pytest.mark.cmd
 @pytest.mark.django_db
-def test_export_tmx_tp_rotate(capfd, tp0, media_test_dir):
+def test_export_tmx_tp_rotate(capfd, tp0, project1, media_test_dir):
     """Export a tp"""
     lang_code = tp0.language.code
     prj_code = tp0.project.code
+    tp1 = project1.translationproject_set.get(language__code=lang_code)
     export_dir = os.path.join(media_test_dir, 'offline_tm')
     call_command('export', '--tmx', '--project=%s' % prj_code,
                  '--language=%s' % lang_code)
     out, err = capfd.readouterr()
 
-    def get_filename():
-        rev = revision.get(tp0.__class__)(tp0.directory).get(key="stats")
+    def get_filename(tp):
+        rev = revision.get(tp.__class__)(tp.directory).get(key="stats")
         filename = '%s.%s.%s.tmx.zip' % (
-            tp0.project.code,
-            tp0.language.code, rev[:10])
+            tp.project.code,
+            tp.language.code, rev[:10])
         return os.path.join(lang_code, filename)
 
-    filename_1 = get_filename()
+    filename_1 = get_filename(tp0)
     assert '%s" has been saved' % filename_1 in out
     assert os.path.exists(os.path.join(export_dir, filename_1))
 
@@ -144,9 +145,10 @@ def test_export_tmx_tp_rotate(capfd, tp0, media_test_dir):
                  '--language=%s' % lang_code)
     out, err = capfd.readouterr()
 
-    filename_2 = get_filename()
+    filename_2 = get_filename(tp0)
     assert '%s" has been saved' % filename_2 in out
     assert os.path.exists(os.path.join(export_dir, filename_2))
+    assert os.path.exists(os.path.join(export_dir, filename_1))
 
     unit = tp0.stores.first().units.first()
     unit.target_f = unit.target_f + ' CHANGED'
@@ -155,11 +157,21 @@ def test_export_tmx_tp_rotate(capfd, tp0, media_test_dir):
                  '--language=%s' % lang_code)
     out, err = capfd.readouterr()
 
-    filename_3 = get_filename()
+    filename_3 = get_filename(tp0)
     assert '%s" has been saved' % filename_3 in out
     assert '%s" has been removed' % filename_1 in out
     assert '%s" has been removed' % filename_2 not in out
 
     assert not os.path.exists(os.path.join(export_dir, filename_1))
+    assert os.path.exists(os.path.join(export_dir, filename_2))
+    assert os.path.exists(os.path.join(export_dir, filename_3))
+
+    call_command('export', '--tmx', '--rotate',
+                 '--project=%s' % tp1.project.code,
+                 '--language=%s' % tp1.language.code)
+    out, err = capfd.readouterr()
+    filename_for_tp1 = get_filename(tp1)
+    assert '%s" has been saved' % filename_for_tp1 in out
+    assert os.path.exists(os.path.join(export_dir, filename_for_tp1))
     assert os.path.exists(os.path.join(export_dir, filename_2))
     assert os.path.exists(os.path.join(export_dir, filename_3))
