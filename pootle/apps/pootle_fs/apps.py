59a69f8d0328bee8ae06db32272d5d7d130825f3
Ryan Northey <ryan@synca.io>
2017-01-25 11:41:57 +0000

    Bumping sw_versions for release

--- pootle/apps/pootle_fs/apps.py
+++ pootle/apps/pootle_fs/apps.py
@@ -15,7 +15,7 @@ class PootleFSConfig(AppConfig):
 
     name = "pootle_fs"
     verbose_name = "Pootle Filesystem synchronisation"
-    version = "0.0.2"
+    version = "0.0.3"
 
     def ready(self):
         importlib.import_module("pootle_fs.models")
