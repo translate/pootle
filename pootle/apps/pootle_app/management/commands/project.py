468b0bdafb0f4744097ee00b74af355fcfb6dc99
Taras Semenenko <taras.semenenko@gmail.com>
2017-01-26 12:15:55 +0300

    Add project cli command with update subcommand

---
+++ pootle/apps/pootle_app/management/commands/project.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+import logging
+import os
+
+# This must be run before importing the rest of the Django libs.
+os.environ['DJANGO_SETTINGS_MODULE'] = 'pootle.settings'
+
+from pootle.core.management.subcommands import CommandWithSubcommands
+
+from .project_commands import UpdateCommand
+
+
+logger = logging.getLogger()
+
+
+class Command(CommandWithSubcommands):
+    help = "Pootle project (via TPTool) API."
+    subcommands = {
+        "update": UpdateCommand,
+    }
