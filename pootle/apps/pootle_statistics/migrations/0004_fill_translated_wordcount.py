e35ee0f7eb1cf1c3522a639bc99a0aab040edba5
Ryan Northey <ryan@synca.io>
2017-01-27 08:06:47 +0000

    Disable old migration
    This migration used the model class directly, and caused future
    migrations to fail

--- pootle/apps/pootle_statistics/migrations/0004_fill_translated_wordcount.py
+++ pootle/apps/pootle_statistics/migrations/0004_fill_translated_wordcount.py
@@ -5,26 +5,8 @@ from django.db import migrations
 
 
 def set_translated_wordcount(apps, schema_editor):
-    from pootle_statistics.models import ScoreLog, TranslationActionCodes
-
-    scorelog_qs = ScoreLog.objects.select_related(
-        'submission',
-        'submission__suggestion',
-        'submission__unit'
-    ).filter(
-        action_code__in=[
-            TranslationActionCodes.NEW,
-            TranslationActionCodes.EDITED,
-            TranslationActionCodes.SUGG_ACCEPTED,
-            TranslationActionCodes.SUGG_REVIEWED_ACCEPTED,
-        ]
-    )
-    for scorelog in scorelog_qs.iterator():
-        translated = scorelog.get_paid_wordcounts()[0]
-        ScoreLog.objects.filter(id=scorelog.id).update(
-            translated_wordcount=translated
-        )
-
+    # this migration created some issues and has been removed
+    pass
 
 class Migration(migrations.Migration):
 
