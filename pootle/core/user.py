92d1822030eb094cbd0c974d009bfa9983944296
Ryan Northey <ryan@synca.io>
2017-02-02 21:46:27 +0000

    Add get_system_user_id function

--- pootle/core/user.py
+++ pootle/core/user.py
@@ -5,3 +5,7 @@ from django.contrib.auth import get_user_model
 
 def get_system_user():
     return get_user_model().objects.get_system_user()
+
+
+def get_system_user_id():
+    return get_system_user().id
