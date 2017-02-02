49b7bc1c6f54318f7d1125df50662f6ecb06aa9b
Ryan Northey <ryan@synca.io>
2017-02-02 08:11:06 +0000

    Remove old code for creating initial submission

--- pootle/apps/pootle_store/models.py
+++ pootle/apps/pootle_store/models.py
@@ -289,7 +289,6 @@ class Unit(AbstractUnit):
         if not hasattr(self, '_log_user'):
             User = get_user_model()
             self._log_user = User.objects.get_system_user()
-        user = kwargs.pop("user", self._log_user)
 
         if created:
             action = UNIT_ADDED
@@ -328,7 +327,7 @@ class Unit(AbstractUnit):
 
         if not created and action:
             action_log(
-                user=self._log_user,
+                user=self.submitted_by or self._log_user,
                 action=action,
                 lang=self.store.translation_project.language.code,
                 unit=self.id,
@@ -359,13 +358,12 @@ class Unit(AbstractUnit):
 
         if action and action == UNIT_ADDED:
             action_log(
-                user=self._log_user,
+                user=self.submitted_by or self._log_user,
                 action=action,
                 lang=self.store.translation_project.language.code,
                 unit=self.id,
                 translation=self.target_f,
                 path=self.store.pootle_path)
-            self.add_initial_submission(user=user)
 
         if source_updated or target_updated:
             if not (created and self.state == UNTRANSLATED):
@@ -416,19 +414,6 @@ class Unit(AbstractUnit):
         from pootle_project.models import Project
         user_projects = Project.accessible_by_user(user)
         return self.store.translation_project.project.code in user_projects
-
-    def add_initial_submission(self, user=None):
-        if self.istranslated() or self.isfuzzy():
-            Submission.objects.create(
-                creation_time=self.creation_time,
-                translation_project=self.store.translation_project,
-                submitter=user or self._log_user,
-                unit=self,
-                store=self.store,
-                type=SubmissionTypes.UNIT_CREATE,
-                field=SubmissionFields.TARGET,
-                new_value=self.target,
-            )
 
     @cached_property
     def unit_syncer(self):
@@ -1333,7 +1318,7 @@ class Store(models.Model, CachedTreeItem, base.TranslationStore):
         newunit.update(unit, user=user)
 
         if self.id:
-            newunit.save(revision=update_revision, user=user)
+            newunit.save(revision=update_revision)
         return newunit
 
     def findunits(self, source, obsolete=False):
