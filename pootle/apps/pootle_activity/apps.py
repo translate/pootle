aa374d18b431309748da3813363c3a1478c24ece
Ryan Northey <ryan@synca.io>
2017-02-07 14:04:05 +0000

    Add pootle_activity app

---
+++ pootle/apps/pootle_activity/apps.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+import importlib
+
+from django.apps import AppConfig
+
+
+class PootleActivityConfig(AppConfig):
+
+    name = "pootle_activity"
+    verbose_name = "Pootle Activity"
+
+    def ready(self):
+        importlib.import_module("pootle_activity.getters")
