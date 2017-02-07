8f8f81c2609572ee3cedf7fa228916ddfc1f0e6a
Taras Semenenko <taras.semenenko@gmail.com>
2017-02-07 20:31:32 +0300

    Test for project commands

--- tests/commands/project.py
+++ tests/commands/project.py
@@ -66,3 +66,83 @@ def test_project_update(capfd, language0, project0, project_foo):
         pootle_path=new_store.pootle_path.replace('project0', 'foo'))
 
     assert new_cloned_store
+
+
+@pytest.mark.cmd
+@pytest.mark.django_db
+def test_project_clone(capfd, language0, project_foo):
+    # fill foo project
+    project_foo.translationproject_set.create(language=language0)
+    call_command('project', 'update', 'project0', '--target-project=foo')
+    call_command('project', 'clone', 'foo', '--target-project=zoo',
+                 '--language=language0')
+
+    out, err = capfd.readouterr()
+    assert (u'Translation project "/language0/foo/" has been cloned into '
+            u'"/language0/zoo/"' in out)
+
+
+@pytest.mark.cmd
+@pytest.mark.django_db
+def test_project_wrong_clone(project_foo):
+    with pytest.raises(CommandError) as e:
+        call_command('project', 'clone', 'project0', '--target-project=foo')
+
+    assert 'Project <foo> already exists.' in e.value
+
+
+@pytest.mark.cmd
+@pytest.mark.django_db
+def test_project_clone_tp_into_existing_project(capfd, project_foo):
+    call_command('project', 'clone', 'project0', '--target-project=foo',
+                 '--language=language0')
+    out, err = capfd.readouterr()
+    assert (u'Translation project "/language0/project0/" has been cloned into '
+            u'"/language0/foo/"' in out)
+
+
+@pytest.mark.cmd
+@pytest.mark.django_db
+def test_project_clone_with_wrong_disk_dir(project_foo):
+    path = project_foo.get_real_path()
+    project_foo.delete()
+    with pytest.raises(CommandError) as e:
+        call_command('project', 'clone', 'project0', '--target-project=foo')
+
+    assert (u'Project <foo> code cannot be created from '
+            u'project <project0> because "%s" directory already exists.' %
+            path) in e.value
+
+
+@pytest.mark.cmd
+@pytest.mark.django_db
+def test_project_move(capfd, language0, project_foo):
+    # fill foo project
+    project_foo.translationproject_set.create(language=language0)
+    call_command('project', 'update', 'project0', '--target-project=foo')
+    call_command('project', 'move', 'foo', '--target-project=woo')
+
+    out, err = capfd.readouterr()
+    assert (u'Translation project "/language0/foo/" has been moved into '
+            u'"/language0/woo/"' in out)
+
+
+@pytest.mark.cmd
+@pytest.mark.django_db
+def test_project_move_tp(capfd):
+    call_command('project', 'move', 'project0', '--language=language0',
+                 '--target-project=foo')
+
+    out, err = capfd.readouterr()
+    assert (u'Translation project "/language0/project0/" has been moved into '
+            u'"/language0/foo/"' in out)
+
+
+@pytest.mark.cmd
+@pytest.mark.django_db
+def test_project_remove(capfd, project_foo):
+    foo = '%s' % project_foo
+    call_command('project', 'remove', 'foo')
+
+    out, err = capfd.readouterr()
+    assert (u'Project "%s" has been deleted' % foo) in out
