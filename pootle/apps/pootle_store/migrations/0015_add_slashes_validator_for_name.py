89d48a23940478a6e9294b0c67943ac303aeab10
Ryan Northey <ryan@synca.io>
2017-02-11 09:38:23 +0000

    Adjust location of store model validator in migration

--- pootle/apps/pootle_store/migrations/0015_add_slashes_validator_for_name.py
+++ pootle/apps/pootle_store/migrations/0015_add_slashes_validator_for_name.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-import pootle_store.models
+import pootle_store.validators
 
 
 class Migration(migrations.Migration):
@@ -15,6 +15,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='store',
             name='name',
-            field=models.CharField(max_length=128, editable=False, validators=[pootle_store.models.validate_no_slashes]),
+            field=models.CharField(max_length=128, editable=False, validators=[pootle_store.validators.validate_no_slashes]),
         ),
     ]
