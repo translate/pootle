eecee9a47483ca6fe1621f5ae6aea5931f43af14
Taras Semenenko <taras.semenenko@gmail.com>
2017-02-13 11:30:22 +0300

    Adjust tests

--- tests/commands/project.py
+++ tests/commands/project.py
@@ -6,7 +6,10 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
+import os
 import pytest
+
+from translate.filters.decorators import Category
 
 from django.core.management import CommandError, call_command
 
@@ -25,7 +28,7 @@ def test_wrong_project_update():
 
 @pytest.mark.cmd
 @pytest.mark.django_db
-def test_project_update(capfd, language0, project0, project_foo):
+def test_project_update(capfd, language0, project0, project_foo, admin):
     from pytest_pootle.factories import StoreDBFactory
 
     call_command('project', 'update', 'project0',
@@ -42,8 +45,12 @@ def test_project_update(capfd, language0, project0, project_foo):
 
     store = tp.stores.first()
     unit = store.units.first()
-    unit.target_f += ' CHANGED'
+    unit.target_f += '\n CHANGED'
     unit.save()
+    unit.update_qualitychecks()
+    check_id = unit.qualitycheck_set.filter(
+        category=Category.CRITICAL).values_list('id', flat=True).first()
+    unit.toggle_qualitycheck(check_id=check_id, false_positive=True, user=admin)
 
     new_store = StoreDBFactory(translation_project=tp)
 
@@ -70,16 +77,24 @@ def test_project_update(capfd, language0, project0, project_foo):
 
 @pytest.mark.cmd
 @pytest.mark.django_db
-def test_project_clone(capfd, language0, project_foo):
-    # fill foo project
-    project_foo.translationproject_set.create(language=language0)
-    call_command('project', 'update', 'project0', '--target-project=foo')
-    call_command('project', 'clone', 'foo', '--target-project=zoo',
-                 '--language=language0')
+def test_project_clone_tp(capfd, po_directory, tutorial, admin):
+    tp = tutorial.translationproject_set.get(language__code='language0')
+    unit = tp.stores.first().units.first()
+    unit.target_f = '%s\n%s' % (unit.source_f, unit.source_f)
+    # set and mute critical check
+    unit.save()
+    unit.update_qualitychecks()
+    check_id = unit.qualitycheck_set.filter(
+        category=Category.CRITICAL).values_list('id', flat=True).first()
+    unit.toggle_qualitycheck(check_id=check_id, false_positive=True, user=admin)
+    call_command('project', 'clone', 'tutorial', '--target-project=zoo',
+                 '--language=language0', '--target-language=language1')
 
     out, err = capfd.readouterr()
-    assert (u'Translation project "/language0/foo/" has been cloned into '
-            u'"/language0/zoo/"' in out)
+    assert (u'Translation project "/language0/tutorial/" has been cloned into '
+            u'"/language1/zoo/"' in out)
+    zoo = Project.objects.get(code='zoo')
+    assert os.listdir(zoo.get_real_path())
 
 
 @pytest.mark.cmd
@@ -131,18 +146,88 @@ def test_project_move(capfd, language0, project_foo):
 @pytest.mark.django_db
 def test_project_move_tp(capfd):
     call_command('project', 'move', 'project0', '--language=language0',
-                 '--target-project=foo')
+                 '--target-project=foo', '--target-language=language1')
 
     out, err = capfd.readouterr()
     assert (u'Translation project "/language0/project0/" has been moved into '
-            u'"/language0/foo/"' in out)
+            u'"/language1/foo/"' in out)
+
+
+@pytest.mark.cmd
+@pytest.mark.django_db
+def test_project_move_tp_within_project(capfd):
+    call_command('project', 'remove', 'project0', '--language=language1')
+    call_command('project', 'move', 'project0', '--language=language0',
+                 '--target-language=language1')
+
+    out, err = capfd.readouterr()
+    assert (u'Translation project "/language0/project0/" has been moved into '
+            u'"/language1/project0/"' in out)
 
 
 @pytest.mark.cmd
 @pytest.mark.django_db
 def test_project_remove(capfd, project_foo):
     foo = '%s' % project_foo
-    call_command('project', 'remove', 'foo')
+    call_command('project', 'remove', 'foo', '--force')
 
     out, err = capfd.readouterr()
     assert (u'Project "%s" has been deleted' % foo) in out
+
+
+@pytest.mark.cmd
+@pytest.mark.django_db
+def test_project_remove_tp(capfd, tp0):
+    call_command('project', 'remove', 'project0', '--language=language0')
+
+    out, err = capfd.readouterr()
+    assert (u'Translation project "%s" has been deleted' % tp0) in out
+
+
+@pytest.mark.cmd
+@pytest.mark.django_db
+def test_project_clone_with_wrong_options():
+    with pytest.raises(CommandError) as e:
+        call_command('project', 'clone', 'project0', '--target-project=foo',
+                     '--target-language=language1')
+    assert ('You must set only one source language via --language option'
+            in ('%s' % e.value))
+
+    with pytest.raises(CommandError) as e:
+        call_command('project', 'clone', 'project0')
+    assert ('At least one of target-language and target-project is required.'
+            in ('%s' % e.value))
+
+
+@pytest.mark.cmd
+@pytest.mark.django_db
+def test_project_tp_move_to_wrong_language():
+    with pytest.raises(CommandError) as e:
+        call_command('project', 'move', 'project0', '--target-project=project1',
+                     '--language=language0', '--target-language=language_foo')
+    assert 'Language matching query does not exist' in ('%s' % e.value)
+
+
+@pytest.mark.cmd
+@pytest.mark.django_db
+def test_project_tp_update(tp0, language1):
+    from pootle_store.constants import UNTRANSLATED, TRANSLATED
+    call_command('project', 'clone', 'project0', '--language=language0',
+                 '--target-project=cloned_project',
+                 '--target-language=language1')
+
+    store = tp0.stores.first()
+    unit = store.units.filter(state=UNTRANSLATED).first()
+    unit.target_f = unit.source_f + ' TRANSLATION'
+    unit.save()
+    cloned_project = Project.objects.get(code='cloned_project')
+    cloned_tp = cloned_project.translationproject_set.get(language=language1)
+    store_path = store.pootle_path.replace('/language0/project0',
+                                           '/language1/cloned_project')
+    cloned_store = cloned_tp.stores.get(pootle_path=store_path)
+    cloned_unit = cloned_store.units.get(unitid_hash=unit.unitid_hash)
+    assert cloned_unit.state == UNTRANSLATED
+    call_command('project', 'update', 'project0', '--language=language0',
+                 '--target-project=cloned_project',
+                 '--target-language=language1')
+    assert cloned_store.units.get(id=cloned_unit.id).state == TRANSLATED
