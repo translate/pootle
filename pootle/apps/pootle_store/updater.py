e75ba926648323013b7e677b1eb142909cfafc0c
Ryan Northey <ryan@synca.io>
2017-02-19 13:15:37 +0000

    Update pootle_store.updater to use unit.change

--- pootle/apps/pootle_store/updater.py
+++ pootle/apps/pootle_store/updater.py
@@ -184,14 +184,14 @@ class UnitUpdater(object):
             changed_with=self.update.submission_type)
 
     def set_commented(self):
-        self.unit.commented_by = self.update.user
-        self.unit.commented_on = self.at
+        self.unit.change.commented_by = self.update.user
+        self.unit.change.commented_on = self.at
 
     def set_submitted(self):
-        self.unit.submitted_by = self.update.user
-        self.unit.submitted_on = self.at
-        self.unit.reviewed_on = None
-        self.unit.reviewed_by = None
+        self.unit.change.submitted_by = self.update.user
+        self.unit.change.submitted_on = self.at
+        self.unit.change.reviewed_on = None
+        self.unit.change.reviewed_by = None
 
     def set_unit(self):
         if self.translator_comment_updated:
@@ -249,7 +249,8 @@ class StoreUpdater(object):
         return count
 
     def units(self, uids):
-        unit_set = self.target_store.unit_set.select_related("submitted_by")
+        unit_set = self.target_store.unit_set.select_related(
+            "change__submitted_by")
         for unit in self.target_store.findid_bulk(uids, unit_set):
             unit.store = self.target_store
             yield unit
