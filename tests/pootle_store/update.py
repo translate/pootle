31cc197cb96c2160e245634d9b8b5cae09f48e83
Ryan Northey <ryan@synca.io>
2017-02-20 23:50:48 +0000

    Cleanup: remove debug tracer from test

--- tests/pootle_store/update.py
+++ tests/pootle_store/update.py
@@ -17,7 +17,6 @@ def test_store_update_new_unit_order(store0):
     new_store.units = (
         [new_store.units[0], new_unit]
         + new_store.units[2:])
-    store0._FOO = 1
     store0.update(
         new_store,
         store_revision=store0.data.max_unit_revision + 1)
