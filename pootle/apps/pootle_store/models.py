88f94c51550ca2b308978a164d2387dca0545708
Ryan Northey <ryan@synca.io>
2017-02-21 17:17:47 +0000

    Cleanup: ensure submitted_by is set

--- pootle/apps/pootle_store/models.py
+++ pootle/apps/pootle_store/models.py
@@ -387,7 +387,7 @@ class Unit(AbstractUnit):
                 changed_with=changed_with)
         if changed:
             self.change.changed_with = changed_with
-            self.change.submitted_by = submitted_by
+            self.change.submitted_by = submitted_by or self.submitted_by
             self.change.reviewed_by = reviewed_by
             self.change.submitted_on = submitted_on
             self.change.reviewed_on = reviewed_on
