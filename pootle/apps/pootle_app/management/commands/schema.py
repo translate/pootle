4fd784bc2bf8639d541d4bdf44cf08c95f7f4205
Taras Semenenko <taras.semenenko@gmail.com>
2017-02-23 12:35:48 +0300

    Add schema app and table subcommands

--- pootle/apps/pootle_app/management/commands/schema.py
+++ pootle/apps/pootle_app/management/commands/schema.py
@@ -10,65 +10,57 @@
 import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'pootle.settings'
 
-from django.core.management.base import BaseCommand, CommandError
-
+from pootle.core.management.subcommands import CommandWithSubcommands
 from pootle.core.schema.base import SchemaTool
+from pootle.core.utils.json import jsonify
+
+from .schema_commands import SchemaAppCommand, SchemaTableCommand
 
 
-class Command(BaseCommand):
-    help = "Print Pootle's current schema state."
+class Command(CommandWithSubcommands):
+    help = "Pootle schema state command."
     requires_system_checks = False
 
+    subcommands = {
+        "app": SchemaAppCommand,
+        "table": SchemaTableCommand,
+    }
+
     def add_arguments(self, parser):
-        parser.add_argument(
-            'args',
-            metavar='app_label',
-            nargs='*',
-            help='Application labels.'
-        )
-        check_or_dump = parser.add_mutually_exclusive_group()
-        check_or_dump.add_argument(
+
+        check_or_dump_or_tables = parser.add_mutually_exclusive_group()
+        check_or_dump_or_tables.add_argument(
             '--check',
             action='store_true',
             default=False,
             dest='check',
-            help='',
+            help='Print schema state.',
         )
-        check_or_dump.add_argument(
+        check_or_dump_or_tables.add_argument(
             '--dump',
             action='store_true',
             default=False,
             dest='dump',
-            help='',
+            help='Print schema settings.',
         )
+        check_or_dump_or_tables.add_argument(
+            '--tables',
+            action='store_true',
+            default=False,
+            dest='tables',
+            help='Print all table names.',
+        )
+        super(Command, self).add_arguments(parser)
 
-    def handle(self, *app_labels, **options):
-        try:
-            schema_tool = SchemaTool(*app_labels)
-        except (LookupError, ImportError) as e:
-            raise CommandError("%s. Are you sure your INSTALLED_APPS setting is correct?" % e)
+    def handle(self, **options):
+        schema_tool = SchemaTool()
+        if options['tables']:
+            self.stdout.write(jsonify(schema_tool.get_tables()))
 
         if options['check']:
             self.stdout.write('%s' % schema_tool.state)
+
         if options['dump']:
-            self.stdout.write('Schema defaults:\n%s' %
-                schema_tool.schema_dumper.get_defaults())
-            for table in schema_tool.get_tables():
-                self.stdout.write(
-                    'Table fields %s:\n%s' % (
-                        table,
-                        schema_tool.schema_dumper.get_table_fields(table),
-                    )
-                )
-                self.stdout.write(
-                    'Table indices %s:\n%s' % (
-                        table,
-                        schema_tool.schema_dumper.get_table_indices(table),
-                    )
-                )
-                self.stdout.write(
-                    'Table constraints %s:\n%s' % (
-                        table,
-                        schema_tool.schema_dumper.get_table_constraints(table),
-                    )
-                )
+            self.stdout.write(
+                jsonify(schema_tool.schema_dumper.get_defaults())
+            )
---
+++ pootle/apps/pootle_app/management/commands/schema_commands/__init__.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'pootle.settings'
+
+from django.core.management.base import BaseCommand, CommandError
+
+from pootle.core.schema.base import SchemaTool
+from pootle.core.utils.json import jsonify
+
+
+class SchemaCommand(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--fields',
+            action='store_true',
+            default=False,
+            help='Table fields.',
+        )
+        parser.add_argument(
+            '--indices',
+            action='store_true',
+            default=False,
+            help='Table indices.',
+        )
+        parser.add_argument(
+            '--constraints',
+            action='store_true',
+            default=False,
+            help='Table constraints.',
+        )
+        super(SchemaCommand, self).add_arguments(parser)
+
+    def handle_table(self, table_name, **options):
+        result = {table_name: {}}
+        all_options = (
+            not options['fields']
+            and not options['indices']
+            and not options['constraints']
+        )
+        if options['fields'] or all_options:
+            result[table_name]['fields'] = \
+                self.schema_tool.get_table_fields(table_name)
+        if options['indices'] or all_options:
+            result[table_name]['indices'] = \
+                self.schema_tool.get_table_indices(table_name)
+        if options['constraints'] or all_options:
+            result[table_name]['constraints'] = \
+                self.schema_tool.get_table_constraints(table_name)
+
+        return result
+
+class SchemaTableCommand(SchemaCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'args',
+            metavar='table_name',
+            nargs='+',
+            help='Table names.'
+        )
+        super(SchemaTableCommand, self).add_arguments(parser)
+
+    def handle(self, *args, **options):
+        self.schema_tool = SchemaTool()
+        all_tables = self.schema_tool.get_tables()
+        if not set(args).issubset(set(all_tables)):
+            raise CommandError("Unrecognized tables: %s" %
+                               list(set(args) - set(all_tables)))
+
+        result = {}
+        for table_name in args:
+            result.update(
+                self.handle_table(table_name, **options)
+            )
+
+        self.stdout.write(jsonify(result))
+
+
+class SchemaAppCommand(SchemaCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'args',
+            metavar='app_label',
+            nargs='+',
+            help='Application labels.'
+        )
+        parser.add_argument(
+            '--tables',
+            action='store_true',
+            default=False,
+            dest='tables',
+            help='Print all table names.',
+        )
+        super(SchemaAppCommand, self).add_arguments(parser)
+
+    def handle(self, *args, **options):
+        try:
+            self.schema_tool = SchemaTool(*args)
+
+        except (LookupError, ImportError) as e:
+            raise CommandError("%s. Are you sure your INSTALLED_APPS "
+                               "setting is correct?" % e)
+
+        if options['tables']:
+            result = dict(apps={})
+            for app_label in args:
+                result['apps'] = {
+                    'name': app_label,
+                    'tables': self.schema_tool.get_app_tables(app_label),
+                }
+
+            self.stdout.write(jsonify(result))
+        else:
+            result = dict(apps={})
+            for app_label in args:
+                result['apps'][app_label] = {
+                    'name': app_label,
+                    'tables': {}
+                }
+                for table_name in self.schema_tool.get_app_tables(app_label):
+                    result['apps'][app_label]['tables'].update(
+                        self.handle_table(table_name, **options)
+                    )
+
+            self.stdout.write(jsonify(result))
