7e8dfc9b0a90eb597089057c9672dcc97c0cb54d
Ryan Northey <ryan@synca.io>
2017-02-24 16:52:59 +0000

    Add unit wordcount getter

--- pootle/apps/pootle_store/getters.py
+++ pootle/apps/pootle_store/getters.py
@@ -6,18 +6,34 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
+from django.conf import settings
 from django.core.exceptions import ValidationError
 
 from pootle.core.delegate import (
     deserializers, frozen, lifecycle, review, search_backend, serializers,
-    uniqueid)
+    uniqueid, wordcount)
 from pootle.core.plugin import getter
 from pootle_config.delegate import (
     config_should_not_be_set, config_should_not_be_appended)
+from pootle_misc.util import import_func
 
 from .models import Suggestion, Unit
 from .unit.search import DBSearchBackend
-from .utils import FrozenUnit, SuggestionsReview, UnitLifecycle, UnitUniqueId
+from .utils import (
+    FrozenUnit, SuggestionsReview, UnitLifecycle, UnitUniqueId, UnitWordcount)
+
+
+wordcounter = None
+
+
+@getter(wordcount, sender=Unit)
+def get_unit_wordcount(**kwargs_):
+    global wordcounter
+
+    if not wordcounter:
+        wordcounter = UnitWordcount(
+            import_func(settings.POOTLE_WORDCOUNT_FUNC))
+    return wordcounter
 
 
 @getter(frozen, sender=Unit)
