15bcb8a9980d394ab3bb8d0fef256526729b1f6a
Ryan Northey <ryan@synca.io>
2017-02-24 10:25:29 +0000

    Remove pootle.core.log.action_log

--- pootle/core/log.py
+++ pootle/core/log.py
@@ -11,46 +11,17 @@ import logging
 
 
 # Log actions
-TRANSLATION_ADDED = 'A'
-TRANSLATION_CHANGED = 'C'
-TRANSLATION_DELETED = 'D'
-UNIT_ADDED = 'UA'
-UNIT_OBSOLETE = 'UO'
-UNIT_RESURRECTED = 'UR'
-UNIT_DELETED = 'UD'
 STORE_ADDED = 'SA'
 STORE_OBSOLETE = 'SO'
 STORE_RESURRECTED = 'SR'
 STORE_DELETED = 'SD'
 CMD_EXECUTED = 'X'
-MUTE_QUALITYCHECK = 'QM'
-UNMUTE_QUALITYCHECK = 'QU'
 SCORE_CHANGED = 'SC'
-
-PAID_TASK_ADDED = 'PTA'
-PAID_TASK_DELETED = 'PTD'
 
 
 def log(message):
     logger = logging.getLogger('action')
     logger.debug(message)
-
-
-def action_log(*args, **kwargs):
-    logger = logging.getLogger('action')
-    d = {}
-    for p in ['user', 'lang', 'action', 'unit', 'path']:
-        d[p] = kwargs.pop(p, '')
-
-    tr = kwargs.pop('translation', '')
-    tr = tr.replace("\\", "\\\\")
-    tr = tr.replace("\n", "\\\n")
-    d['translation'] = tr
-
-    msg = (u"%(user)s\t%(action)s\t%(lang)s\t"
-           "%(unit)s\t%(path)s\t%(translation)s" % d)
-
-    logger.debug(msg)
 
 
 def cmd_log(*args, **kwargs):
