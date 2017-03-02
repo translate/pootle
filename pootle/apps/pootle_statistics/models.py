7c85c0c243261f0d196fed016e35864981123c8a
Ryan Northey <ryan@synca.io>
2017-03-02 12:52:01 +0000

    Remove UN/MUTE_CHECK submission types

--- pootle/apps/pootle_statistics/models.py
+++ pootle/apps/pootle_statistics/models.py
@@ -33,8 +33,6 @@ class SubmissionTypes(object):
     WEB = 1  # Interactive web editing
     UPLOAD = 4  # Uploading an offline file
     SYSTEM = 5  # Batch actions performed offline
-    MUTE_CHECK = 6  # Mute QualityCheck
-    UNMUTE_CHECK = 7  # Unmute QualityCheck
 
     # Combined types that rely on other types (useful for querying)
     # Please use the `_TYPES` suffix to make it clear they're not core
