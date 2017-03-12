ba49171b0b5241176994cba91bdc7d62f12df32d
Ryan Northey <ryan@synca.io>
2017-03-12 16:38:08 +0000

    Refactor UnsecuredCommentForm to accept request_user

--- pootle/apps/pootle_comment/forms.py
+++ pootle/apps/pootle_comment/forms.py
@@ -7,9 +7,10 @@
 # AUTHORS file for copyright and authorship information.
 
 from datetime import datetime
+import time
 
-from django import forms
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.utils.functional import cached_property
 
 from django_comments.forms import CommentForm as DjCommentForm
@@ -25,25 +26,13 @@ User = get_user_model()
 
 
 class CommentForm(DjCommentForm):
-    user = forms.ModelChoiceField(queryset=User.objects.all())
-
-    def __init__(self, target_object, data=None, *args, **kwargs):
-        if data:
-            data["object_pk"] = str(target_object.pk)
-            data["content_type"] = str(target_object._meta)
-            if data.get("user"):
-                data["user"] = str(data["user"].pk)
-
-        super(CommentForm, self).__init__(
-            target_object, data, *args, **kwargs)
-
-        if data and data.get("user"):
-            self.fields["name"].required = False
-            self.fields["email"].required = False
 
     @cached_property
     def comment(self):
-        return self.get_comment_object()
+        try:
+            return self.get_comment_object()
+        except ValueError as e:
+            raise ValidationError(e)
 
     def clean(self):
         super(CommentForm, self).clean()
@@ -56,7 +45,6 @@ class CommentForm(DjCommentForm):
 
     def save(self):
         comment = self.comment
-        comment.user = self.cleaned_data["user"]
         comment.submit_date = make_aware(datetime.now())
         comment.save()
         comment_was_saved.send(
@@ -65,9 +53,44 @@ class CommentForm(DjCommentForm):
 
 
 class UnsecuredCommentForm(CommentForm):
+    """This form does not check for security hash, and uses the view's
+    request.user
+    """
 
-    def __init__(self, target_object, data=None, *args, **kwargs):
+    def __init__(self, target_object, request_user, data=None, *args, **kwargs):
+        self.request_user = request_user
+        self.timestamp = str(int(time.time()))
+        mangled_fields = [
+            "name", "timestamp", "object_pk", "security_hash",
+            "content_type", "email"]
         super(UnsecuredCommentForm, self).__init__(
             target_object, data, *args, **kwargs)
-        if data:
-            data.update(self.generate_security_data())
+        for field in mangled_fields:
+            self.fields[field].required = False
+
+    def clean_email(self):
+        return self.request_user.email
+
+    def clean_name(self):
+        return self.request_user.display_name
+
+    def clean_content_type(self):
+        return str(self.target_object._meta)
+
+    def clean_object_pk(self):
+        return str(self.target_object._get_pk_val())
+
+    def clean_timestamp(self):
+        return self.timestamp
+
+    def clean_security_hash(self):
+        return self.initial_security_hash(self.timestamp)
+
+    def save(self):
+        comment = self.comment
+        comment.user = self.request_user
+        comment.submit_date = make_aware(datetime.now())
+        comment.save()
+        comment_was_saved.send(
+            sender=comment.__class__,
+            comment=comment)
