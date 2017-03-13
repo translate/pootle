9ee1fabe9c348b06fdd04ee8798aeb05ffa143db
Ryan Northey <ryan@synca.io>
2017-03-13 23:02:07 +0000

    Remove unused test

--- tests/forms/unit.py
+++ tests/forms/unit.py
@@ -13,9 +13,9 @@ from django.forms import Form
 from translate.misc import multistring
 
 from pootle_app.models.permissions import get_matching_permissions
-from pootle_store.forms import (MultiStringFormField, MultiStringWidget,
-                                UnitStateField, unit_form_factory)
-from pootle_store.constants import FUZZY, TRANSLATED, UNTRANSLATED
+from pootle_store.forms import (
+    MultiStringFormField, MultiStringWidget, unit_form_factory)
+from pootle_store.constants import FUZZY
 
 
 def _create_post_request(rf, directory, user, url='/', data=None):
@@ -81,18 +81,6 @@ def test_submit_fuzzy(rf, po_directory, admin, default, store0):
     user_form = _create_unit_form(request, language, unit)
     assert not user_form.is_valid()
     assert 'state' in user_form.errors
-
-
-def test_unit_state():
-    """Tests how checkbox states (as strings) map to booleans."""
-    field = UnitStateField(required=False)
-    assert field.clean(str(FUZZY))
-    assert field.clean(str(TRANSLATED))
-    assert field.clean(str(UNTRANSLATED))
-    assert field.clean(True)
-    assert not field.clean('True')  # Unknown state value evaluates to False
-    assert not field.clean(False)
-    assert not field.clean('False')
 
 
 @pytest.mark.parametrize('nplurals, decompressed_value', [
