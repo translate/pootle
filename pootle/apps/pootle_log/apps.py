ba1beda09e6a16d539f20e30cddc9023c7fe658d
Ryan Northey <ryan@synca.io>
2017-03-14 13:48:52 +0000

    Add pootle_log boilerplate

---
+++ pootle/apps/pootle_log/apps.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+from django.apps import AppConfig
+
+
+class PootleLogConfig(AppConfig):
+
+    name = "pootle_log"
+    verbose_name = "Pootle Log"
+    version = "0.0.1"
