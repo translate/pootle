10c82b9bdd63685e15072725884d36ad57d3ba05
Taras Semenenko <taras.semenenko@gmail.com>
2017-03-14 18:44:36 +0300

    Add UserStoreScore model

--- pootle/apps/pootle_score/models.py
+++ pootle/apps/pootle_score/models.py
@@ -11,25 +11,35 @@ from django.db import models
 from .abstracts import AbstractUserScore
 
 
+class UserStoreScore(AbstractUserScore):
+
+    class Meta(AbstractUserScore.Meta):
+        db_table = "pootle_user_store_score"
+        unique_together = ["date", "store", "user"]
+
+    store = models.ForeignKey(
+        "pootle_store.Store",
+        on_delete=models.CASCADE,
+        db_index=True,
+        related_name="user_scores")
+
+    @property
+    def context(self):
+        return self.store
+
+
 class UserTPScore(AbstractUserScore):
 
     class Meta(AbstractUserScore.Meta):
         db_table = "pootle_user_tp_score"
         unique_together = ["date", "tp", "user"]
 
-    def __str__(self):
-        return (
-            "%s(%s) %s score: %s, suggested: %s, translated: %s, reviewed: %s"
-            % (self.user.username,
-               self.tp.pootle_path,
-               self.date,
-               self.score,
-               self.suggested,
-               self.translated,
-               self.reviewed))
-
     tp = models.ForeignKey(
         "pootle_translationproject.TranslationProject",
         on_delete=models.CASCADE,
         db_index=True,
         related_name="user_scores")
+
+    @property
+    def context(self):
+        return self.tp
