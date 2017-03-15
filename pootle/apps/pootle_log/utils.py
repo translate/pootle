d82c96689e8741e9548c8e760e4cd86d74011785
Ryan Northey <ryan@synca.io>
2017-03-15 06:36:22 +0000

    Add pootle_log.Log
    base log class for retrieving LogEvents

--- pootle/apps/pootle_log/utils.py
+++ pootle/apps/pootle_log/utils.py
@@ -6,6 +6,10 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
+from django.utils.functional import cached_property
+
+from pootle_statistics.models import SubmissionFields, SubmissionTypes
+
 
 class LogEvent(object):
 
@@ -18,3 +22,48 @@ class LogEvent(object):
         self.value = value
         self.old_value = old_value
         self.revision = revision
+
+
+class Log(object):
+
+    @cached_property
+    def event(self):
+        return LogEvent
+
+    @cached_property
+    def subfields(self):
+        return {
+            getattr(SubmissionFields, n): n.lower()
+            for n
+            in ["SOURCE", "TARGET", "STATE", "COMMENT", "CHECK"]}
+
+    @cached_property
+    def subtypes(self):
+        return {
+            getattr(SubmissionTypes, n): n.lower()
+            for n in ["WEB", "UPLOAD", "SYSTEM"]}
+
+    def filter_path(self, qs, path=None, field="unit__store__pootle_path"):
+        return (
+            qs.filter(**{"%s__startswith" % field: path})
+            if path is not None
+            else qs)
+
+    def filter_store(self, qs, store=None, field="unit__store_id"):
+        return (
+            qs.filter(**{field: store})
+            if store is not None
+            else qs)
+
+    def filter_timestamps(self, qs, start=None, end=None, field="creation_time"):
+        if start is not None:
+            qs = qs.filter(**{"%s__gte" % field: start})
+        if end is not None:
+            qs = qs.filter(**{"%s__lt" % field: end})
+        return qs
+
+    def filter_user(self, qs, user=None, field="submitter_id"):
+        return (
+            qs.filter(**{field: user})
+            if user is not None
+            else qs)
