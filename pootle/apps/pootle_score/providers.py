b828b6061a7edbb03bdb1b4c4a5c7e2e83cfb3b5
Ryan Northey <ryan@synca.io>
2017-03-15 20:55:28 +0000

    Add pootle_score.providers

---
+++ pootle/apps/pootle_score/providers.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+from pootle.core.delegate import event_score
+from pootle.core.plugin import provider
+from pootle_log.utils import LogEvent
+
+from . import scores
+
+
+@provider(event_score, sender=LogEvent)
+def event_log_score_provider(**kwargs_):
+    return dict(
+        suggestion_accepted=scores.SuggestionAcceptedScore,
+        suggestion_rejected=scores.SuggestionRejectedScore)
