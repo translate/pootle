ee30efd77448a399901eb8056302bd298b6d7eab
Ryan Northey <ryan@synca.io>
2017-03-16 17:36:12 +0000

    Add update_scores handlers
    for suggestion, submission and unit_create, and to trigger tp.update
    when storescores are changed

--- pootle/apps/pootle_score/receivers.py
+++ pootle/apps/pootle_score/receivers.py
@@ -6,10 +6,67 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
+from django.contrib.auth import get_user_model
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from pootle_statistics.models import ScoreLog, TranslationActionCodes
+from pootle.core.delegate import score_updater
+from pootle.core.signals import update_scores
+from pootle_statistics.models import (
+    ScoreLog, Submission, TranslationActionCodes)
+from pootle_store.models import Store, Suggestion
+from pootle_translationproject.models import TranslationProject
+
+
+from .models import UserStoreScore
+
+
+@receiver(update_scores, sender=TranslationProject)
+def update_store_scores_handler(**kwargs):
+    tp = kwargs["instance"]
+    score_updater.get(tp.__class__)(tp).update()
+
+
+@receiver(update_scores, sender=Store)
+def update_tp_scores_handler(**kwargs):
+    store = kwargs["instance"]
+    score_updater.get(store.__class__)(store).update()
+
+
+@receiver(post_save, sender=UserStoreScore)
+def handle_store_score_updated(**kwargs):
+    tp = kwargs["instance"].store.translation_project
+    update_scores.send(tp.__class__, instance=tp)
+
+
+@receiver(post_save, sender=Suggestion)
+def handle_suggestion_change(**kwargs):
+    suggestion = kwargs["instance"]
+    is_system_user = (
+        (suggestion.state.name == "pending"
+         and (suggestion.user
+              == get_user_model().objects.get_system_user()))
+        or (suggestion.state.name != "pending"
+            and (suggestion.reviewer
+                 == get_user_model().objects.get_system_user())))
+    if is_system_user:
+        return
+    update_scores.send(
+        suggestion.unit.store.__class__,
+        instance=suggestion.unit.store)
+
+
+@receiver(post_save, sender=Submission)
+def handle_submission_added(**kwargs):
+    submission = kwargs["instance"]
+    is_system_user = (
+        submission.submitter
+        == get_user_model().objects.get_system_user())
+    if is_system_user:
+        return
+    update_scores.send(
+        submission.unit.store.__class__,
+        instance=submission.unit.store)
 
 
 @receiver(post_save, sender=ScoreLog)
