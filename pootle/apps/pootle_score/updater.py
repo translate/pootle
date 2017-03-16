0f73e07f9dede2a1565a4e0ad60275becaadf172
Ryan Northey <ryan@synca.io>
2017-03-16 20:19:13 +0000

    Cleanup: factor base ScoreUpdater

--- pootle/apps/pootle_score/updater.py
+++ pootle/apps/pootle_score/updater.py
@@ -16,21 +16,68 @@ from pootle_log.utils import LogEvent
 from pootle_score.models import UserStoreScore
 
 
-class StoreScoreUpdater(object):
+class ScoreUpdater(object):
     event_class = LogEvent
-    score_model = UserStoreScore
 
-    def __init__(self, store, *args, **kwargs):
-        self.store = store
-        self.user = kwargs.get('user')
+    def __init__(self, context, *args, **kwargs):
+        self.context = context
 
     @cached_property
     def logs(self):
-        return log.get(self.store.__class__)(self.store)
+        return log.get(self.context.__class__)(self.context)
 
     @cached_property
     def scoring(self):
         return event_score.gather(self.event_class)
+
+    def delete_scores(self, scores):
+        self.find_existing_scores(
+            scores).select_for_update().delete()
+
+    def find_existing_scores(self, scores):
+        existing_scores = self.score_model.objects.none()
+        score_iterator = self.iterate_scores(scores)
+        for timestamp, user, user_scores in score_iterator:
+            existing_scores = (
+                existing_scores
+                | self.score_model.objects.filter(
+                    user_id=user,
+                    date=timestamp,
+                    **{self.related_field: self.context.pk}))
+        return existing_scores
+
+    def new_scores(self, scores):
+        score_iterator = self.iterate_scores(scores)
+        for timestamp, user, user_scores in score_iterator:
+            user_scores.update(
+                {self.related_field: self.context.pk})
+            yield self.score_model(
+                date=timestamp,
+                user_id=user,
+                **user_scores)
+
+    def create_scores(self, scores):
+        self.score_model.objects.bulk_create(self.new_scores(scores))
+
+    def set_scores(self, calculated_scores):
+        self.delete_scores(calculated_scores)
+        self.create_scores(calculated_scores)
+
+    def update(self):
+        self.set_scores(self.calculate())
+
+
+class StoreScoreUpdater(ScoreUpdater):
+    score_model = UserStoreScore
+    related_field = "store_id"
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.get('user')
+        super(StoreScoreUpdater, self).__init__(*args, **kwargs)
+
+    @property
+    def store(self):
+        return self.context
 
     def score_event(self, event, calculated_scores):
         if event.action not in self.scoring:
@@ -66,38 +113,3 @@ class StoreScoreUpdater(object):
         for timestamp, date_scores in scores.items():
             for user, user_scores in date_scores.items():
                 yield timestamp, user, user_scores
-
-    def find_existing_scores(self, scores):
-        existing_scores = self.score_model.objects.none()
-        score_iterator = self.iterate_scores(scores)
-        for timestamp, user, user_scores in score_iterator:
-            existing_scores = (
-                existing_scores
-                | self.score_model.objects.filter(
-                    user_id=user,
-                    store_id=self.store.id,
-                    date=timestamp))
-        return existing_scores
-
-    def delete_scores(self, scores):
-        self.find_existing_scores(
-            scores).select_for_update().delete()
-
-    def new_scores(self, scores):
-        score_iterator = self.iterate_scores(scores)
-        for timestamp, user, user_scores in score_iterator:
-            yield self.score_model(
-                date=timestamp,
-                user_id=user,
-                store_id=self.store.id,
-                **user_scores)
-
-    def create_scores(self, scores):
-        self.score_model.objects.bulk_create(self.new_scores(scores))
-
-    def set_scores(self, calculated_scores):
-        self.delete_scores(calculated_scores)
-        self.create_scores(calculated_scores)
-
-    def update(self):
-        self.set_scores(self.calculate())
