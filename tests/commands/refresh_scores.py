4fc7b8b563d7e86e106c1f01bd2c2955e4534cbe
Taras Semenenko <taras.semenenko@gmail.com>
2017-03-17 12:57:15 +0300

    Replace old refresh_scores tests with dummy test

--- tests/commands/refresh_scores.py
+++ tests/commands/refresh_scores.py
@@ -13,38 +13,8 @@ from django.core.management import call_command
 
 @pytest.mark.cmd
 @pytest.mark.django_db
-def test_refresh_scores_recalculate(capfd, admin, member):
+def test_refresh_scores(capfd):
     """Recalculate scores."""
-    # delete the 2 most prolific contribs to speed up
-    member.delete()
-    admin.delete()
     call_command('refresh_scores')
     out, err = capfd.readouterr()
-    assert 'Score for user system set to' in out
-
-
-@pytest.mark.cmd
-@pytest.mark.django_db
-def test_refresh_scores_recalculate_user(capfd):
-    """Recalculate scores for given users."""
-    call_command('refresh_scores', '--user=system')
-    out, err = capfd.readouterr()
-    assert 'Score for user system set to' in out
-
-
-@pytest.mark.cmd
-@pytest.mark.django_db
-def test_refresh_scores_reset_user(capfd):
-    """Set scores to zero for given users."""
-    call_command('refresh_scores', '--reset', '--user=system')
-    out, err = capfd.readouterr()
-    assert 'Scores for specified users were reset to 0.' in out
-
-
-@pytest.mark.cmd
-@pytest.mark.django_db
-def test_refresh_scores_reset(capfd):
-    """Set scores to zero."""
-    call_command('refresh_scores', '--reset')
-    out, err = capfd.readouterr()
-    assert 'Scores for all users were reset to 0.' in out
+    assert 'All done' in out
