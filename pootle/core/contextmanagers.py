1ab4ee08667856abc0383b1e4c1975f42994cb23
Ryan Northey <ryan@synca.io>
2017-03-18 10:51:42 +0000

    Adjust update_data_after to allow multiple signals
    and include update_scores in default suppression

--- pootle/core/contextmanagers.py
+++ pootle/core/contextmanagers.py
@@ -6,9 +6,9 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
-from contextlib import contextmanager
+from contextlib import contextmanager, nested
 
-from pootle.core.signals import update_data
+from pootle.core.signals import update_data, update_scores
 
 
 @contextmanager
@@ -24,9 +24,9 @@ def suppress_signal(signal):
 
 
 @contextmanager
-def keep_data(keep=True):
+def keep_data(keep=True, signals=(update_data, update_scores)):
     if keep:
-        with suppress_signal(update_data):
+        with nested(*[suppress_signal(s) for s in signals]):
             yield
     else:
         yield
@@ -34,11 +34,13 @@ def keep_data(keep=True):
 
 @contextmanager
 def update_data_after(sender, **kwargs):
-    with keep_data():
+    signals = kwargs.get("signals", [update_data, update_scores])
+    with keep_data(signals=signals):
         yield
     if "kwargs" in kwargs:
         kwargs.update(kwargs.pop("kwargs"))
-    update_data.send(
-        sender.__class__,
-        instance=sender,
-        **kwargs)
+    for signal in signals:
+        signal.send(
+            sender.__class__,
+            instance=sender,
+            **kwargs)
