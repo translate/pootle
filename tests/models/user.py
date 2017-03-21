e158089f6f5d66a48005de3ced1db208b2ec98b1
Ryan Northey <ryan@synca.io>
2017-03-21 08:42:27 +0000

    Update tests to use unit.change.commented

--- tests/models/user.py
+++ tests/models/user.py
@@ -79,7 +79,7 @@ def _test_before_evil_user_updated(store, member, teststate=False):
 
     # And added a comment on the unit.
     assert unit.translator_comment == "NICE COMMENT"
-    assert unit.commented_by == member
+    assert unit.change.commented_by == member
 
     # Only 1 unit round here.
     assert store.units.count() == 1
@@ -100,7 +100,7 @@ def _test_after_evil_user_updated(store, evil_member):
 
     # And added their own comment.
     assert unit.translator_comment == "EVIL COMMENT"
-    assert unit.commented_by == evil_member
+    assert unit.change.commented_by == evil_member
 
     # Evil member has added another unit.
     assert store.units.count() == 2
@@ -115,7 +115,7 @@ def _test_user_purging(store, member, evil_member, purge):
 
     # Get intitial change times
     initial_submission_time = unit.submitted_on
-    initial_comment_time = unit.commented_on
+    initial_comment_time = unit.change.commented_on
     initial_review_time = unit.reviewed_on
 
     # Test state before evil user has updated.
@@ -136,7 +136,7 @@ def _test_user_purging(store, member, evil_member, purge):
 
         # Times have changed
         assert unit.submitted_on != initial_submission_time
-        assert unit.commented_on != initial_comment_time
+        assert unit.change.commented_on != initial_comment_time
         assert unit.reviewed_on != initial_review_time
 
     # Test state after evil user has updated.
@@ -152,7 +152,7 @@ def _test_user_purging(store, member, evil_member, purge):
 
     # Times are back to previous times - by any precision
     assert unit.submitted_on == initial_submission_time
-    assert unit.commented_on == initial_comment_time
+    assert unit.change.commented_on == initial_comment_time
     assert unit.reviewed_on == initial_review_time
 
     # State is be back to how it was before evil user updated.
