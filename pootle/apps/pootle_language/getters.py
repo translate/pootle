8acdfba400bde784e113eb8dcc5973103ede1b69
Ryan Northey <ryan@synca.io>
2017-04-01 17:22:18 +0000

    Add site_languages getter

--- pootle/apps/pootle_language/getters.py
+++ pootle/apps/pootle_language/getters.py
@@ -6,12 +6,21 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
-from pootle.core.delegate import language_team
+from pootle.core.delegate import language_team, site_languages
 from pootle.core.plugin import getter
 
 from .teams import LanguageTeam
+from .utils import SiteLanguages
+
+
+_site_languages = SiteLanguages()
 
 
 @getter(language_team)
 def language_team_getter(**kwargs_):
     return LanguageTeam
+
+
+@getter(site_languages)
+def site_languages_getter(**kwargs_):
+    return _site_languages
