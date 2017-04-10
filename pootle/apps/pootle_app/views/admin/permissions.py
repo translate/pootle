024152ab528cee33e551c6c6e3f56702279f718e
Matthieu Denoux <matthieu.denoux@gmail.com>
2017-04-10 15:36:54 +0200

    Removes edit permission and renames addproject to add_project.

--- pootle/apps/pootle_app/views/admin/permissions.py
+++ pootle/apps/pootle_app/views/admin/permissions.py
@@ -20,7 +20,7 @@ from pootle_misc.forms import GroupedModelChoiceField
 User = get_user_model()
 
 PERMISSIONS = {
-    'positive': ['view', 'suggest', 'translate', 'review', 'administrate', 'addproject', 'editproject'],
+    'positive': ['view', 'suggest', 'translate', 'review', 'administrate', 'add_project'],
     'negative': ['hide'],
 }
 
