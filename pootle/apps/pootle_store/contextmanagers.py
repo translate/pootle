e79500315e1ccdcd54b9939b312702801547ab3f
Ryan Northey <ryan@synca.io>
2017-04-23 20:01:04 +0000

    Improve event bubbling in update_data_after

--- pootle/apps/pootle_store/contextmanagers.py
+++ pootle/apps/pootle_store/contextmanagers.py
@@ -8,19 +8,42 @@
 
 from contextlib import contextmanager
 
+from django.dispatch import receiver
+
 from pootle.core.contextmanagers import keep_data
 from pootle.core.signals import update_data, update_scores
+
+
+class Updated:
+    data = False
+    scores = False
 
 
 @contextmanager
 def update_data_after(sender, **kwargs):
     signals = [update_data, update_scores]
     with keep_data(signals=signals):
+        updated = Updated()
+
+        @receiver(update_data, sender=sender.__class__)
+        def handle_update_data(**kwargs):
+            updated.data = True
+
+        @receiver(update_scores, sender=sender.__class__)
+        def handle_update_scores(**kwargs):
+            updated.scores = True
+
         yield
+
     if "kwargs" in kwargs:
         kwargs.update(kwargs.pop("kwargs"))
-    for signal in signals:
-        signal.send(
+    if updated.data:
+        update_data.send(
+            sender.__class__,
+            instance=sender,
+            **kwargs)
+    if updated.scores:
+        update_scores.send(
             sender.__class__,
             instance=sender,
             **kwargs)
