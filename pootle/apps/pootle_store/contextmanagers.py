8ba0ccd37b42046c6cb98623fb596d9ba3d8d59e
Ryan Northey <ryan@synca.io>
2017-04-24 13:18:03 +0000

    Add revision and checks suppression to update_store_after

--- pootle/apps/pootle_store/contextmanagers.py
+++ pootle/apps/pootle_store/contextmanagers.py
@@ -11,39 +11,75 @@ from contextlib import contextmanager
 from django.dispatch import receiver
 
 from pootle.core.contextmanagers import keep_data
-from pootle.core.signals import update_data, update_scores
+from pootle.core.signals import (
+    update_checks, update_data, update_revisions, update_scores)
+
+from .models import Unit
 
 
 class Updated:
     data = False
-    scores = False
+    scores = set()
+    checks = set()
+    revisions = False
 
 
 @contextmanager
 def update_store_after(sender, **kwargs):
-    signals = [update_data, update_scores]
+    signals = [
+        update_checks,
+        update_data,
+        update_revisions,
+        update_scores]
+
     with keep_data(signals=signals):
         updated = Updated()
+
+        @receiver(update_checks, sender=Unit)
+        def handle_update_checks(**kwargs):
+            updated.checks.add(kwargs["instance"].id)
 
         @receiver(update_data, sender=sender.__class__)
         def handle_update_data(**kwargs):
             updated.data = True
+            update_data.disconnect(
+                handle_update_data,
+                sender=sender.__class__)
 
         @receiver(update_scores, sender=sender.__class__)
         def handle_update_scores(**kwargs):
-            updated.scores = True
-
+            updated.scores = (
+                updated.scores
+                | set(kwargs.get("users") or []))
         yield
 
     if "kwargs" in kwargs:
         kwargs.update(kwargs.pop("kwargs"))
-    if updated.data:
-        update_data.send(
-            sender.__class__,
-            instance=sender,
-            **kwargs)
-    if updated.scores:
-        update_scores.send(
-            sender.__class__,
-            instance=sender,
-            **kwargs)
+    with keep_data(signals=(update_revisions, )):
+
+        @receiver(update_revisions)
+        def handle_update_revisions(**kwargs):
+            updated.revisions = True
+
+        if updated.checks:
+            update_checks.send(
+                sender.__class__,
+                instance=sender,
+                units=updated.checks,
+                **kwargs)
+        if updated.data:
+            update_data.send(
+                sender.__class__,
+                instance=sender,
+                **kwargs)
+        if updated.scores:
+            update_scores.send(
+                sender.__class__,
+                instance=sender,
+                users=updated.scores,
+                **kwargs)
+    if updated.revisions:
+        update_revisions.send(
+            sender.parent.__class__,
+            instance=sender.parent,
+            keys=["stats", "checks"])
