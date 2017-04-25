f8a5d1469224e3b4ff33de6008a1efe4d9200f41
Dwayne Bailey <dwayne@translate.org.za>
2017-04-25 18:28:07 +0100

    l10n: geneate LINGUAS with set of required languages
    This allows us to use both percentage complete plus a list of language
    we require regardless of completion level.

--- pootle/tools/make-LINGUAS.sh
+++ pootle/tools/make-LINGUAS.sh
@@ -6,13 +6,18 @@ cd $script_location/../locale
 
 default_target=80
 target=${1:-$default_target}
+shift
+required_langs=$*
 
 for lang in $(ls)
 do
 	if [ -d "$lang" -a "$lang" != "templates" ]; then
-		completeness=$(pocount --no-color $(find $lang -name "*.po") | egrep "^Translated" | cut -d"(" -f2- | cut -d")" -f1 | sed "s/%//" | tail -1)
+        completeness=$(pocount --no-color $(find $lang -name "*.po" -type f) | egrep "^Translated" | cut -d"(" -f2- | cut -d")" -f1 | sed "s/%//" | tail -1)
 		if [[ $completeness -ge $target ]]; then
 			echo $lang
+        elif [[ $required_langs =~ (^|[[:space:]])$lang($|[[:space:]]) ]]; then
+            echo $lang
+            >&2 echo "$lang included as a default language (completenes only $completeness)"
 		fi
 	fi
 done
