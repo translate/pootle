76b02ca8d3b1b9f2d80f233da4e851d5d4f3775b
Ryan Northey <ryan@synca.io>
2017-04-29 09:34:43 +0000

    Add accounts app setup

---
+++ pootle/apps/accounts/apps.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+import importlib
+
+from django.apps import AppConfig
+
+
+class AccountsConfig(AppConfig):
+
+    name = "accounts"
+    verbose_name = "Accounts"
+    version = "0.0.1"
+
+    def ready(self):
+        importlib.import_module("accounts.getters")
+        importlib.import_module("accounts.receivers")
