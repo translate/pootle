53a2e113086192db6b28597b08a3c1c9bcc16a09
Ryan Northey <ryan@synca.io>
2017-05-03 21:14:59 +0000

    Add tests for suggestion accept/reject

--- tests/pootle_translationproject/contextmanagers.py
+++ tests/pootle_translationproject/contextmanagers.py
@@ -160,6 +160,149 @@ class SuggestionAddTest(object):
             == original["tp_dir_revision"][0].pk)
 
 
+class SuggestionAcceptTest(object):
+
+    def __init__(self, unit):
+        self.unit = unit
+
+    def _test_check_data(self, original, updated):
+        # check data
+        assert (
+            updated["store_checks_data"]
+            == original["store_checks_data"])
+        assert (
+            updated["tp_checks_data"]
+            == original["tp_checks_data"])
+
+    def _test_scores(self, original, updated):
+        # scores
+        assert (
+            updated["store_score"]["score"]
+            > original["store_score"]["score"])
+        assert (
+            updated["store_score"]["translated"]
+            > original["store_score"]["translated"])
+        assert (
+            updated["tp_score"]["score"]
+            > original["tp_score"]["score"])
+        assert (
+            updated["tp_score"]["translated"]
+            > original["tp_score"]["translated"])
+
+    def _test_data(self, original, updated):
+        # store and tp data
+        assert (
+            updated["store_data"]["critical_checks"]
+            == original["store_data"]["critical_checks"])
+        assert (
+            updated["store_data"]["translated_words"]
+            > original["store_data"]["translated_words"])
+        assert (
+            updated["store_data"]["pending_suggestions"]
+            == original["store_data"]["pending_suggestions"] - 1)
+        assert (
+            updated["tp_data"]["critical_checks"]
+            == original["tp_data"]["critical_checks"])
+        assert (
+            updated["tp_data"]["translated_words"]
+            > original["tp_data"]["translated_words"])
+        assert (
+            updated["tp_data"]["pending_suggestions"]
+            == original["tp_data"]["pending_suggestions"] - 1)
+
+    def _test_revisions(self, original, updated):
+        # unit and directory revisions
+        assert updated["unit_revision"] > original["unit_revision"]
+        assert (
+            updated["store_data"]["max_unit_revision"]
+            == updated["unit_revision"])
+        assert (
+            updated["store_data"]["max_unit_revision"]
+            > original["store_data"]["max_unit_revision"])
+        assert (
+            updated["dir_revision"][0].value
+            != original["dir_revision"][0].value)
+        assert (
+            updated["dir_revision"][0].pk
+            == original["dir_revision"][0].pk)
+        assert (
+            updated["tp_dir_revision"][0].value
+            != original["tp_dir_revision"][0].value)
+        assert (
+            updated["tp_dir_revision"][0].pk
+            == original["tp_dir_revision"][0].pk)
+
+
+class SuggestionRejectTest(object):
+
+    def __init__(self, unit):
+        self.unit = unit
+
+    def _test_check_data(self, original, updated):
+        # check data
+        assert (
+            updated["store_checks_data"]
+            == original["store_checks_data"])
+        assert (
+            updated["tp_checks_data"]
+            == original["tp_checks_data"])
+
+    def _test_scores(self, original, updated):
+        # scores
+        assert (
+            updated["store_score"]["score"]
+            > original["store_score"]["score"])
+        assert (
+            updated["store_score"]["translated"]
+            == original["store_score"]["translated"])
+        assert (
+            updated["tp_score"]["score"]
+            > original["tp_score"]["score"])
+        assert (
+            updated["tp_score"]["translated"]
+            == original["tp_score"]["translated"])
+
+    def _test_data(self, original, updated):
+        # store and tp data
+        assert (
+            updated["store_data"]["critical_checks"]
+            == original["store_data"]["critical_checks"])
+        assert (
+            updated["store_data"]["translated_words"]
+            == original["store_data"]["translated_words"])
+        assert (
+            updated["store_data"]["pending_suggestions"]
+            == original["store_data"]["pending_suggestions"] - 1)
+        assert (
+            updated["tp_data"]["critical_checks"]
+            == original["tp_data"]["critical_checks"])
+        assert (
+            updated["tp_data"]["translated_words"]
+            == original["tp_data"]["translated_words"])
+        assert (
+            updated["tp_data"]["pending_suggestions"]
+            == original["tp_data"]["pending_suggestions"] - 1)
+
+    def _test_revisions(self, original, updated):
+        # unit and directory revisions
+        assert updated["unit_revision"] == original["unit_revision"]
+        assert (
+            updated["store_data"]["max_unit_revision"]
+            == original["store_data"]["max_unit_revision"])
+        assert (
+            updated["dir_revision"][0].value
+            != original["dir_revision"][0].value)
+        assert (
+            updated["dir_revision"][0].pk
+            == original["dir_revision"][0].pk)
+        assert (
+            updated["tp_dir_revision"][0].value
+            != original["tp_dir_revision"][0].value)
+        assert (
+            updated["tp_dir_revision"][0].pk
+            == original["tp_dir_revision"][0].pk)
+
+
 @pytest.mark.django_db
 def test_contextmanager_update_tp_after_checks(tp0, store0, member,
                                                update_unit_test):
@@ -191,10 +334,29 @@ def test_contextmanager_update_tp_after_checks(tp0, store0, member,
 def test_contextmanager_update_tp_after_suggestion(tp0, store0, member,
                                                    update_unit_test):
     unit = store0.units.first()
+    sugg_review = review.get(Suggestion)
 
     with update_unit_test(SuggestionAddTest(unit)):
         with update_tp_after(tp0):
+            sugg_text = str(unit.source_f).replace("U", "X")
             sugg1, created_ = review.get(Suggestion)().add(
                 unit,
-                unit.source_f + 'CONTEXT SUGGESTION',
+                sugg_text,
                 user=member)
+
+    with update_unit_test(SuggestionAcceptTest(unit)):
+        with update_tp_after(tp0):
+            sugg_review(suggestions=[sugg1], reviewer=member).accept()
+
+    unit = store0.units.exclude(pk=unit.pk).first()
+    with update_unit_test(SuggestionAddTest(unit)):
+        with update_tp_after(tp0):
+            sugg_text = str(unit.source_f).replace("U", "X")
+            sugg1, created_ = review.get(Suggestion)().add(
+                unit,
+                sugg_text,
+                user=member)
+
+    with update_unit_test(SuggestionRejectTest(unit)):
+        with update_tp_after(tp0):
+            sugg_review(suggestions=[sugg1], reviewer=member).reject()
