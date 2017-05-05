f704f0d6c92add71dec5ccd666a100f76f753859
Ryan Northey <ryan@synca.io>
2017-05-05 17:42:29 +0000

    Update no_update_data signal suppression

--- pytest_pootle/fixtures/signals.py
+++ pytest_pootle/fixtures/signals.py
@@ -6,9 +6,13 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
+from contextlib import contextmanager
 from datetime import date
 
 import pytest
+
+from pootle.core.contextmanagers import keep_data
+from pootle.core.signals import update_data
 
 
 class UpdateUnitTest(object):
@@ -92,3 +96,14 @@ class UpdateUnitTest(object):
 @pytest.fixture
 def update_unit_test():
     return UpdateUnitTest
+
+
+@contextmanager
+def _no_update_data():
+    with keep_data(signals=(update_data, )):
+        yield
+
+
+@pytest.fixture
+def no_update_data():
+    return _no_update_data
