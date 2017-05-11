4058c3425cf5e7552e685a503c85df6e4f8405c7
Taras Semenenko <taras.semenenko@gmail.com>
2017-05-11 09:43:47 +0300

    Add test for unit timeline grouped_events

--- tests/pootle_store/timeline.py
+++ tests/pootle_store/timeline.py
@@ -12,12 +12,169 @@ from datetime import timedelta
 
 from django.utils import timezone
 
-from pootle.core.delegate import comparable_event, review
+from pootle.core.delegate import comparable_event, grouped_events, review
 from pootle_store.constants import TRANSLATED, UNTRANSLATED
-from pootle_store.models import Suggestion
+from pootle_statistics.models import Submission
+from pootle_store.models import Suggestion, Unit
 from pootle_store.unit.timeline import (
     ACTION_ORDER, ComparableUnitTimelineLogEvent as ComparableLogEvent,
-    UnitTimelineLog)
+    UnitTimelineGroupedEvents as GroupedEvents, UnitTimelineLog)
+
+
+def _latest_submission(unit, limit):
+    return [
+        x for x in unit.submission_set.order_by('-id')[:limit]]
+
+
+def _get_group_user(group):
+    for event in group:
+        if event.action == 'suggestion_accepted':
+            return event.user
+    return group[0].user
+
+
+def _format_dt(dt, no_microseconds=False):
+    if no_microseconds:
+        return dt.strftime("%Y-%m-%d %H:%M:%S")
+    return dt.strftime("%Y-%m-%d %H:%M:%S:%f")
+
+
+@pytest.mark.django_db
+def test_timeline_groups(store0, admin, member, member2, system):
+    expected = []
+
+    # 8 - unit_created
+    unit = store0.addunit(store0.UnitClass(source="Foo"), user=system)
+    unit.refresh_from_db()
+    current_time = unit.creation_time.replace(microsecond=0)
+    no_microseconds = (unit.creation_time == current_time)
+    if no_microseconds:
+        Unit.objects.filter(id=unit.id).update(
+            creation_time=current_time)
+    unit.refresh_from_db()
+    expected[:0] = [(set(['unit_created']),
+                     _format_dt(unit.creation_time, no_microseconds),
+                     system)]
+
+    # 7 - suggestion_created
+    suggestion_0, __ = review.get(Suggestion)().add(
+        unit,
+        "Suggestion for Foo",
+        user=member)
+    if no_microseconds:
+        current_time += timedelta(seconds=1)
+        Suggestion.objects.filter(id=suggestion_0.id).update(
+            creation_time=current_time)
+        suggestion_0.refresh_from_db()
+    expected[:0] = [(set(['suggestion_created']),
+                     _format_dt(suggestion_0.creation_time, no_microseconds),
+                     member)]
+
+    # 6 - suggestion_created
+    suggestion_1, __ = review.get(Suggestion)().add(
+        unit,
+        "Another suggestion for Foo",
+        user=member2)
+    if no_microseconds:
+        current_time += timedelta(seconds=1)
+        Suggestion.objects.filter(id=suggestion_1.id).update(
+            creation_time=current_time)
+        suggestion_1.refresh_from_db()
+    expected[:0] = [(set(['suggestion_created']),
+                     _format_dt(suggestion_1.creation_time, no_microseconds),
+                     member2)]
+
+    # 5 - comment_updated
+    unit.translator_comment = "This is a comment!"
+    unit.save(user=member)
+    submission = _latest_submission(unit, 1)[0]
+    if no_microseconds:
+        current_time += timedelta(seconds=1)
+        Submission.objects.filter(id=submission.id).update(
+            creation_time=current_time)
+        submission.refresh_from_db()
+    unit.refresh_from_db()
+    expected[:0] = [(set(['comment_updated']),
+                     _format_dt(submission.creation_time, no_microseconds),
+                     member)]
+
+    # 4 - suggestion_accepted, target_updated, state_changed
+    review.get(Suggestion)([suggestion_0], admin).accept()
+    if no_microseconds:
+        current_time += timedelta(seconds=1)
+        Suggestion.objects.filter(id=suggestion_0.id).update(
+            review_time=current_time)
+        Submission.objects.filter(suggestion_id=suggestion_0.id).update(
+            creation_time=current_time)
+        suggestion_0.refresh_from_db()
+    unit.refresh_from_db()
+    expected[:0] = [(set(['suggestion_accepted', 'target_updated',
+                          'state_changed']),
+                     _format_dt(suggestion_0.review_time, no_microseconds),
+                     admin)]
+
+    # 3 - target_updated
+    unit.target = "Overwritten translation for Foo"
+    unit.save(user=member2)
+    submission = _latest_submission(unit, 1)[0]
+    if no_microseconds:
+        current_time += timedelta(seconds=1)
+        Submission.objects.filter(id=submission.id).update(
+            creation_time=current_time)
+        submission.refresh_from_db()
+    unit.refresh_from_db()
+    expected[:0] = [(set(['target_updated']),
+                     _format_dt(submission.creation_time, no_microseconds),
+                     member2)]
+
+    # 2 - target_updated, state_changed
+    unit.target = ""
+    unit.save(user=admin)
+    submissions = _latest_submission(unit, 2)
+    if no_microseconds:
+        current_time += timedelta(seconds=1)
+        Submission.objects.filter(id__in=[x.id for x in submissions]).update(
+            creation_time=current_time)
+        for sub in submissions:
+            sub.refresh_from_db()
+    unit.refresh_from_db()
+    expected[:0] = [(set(['target_updated', 'state_changed']),
+                     _format_dt(submissions[0].creation_time, no_microseconds),
+                     admin)]
+
+    # 1 - suggestion_rejected
+    review.get(Suggestion)([suggestion_1], admin).reject()
+    if no_microseconds:
+        current_time += timedelta(seconds=1)
+        Suggestion.objects.filter(id=suggestion_1.id).update(
+            review_time=current_time)
+        suggestion_1.refresh_from_db()
+    expected[:0] = [(set(['suggestion_rejected']),
+                     _format_dt(suggestion_1.review_time, no_microseconds),
+                     admin)]
+
+    # 0 - comment_updated
+    unit.translator_comment = ""
+    unit.save(user=admin)
+    submission = _latest_submission(unit, 1)[0]
+    if no_microseconds:
+        current_time += timedelta(seconds=1)
+        Submission.objects.filter(id=submission.id).update(
+            creation_time=current_time)
+        submission.refresh_from_db()
+    unit.refresh_from_db()
+    expected[:0] = [(set(['comment_updated']),
+                     _format_dt(submission.creation_time, no_microseconds),
+                     admin)]
+
+    log = UnitTimelineLog(unit)
+    grouped_events_class = grouped_events.get(log.__class__)
+    assert grouped_events_class == GroupedEvents
+    groups = [list(x) for _j, x in grouped_events_class(log).grouped_events()]
+    result = [(set([y.action for y in x]),
+               _format_dt(x[0].timestamp, no_microseconds),
+               _get_group_user(x)) for x in groups]
+    assert expected == result
 
 
 @pytest.mark.django_db
