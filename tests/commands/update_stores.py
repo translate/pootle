b3e3b881dc22fafa57ce45be5920c00e94bdbe14
Ryan Northey <ryan@synca.io>
2017-05-13 17:14:53 +0000

    Adjust test to flush buffer before calling cmd

--- tests/commands/update_stores.py
+++ tests/commands/update_stores.py
@@ -32,6 +32,7 @@ def test_update_stores_noargs(capfd, project0_nongnu, project1, language1):
 def test_update_stores_project_tree_none(capfd, project0):
     project0.treestyle = 'pootle_fs'
     project0.save()
+    capfd.readouterr()
     call_command(
         "update_stores", "--atomic=all", "--project", project0.code)
     out, err = capfd.readouterr()
