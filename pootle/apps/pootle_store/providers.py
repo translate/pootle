2ee8cc91d50019dd82182000321563098ce2e9a4
Taras Semenenko <taras.semenenko@gmail.com>
2017-05-15 16:42:16 +0300

    Add event_formatters provider

--- pootle/apps/pootle_store/providers.py
+++ pootle/apps/pootle_store/providers.py
@@ -7,8 +7,10 @@
 # AUTHORS file for copyright and authorship information.
 
 from pootle.core.delegate import (
-    format_diffs, format_syncers, format_updaters, unitid)
+    event_formatters, format_diffs, format_syncers, format_updaters, unitid)
 from pootle.core.plugin import provider
+from pootle_log.utils import LogEvent
+from pootle_store.unit import timeline
 
 from .diff import DiffableStore
 from .models import Unit
@@ -36,3 +38,16 @@ def register_format_updaters(**kwargs_):
 @provider(unitid, sender=Unit)
 def gather_unitid_providers(**kwargs_):
     return dict(default=DefaultUnitid)
+
+
+@provider(event_formatters, sender=LogEvent)
+def gather_event_formatters(**kwargs_):
+    return dict(
+        suggestion_created=timeline.SuggestionAddedEvent,
+        suggestion_rejected=timeline.SuggestionRejectedEvent,
+        target_updated=timeline.TargetUpdatedEvent,
+        state_changed=timeline.UnitStateChangedEvent,
+        unit_created=timeline.UnitCreatedEvent,
+        comment_updated=timeline.CommentUpdatedEvent,
+        check_muted=timeline.CheckMutedEvent,
+        check_unmuted=timeline.CheckUnmutedEvent)
