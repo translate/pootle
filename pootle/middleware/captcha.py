8bd59d5f620e3c3dda79a29f650fa54857075a81
Ryan Northey <ryan@synca.io>
2017-05-20 11:38:54 +0000

    Add paths xhr to captcha exempt pages

--- pootle/middleware/captcha.py
+++ pootle/middleware/captcha.py
@@ -26,7 +26,8 @@ CAPTCHA_EXEMPT_URLPATTERNS = (
     'account_reset_password_from_key',
     'pootle-social-verify',
     'pootle-contact',
-)
+    'pootle-tp-paths',
+    'pootle-project-paths')
 
 
 class CaptchaMiddleware(MiddlewareMixin):
