0d2e5ae06fa1e5619f46c4a91333e16d79261583
Ryan Northey <ryan@synca.io>
2017-05-20 16:53:07 +0000

    Add useradmin initialization

--- pootle/static/js/admin/app.js
+++ pootle/static/js/admin/app.js
@@ -8,6 +8,8 @@
 
 import 'imports?Backbone=>require("backbone")!backbone-move';
 
+import $ from 'jquery';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -22,13 +24,37 @@ import AdminRouter from './AdminRouter';
 
 window.PTL = window.PTL || {};
 
-
 const itemTypes = {
   user: User,
   language: Language,
   project: Project,
 };
 
+PTL.userAdmin = {
+  init() {
+    $(document).ready(() => {
+      const processResults = (data) => {
+        const results = {
+          results: $.map(data.items.results, (result) => {
+            const item = {
+              id: result.id,
+              text: result.text,
+            };
+            return item;
+          }),
+        };
+        results.pagination = { more: data.items.more_results };
+        return results;
+      };
+      $('.js-s2-new-members').on(
+	's2-process-data', (evt, data) => PTL.s2.processData(data));
+      $('.js-s2-new-members').on(
+	's2-process-results', (evt, results) => processResults(results));
+      $('.js-s2-new-members').on(
+	's2-template-result', (evt, result) => PTL.s2.templateResult(result));
+    });
+  },
+};
 
 PTL.admin = {
 
