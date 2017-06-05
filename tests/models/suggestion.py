a824b34c0f8d395f93e46b005b66cbb2e810d135
Leandro Regueiro <leandro.regueiro@gmail.com>
2017-06-05 19:25:25 +0200

    Add tests for escaped outgoing emails

--- tests/models/suggestion.py
+++ tests/models/suggestion.py
@@ -8,6 +8,8 @@
 
 import pytest
 
+from django.template.defaultfilters import escape
+
 from pootle.core.delegate import review
 from pootle_store.models import Suggestion
 
@@ -27,3 +29,49 @@ def test_hash(store0):
 
     suggestion.target = "gras++"
     assert first_hash != second_hash != suggestion.target_hash
+
+
+@pytest.mark.django_db
+def test_accept_suggestion_with_comment_email_escaped(store0, mailoutbox):
+    """Tests that email sent on accept suggestions with comment is escaped."""
+    unit = store0.units[0]
+    suggestion_review_cls = review.get(Suggestion)
+    review_instance = suggestion_review_cls()
+
+    suggestion, created_ = review_instance.add(unit, "gras")
+    comment = "Very nice"
+    suggestion_review_cls([suggestion]).accept(comment=comment)
+    assert len(mailoutbox) == 1
+    message = mailoutbox[0]
+    assert comment in message.body
+
+    suggestion, created_ = review_instance.add(unit, "gras++")
+    comment = "Good job not translating <tag> tags"
+    suggestion_review_cls([suggestion]).accept(comment=comment)
+    assert len(mailoutbox) == 2
+    message = mailoutbox[1]
+    assert comment not in message.body
+    assert escape(comment) in message.body
+
+
+@pytest.mark.django_db
+def test_reject_suggestion_with_comment_email_escaped(store0, mailoutbox):
+    """Tests that email sent on reject suggestions with comment is escaped."""
+    unit = store0.units[0]
+    suggestion_review_cls = review.get(Suggestion)
+    review_instance = suggestion_review_cls()
+
+    suggestion, created_ = review_instance.add(unit, "gras")
+    comment = "It is wrong"
+    suggestion_review_cls([suggestion]).reject(comment=comment)
+    assert len(mailoutbox) == 1
+    message = mailoutbox[0]
+    assert comment in message.body
+
+    suggestion, created_ = review_instance.add(unit, "gras++")
+    comment = "The <tag> must not be translated"
+    suggestion_review_cls([suggestion]).reject(comment=comment)
+    assert len(mailoutbox) == 2
+    message = mailoutbox[1]
+    assert comment not in message.body
+    assert escape(comment) in message.body
