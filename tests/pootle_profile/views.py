6aa80dc607eca2686533aa52667b39b04c9eba5d
Ryan Northey <ryan@synca.io>
2017-06-06 13:04:32 +0000

    Add test to ensure user profile view doesnt leak

--- tests/pootle_profile/views.py
+++ tests/pootle_profile/views.py
@@ -10,6 +10,7 @@ import pytest
 
 from django.urls import reverse
 
+from pootle.core.debug import memusage
 from pootle_profile.utils import UserProfile
 from pootle_profile.views import UserDetailView
 
@@ -24,3 +25,23 @@ def test_view_user_detail(client, member, system):
     profile = response.context["profile"]
     assert isinstance(profile, UserProfile)
     assert profile.user == member
+
+
+@pytest.mark.django_db
+def test_view_user_detail_garbage(member, client, request_users):
+    user = request_users["user"]
+    client.login(
+        username=user.username,
+        password=request_users["password"])
+    response = client.get(
+        reverse(
+            'pootle-user-profile',
+            kwargs=dict(username=member.username)))
+    assert response.status_code == 200
+    for i in xrange(0, 2):
+        with memusage() as usage:
+            client.get(
+                reverse(
+                    'pootle-user-profile',
+                    kwargs=dict(username=member.username)))
+        assert usage["used"] == 0
