612f7b42bf9de5eb3609eeabb57a7c38c7ea81a3
Ryan Northey <ryan@synca.io>
2017-06-16 15:15:38 +0000

    Hook up getter for language_code util

--- pootle/apps/pootle_language/getters.py
+++ pootle/apps/pootle_language/getters.py
@@ -6,7 +6,8 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
-from pootle.core.delegate import language_team, site_languages
+from pootle.core.delegate import language_code, language_team, site_languages
+from pootle.core.language import LanguageCode
 from pootle.core.plugin import getter
 
 from .teams import LanguageTeam
@@ -14,6 +15,11 @@ from .utils import SiteLanguages
 
 
 _site_languages = SiteLanguages()
+
+
+@getter(language_code)
+def language_code_getter(**kwargs_):
+    return LanguageCode
 
 
 @getter(language_team)
