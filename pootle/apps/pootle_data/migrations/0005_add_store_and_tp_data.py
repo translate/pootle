12236de467e871615bc1c0e1593e6370ec1b7651
Ryan Northey <ryan@synca.io>
2017-07-14 06:28:13 +0000

    Remove update_data migrations
    this did not work as the update_data utils are not migration-safe
    
    we could copy out the utils code into the migration, but it seems
    better to just separate out this into a cli util, and run separately

--- pootle/apps/pootle_data/migrations/0005_add_store_and_tp_data.py
+++ pootle/apps/pootle_data/migrations/0005_add_store_and_tp_data.py
@@ -5,32 +5,12 @@ import logging
 
 from django.db import migrations, models
 
-from pootle_data.store_data import StoreDataTool
-from pootle_data.tp_data import TPDataTool
-
-
-logger = logging.getLogger(__name__)
-
-
-def update_store_stats(apps, schema_editor):
-    Store = apps.get_model("pootle_store.Store")
-    TranslationProject = apps.get_model(
-        "pootle_translationproject.TranslationProject")
-    for store in Store.objects.iterator():
-        logger.info("Set stats for Store: %s" % store.pootle_path)
-        StoreDataTool(store).update()
-    for tp in TranslationProject.objects.iterator():
-        logger.info("Set stats for translation project: %s" % tp.pootle_path)
-        TPDataTool(tp).update()
-
 
 class Migration(migrations.Migration):
 
     dependencies = [
         ('pootle_data', '0004_remove_last_updated'),
-        ('pootle_store', '0027_unit_created_by_squashed_0055_fill_unit_source_data'),
     ]
 
     operations = [
-        migrations.RunPython(update_store_stats),
     ]
--- pootle/apps/pootle_data/migrations/0007_add_store_and_tp_data_again.py
+++ pootle/apps/pootle_data/migrations/0007_add_store_and_tp_data_again.py
@@ -5,24 +5,6 @@ import logging
 
 from django.db import migrations, models
 
-from pootle_data.store_data import StoreDataTool
-from pootle_data.tp_data import TPDataTool
-
-
-logger = logging.getLogger(__name__)
-
-
-def update_tp_and_store_stats(apps, schema_editor):
-    Store = apps.get_model("pootle_store.Store")
-    TranslationProject = apps.get_model(
-        "pootle_translationproject.TranslationProject")
-    for store in Store.objects.iterator():
-        logger.info("Set stats for Store: %s" % store.pootle_path)
-        StoreDataTool(store).update()
-    for tp in TranslationProject.objects.iterator():
-        logger.info("Set stats for TP: %s" % tp.pootle_path)
-        TPDataTool(tp).update()
-
 
 class Migration(migrations.Migration):
 
@@ -31,5 +13,4 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(update_tp_and_store_stats),
     ]
