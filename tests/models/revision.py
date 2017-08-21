a91e054a31f06f2013db7fdaba4e951c85133df1
Ryan Northey <ryan@synca.io>
2017-08-21 07:54:51 +0000

    Update revision tests to not use store.sync

--- tests/models/revision.py
+++ tests/models/revision.py
@@ -11,16 +11,10 @@ import pytest
 from pootle.core.models import Revision
 from pootle_store.models import Unit
 
-from .unit import _update_translation
-
 
 @pytest.mark.django_db
 def test_max_revision(revision, project0_nongnu, store0):
     """Tests `max_revision()` gets the latest revision."""
-    store0.sync()
-
-    # update a store first, initial_revision = 1 after this update
-    store0.update(store0.file.store)
 
     initial_max_revision = Unit.max_revision()
     initial_revision = Revision.get()
@@ -28,9 +22,11 @@ def test_max_revision(revision, project0_nongnu, store0):
 
     # Let's make 10 translation updates, this must also update their revision
     # numbers
+    unit = store0.units.first()
     for i in range(10):
-        _update_translation(store0, 0, {'target': str(i)},
-                            sync=False)
+        unit.target = str(i)
+        unit.save()
+        unit.refresh_from_db()
 
     end_max_revision = Unit.max_revision()
     end_revision = Revision.get()
@@ -45,18 +41,17 @@ def test_max_revision(revision, project0_nongnu, store0):
 def test_revision_incr(store0):
     """Tests revision is incremented when units change."""
     previous_revision = Revision.get()
-    db_unit = _update_translation(store0, 0, {'target': [u'Fleisch']},
-                                  sync=False)
-
+    db_unit = store0.units.exclude(target_f="").first()
+    db_unit.target = "CHANGED"
+    db_unit.save()
     assert db_unit.revision != previous_revision
     assert Revision.get() != previous_revision
     assert db_unit.revision == Revision.get()
 
+    db_unit.refresh_from_db()
     previous_revision = Revision.get()
-
-    db_unit = _update_translation(store0, 0, {'target': u'Lachs'},
-                                  sync=False)
-
+    db_unit.target = ""
+    db_unit.save()
     assert db_unit.revision != previous_revision
     assert Revision.get() != previous_revision
     assert db_unit.revision == Revision.get()
