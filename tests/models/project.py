87b5a8bc6903c45eb700fc48a385dde87ba0119b
Ryan Northey <ryan@synca.io>
2017-08-27 11:28:55 +0000

    Remove redundant project.save test

--- tests/models/project.py
+++ tests/models/project.py
@@ -6,8 +6,6 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
-import os
-
 import pytest
 
 from django.core.exceptions import ValidationError
@@ -307,24 +305,6 @@ def test_root_hide_permissions(po_directory, nobody, default, admin, hide,
     assert items_equal(Project.accessible_by_user(nobody), ALL_PROJECTS)
     assert items_equal(Project.accessible_by_user(foo_user), ALL_PROJECTS)
     assert items_equal(Project.accessible_by_user(bar_user), ALL_PROJECTS)
-
-
-@pytest.mark.django_db
-def test_project_create_with_none_treestyle(english, templates, settings):
-    project = Project.objects.create(
-        code="foo",
-        fullname="bar",
-        source_language=english,
-        treestyle='pootle_fs')
-    assert not os.path.exists(
-        os.path.join(
-            settings.POOTLE_TRANSLATION_DIRECTORY,
-            project.code))
-    project.save()
-    assert not os.path.exists(
-        os.path.join(
-            settings.POOTLE_TRANSLATION_DIRECTORY,
-            project.code))
 
 
 @pytest.mark.django_db
