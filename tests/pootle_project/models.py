436f8749180aaa14585ab91519b1d1222daafb73
Ryan Northey <ryan@synca.io>
2017-09-06 16:18:27 +0000

    Add test for Project.create defaults

---
+++ tests/pootle_project/models.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) Pootle contributors.
+#
+# This file is a part of the Pootle project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
+
+import os
+
+import pytest
+
+from pootle_project.models import Project
+
+
+@pytest.mark.django_db
+def test_project_create_defaults(settings, english):
+    foo0 = Project.objects.create(
+        code="foo0",
+        fullname="Project Foo 0",
+        source_language=english)
+    assert foo0.config["pootle_fs.fs_type"] == "localfs"
+    assert (
+        foo0.config["pootle_fs.fs_url"]
+        == os.path.join(
+            settings.POOTLE_TRANSLATION_DIRECTORY,
+            foo0.code))
