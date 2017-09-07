efa271c2935e960199ede33cdf9f47ea492c28dd
Ryan Northey <ryan@synca.io>
2017-09-07 13:59:40 +0000

    Add test for sync stores warning

--- tests/commands/sync_stores.py
+++ tests/commands/sync_stores.py
@@ -6,6 +6,8 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
+import os
+
 import pytest
 
 from django.core.management import call_command
@@ -26,3 +28,24 @@ def test_sync_stores_noargs(capfd, tmpdir):
     # FIXME we should work out how to get something here
     assert out == ''
     assert err == ''
+
+
+@pytest.mark.cmd
+@pytest.mark.django_db
+def test_sync_stores_warn_on_conflict(caplog, project0, language0,
+                                      tmpdir, settings):
+    """Check warning if there are conflicting store/files"""
+    settings.POOTLE_FS_WORKING_PATH = str(tmpdir.mkdir("__fs_tmp__"))
+    fs_tmp = tmpdir.mkdir(project0.code)
+    project0.config["pootle_fs.fs_url"] = str(fs_tmp)
+    tp_tmp = fs_tmp.mkdir(language0.code)
+    conflict_po = os.path.join(str(tp_tmp), "complex.po")
+    with open(conflict_po, "w") as f:
+        f.write("this file should conflict with complex.po in db")
+
+    # the above code will create a conflict, if env changes this
+    # test will need to be updated
+    call_command('sync_stores', "--project=%s" % project0.code)
+    rec = caplog.records[-1]
+    assert rec.levelname == "WARNING"
+    assert "tell pootle how to merge" in rec.message
