959351a6d83d88c2d307f07d225fb33c698ad203
Ryan Northey <ryan@synca.io>
2017-09-15 12:01:01 +0000

    Use OrderedDict for FS_PRESETS

--- pootle/apps/pootle_fs/presets.py
+++ pootle/apps/pootle_fs/presets.py
@@ -6,11 +6,17 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
+from collections import OrderedDict
+
 from pootle.i18n.gettext import ugettext_lazy as _
 
 
-FS_PRESETS = (
-    ("/po/<language_code>.<ext>", _("GNU style")),
-    ("/<language_code>/<dir_path>/<filename>.<ext>", _("non-GNU style")),
-    ("/locale/<language_code>/LC_MESSAGES/<filename>.<ext>", _("Django style")),
-    ("", _("Custom")))
+FS_PRESETS = OrderedDict(
+    gnu=("/po/<language_code>.<ext>", _("GNU style")),
+    nongnu=(
+        "/<language_code>/<dir_path>/<filename>.<ext>",
+        _("non-GNU style")),
+    django=(
+        "/locale/<language_code>/LC_MESSAGES/<filename>.<ext>",
+        _("Django style")),
+    custom=("", _("Custom")))
