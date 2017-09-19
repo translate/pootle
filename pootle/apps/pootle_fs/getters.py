62f140b433715eadfb9dcbb8e6de3ef2d872e600
Ryan Northey <ryan@synca.io>
2017-09-19 10:06:58 +0000

    Ensure fs url and mapping configs are validated

--- pootle/apps/pootle_fs/getters.py
+++ pootle/apps/pootle_fs/getters.py
@@ -6,11 +6,16 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
+from django.core.exceptions import ValidationError
+
 from pootle.core.delegate import response, state
 from pootle.core.plugin import getter
+from pootle_config.delegate import (
+    config_should_not_be_set, config_should_not_be_appended)
+from pootle_project.models import Project
 
 from .delegate import (
-    fs_file, fs_finder, fs_matcher, fs_resources,
+    fs_file, fs_finder, fs_matcher, fs_plugins, fs_resources,
     fs_translation_mapping_validator, fs_url_validator)
 from .files import FSFile
 from .finder import TranslationFileFinder, TranslationMappingValidator
@@ -59,3 +64,20 @@ def fs_url_validator_getter(**kwargs_):
 @getter(fs_translation_mapping_validator)
 def fs_translation_mapping_validator_getter(**kwargs_):
     return TranslationMappingValidator
+
+
+@getter([config_should_not_be_set, config_should_not_be_appended], sender=Project)
+def fs_url_config_validator(**kwargs):
+    if kwargs["key"] == "pootle_fs.fs_url":
+        fs_type = kwargs["instance"].config.get("pootle_fs.fs_type")
+        if not fs_type:
+            raise ValidationError("You cannot set fs_url until fs_type is set")
+        plugin_class = fs_plugins.gather(Project).get(fs_type)
+        if plugin_class:
+            validator = fs_url_validator.get(plugin_class)
+            if validator:
+                validator().validate(kwargs["value"])
+    if kwargs["key"] == "pootle_fs.translation_mapping":
+        validator = fs_translation_mapping_validator.get()
+        for path in kwargs["value"].values():
+            validator(path).validate()
