248e968031ece660479fef69f3ef6b1b16b1b372
fazalpsinfo-cmyk <fazalpsinfo@gmail.com>
2026-07-20 19:26:25 +0530

    fix: typo overriden -> overridden

--- pootle/urls.py
+++ pootle/urls.py
@@ -15,7 +15,7 @@ from pootle.core.delegate import url_patterns
 
 urlpatterns = []
 
-# Allow url handlers to be overriden by plugins
+# Allow url handlers to be overridden by plugins
 for delegate_urls in url_patterns.gather().values():
     urlpatterns += delegate_urls
 
